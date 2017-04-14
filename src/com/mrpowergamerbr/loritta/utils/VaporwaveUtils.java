aedc0b13f642feeef6d30e4846d1178245e83787
MrPowerGamerBR <leonardomalaman@gmail.com>
2017-04-14 18:24:53 -0300

    Transformar a mensagem em lowercase antes de aplicar o efeito vaporwave

--- src/com/mrpowergamerbr/loritta/utils/VaporwaveUtils.java
+++ src/com/mrpowergamerbr/loritta/utils/VaporwaveUtils.java
@@ -2,6 +2,7 @@ package com.mrpowergamerbr.loritta.utils;
 
 public class VaporwaveUtils {
 	public static String vaporwave(String str) {
+		str = str.toLowerCase(); // Como a gente abusa dos códigos unicode, é necessário dar lowercase antes de aplicar o efeito
 		StringBuilder sb = new StringBuilder();
 		for (char c : str.toCharArray()) {
 			if (Character.isSpaceChar(c)) {
