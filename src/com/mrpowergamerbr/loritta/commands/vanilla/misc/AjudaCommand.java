b63eb289273036cb255b00f5b78e5741027c89e0
MrPowerGamerBR <leonardomalaman@gmail.com>
2017-04-16 22:48:29 -0300

    Corrigir a +ajuda

--- src/com/mrpowergamerbr/loritta/commands/vanilla/misc/AjudaCommand.java
+++ src/com/mrpowergamerbr/loritta/commands/vanilla/misc/AjudaCommand.java
@@ -41,23 +41,24 @@ public class AjudaCommand extends CommandBase {
 		aboutMe.setTitle("Sobre o Criador", null);
 		aboutMe.setThumbnail("http://i.imgur.com/nhBZ8i4.png");
 		aboutMe.setDescription("Loritta foi criado pelo MrPowerGamerBR. :wink:");
-		aboutMe.addField("Website:", "http://mrpowergamerbr.com/", true);
-		aboutMe.addField("Discord:", "MrPowerGamerBR#4185", true);
-		aboutMe.addField("Twitter:", "@mrpowergamerbr", true);
+		aboutMe.addField("Website", "http://mrpowergamerbr.com/", true);
+		aboutMe.addField("Discord", "MrPowerGamerBR#4185", true);
+		aboutMe.addField("Twitter", "@mrpowergamerbr", true);
 
 		EmbedBuilder sparklyPower = new EmbedBuilder();
 		sparklyPower.setTitle("Reclames do Plim Plim #1", null);
 		sparklyPower.setThumbnail("http://sparklypower.net/SparklyPower_Logo_250.png");
 		sparklyPower.setDescription("Gostou da qualidade do Loritta? Gosta de Minecraft? Survival? Que tal jogar no SparklyPower então? :slight_smile:");
-		sparklyPower.addField("Website:", "http://sparklypower.net/", true);
-		sparklyPower.addField("IP:", "jogar.sparklypower.net (Versão 1.11.2)", true);
+		sparklyPower.addField("Website", "https://sparklypower.net/", true);
+		sparklyPower.addField("IP", "jogar.sparklypower.net (Versão 1.11.2)", true);
 
 		EmbedBuilder additionalInfoEmbed = new EmbedBuilder();
 		additionalInfoEmbed.setTitle("Informações Adicionais", null);
-		additionalInfoEmbed.setDescription("[Todos os comandos da Loritta](google.com)\n"
-				+ "[Discord da nossa querida Loritta](google.com)\n"
-				+ "[Adicione a Loritta no seu servidor!](google.com)\n"
-				+ "[Amou o Loritta? Tem dinheirinho de sobra? Então doe!](google.com)");
+		additionalInfoEmbed.setDescription("[Todos os comandos da Loritta](https://loritta.website/comandos)\n"
+				+ "[Discord da nossa querida Loritta](https://discord.gg/3rXgN8x)\n"
+				+ "[Adicione a Loritta no seu servidor!](https://loritta.website/auth)\n"
+				+ "[Amou o Loritta? Tem dinheirinho de sobra? Então doe!](https://loritta.website/doar)\n"
+				+ "[Website do MrPowerGamerBR](https://mrpowergamerbr.com/)");
 
 		CommandOptions cmdOptions = context.getConfig().getCommandOptionsFor(this);
 
