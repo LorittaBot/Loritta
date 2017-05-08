5773ecbb914913a460741d3573788812c5b31612
MrPowerGamerBR <leonardomalaman@gmail.com>
2017-05-08 19:56:52 -0300

    Como parsear coisas não-JSON usando JSON? Sexta, no Globo Repórter.

--- src/com/mrpowergamerbr/loritta/utils/translate/GoogleTranslateUtils.java
+++ src/com/mrpowergamerbr/loritta/utils/translate/GoogleTranslateUtils.java
@@ -5,6 +5,7 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
+import java.util.List;
 
 import org.json.JSONArray;
 
@@ -32,13 +33,27 @@ public class GoogleTranslateUtils {
             while ((inputLine = in.readLine()) != null) {
                 builder.append(inputLine);
             }
-            // CHANGED!!!!
             String s = builder.toString();
+            StringBuilder sb = new StringBuilder();
+            
+            // Como parsear coisas não-JSON usando JSON? Sexta, no Globo Repórter.
+            // Na verdade o website NÃO retorna um JSON válido, e nós NÃO devemos parsear assim!
+            // Mas como o código não fui eu que fiz, eu só resolvi dar uma melhorada para corrigir
+            // um pequeno bug que o texto não era pegado 100%.
+            // 
+            // Ou seja, isto é algo que funciona que NÃO deveria funcionar, e que provavelmente só
+            // funciona porque o JSON do Java é lenient e não strict!
             JSONArray jsonArray = new JSONArray(s);
             JSONArray jsonArray2 = (JSONArray) jsonArray.get(0);
-            JSONArray jsonArray3 = (JSONArray) jsonArray2.get(0);
+            for (Object obj : jsonArray2.toList()) {
+            	if (obj instanceof List) {
+            		List<Object> listInsideArray = (List<Object>) obj;
+            		sb.append(listInsideArray.get(0));
+            	}
+            }
+            
             in.close();
-            return jsonArray3.get(0).toString();
+            return sb.toString();
         } catch (Exception e) {
             e.printStackTrace();
         }
