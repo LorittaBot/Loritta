a16bcadb64e3ac4a97b7c1b757b22ffdac600362
MrPowerGamerBR <leonardomalaman@gmail.com>
2017-05-19 17:31:23 -0300

    Adicionar "+volume"

--- src/com/mrpowergamerbr/loritta/commands/vanilla/music/VolumeCommand.java
+++ src/com/mrpowergamerbr/loritta/commands/vanilla/music/VolumeCommand.java
@@ -24,7 +24,7 @@ public class VolumeCommand extends CommandBase {
 
 	@Override
 	public List<String> getExample() {
-		return Arrays.asList("5");
+		return Arrays.asList("100", "66", "33");
 	}
 
 	@Override
@@ -36,13 +36,21 @@ public class VolumeCommand extends CommandBase {
 	public void run(CommandContext context) {
 		GuildMusicManager manager = LorittaLauncher.getInstance().getGuildAudioPlayer(context.getGuild());
 		if (context.getHandle().hasPermission(Permission.MANAGE_SERVER)) {
-			if (context.getArgs().length == 1) {	
+			if (context.getArgs().length >= 1) {	
 				try {
 					int vol = Integer.valueOf(context.getArgs()[0]);
+					if (vol > 100) {
+						context.sendMessage(context.getAsMention(true) + " você quer ficar surdo? Bem, você pode querer, mas eu também estou escutando e eu não quero.");
+						return;
+					}
+					if (0 > vol) {
+						context.sendMessage(context.getAsMention(true) + " não cara, colocar números negativos não irá deixar a música tão mutada que ela é banida do planeta terra.");
+						return;
+					}
 					if (manager.player.getVolume() > vol) {
 						context.sendMessage(context.getAsMention(true) + " irei diminuir o volume do batidão! Desculpe se eu te incomodei com a música alta...");
 					} else {
-						context.sendMessage(context.getAsMention(true) + " irei aumenta o volume do batidão! Se segura aí que agora você vai sentir as ondas sonoras!");
+						context.sendMessage(context.getAsMention(true) + " irei aumentar o volume do batidão! Se segura aí que agora você vai sentir as ondas sonoras!");
 					}
 					manager.player.setVolume(Integer.valueOf(context.getArgs()[0]));
 				} catch (Exception e) {
