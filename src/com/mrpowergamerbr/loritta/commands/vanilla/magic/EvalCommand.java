8c79ecf9ccb5e32988b46b32dda45840f11346d2
MrPowerGamerBR <leonardomalaman@gmail.com>
2017-05-20 18:21:14 -0300

    Mostrar o erro em um embed ao usar "+eval"

--- src/com/mrpowergamerbr/loritta/commands/vanilla/magic/EvalCommand.java
+++ src/com/mrpowergamerbr/loritta/commands/vanilla/magic/EvalCommand.java
@@ -1,5 +1,6 @@
 package com.mrpowergamerbr.loritta.commands.vanilla.magic;
 
+import java.awt.Color;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,10 +9,14 @@ import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
+
 import com.mrpowergamerbr.loritta.Loritta;
 import com.mrpowergamerbr.loritta.commands.CommandBase;
 import com.mrpowergamerbr.loritta.commands.CommandCategory;
 import com.mrpowergamerbr.loritta.commands.CommandContext;
+
+import net.dv8tion.jda.core.EmbedBuilder;
 
 public class EvalCommand extends CommandBase {
 	@Override
@@ -45,7 +50,13 @@ public class EvalCommand extends CommandBase {
 				
 				context.sendMessage(String.valueOf(returnedValue)); // Value of, já que nós não sabemos qual tipo esse objeto é
 			} catch (ScriptException | NoSuchMethodException e) {
-				e.printStackTrace();
+				EmbedBuilder builder = new EmbedBuilder();
+				builder.setTitle("❌ Ih Serjão Sujou! 🤦", "https://youtu.be/G2u8QGY25eU");
+				ExceptionUtils.getStackTrace(e);
+				builder.setDescription("```" + e.getCause().getMessage().trim() + "```");
+				builder.setFooter("Aprender a programar seria bom antes de me forçar a executar códigos que não funcionam 😢", null);
+				builder.setColor(Color.RED);
+				context.sendMessage(builder.build());
 			}
 		} else {
 			// Sem permissão
