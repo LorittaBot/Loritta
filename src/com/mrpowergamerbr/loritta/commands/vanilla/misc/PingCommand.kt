426cef7225eae74c9d005dea6cef345be3b92025
MrPowerGamerBR <leonardomalaman@gmail.com>
2017-05-22 21:49:27 -0300

    Loritta agora usa Kotlin

---
+++ src/com/mrpowergamerbr/loritta/commands/vanilla/misc/PingCommand.kt
@@ -0,0 +1,19 @@
+package com.mrpowergamerbr.loritta.commands.vanilla.misc
+
+import com.mrpowergamerbr.loritta.LorittaLauncher
+import com.mrpowergamerbr.loritta.commands.CommandBase
+import com.mrpowergamerbr.loritta.commands.CommandContext
+
+class PingCommand : CommandBase() {
+    override fun getLabel(): String {
+        return "ping"
+    }
+
+    override fun getDescription(): String {
+        return "Um comando de teste para ver se eu estou funcionando, recomendo que você deixe isto ligado para testes!"
+    }
+
+    override fun run(context: CommandContext) {
+        context.sendMessage(context.getAsMention(true) + "🏓 Pong! " + LorittaLauncher.getInstance().jda.ping + "ms")
+    }
+}
