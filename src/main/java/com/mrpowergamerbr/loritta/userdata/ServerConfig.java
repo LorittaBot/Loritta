4f3b88e53786c7b439813bd67d1934bc83e9e23a
MrPowerGamerBR <leonardomalaman@gmail.com>
2017-05-31 14:22:04 -0300

    Opção para ignorar avisos de usuários retardados que querem ser dementes tentando floodar os comandos da Loritta em canais que não é para usar comandos dela.

--- src/main/java/com/mrpowergamerbr/loritta/userdata/ServerConfig.java
+++ src/main/java/com/mrpowergamerbr/loritta/userdata/ServerConfig.java
@@ -1,23 +1,21 @@
 package com.mrpowergamerbr.loritta.userdata;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
-import org.mongodb.morphia.annotations.Entity;
-import org.mongodb.morphia.annotations.Id;
-import org.mongodb.morphia.annotations.IndexOptions;
-import org.mongodb.morphia.annotations.Indexed;
-
 import com.mrpowergamerbr.loritta.LorittaLauncher;
 import com.mrpowergamerbr.loritta.commands.CommandBase;
 import com.mrpowergamerbr.loritta.commands.CommandOptions;
 import com.mrpowergamerbr.loritta.commands.custom.CustomCommand;
 import com.mrpowergamerbr.loritta.whistlers.Whistler;
-
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import lombok.experimental.Accessors;
+import org.mongodb.morphia.annotations.Entity;
+import org.mongodb.morphia.annotations.Id;
+import org.mongodb.morphia.annotations.IndexOptions;
+import org.mongodb.morphia.annotations.Indexed;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 @Getter
 @Setter
@@ -25,47 +23,48 @@ import lombok.experimental.Accessors;
 @Entity(value = "servers")
 @ToString
 public class ServerConfig {
-	@Id
-	@Indexed(options = @IndexOptions(unique = true))
-	String guildId; // Guild ID
-	String commandPrefix = "+"; // Command Prefix (example: +help or .help or etc)
-	ArrayList<String> disabledCommands = new ArrayList<String>(); // Comandos desativados
-	DebugOptions debugOptions = new DebugOptions();
-	boolean deleteMessageAfterCommand; // Deletar mensagem do comando após executar ele?
-	long creationDate = System.currentTimeMillis();
-	
-	HashMap<String, CommandOptions> commandOptions = new HashMap<String, CommandOptions>(); // Command Options
-	// Os command options são salvos assim:
-	// CommandBase.getClass().getSimpleName() - CommandOptions
+    @Id
+    @Indexed(options = @IndexOptions(unique = true))
+    String guildId; // Guild ID
+    String commandPrefix = "+"; // Command Prefix (example: +help or .help or etc)
+    ArrayList<String> disabledCommands = new ArrayList<String>(); // Comandos desativados
+    DebugOptions debugOptions = new DebugOptions();
+    boolean deleteMessageAfterCommand; // Deletar mensagem do comando após executar ele?
+    long creationDate = System.currentTimeMillis();
 
-	// boolean warnOnFail; // Avisar ao usuário quando escrever o comando errado?
-	boolean explainOnCommandRun = true; // Explicar quando rodar *comando*? (Ou quando usar *comando* :shrug:)
-	boolean explainInPrivate = false; // Caso explainOnCommandRun estiver ativado, é para explicar APENAS no privado ou mandar no global?
-	boolean commandOutputInPrivate = false; // É para mandar o output (ou seja, tudo do comando) no privado em vez de mandar no global?
-	public boolean mentionOnCommandOutput = true; // Caso esteja ativado, a Loritta irá marcar quem executou na mensagem resposta
-	
-	ArrayList<Whistler> whistlers = new ArrayList<Whistler>(); // Whistlers
-	
-	ArrayList<CustomCommand> customCommands = new ArrayList<CustomCommand>(); // Comandos customizados
+    HashMap<String, CommandOptions> commandOptions = new HashMap<String, CommandOptions>(); // Command Options
+    // Os command options são salvos assim:
+    // CommandBase.getClass().getSimpleName() - CommandOptions
 
-	JoinLeaveConfig joinLeaveConfig = new JoinLeaveConfig();
-	MusicConfig musicConfig = new MusicConfig();
-	
-	public CommandOptions getCommandOptionsFor(CommandBase cmd) {
-		if (commandOptions.containsKey(cmd.getClass().getSimpleName())) {
-			return commandOptions.get(cmd.getClass().getSimpleName());
-		}
-		try {
-			return (CommandOptions) LorittaLauncher.getInstance().getCommandManager().getDefaultCmdOptions().get(cmd.getClass().getSimpleName()).newInstance();
-		} catch (InstantiationException | IllegalAccessException e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
-	
-	@Getter
-	@Setter
-	public static class DebugOptions extends CommandOptions {
-		boolean enableAllModules; // Caso ativado, TODAS as modules estarão ativadas
-	}
+    // boolean warnOnFail; // Avisar ao usuário quando escrever o comando errado?
+    boolean explainOnCommandRun = true; // Explicar quando rodar *comando*? (Ou quando usar *comando* :shrug:)
+    boolean explainInPrivate = false; // Caso explainOnCommandRun estiver ativado, é para explicar APENAS no privado ou mandar no global?
+    boolean commandOutputInPrivate = false; // É para mandar o output (ou seja, tudo do comando) no privado em vez de mandar no global?
+    boolean warnOnNoPermission = false; // Avisar quando a Loritta não tem permissão para falar em um canal específico
+    public boolean mentionOnCommandOutput = true; // Caso esteja ativado, a Loritta irá marcar quem executou na mensagem resposta
+
+    ArrayList<Whistler> whistlers = new ArrayList<Whistler>(); // Whistlers
+
+    ArrayList<CustomCommand> customCommands = new ArrayList<CustomCommand>(); // Comandos customizados
+
+    JoinLeaveConfig joinLeaveConfig = new JoinLeaveConfig();
+    MusicConfig musicConfig = new MusicConfig();
+
+    public CommandOptions getCommandOptionsFor(CommandBase cmd) {
+        if (commandOptions.containsKey(cmd.getClass().getSimpleName())) {
+            return commandOptions.get(cmd.getClass().getSimpleName());
+        }
+        try {
+            return (CommandOptions) LorittaLauncher.getInstance().getCommandManager().getDefaultCmdOptions().get(cmd.getClass().getSimpleName()).newInstance();
+        } catch (InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Getter
+    @Setter
+    public static class DebugOptions extends CommandOptions {
+        boolean enableAllModules; // Caso ativado, TODAS as modules estarão ativadas
+    }
 }
--- src/main/java/com/mrpowergamerbr/loritta/utils/LorittaUtils.java
+++ src/main/java/com/mrpowergamerbr/loritta/utils/LorittaUtils.java
@@ -1,17 +1,18 @@
 package com.mrpowergamerbr.loritta.utils;
 
 import com.mrpowergamerbr.loritta.userdata.ServerConfig;
-
 import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.entities.TextChannel;
 
 public class LorittaUtils {
-	public static void warnOwnerNoPermission(Guild guild, TextChannel textChannel, ServerConfig serverConf) {
-		for (Member member : guild.getMembers()) {
-			if (member.isOwner()) {
-				member.getUser().openPrivateChannel().complete().sendMessage("Hey, eu estou sem permissão no **" + textChannel.getName() + "** na guild **" + guild.getName() + "**! Você pode configurar o meu grupo para poder falar lá? Obrigada! 😊").complete();
-			}
-		}
-	}
+    public static void warnOwnerNoPermission(Guild guild, TextChannel textChannel, ServerConfig serverConf) {
+        if (serverConf.warnOnNoPermission()) {
+            for (Member member : guild.getMembers()) {
+                if (member.isOwner()) {
+                    member.getUser().openPrivateChannel().complete().sendMessage("Hey, eu estou sem permissão no **" + textChannel.getName() + "** na guild **" + guild.getName() + "**! Você pode configurar o meu grupo para poder falar lá? Obrigada! 😊").complete();
+                }
+            }
+        }
+    }
 }
