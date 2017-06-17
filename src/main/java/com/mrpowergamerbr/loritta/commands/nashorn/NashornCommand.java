6dbb8dabea530f1bb47ace566ad5f9f503417afa
MrPowerGamerBR <leonardomalaman@gmail.com>
2017-06-17 10:21:40 -0300

    Adicionar mais funções inline, adicionar aliases para comandos JavaScript

--- src/main/java/com/mrpowergamerbr/loritta/commands/nashorn/NashornCommand.java
+++ src/main/java/com/mrpowergamerbr/loritta/commands/nashorn/NashornCommand.java
@@ -12,27 +12,33 @@ import org.apache.commons.lang3.exception.ExceptionUtils;
 import javax.script.Invocable;
 import javax.script.ScriptEngine;
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
-// Comandos usando a Nashorn Engine
+/**
+ * Comandos usando a Nashorn Engine
+ */
 public class NashornCommand {
 	String label;
 	String javaScript;
+	List<String> aliases = new ArrayList<>();
+
+	public NashornCommand() {}
 
 	public NashornCommand(String label, String javaScript) {
 		this.label = label;
 		this.javaScript = javaScript;
 	}
 
-	@Deprecated public boolean handle(MessageReceivedEvent ev, String message, ServerConfig conf) {
-		System.out.println("Executar comando...");
+	@Deprecated
+	public boolean handle(MessageReceivedEvent ev, String message, ServerConfig conf) {
 		if (message.startsWith(conf.commandPrefix + label)) {
-			System.out.println("Vamos executar!");
 			ev.getChannel().sendTyping().complete();
 			Loritta.setExecutedCommands(Loritta.getExecutedCommands() + 1);
 			String cmd = label;
@@ -72,12 +78,15 @@ public class NashornCommand {
 		String blacklisted = "var quit=function(){throw 'Operação não suportada: quit';};var exit=function(){throw 'Operação não suportada: exit';};var print=function(){throw 'Operação não suportada: print';};var echo=function(){throw 'Operação não suportada: echo';};var readLine=function(){throw 'Operação não suportada: readLine';};var readFully=function(){throw 'Operação não suportada: readFully';};var load=function(){throw 'Operação não suportada: load';};var loadWithNewGlobal=function(){throw 'Operação não suportada: loadWithNewGlobal';};";
 		// Funções inline para facilitar a programação de comandos
 		String inlineMethods = "var loritta=function(){ return utils.loritta(); };\n"
-				+ "var pegarConteúdoDeUmaURL=function(url){ return utils.getURL(url); };";
+				+ "var pegarConteúdoDeUmaURL=function(url){ return utils.getURL(url); };\n"
+				+ "var responder=function(mensagem){ contexto.responder(mensagem); };\n"
+				+ "var enviarMensagem=function(mensagem){ contexto.enviarMensagem(mensagem); };\n"
+				+ "var pegarArgumento(index) { return contexto.pegarArgumento(index); };\n"
+				+ "var argumento(index, texto) { return contexto.argumento(index, texto); };";
 		try {
 			ScheduledExecutorService executor = Executors.newScheduledThreadPool(2);
 			Future<Void> future = executor.submit(new NashornTask(engine, blacklisted + " function nashornCommand(contexto, utils) {" + inlineMethods + javaScript + "}", ogContext, context));
 			future.get(3, TimeUnit.SECONDS);
-			System.out.println("Feito!");
 		} catch (Exception e) {
 			e.printStackTrace();
 			EmbedBuilder builder = new EmbedBuilder();
@@ -94,7 +103,8 @@ public class NashornCommand {
 	}
 
 	class NashornClassFilter implements ClassFilter {
-		@Override public boolean exposeToScripts(String s) {
+		@Override
+		public boolean exposeToScripts(String s) {
 			return false;
 		}
 	}
--- src/main/java/com/mrpowergamerbr/loritta/commands/nashorn/NashornContext.java
+++ src/main/java/com/mrpowergamerbr/loritta/commands/nashorn/NashornContext.java
@@ -2,6 +2,10 @@ package com.mrpowergamerbr.loritta.commands.nashorn;
 
 import com.mrpowergamerbr.loritta.commands.CommandContext;
 
+/**
+ * Contexto do comando Nashorn executado, é simplesmente um wrapper "seguro" para comandos em JavaScript, para que
+ * a Loritta possa controlar os comandos executados de uma maneira segura (para não abusarem da API do Discord)
+ */
 public class NashornContext {
 	public String mensagem;
 	private CommandContext context; // Context original, jamais poderá ser usado pelo script!
@@ -45,5 +49,13 @@ public class NashornContext {
 		sentMessages++;
 		diff = System.currentTimeMillis();
 		context.sendMessage(mensagem);
+	}
+
+	public String pegarArgumento(int idx) {
+		return context.getArgs()[idx];
+	}
+
+	public boolean argumento(int idx, String mensagem) {
+		return mensagem.equals(context.getArgs()[idx]);
 	}
 }
