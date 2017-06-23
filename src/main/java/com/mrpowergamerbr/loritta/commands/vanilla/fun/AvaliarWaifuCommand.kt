adb6768a3572a382d2a77c648077efffb74950b7
MrPowerGamerBR <leonardomalaman@gmail.com>
2017-06-22 23:08:57 -0300

    Melhorar o "+avaliarwaifu", agora com motivos filosóficos sobre sua Waifu!

--- src/main/java/com/mrpowergamerbr/loritta/commands/vanilla/fun/AvaliarWaifuCommand.kt
+++ src/main/java/com/mrpowergamerbr/loritta/commands/vanilla/fun/AvaliarWaifuCommand.kt
@@ -32,12 +32,47 @@ class AvaliarWaifuCommand : CommandBase() {
 				joined = context.message.mentionedUsers[0].name;
 			}
 			var random = SplittableRandom(Calendar.getInstance().get(Calendar.DAY_OF_YEAR) + joined.hashCode().toLong()) // Usar um random sempre com a mesma seed
-			var nota = random.nextInt(0, 11).toString();
+			var nota = random.nextInt(0, 11);
 
-			if (joined == "Loritta") {
-				nota = "∞";
+			var reason = "Simplesmente perfeita! Não trocaria de Waifu se fosse você!"
+
+			if (nota == 9) {
+				reason = "Uma Waifu excelente, ótima escolha. <:osama:325332212255948802>";
 			}
-			context.sendMessage(context.getAsMention(true) + "Eu dou uma nota **$nota/10** para `$joined`!");
+			if (nota == 8) {
+				reason = "Uma Waifu que acerta em todos os pontos bons da vida.";
+			}
+			if (nota == 7) {
+				reason = "Nem todas as Waifus são perfeitas, mas qual seria a graça de viver com alguém perfeito? 😊";
+			}
+			if (nota == 6) {
+				reason = "Se fosse nota de escola sua Waifu ela seria \"acima da média\"";
+			}
+			if (nota == 5) {
+				reason = "Nem tão ruim, nem tão boa, bem \"normal\"";
+			}
+			if (nota == 4) {
+				reason = "Não que a sua Waifu seja ruim, pelo contrário! Ela tem potencial para ser algo mais *interessante*!";
+			}
+			if (nota == 3) {
+				reason = "Sua Waifu precisa de mais substância.";
+			}
+			if (nota == 2) {
+				reason = "Não é por nada não mas, se eu você fosse, eu trocaria de Waifu...";
+			}
+			if (nota == 1) {
+				reason = "Sem chance, troca de Waifu hoje mesmo para garantir sua sanidade.";
+			}
+			if (nota == 0) {
+				reason = "🤦 Troque de Waifu por favor.";
+			}
+			var strNota = nota.toString();
+			if (joined == "Loritta") {
+				strNota = "∞";
+				reason = "Sou perfeita!"
+			}
+
+			context.sendMessage(context.getAsMention(true) + "Eu dou uma nota **$strNota/10** para `$joined`! **$reason**");
 		} else {
 			this.explain(context);
 		}
