b1631533d012e02d805a77b755eb1d3a0f446af6
MrPowerGamerBR <leonardomalaman@gmail.com>
2017-06-27 14:42:43 -0300

    Alterar a função do comando "+amino", atualizar o AminoREAPI

--- src/main/java/com/mrpowergamerbr/loritta/commands/vanilla/utils/AminoCommand.kt
+++ src/main/java/com/mrpowergamerbr/loritta/commands/vanilla/utils/AminoCommand.kt
@@ -1,9 +1,12 @@
 package com.mrpowergamerbr.loritta.commands.vanilla.utils
 
+import com.mrpowergamerbr.aminoreapi.AminoClient
+import com.mrpowergamerbr.loritta.Loritta
 import com.mrpowergamerbr.loritta.commands.CommandBase
 import com.mrpowergamerbr.loritta.commands.CommandCategory
 import com.mrpowergamerbr.loritta.commands.CommandContext
 import com.mrpowergamerbr.loritta.utils.LorittaUtils
+import net.dv8tion.jda.core.EmbedBuilder
 
 
 class AminoCommand : CommandBase() {
@@ -24,13 +27,34 @@ class AminoCommand : CommandBase() {
 	}
 
 	override fun run(context: CommandContext) {
-		var image = LorittaUtils.getImageFromContext(context, 0);
+		if (context.args.size > 1 && context.args[0] == "pesquisar") {
+			// Pesquisar uma comunidade no Amino
+			var aminoClient = AminoClient(Loritta.config.aminoEmail, Loritta.config.aminoPassword, Loritta.config.aminoDeviceId);
+			aminoClient.login();
 
-		if (!LorittaUtils.isValidImage(context, image)) {
-			return;
+			var args = context.args.sliceArray(1..context.rawArgs.size - 1).joinToString(" ");
+			var communities = aminoClient.searchCommunities(args, 0, 1, "pt", 1);
+
+			if (communities.isEmpty()) {
+				// Ok, não encontramos nada... mas que tal nós pesquisarmos em inglês?
+				communities = aminoClient.searchCommunities(args, 0, 1, "en", 1);
+			}
+			if (communities.isNotEmpty()) {
+				var community = communities[0];
+
+				var embed = EmbedBuilder();
+				embed.setTitle("<:amino:329308203684724737> " + community.name)
+				embed.setDescription(community.tagline);
+				embed.addField("Link", community.link, true);
+				embed.addField("ID", community.ndcId.toString(), true);
+				embed.addField("Membros", community.membersCount.toString(), true);
+				embed.addField("Linguagem", community.primaryLanguage.toString(), true);
+				embed.setThumbnail(community.icon)
+
+				context.sendMessage(embed.build());
+			} else {
+				context.sendMessage(LorittaUtils.ERROR + " | " + context.getAsMention(true) + "Não encontrei nenhuma comunidade chamada `$args`!")
+			}
 		}
-
-		// Hora de enviar a imagem!
-		context.sendFile(image, "amino.png", context.getAsMention(true));
 	}
 }
