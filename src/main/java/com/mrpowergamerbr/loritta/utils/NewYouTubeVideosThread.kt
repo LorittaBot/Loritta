156af109c5913cb0d259fa6fcb311ecc3a22b1fb
MrPowerGamerBR <leonardomalaman@gmail.com>
2017-07-01 15:59:03 -0300

    A API do YouTube é um lixo... Tentar corrigir problema de sincronia de vídeos recentes

--- src/main/java/com/mrpowergamerbr/loritta/utils/NewYouTubeVideosThread.kt
+++ src/main/java/com/mrpowergamerbr/loritta/utils/NewYouTubeVideosThread.kt
@@ -2,22 +2,28 @@ package com.mrpowergamerbr.loritta.utils
 
 import com.github.kevinsawicki.http.HttpRequest
 import com.github.salomonbrys.kotson.get
+import com.github.salomonbrys.kotson.string
 import com.google.gson.JsonParser
 import com.mongodb.client.model.Filters
 import com.mrpowergamerbr.loritta.Loritta
 import com.mrpowergamerbr.loritta.LorittaLauncher
 import com.mrpowergamerbr.loritta.userdata.ServerConfig
 import org.jsoup.Jsoup
+import org.jsoup.parser.Parser
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class NewYouTubeVideosThread : Thread() {
 	val lastVideos = HashMap<String, String>(); // HashMap usada para guardar o ID do último vídeo
+	val lastVideosTime = HashMap<String, String>(); // HashMap usada para guardar a data do último vídeo
 
 	override fun run() {
 		super.run()
 
 		while (true) {
 			checkNewVideos();
-			Thread.sleep(2500); // Só 2.5s de delay!
+			Thread.sleep(5000); // Só 2.5s de delay!
 		}
 	}
 
@@ -69,20 +75,89 @@ class NewYouTubeVideosThread : Thread() {
 							var json = parser.parse(response);
 							var playlistId = json.get("items").asJsonArray[0].get("contentDetails").asJsonObject.get("relatedPlaylists").asJsonObject.get("uploads").asString;
 
-							var newVideos = HttpRequest.get("https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=3&playlistId=$playlistId&key=${Loritta.config.youtubeKey}")
+							var source = "playlist";
+
+							// Vamos verificar os novos vídeos de vários jeitos
+							var newVideos = HttpRequest.get("https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&order=date&maxResults=1&playlistId=$playlistId&key=${Loritta.config.youtubeKey}")
+									.header("Cache-Control", "max-age=0, no-cache") // YouPobre(tm)
+									.useCaches(false) // YouPobre(tm)
+									.userAgent("Mozilla/5.0 (Windows NT 10.0; WOW64; rv:54.0) Gecko/20100101 Firefox/" + Loritta.random.nextInt(50, 55) + ".0")
 									.body();
 
-							var videosJson = parser.parse(newVideos);
+							var newVideosSearch = HttpRequest.get("https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&order=date&channelId=${youTubeConfig.channelId}&key=${Loritta.config.youtubeKey}")
+									.header("Cache-Control", "max-age=0, no-cache") // YouPobre(tm)
+									.useCaches(false) // YouPobre(tm)
+									.userAgent("Mozilla/5.0 (Windows NT 10.0; WOW64; rv:54.0) Gecko/20100101 Firefox/" + Loritta.random.nextInt(50, 55) + ".0")
+									.body();
+
+							var rssFeed = HttpRequest.get("https://www.youtube.com/feeds/videos.xml?channel_id=${youTubeConfig.channelId}")
+									.header("Cache-Control", "max-age=0, no-cache") // YouPobre(tm)
+									.useCaches(false) // YouPobre(tm)
+									.userAgent("Mozilla/5.0 (Windows NT 10.0; WOW64; rv:54.0) Gecko/20100101 Firefox/" + Loritta.random.nextInt(50, 55) + ".0")
+									.body();
+
+							var jsoup = Jsoup.parse(rssFeed, "", Parser.xmlParser())
+
+							var videosJson = JsonParser().parse(newVideos);
 							var jsonItem = videosJson.get("items").asJsonArray[0];
+
+							var searchJson = JsonParser().parse(newVideosSearch);
+							var jsonSearch = searchJson.get("items").asJsonArray[0];
 
 							var lastId = lastVideos.getOrDefault(guild.id, null);
 
 							var snippet = jsonItem.get("snippet").asJsonObject
+							var searchSnippet = jsonSearch.get("snippet").asJsonObject
 
 							var title = snippet.get("title").asString;
 							var description = snippet.get("description").asString;
 							var channelTitle = snippet.get("channelTitle").asString;
 							var videoId = snippet.get("resourceId").asJsonObject.get("videoId").asString;
+
+							var datePlaylist = snippet["publishedAt"].string;
+							var dateSearch = searchSnippet["publishedAt"].string;
+							var dateRss = jsoup.select("feed entry published").first().text()
+							var date: String = datePlaylist;
+
+							var playlistCalendar = javax.xml.bind.DatatypeConverter.parseDateTime(datePlaylist);
+							var searchCalendar = javax.xml.bind.DatatypeConverter.parseDateTime(dateSearch);
+							var rssCalendar = javax.xml.bind.DatatypeConverter.parseDateTime(dateRss);
+
+							var currentCalendar = playlistCalendar;
+
+							val tz = TimeZone.getTimeZone("UTC")
+							val df = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'") // Quoted "Z" to indicate UTC, no timezone offset
+							df.timeZone = tz
+							dateRss = df.format(rssCalendar.time) // Agora vamos guardar a data verdadeira!
+
+							if (searchCalendar.after(currentCalendar)) { // Se o vídeo do search endpoint é mais recente que o da playlist...
+								source = "search"
+								date = dateSearch;
+								title = searchSnippet.get("title").asString;
+								description = searchSnippet.get("description").asString;
+								channelTitle = searchSnippet.get("channelTitle").asString;
+								videoId = jsonSearch.get("id").asJsonObject.get("videoId").asString;
+								currentCalendar = searchCalendar
+							}
+
+							if (rssCalendar.after(currentCalendar)) { // Se o vídeo do search endpoint é mais recente que o da playlist...
+								source = "rss"
+								date = dateRss;
+								title = jsoup.select("feed entry title").first().text()
+								description = jsoup.select("feed entry media:group media:description").first().text()
+								channelTitle = jsoup.select("feed entry author name").first().text()
+								videoId = jsoup.select("feed entry yt:videoId").first().text()
+								currentCalendar = rssCalendar
+							}
+
+							val lastDate = lastVideosTime.getOrDefault(guild.id, null);
+							if (lastDate != null) {
+								val lastCalendar = javax.xml.bind.DatatypeConverter.parseDateTime(lastDate);
+
+								if (currentCalendar.before(lastCalendar) || currentCalendar.equals(lastCalendar)) {
+									continue; // Na verdade o vídeo atual é mais velho! Ignore então! :)
+								}
+							}
 
 							if (lastId == null) {
 								// Se é null, só salve o ID do último vídeo atual e ignore!
@@ -102,6 +177,9 @@ class NewYouTubeVideosThread : Thread() {
 								textChannel.sendMessage(message).complete();
 
 								lastVideos.put(guild.id, videoId);
+								lastVideosTime.put(guild.id, date);
+
+								println("Atualizado pela source: $source")
 							}
 						}
 					}
