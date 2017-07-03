bab30693a0ce23f21560a5bb798e704bc41f6a64
MrPowerGamerBR <leonardomalaman@gmail.com>
2017-07-03 17:24:48 -0300

    :art: Colocar a cor azul claro da Loritta no "+convidar"

--- src/main/java/com/mrpowergamerbr/loritta/commands/vanilla/discord/InviteCommand.kt
+++ src/main/java/com/mrpowergamerbr/loritta/commands/vanilla/discord/InviteCommand.kt
@@ -4,6 +4,7 @@ import com.mrpowergamerbr.loritta.commands.CommandBase
 import com.mrpowergamerbr.loritta.commands.CommandCategory
 import com.mrpowergamerbr.loritta.commands.CommandContext
 import net.dv8tion.jda.core.EmbedBuilder
+import java.awt.Color
 import java.util.*
 
 class InviteCommand : CommandBase() {
@@ -27,6 +28,7 @@ class InviteCommand : CommandBase() {
         var embed = EmbedBuilder()
                 .setDescription("Você quer me adicionar em outros servidores/guilds do Discord? Então clique [aqui](https://discordapp.com/oauth2/authorize?client_id=297153970613387264&scope=bot&permissions=2080374975) para me adicionar em outro servidor!\n\nSe você quiser configurar algumas coisas (como o meu prefixo, comandos ativados, etc) então acesse o painel de administração clicando [aqui](http://loritta.website/auth)!\n\nE, é claro, entre na guild da Loritta para dar sugestões, reportar bugs e muito mais! https://discord.gg/3rXgN8x")
                 .setThumbnail("http://loritta.website/assets/img/loritta_guild_v4.png")
+                .setColor(Color(0, 193, 223))
                 .build()
 
         context.sendMessage(embed)
