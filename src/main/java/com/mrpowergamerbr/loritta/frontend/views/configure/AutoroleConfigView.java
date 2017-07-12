aa6b3a8646fb33c51a094b6fc5a141fb1f0dc355
MrPowerGamerBR <leonardomalaman@gmail.com>
2017-07-12 18:26:36 -0300

    :ambulance: | Filtrar cargos inválidos

--- src/main/java/com/mrpowergamerbr/loritta/frontend/views/configure/AutoroleConfigView.java
+++ src/main/java/com/mrpowergamerbr/loritta/frontend/views/configure/AutoroleConfigView.java
@@ -8,15 +8,37 @@ import com.mrpowergamerbr.loritta.frontend.utils.RenderContext;
 import com.mrpowergamerbr.loritta.userdata.AutoroleConfig;
 import com.mrpowergamerbr.loritta.userdata.ServerConfig;
 import com.mrpowergamerbr.temmiediscordauth.TemmieDiscordAuth;
+import net.dv8tion.jda.core.entities.Guild;
+import net.dv8tion.jda.core.entities.Role;
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class AutoroleConfigView {
 	public static PebbleTemplate render(RenderContext context, TemmieDiscordAuth temmie, ServerConfig sc)
 			throws PebbleException {
+		// Filtrar roles inválidas
+		AutoroleConfig autoroleConfig = sc.autoroleConfig;
+		List<String> toRemove = new ArrayList<String>();
+		Guild guild = (Guild) context.contextVars().get("currentJdaServer");
+		for (String roleId : autoroleConfig.getRoles()) {
+			try {
+				Role role = guild.getRoleById(roleId);
+				if (role == null) {
+					toRemove.add(roleId);
+				}
+			} catch (Exception e) {
+				toRemove.add(roleId);
+			}
+		}
+		if (!toRemove.isEmpty()) {
+			autoroleConfig.getRoles().removeAll(toRemove);
+			sc.autoroleConfig(autoroleConfig);
+			LorittaLauncher.getInstance().getDs().save(sc);
+		}
 		if (context.request().param("autoroles").isSet()) { // O usuário está salvando as configurações?
-			AutoroleConfig autoroleConfig = sc.autoroleConfig;
 			autoroleConfig.setEnabled(context.request().param("enableModule").isSet());
 			autoroleConfig.setRoles(Arrays.asList(context.request().param("autoroles").value().split(";")));
 			sc.autoroleConfig(autoroleConfig);
