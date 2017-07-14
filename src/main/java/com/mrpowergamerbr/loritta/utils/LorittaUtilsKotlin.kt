f0094286a834297ce2a1cf842bd82c34dd308ac2
MrPowerGamerBR <leonardomalaman@gmail.com>
2017-07-14 13:02:50 -0300

    :ambulance: + :recycle: | Corrigir vídeos com comentários desativados falhando ao adicionar na DJ Loritta, usar Kotson no fillTrackMetadata(...)

--- src/main/java/com/mrpowergamerbr/loritta/utils/LorittaUtilsKotlin.kt
+++ src/main/java/com/mrpowergamerbr/loritta/utils/LorittaUtilsKotlin.kt
@@ -3,6 +3,7 @@ package com.mrpowergamerbr.loritta.utils
 import com.github.kevinsawicki.http.HttpRequest
 import com.github.salomonbrys.kotson.array
 import com.github.salomonbrys.kotson.get
+import com.github.salomonbrys.kotson.obj
 import com.github.salomonbrys.kotson.string
 import com.google.gson.JsonParser
 import com.mrpowergamerbr.loritta.Loritta
@@ -65,24 +66,32 @@ object LorittaUtilsKotlin {
 	@JvmStatic
 	fun fillTrackMetadata(track: AudioTrackWrapper) {
 		if (track.track.sourceManager.sourceName == "youtube") { // Se é do YouTube, então vamos preencher com algumas informações "legais"
-			val playingTrack = track.track;
-			val videoId = playingTrack.info.uri.substring(playingTrack.info.uri.length - 11..playingTrack.info.uri.length - 1)
-			val response = HttpRequest.get("https://www.googleapis.com/youtube/v3/videos?id=${videoId}&part=snippet,statistics&key=${Loritta.config.youtubeKey}").body();
-			val parser = JsonParser();
-			val json = parser.parse(response).asJsonObject;
-			val item = json.get("items").asJsonArray.get(0)
-			val snippet = item.get("snippet").asJsonObject
-			val statistics = item.get("statistics").asJsonObject
+			try {
+				val playingTrack = track.track;
+				val videoId = playingTrack.info.uri.substring(playingTrack.info.uri.length - 11..playingTrack.info.uri.length - 1)
+				val response = HttpRequest.get("https://www.googleapis.com/youtube/v3/videos?id=${videoId}&part=snippet,statistics&key=${Loritta.config.youtubeKey}").body();
+				val parser = JsonParser();
+				val json = parser.parse(response).asJsonObject;
+				val item = json["items"][0]
+				val snippet = item["snippet"].obj
+				val statistics = item["statistics"].obj
 
-			var channelResponse = HttpRequest.get("https://www.googleapis.com/youtube/v3/channels?part=snippet&id=${snippet.get("channelId").asString}&fields=items%2Fsnippet%2Fthumbnails&key=${Loritta.config.youtubeKey}").body();
-			var channelJson = parser.parse(channelResponse).asJsonObject;
+				var channelResponse = HttpRequest.get("https://www.googleapis.com/youtube/v3/channels?part=snippet&id=${snippet.get("channelId").asString}&fields=items%2Fsnippet%2Fthumbnails&key=${Loritta.config.youtubeKey}").body();
+				var channelJson = parser.parse(channelResponse).obj;
 
-			track.metadata.put("viewCount", statistics.get("viewCount").asString)
-			track.metadata.put("likeCount", statistics.get("likeCount").asString)
-			track.metadata.put("dislikeCount", statistics.get("dislikeCount").asString)
-			track.metadata.put("commentCount", statistics.get("commentCount").asString)
-			track.metadata.put("thumbnail", snippet.get("thumbnails").asJsonObject.get("high").asJsonObject.get("url").asString)
-			track.metadata.put("channelIcon", channelJson.get("items").asJsonArray[0].asJsonObject.get("snippet").asJsonObject.get("thumbnails").asJsonObject.get("high").asJsonObject.get("url").asString)
+				track.metadata.put("viewCount", statistics["viewCount"].string)
+				track.metadata.put("likeCount", statistics["likeCount"].string)
+				track.metadata.put("dislikeCount", statistics["dislikeCount"].string)
+				if (statistics.has("commentCount")) {
+					track.metadata.put("commentCount", statistics["commentCount"].string)
+				} else {
+					track.metadata.put("commentCount", "Comentários desativados")
+				}
+				track.metadata.put("thumbnail", snippet["thumbnails"]["high"]["url"].string)
+				track.metadata.put("channelIcon", channelJson["items"][0]["snippet"]["thumbnails"]["high"]["url"].string)
+			} catch (e: Exception) {
+				e.printStackTrace()
+			}
 		}
 	}
 
