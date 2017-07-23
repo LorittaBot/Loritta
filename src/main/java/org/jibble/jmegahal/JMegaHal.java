7d161a49544f7d5d768868d90f66a0ad4727f1d9
MrPowerGamerBR <leonardomalaman@gmail.com>
2017-07-22 22:53:13 -0300

    :bug: | word, e não words

--- src/main/java/org/jibble/jmegahal/JMegaHal.java
+++ src/main/java/org/jibble/jmegahal/JMegaHal.java
@@ -173,7 +173,7 @@ public class JMegaHal implements Serializable {
 		LinkedList<String> parts = new LinkedList<String>();
 
 		Quad[] quads;
-		if (words != null && words.containsKey(word)) {
+		if (word != null && words.containsKey(word)) {
 			quads = (Quad[]) ((HashSet<?>) words.get(word)).toArray(new Quad[0]);
 		}
 		else {
