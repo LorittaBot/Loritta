16c19da85f547ec4db03d3bd089477816809ad12
Leonardo Malaman <leonardomalaman@gmail.com>
2017-07-30 10:11:25 -0300

    :sparkles: | Dar commit nas coisas que eu nem lembro mais o que eram

--- src/main/java/com/mrpowergamerbr/loritta/frontend/views/CitySimulatorView.kt
+++ src/main/java/com/mrpowergamerbr/loritta/frontend/views/CitySimulatorView.kt
@@ -1,10 +1,8 @@
 package com.mrpowergamerbr.loritta.frontend.views
 
 import com.mrpowergamerbr.loritta.frontend.utils.RenderContext
-import com.mrpowergamerbr.loritta.utils.LorittaImage
-import com.mrpowergamerbr.loritta.utils.LorittaUtils
-import com.mrpowergamerbr.loritta.utils.lorittaShards
-import com.mrpowergamerbr.loritta.utils.toBufferedImage
+import com.mrpowergamerbr.loritta.userdata.LorittaServerUserData
+import com.mrpowergamerbr.loritta.utils.*
 import java.awt.Color
 import java.awt.image.BufferedImage
 import java.io.ByteArrayOutputStream
@@ -20,15 +18,34 @@ object CitySimulatorView {
 		val guild = lorittaShards.getGuildById(guildId)
 
 		if (guild != null) {
-			val oneByOneResidential = mutableMapOf(
+			val oneByOneResidentialRich = mutableMapOf(
 					4 to SimCityBuilding("house0", 20),
 					5 to SimCityBuilding("house1", 18)
+			)
+			val oneByOneResidentialMedium = mutableMapOf(
+					10 to SimCityBuilding("mc_house0", 18)
+			)
+			val oneByOneResidentialLow = mutableMapOf(
+					20 to SimCityBuilding("lc_house0", 16)
 			)
 			val twoByTwoResidential = mutableMapOf(
 					100 to SimCityBuilding("apts0", 34)
 			)
+
+			val oneByOneComercial = mutableMapOf(
+					1000 to SimCityBuilding("sm_comercial0", 42),
+					1001 to SimCityBuilding("sm_comercial1", 24),
+					1002 to SimCityBuilding("sm_comercial2", 33),
+					1003 to SimCityBuilding("sm_comercial3", 31),
+					1004 to SimCityBuilding("sm_comercial4", 19),
+					1005 to SimCityBuilding("sm_comercial5", 26),
+					1006 to SimCityBuilding("sm_comercial6", 17),
+					1007 to SimCityBuilding("sm_comercial7", 22)
+			)
+
 			// println("City test!")
 			val grid = Array(256, init = { IntArray(256) })
+			val ownerGrid = Array(256, init = { Array<String>(256, init = { "Ninguém..." }) })
 
 			val random = SplittableRandom(guild.idLong) // Usar o ID da guild como seed
 
@@ -37,10 +54,48 @@ object CitySimulatorView {
 			// city center = 2
 			// road = 3
 			// house1 = 4
+			// comercial0 = 1000
+
 			// reserved = -1
 			val centerX = 127
 			val centerY = 127
 			val population = guild.members.size
+			var buildingCount = population
+			var comercialBuildings = 0
+
+			// Carregar os userdatas dos usuários
+			val serverConfig = loritta.getServerConfigForGuild(guildId)
+			val userDataClone = HashMap<String, LorittaServerUserData>(serverConfig.userData)
+
+			guild.members.forEach {
+				if (!userDataClone.containsKey(it.user.id)) {
+					userDataClone.put(it.user.id, LorittaServerUserData())
+				}
+			}
+
+			val userDataEntries = userDataClone.entries
+
+			val idToComercial = mutableMapOf<String, Int>()
+
+			userDataEntries.sortedBy {
+				it.value.xp
+			}
+
+			// Adicionar os prédios comerciais
+			userDataEntries.forEach {
+				comercialBuildings += it.value.getCurrentLevel().currentLevel - 1
+				if (it.value.getCurrentLevel().currentLevel > 1) {
+					idToComercial.put(it.key, it.value.getCurrentLevel().currentLevel - 1)
+				}
+			}
+
+			val filledUserData = userDataEntries.toMutableList()
+
+			buildingCount += comercialBuildings
+
+			// XP do usuário com mais XP no servidor
+			val maxXp = filledUserData.toList()[0].value.xp
+
 			var popCheck = population
 			val gridValues = mutableListOf<GridValue>()
 			val image = BufferedImage(256, 256, BufferedImage.TYPE_INT_ARGB_PRE)
@@ -55,7 +110,7 @@ object CitySimulatorView {
 					// println(distance)
 					// println((distance - fartest))
 					gridValues.add(GridValue(x, y, distance))
-					if (random.nextInt((distance - fartest).toInt(), 1) in -5..0) {
+					if (random.nextInt((distance - fartest).toInt(), 1) in -15..0) {
 						grid[x][y] = 1
 					}
 				}
@@ -98,7 +153,7 @@ object CitySimulatorView {
 					roadX = 132;
 					roadY = 122;
 				}
-				for (i in 0..population / 8) {
+				for (i in 0..buildingCount / 8) {
 					val distance = Math.sqrt(Math.pow((centerX - roadX).toDouble(), 2.toDouble()) + Math.pow((centerY - roadY).toDouble(), 2.toDouble()));
 
 					if (ticks > 2 && random.nextInt((distance - fartest).toInt(), 1) in -15..0) {
@@ -149,20 +204,51 @@ object CitySimulatorView {
 
 			gridValues.sortBy { it.distanceToCore }
 			var riskyFactor = 0
+			var comercialBuildingsCheck = comercialBuildings
 			endpopulation@ for (z in 0..5) {
 				for ((x, y) in gridValues) {
 					val result = isNearRoad(x, y, grid);
 					if (result.canBuild && riskyFactor >= result.riskyFactor) {
-						if (canBuildAt(x, y, 2, grid)) {
-							grid[x][y] = twoByTwoResidential.entries.toList()[random.nextInt(twoByTwoResidential.entries.size)].key
-							setBuildingType(x, y, 2, 100, grid)
-							popCheck -= 1
+						var toBuildNow = random.nextInt(0, 2)
+						if (toBuildNow == 0) {
+							val userLivingHere = userDataClone.toList()[population - popCheck]
+							ownerGrid[x][y] = guild.getMemberById(userLivingHere.first)?.effectiveName ?: "Saiu do servidor..."
+							if (canBuildAt(x, y, 2, grid)) {
+								grid[x][y] = twoByTwoResidential.entries.toList()[random.nextInt(twoByTwoResidential.entries.size)].key
+								setBuildingType(x, y, 2, 100, grid)
+								popCheck -= 1
+							}
+							if (canBuildAt(x, y, 1, grid)) {
+								if (userLivingHere.second.xp.toDouble() / maxXp.toDouble() >= 66) {
+									grid[x][y] = oneByOneResidentialRich.entries.toList()[random.nextInt(oneByOneResidentialRich.entries.size)].key
+								} else if (userLivingHere.second.xp.toDouble() / maxXp.toDouble() >= 33) {
+									grid[x][y] = oneByOneResidentialMedium.entries.toList()[random.nextInt(oneByOneResidentialMedium.entries.size)].key
+								} else {
+									grid[x][y] = oneByOneResidentialLow.entries.toList()[random.nextInt(oneByOneResidentialLow.entries.size)].key
+								}
+								popCheck -= 1
+							}
+						} else if (toBuildNow == 1) {
+							if (canBuildAt(x, y, 1, grid)) {
+								grid[x][y] = oneByOneComercial.entries.toList()[random.nextInt(oneByOneComercial.entries.size)].key
+								comercialBuildingsCheck -= 1
+
+								val owner = idToComercial.entries.toMutableList()[random.nextInt(idToComercial.entries.size)]
+
+								ownerGrid[x][y] = guild.getMemberById(owner.key)?.effectiveName ?: "Saiu do servidor..."
+
+								var quantity = owner.value
+
+								quantity -= 1
+
+								if (quantity == 0) {
+									idToComercial.remove(owner.key)
+								} else {
+									idToComercial.put(owner.key, quantity)
+								}
+							}
 						}
-						if (canBuildAt(x, y, 1, grid)) {
-							grid[x][y] = oneByOneResidential.entries.toList()[random.nextInt(oneByOneResidential.entries.size)].key
-							popCheck -= 1
-						}
-						if (0 >= popCheck) {
+						if (0 >= popCheck && comercialBuildingsCheck >= 0) {
 							break@endpopulation
 						}
 					}
@@ -181,7 +267,6 @@ object CitySimulatorView {
 			  src="https://code.jquery.com/jquery-3.2.1.min.js"
 			  integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
 			  crossorigin="anonymous"></script>
-<script src="https://loritta.website/assets/loricity/jquery.glow.js"></script>
 <style>
 body {
 background-image: url("https://loritta.website/assets/loricity/background.png");
@@ -231,25 +316,38 @@ var isHovering = false
 var tso = new Audio('https://loritta.website/assets/loricity/tsosas2_v2.mp3');
 tso.play();
 tso.volume = 0.2;
+var lastHover = null
 ${'$'}(document).on('mousemove', function(e){
 	var details = e.target
+    if (lastHover != details) {
+        ${'$'}(lastHover).css({
+"filter": "none",
+"z-index": ${'$'}(lastHover).attr('data-zindex')
+});
+}
 	if (${'$'}(details).attr('data-sctitle')){
+        lastHover = details
         if (!isHovering) {
             var audio = new Audio('https://loritta.website/assets/loricity/snd_mouseover.ogg');
             audio.play();
         }
-        ${'$'}(details).glow({ radius: "20", color:"green"});
         isHovering = true
         ${'$'}('#popup').html(${'$'}(details).attr('data-sctitle'))
 		${'$'}('#popup').css({
 		   left:  e.pageX + 1,
 		   top:   e.pageY + 1,
-			"display": "inherit"
+			"display": "inherit",
+           "z-index": "999999"
 		});
+        ${'$'}(details).css({
+"filter": "drop-shadow(1px 1px 0 white) drop-shadow(-1px 1px 0 white) drop-shadow(1px -1px 0 white) drop-shadow(-1px -1px 0 white)",
+"z-index": "999998"
+});
 	} else {
         isHovering = false
 		${'$'}('#popup').css({
-			"display": "none"
+			"display": "none",
+            "filter:": "inherit",
 		});
 	}
 });
@@ -293,17 +391,34 @@ ${'$'}(document).on('mousemove', function(e){
 					if (bld == 3) {
 						graphics.color = Color.BLUE
 						graphics.fillRect(x, y, 1, 1)
-						strBuilder.append("<img src=\"https://loritta.website/assets/loricity/road_v2.png\" data-sctitle=\"Uma rua qualquer...</br></br>$x, $y\" style=\"cursor:crosshair; z-index: -$y; top: ${currentY + (320 - 16)}px; left: ${currentX}px;\">")
+						strBuilder.append("<img src=\"https://loritta.website/assets/loricity/road_v2.png\" data-zindex=\"$y\" data-sctitle=\"Uma rua qualquer...</br></br>$x, $y\" style=\"cursor:crosshair; z-index: -$y; top: ${currentY + (320 - 16)}px; left: ${currentX}px;\">")
 					}
-					if (oneByOneResidential.containsKey(bld)) {
+					val owner = ownerGrid[x][y]
+					if (oneByOneResidentialRich.containsKey(bld)) {
 						graphics.color = Color.RED
 						graphics.fillRect(x, y, 1, 1)
-						val scBld = oneByOneResidential.get(bld)!!
-						strBuilder.append("<img src=\"https://loritta.website/assets/loricity/${scBld.tileName}.png\" data-sctitle=\"${guild.members[popIndex++].effectiveName}</br></br>$x, $y\" style=\"z-index: -$y; top: ${currentY + (320 - scBld.size)}px; left: ${currentX}px; cursor:crosshair;\">")
+						val scBld = oneByOneResidentialRich.get(bld)!!
+						strBuilder.append("<img src=\"https://loritta.website/assets/loricity/${scBld.tileName}.png\" data-zindex=\"$y\" data-sctitle=\"${owner}</br></br>$x, $y\" style=\"z-index: -$y; top: ${currentY + (320 - scBld.size)}px; left: ${currentX}px; cursor:crosshair;\">")
+					}
+					if (oneByOneResidentialMedium.containsKey(bld)) {
+						graphics.color = Color.RED
+						graphics.fillRect(x, y, 1, 1)
+						val scBld = oneByOneResidentialMedium.get(bld)!!
+						strBuilder.append("<img src=\"https://loritta.website/assets/loricity/${scBld.tileName}.png\" data-zindex=\"$y\" data-sctitle=\"${owner}</br></br>$x, $y\" style=\"z-index: -$y; top: ${currentY + (320 - scBld.size)}px; left: ${currentX}px; cursor:crosshair;\">")
+					}
+					if (oneByOneResidentialLow.containsKey(bld)) {
+						graphics.color = Color.RED
+						graphics.fillRect(x, y, 1, 1)
+						val scBld = oneByOneResidentialLow.get(bld)!!
+						strBuilder.append("<img src=\"https://loritta.website/assets/loricity/${scBld.tileName}.png\" data-zindex=\"$y\" data-sctitle=\"${owner}</br></br>$x, $y\" style=\"z-index: -$y; top: ${currentY + (320 - scBld.size)}px; left: ${currentX}px; cursor:crosshair;\">")
 					}
 					if (twoByTwoResidential.containsKey(bld)) {
 						val scBld = twoByTwoResidential.get(bld)!!
-						strBuilder.append("<img src=\"https://loritta.website/assets/loricity/${scBld.tileName}.png\" data-sctitle=\"${guild.members[popIndex++].effectiveName}</br></br>$x, $y\" style=\"z-index: -$y; top: ${currentY + (329 - scBld.size)}px; left: ${currentX}px; cursor:crosshair;\">")
+						strBuilder.append("<img src=\"https://loritta.website/assets/loricity/${scBld.tileName}.png\" data-zindex=\"$y\" data-sctitle=\"${owner}</br></br>$x, $y\" style=\"z-index: -$y; top: ${currentY + (329 - scBld.size)}px; left: ${currentX}px; cursor:crosshair;\">")
+					}
+					if (oneByOneComercial.containsKey(bld)) {
+						val scBld = oneByOneComercial.get(bld)!!
+						strBuilder.append("<img src=\"https://loritta.website/assets/loricity/${scBld.tileName}.png\" data-zindex=\"$y\" data-sctitle=\"Prédio comercial de ${owner}</br></br>$x, $y\" style=\"z-index: -$y; top: ${currentY + (320 - scBld.size)}px; left: ${currentX}px; cursor:crosshair;\">")
 					}
 					currentX += 16
 					currentY -= 8
