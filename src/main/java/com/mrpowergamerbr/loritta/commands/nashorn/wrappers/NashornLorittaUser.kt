509eee42d14e33191f5fadcd1de8ef5c2859c319
Leonardo Malaman <leonardomalaman@gmail.com>
2017-08-01 13:21:33 -0300

    :bug: | Corrigir pequenos bugs no último commit

--- src/main/java/com/mrpowergamerbr/loritta/commands/nashorn/wrappers/NashornLorittaUser.kt
+++ src/main/java/com/mrpowergamerbr/loritta/commands/nashorn/wrappers/NashornLorittaUser.kt
@@ -3,7 +3,7 @@ package com.mrpowergamerbr.loritta.commands.nashorn.wrappers
 import com.mrpowergamerbr.loritta.userdata.LorittaServerUserData
 import net.dv8tion.jda.core.entities.Member
 
-class NashornLorittaUser(private val member: Member, private val userData: LorittaServerUserData) : NashornMember(member) {
+class NashornLorittaUser(private val backedMember: Member, private val userData: LorittaServerUserData) : NashornMember(backedMember) {
 	fun getXp(): Int {
 		return userData.xp
 	}
