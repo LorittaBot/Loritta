c7fa13ca8352e9f1dd5c9680d789b434a3572893
Leonardo Malaman <leonardomalaman@gmail.com>
2017-08-13 18:29:05 -0300

    :bug: | Corrigir "+playlist"

--- src/main/java/com/mrpowergamerbr/loritta/listeners/MusicMessageListener.kt
+++ src/main/java/com/mrpowergamerbr/loritta/listeners/MusicMessageListener.kt
@@ -2,6 +2,7 @@ package com.mrpowergamerbr.loritta.listeners
 
 import com.mrpowergamerbr.loritta.Loritta
 import com.mrpowergamerbr.loritta.LorittaLauncher
+import com.mrpowergamerbr.loritta.commands.CommandContext
 import com.mrpowergamerbr.loritta.userdata.LorittaServerUserData
 import com.mrpowergamerbr.loritta.utils.LorittaUtils
 import com.mrpowergamerbr.loritta.utils.LorittaUtilsKotlin
@@ -10,6 +11,7 @@ import com.mrpowergamerbr.loritta.utils.save
 import net.dv8tion.jda.core.MessageBuilder
 import net.dv8tion.jda.core.entities.ChannelType
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent
+import net.dv8tion.jda.core.events.message.react.GenericMessageReactionEvent
 import net.dv8tion.jda.core.hooks.ListenerAdapter
 import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
@@ -46,6 +48,30 @@ class MusicMessageListener(internal val loritta: Loritta) : ListenerAdapter() {
 					e.printStackTrace()
 					LorittaUtilsKotlin.sendStackTrace(event.message, e)
 				}
+			}
+		}
+	}
+
+	override fun onGenericMessageReaction(e: GenericMessageReactionEvent) {
+		if (e.user.isBot) {
+			return
+		} // Ignorar reactions de bots
+
+		if (loritta.messageContextCache.containsKey(e.messageId)) {
+			try {
+				val context = LorittaLauncher.getInstance().messageContextCache[e.messageId] as CommandContext
+				val t = object : Thread() {
+					override fun run() {
+						val msg = e.channel.getMessageById(e.messageId).complete()
+						if (msg != null) {
+							context.cmd.onCommandReactionFeedback(context, e, msg)
+						}
+					}
+				}
+				t.start()
+			} catch (exception: Exception) {
+				exception.printStackTrace()
+				LorittaUtilsKotlin.sendStackTrace("[`${e.guild.name}`] **onGenericMessageReaction ${e.member.user.name}**", exception)
 			}
 		}
 	}
