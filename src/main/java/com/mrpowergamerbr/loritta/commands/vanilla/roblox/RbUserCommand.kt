80bd0ca7286c524c035b13d1b431149ee17d881e
Leonardo Malaman <leonardomalaman@gmail.com>
2017-08-29 20:51:48 -0300

    :art: + :bug: | Mais exemplos, corrigir bug

--- src/main/java/com/mrpowergamerbr/loritta/commands/vanilla/roblox/RbUserCommand.kt
+++ src/main/java/com/mrpowergamerbr/loritta/commands/vanilla/roblox/RbUserCommand.kt
@@ -18,9 +18,7 @@ import net.dv8tion.jda.core.EmbedBuilder
 import org.jsoup.Jsoup
 import java.awt.Color
 import java.awt.image.BufferedImage
-import java.io.File
 import java.net.URLEncoder
-import javax.imageio.ImageIO
 
 class RbUserCommand : CommandBase() {
 	override fun getLabel(): String {
@@ -40,7 +38,7 @@ class RbUserCommand : CommandBase() {
 	}
 
 	override fun getExample(): List<String> {
-		return listOf("Monerk")
+		return listOf("cazum8", "lol738236")
 	}
 
 	override fun run(context: CommandContext) {
@@ -52,150 +50,148 @@ class RbUserCommand : CommandBase() {
 
 			val response = JsonParser().parse(body).obj
 
-			val users = response["UserSearchResults"].array
+			if (response["UserSearchResults"].isJsonArray) {
+				val users = response["UserSearchResults"].array
 
-			if (users.size() > 0) {
-				val user = users[0]
+				if (users.size() > 0) {
+					val user = users[0]
 
-				val userId = user["UserId"].long
-				val name = user["Name"].string
-				val blurb = user["Blurb"].string
-				val isOnline = user["IsOnline"].bool
+					val userId = user["UserId"].long
+					val name = user["Name"].string
+					val blurb = user["Blurb"].string
+					val isOnline = user["IsOnline"].bool
 
-				val avatarBody = HttpRequest.get("https://www.roblox.com/search/users/avatar?isHeadshot=false&userIds=$userId")
-						.body()
-
-				val avatarResponse = JsonParser().parse(avatarBody).obj
-
-				// {"PlayerAvatars":[{"Thumbnail":{"Final":true,"Url":"https://t0.rbxcdn.com/fff65b7dc56eefa902fe543b2665da42","RetryUrl":null,"UserId":37271405,"EndpointType":"Avatar"},"UserId":37271405},{"Thumbnail":{"Final":true,"Url":"https://t1.rbxcdn.com/2083a073d0cc644478d06d266c2cc4d6","RetryUrl":null,"UserId":315274565,"EndpointType":"Avatar"},"UserId":315274565}]}
-				val avatar = avatarResponse["PlayerAvatars"].array[0]["Thumbnail"]["Url"].string
-
-				val page = Jsoup.connect("https://www.roblox.com/users/$userId/profile")
-						.get()
-						.body()
-
-				val stats = page.getElementsByClass("profile-stat")
-
-				val joinDate = stats[0].getElementsByClass("text-lead")[0].text()
-				val placeVisits = stats[1].getElementsByClass("text-lead")[0].text()
-				val forumPosts = stats[2].getElementsByClass("text-lead")[0].text()
-
-				// SEGUINDO
-				val followingBody = HttpRequest.get("https://www.roblox.com/users/friends/list-json?currentPage=0&friendsType=Following&imgHeight=100&imgWidth=100&pageSize=18&userId=$userId")
-						.body()
-
-				val followingResponse = JsonParser().parse(followingBody)
-
-				val totalFollowing = followingResponse["TotalFriends"].int
-
-				// SEGUIDORES
-				val followersBody = HttpRequest.get("https://www.roblox.com/users/friends/list-json?currentPage=0&friendsType=Followers&imgHeight=100&imgWidth=100&pageSize=18&userId=$userId")
-						.body()
-
-				val followersResponse = JsonParser().parse(followersBody)
-
-				val totalFollowers = followersResponse["TotalFriends"].int
-
-				// AMIGOS
-				val friendsBody = HttpRequest.get("https://www.roblox.com/users/friends/list-json?currentPage=0&friendsType=AllFriends&imgHeight=100&imgWidth=100&pageSize=18&userId=$userId")
-						.body()
-
-				val friendsResponse = JsonParser().parse(friendsBody)
-
-				val totalFriends = friendsResponse["TotalFriends"].int
-
-				var bufferedImage = BufferedImage(333, 250, BufferedImage.SCALE_SMOOTH)
-
-				val graphics = bufferedImage.graphics
-				graphics.color = Color(255, 255, 255)
-				graphics.fillRect(0, 0, 333, 250)
-
-				var x = 0
-				var y = 0
-
-				run {
-					val robloxCollectionsResponse = HttpRequest.get("https://www.roblox.com/users/profile/robloxcollections-json?userId=$userId")
+					val avatarBody = HttpRequest.get("https://www.roblox.com/search/users/avatar?isHeadshot=false&userIds=$userId")
 							.body()
 
-					val robloxCollections = JsonParser().parse(robloxCollectionsResponse).obj
+					val avatarResponse = JsonParser().parse(avatarBody).obj
 
-					for (coll in robloxCollections["CollectionsItems"].array) {
-						if (x >= 333) {
-							y += 55
-							x = 0
-						}
+					// {"PlayerAvatars":[{"Thumbnail":{"Final":true,"Url":"https://t0.rbxcdn.com/fff65b7dc56eefa902fe543b2665da42","RetryUrl":null,"UserId":37271405,"EndpointType":"Avatar"},"UserId":37271405},{"Thumbnail":{"Final":true,"Url":"https://t1.rbxcdn.com/2083a073d0cc644478d06d266c2cc4d6","RetryUrl":null,"UserId":315274565,"EndpointType":"Avatar"},"UserId":315274565}]}
+					val avatar = avatarResponse["PlayerAvatars"].array[0]["Thumbnail"]["Url"].string
 
-						val thumbnailUrl = coll["Thumbnail"]["Url"].string
-
-						val thumbnail = LorittaUtils.downloadImage(thumbnailUrl).getScaledInstance(55, 55, BufferedImage.SCALE_SMOOTH)
-						bufferedImage.graphics.drawImage(thumbnail, x, y, null)
-						x += 55
-					}
-				}
-
-				y += 55
-				x = 0
-				run {
-					val robloxCollectionsResponse = HttpRequest.get("https://www.roblox.com/users/profile/playerassets-json?assetTypeId=21&userId=$userId")
+					val page = Jsoup.connect("https://www.roblox.com/users/$userId/profile")
+							.get()
 							.body()
 
-					val robloxCollections = JsonParser().parse(robloxCollectionsResponse).obj
+					val stats = page.getElementsByClass("profile-stat")
 
-					for (coll in robloxCollections["Assets"].array) {
-						if (x >= 333) {
-							y += 55
-							x = 0
-						}
+					val joinDate = stats[0].getElementsByClass("text-lead")[0].text()
+					val placeVisits = stats[1].getElementsByClass("text-lead")[0].text()
+					val forumPosts = stats[2].getElementsByClass("text-lead")[0].text()
 
-						val thumbnailUrl = coll["Thumbnail"]["Url"].string
+					// SEGUINDO
+					val followingBody = HttpRequest.get("https://www.roblox.com/users/friends/list-json?currentPage=0&friendsType=Following&imgHeight=100&imgWidth=100&pageSize=18&userId=$userId")
+							.body()
 
-						val thumbnail = LorittaUtils.downloadImage(thumbnailUrl).getScaledInstance(55, 55, BufferedImage.SCALE_SMOOTH)
-						bufferedImage.graphics.drawImage(thumbnail, x, y, null)
-						x += 55
-					}
-				}
+					val followingResponse = JsonParser().parse(followingBody)
 
-				y += 55
-				x = 0
-				run {
-					val gameCardsThumbnail = page.getElementsByClass("game-card-thumb")
-					for (gameCard in gameCardsThumbnail) {
-						if (x >= 333) {
-							y += 55
-							x = 0
-						}
+					val totalFollowing = followingResponse["TotalFriends"].int
 
-						val thumbnailUrl = gameCard.attr("src")
+					// SEGUIDORES
+					val followersBody = HttpRequest.get("https://www.roblox.com/users/friends/list-json?currentPage=0&friendsType=Followers&imgHeight=100&imgWidth=100&pageSize=18&userId=$userId")
+							.body()
 
-						if (thumbnailUrl != null) {
-							val thumbnail = LorittaUtils.downloadImage(thumbnailUrl)?.getScaledInstance(55, 55, BufferedImage.SCALE_SMOOTH) ?: continue
+					val followersResponse = JsonParser().parse(followersBody)
 
+					val totalFollowers = followersResponse["TotalFriends"].int
+
+					// AMIGOS
+					val friendsBody = HttpRequest.get("https://www.roblox.com/users/friends/list-json?currentPage=0&friendsType=AllFriends&imgHeight=100&imgWidth=100&pageSize=18&userId=$userId")
+							.body()
+
+					val friendsResponse = JsonParser().parse(friendsBody)
+
+					val totalFriends = friendsResponse["TotalFriends"].int
+
+					var bufferedImage = BufferedImage(333, 250, BufferedImage.SCALE_SMOOTH)
+
+					var x = 0
+					var y = 0
+
+					run {
+						val robloxCollectionsResponse = HttpRequest.get("https://www.roblox.com/users/profile/robloxcollections-json?userId=$userId")
+								.body()
+
+						val robloxCollections = JsonParser().parse(robloxCollectionsResponse).obj
+
+						for (coll in robloxCollections["CollectionsItems"].array) {
+							if (x >= 333) {
+								y += 55
+								x = 0
+							}
+
+							val thumbnailUrl = coll["Thumbnail"]["Url"].string
+
+							val thumbnail = LorittaUtils.downloadImage(thumbnailUrl).getScaledInstance(55, 55, BufferedImage.SCALE_SMOOTH)
 							bufferedImage.graphics.drawImage(thumbnail, x, y, null)
 							x += 55
 						}
 					}
-				}
 
-				bufferedImage = bufferedImage.getSubimage(0, 0, 333, y + 55)
-				val embed = EmbedBuilder().apply {
-					setTitle("<:roblox:352208393571663873>${if (isOnline) "<:online:313956277808005120>" else "<:offline:313956277237710868>"}$name")
-					if (blurb.isNotEmpty()) {
-						setDescription(blurb)
+					y += 55
+					x = 0
+					run {
+						val robloxCollectionsResponse = HttpRequest.get("https://www.roblox.com/users/profile/playerassets-json?assetTypeId=21&userId=$userId")
+								.body()
+
+						val robloxCollections = JsonParser().parse(robloxCollectionsResponse).obj
+
+						for (coll in robloxCollections["Assets"].array) {
+							if (x >= 333) {
+								y += 55
+								x = 0
+							}
+
+							val thumbnailUrl = coll["Thumbnail"]["Url"].string
+
+							val thumbnail = LorittaUtils.downloadImage(thumbnailUrl).getScaledInstance(55, 55, BufferedImage.SCALE_SMOOTH)
+							bufferedImage.graphics.drawImage(thumbnail, x, y, null)
+							x += 55
+						}
 					}
-					setColor(Color(226, 35, 26))
-					addField("\uD83D\uDCBB ${context.locale.get("RBUSER_ID_DO_ROBLOX")}", userId.toString(), true)
-					addField("\uD83D\uDCC5 ${context.locale.get("RBUSER_JOIN_DATE")}", joinDate, true)
-					addField("\uD83D\uDC40 ${context.locale.get("RBUSER_PLACE_VISITS")}", placeVisits, true)
-					addField("\uD83D\uDCDD ${context.locale.get("RBUSER_FORUM_POSTS")}", forumPosts, true)
-					addField("\uD83D\uDE4B ${context.locale.get("RBUSER_SOCIAL")}", "**\uD83D\uDC3E ${context.locale.get("RBUSER_FOLLOWING")}**: $totalFollowing\n**<:twitt_starstruck:352216844603752450> ${context.locale.get("RBUSER_FOLLOWERS")}**: $totalFollowers\n**\uD83D\uDE0E ${context.locale.get("RBUSER_FRIENDS")}**: $totalFriends\n", true)
-					setImage("attachment://roblox.png")
-					setThumbnail(avatar)
-				}
 
-				context.sendFile(bufferedImage, "roblox.png", embed.build())
-			} else {
-				context.sendMessage(LorittaUtils.ERROR + " **|** " + context.locale.get("RBUSER_COULDNT_FIND", username) + " \uD83D\uDE22")
+					y += 55
+					x = 0
+					run {
+						val gameCardsThumbnail = page.getElementsByClass("game-card-thumb")
+						for (gameCard in gameCardsThumbnail) {
+							if (x >= 333) {
+								y += 55
+								x = 0
+							}
+
+							val thumbnailUrl = gameCard.attr("src")
+
+							if (thumbnailUrl != null) {
+								val thumbnail = LorittaUtils.downloadImage(thumbnailUrl)?.getScaledInstance(55, 55, BufferedImage.SCALE_SMOOTH) ?: continue
+
+								bufferedImage.graphics.drawImage(thumbnail, x, y, null)
+								x += 55
+							}
+						}
+					}
+
+					bufferedImage = bufferedImage.getSubimage(0, 0, 333, y + 55)
+					val embed = EmbedBuilder().apply {
+						setTitle("<:roblox:352208393571663873>${if (isOnline) "<:online:313956277808005120>" else "<:offline:313956277237710868>"}$name")
+						if (blurb.isNotEmpty()) {
+							setDescription(blurb)
+						}
+						setColor(Color(226, 35, 26))
+						addField("\uD83D\uDCBB ${context.locale.get("RBUSER_ID_DO_ROBLOX")}", userId.toString(), true)
+						addField("\uD83D\uDCC5 ${context.locale.get("RBUSER_JOIN_DATE")}", joinDate, true)
+						addField("\uD83D\uDC40 ${context.locale.get("RBUSER_PLACE_VISITS")}", placeVisits, true)
+						addField("\uD83D\uDCDD ${context.locale.get("RBUSER_FORUM_POSTS")}", forumPosts, true)
+						addField("\uD83D\uDE4B ${context.locale.get("RBUSER_SOCIAL")}", "**\uD83D\uDC3E ${context.locale.get("RBUSER_FOLLOWING")}**: $totalFollowing\n**<:twitt_starstruck:352216844603752450> ${context.locale.get("RBUSER_FOLLOWERS")}**: $totalFollowers\n**\uD83D\uDE0E ${context.locale.get("RBUSER_FRIENDS")}**: $totalFriends\n", true)
+						setImage("attachment://roblox.png")
+						setThumbnail(avatar)
+					}
+
+					context.sendFile(bufferedImage, "roblox.png", embed.build())
+					return
+				}
 			}
+			context.sendMessage(LorittaUtils.ERROR + " **|** " + context.locale.get("RBUSER_COULDNT_FIND", username) + " \uD83D\uDE22")
 		} else {
 			context.explain()
 		}
