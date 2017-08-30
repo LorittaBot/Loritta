a3c497b301e4a6755f23e9e7025ae369f2a48d45
Leonardo Malaman <leonardomalaman@gmail.com>
2017-08-29 22:56:46 -0300

    :sparkles: | Adicionar comandos do Roblox na ajuda da Loritta

--- src/main/java/com/mrpowergamerbr/loritta/commands/vanilla/misc/AjudaCommand.kt
+++ src/main/java/com/mrpowergamerbr/loritta/commands/vanilla/misc/AjudaCommand.kt
@@ -62,6 +62,7 @@ class AjudaCommand : CommandBase() {
 			val minecraftCmds = getCommandsFor(context, disabledCommands, CommandCategory.MINECRAFT, "http://i.imgur.com/gKBHNzL.png")
 			val undertaleCmds = getCommandsFor(context, disabledCommands, CommandCategory.UNDERTALE, "http://vignette2.wikia.nocookie.net/animal-jam-clans-1/images/0/08/Annoying_dog_101.gif/revision/latest?cb=20151231033006")
 			val pokemonCmds = getCommandsFor(context, disabledCommands, CommandCategory.POKEMON, "http://i.imgur.com/2l5kKCp.png")
+			val robloxCmds = getCommandsFor(context, disabledCommands, CommandCategory.ROBLOX, "https://media.discordapp.net/attachments/297732013006389252/352269723385462787/download.png")
 			val musicCmds = getCommandsFor(context, disabledCommands, CommandCategory.MUSIC, "http://i.imgur.com/C9idIUF.png")
 			val funCmds = getCommandsFor(context, disabledCommands, CommandCategory.FUN, "http://i.imgur.com/ssNe7dx.png")
 			val imagesCmds = getCommandsFor(context, disabledCommands, CommandCategory.IMAGES, "http://i.imgur.com/ssNe7dx.png")
@@ -102,6 +103,9 @@ class AjudaCommand : CommandBase() {
 			}
 			if (pokemonCmds != null) {
 				fastEmbedSend(context, pokemonCmds);
+			}
+			if (robloxCmds != null) {
+				fastEmbedSend(context, robloxCmds);
 			}
 			if (musicCmds != null) {
 				fastEmbedSend(context, musicCmds);
@@ -170,6 +174,8 @@ class AjudaCommand : CommandBase() {
 			color = Color(255, 13, 0);
 		} else if (cat == CommandCategory.MINECRAFT) {
 			color = Color(50, 141, 145);
+		} else if (cat == CommandCategory.ROBLOX) {
+			color = Color(226, 35, 26);
 		} else if (cat == CommandCategory.MISC) {
 			color = Color(255, 176, 0);
 		} else if (cat == CommandCategory.UTILS) {
