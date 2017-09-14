acd4a4af4e8e3dac90a0409f3302bb586cf752b6
Leonardo Malaman <leonardomalaman@gmail.com>
2017-09-14 10:18:49 -0300

    :sparkles: + :recycle: | Refactor, adicionar botão para baixar vídeo em MP3 no "+ytmp3"

--- src/main/java/com/mrpowergamerbr/loritta/commands/vanilla/fun/YouTubeCommand.kt
+++ src/main/java/com/mrpowergamerbr/loritta/commands/vanilla/fun/YouTubeCommand.kt
@@ -10,6 +10,7 @@ import com.mrpowergamerbr.loritta.commands.CommandBase
 import com.mrpowergamerbr.loritta.commands.CommandCategory
 import com.mrpowergamerbr.loritta.commands.CommandContext
 import com.mrpowergamerbr.loritta.utils.Constants
+import com.mrpowergamerbr.loritta.utils.MiscUtils
 import com.mrpowergamerbr.loritta.utils.YouTubeUtils
 import com.mrpowergamerbr.loritta.utils.locale.BaseLocale
 import com.mrpowergamerbr.loritta.utils.loritta
@@ -36,7 +37,7 @@ class YouTubeCommand : CommandBase() {
 	}
 
 	override fun getDescription(locale: BaseLocale): String {
-		return locale.get("YOUTUBE_DESCRIPTION")
+		return locale["YOUTUBE_DESCRIPTION"]
 	}
 
 	override fun getExample(): List<String> {
@@ -52,18 +53,18 @@ class YouTubeCommand : CommandBase() {
 	}
 
 	override fun run(context: CommandContext) {
-		if (context.args.size >= 1) {
+		if (context.args.isNotEmpty()) {
 			var query = context.args.joinToString(" ");
 			val items = YouTubeUtils.searchVideosOnYouTube(query)
 
 			if (items.isNotEmpty()) {
 				var format = "";
 				var youtubeKey = Loritta.config.youtubeKey;
-				for (i in 0..Math.min(5, items.size) - 1) {
+				for (i in 0 until Math.min(5, items.size)) {
 					var item = items[i];
 					var response = HttpRequest.get("https://www.googleapis.com/youtube/v3/videos?id=${item.id.videoId}&part=contentDetails&key=${youtubeKey}").body();
-					var parser = JsonParser();
-					var json = parser.parse(response).asJsonObject;
+					var parser = JsonParser()
+					var json = parser.parse(response).asJsonObject
 					var strDuration = json["items"].array[0]["contentDetails"]["duration"].string
 					var duration = java.time.Duration.parse(strDuration)
 					var inSeconds = duration.get(java.time.temporal.ChronoUnit.SECONDS); // Nós não podemos pegar o tempo diretamente porque é "unsupported"
@@ -78,10 +79,9 @@ class YouTubeCommand : CommandBase() {
 				embed.setTitle("<:youtube:314349922885566475> ${context.locale.YOUTUBE_RESULTS_FOR.msgFormat(query)}");
 				var mensagem = context.sendMessage(context.getAsMention(true), embed.build());
 				// Adicionar os reactions
-				for (i in 0..Math.min(5, items.size) - 1) {
+				for (i in 0 until Math.min(5, items.size)) {
 					mensagem.addReaction(indexes[i]).complete();
 				}
-				// mensagem.addReaction("❌").complete();
 				return;
 			} else {
 				context.sendMessage(Constants.ERROR + " **|** " + context.getAsMention(true) + context.locale.YOUTUBE_COULDNT_FIND.msgFormat(query))
@@ -93,25 +93,31 @@ class YouTubeCommand : CommandBase() {
 
 	override fun onCommandReactionFeedback(context: CommandContext, e: GenericMessageReactionEvent, msg: Message) {
 		if (e.user == context.userHandle) { // Somente quem executou o comando pode interagir!
-			if (context.metadata.contains("playMusic")) {
-				var item = context.metadata.get("playMusic") as YouTubeItem;
-
-				if (loritta.checkAndLoad(context, "https://youtu.be/${item.id.videoId}")) {
-					context.metadata.remove("playMusic")
+			if (context.metadata.contains("currentItem")) {
+				var item = context.metadata["currentItem"] as YouTubeItem;
+				if (e.reactionEmote.name == "▶") {
+					if (loritta.checkAndLoad(context, "https://youtu.be/${item.id.videoId}")) {
+						context.metadata.remove("currentItem")
+					}
 				}
-				return;
+
+				if (e.reactionEmote.name == "\uD83D\uDCE5") {
+					MiscUtils.sendYouTubeVideoMp3(context, "https://youtu.be/${item.id.videoId}")
+				}
+				return
 			}
+
 			var item: YouTubeItem;
 			if (e.reactionEmote.name == "1⃣") {
-				item = context.metadata.get("0") as YouTubeItem;
+				item = context.metadata["0"] as YouTubeItem;
 			} else if (e.reactionEmote.name == "2⃣") {
-				item = context.metadata.get("1") as YouTubeItem;
+				item = context.metadata["1"] as YouTubeItem;
 			} else if (e.reactionEmote.name == "3⃣") {
-				item = context.metadata.get("2") as YouTubeItem;
+				item = context.metadata["2"] as YouTubeItem;
 			} else if (e.reactionEmote.name == "4⃣") {
-				item = context.metadata.get("3") as YouTubeItem;
+				item = context.metadata["3"] as YouTubeItem;
 			} else {
-				item = context.metadata.get("4") as YouTubeItem;
+				item = context.metadata["4"] as YouTubeItem;
 			}
 
 			// Remover todos os reactions
@@ -123,17 +129,20 @@ class YouTubeCommand : CommandBase() {
 			var channelId = item.snippet.channelId;
 			embed.setThumbnail("http://i.ytimg.com/i/$channelId/1.jpg");
 			embed.addField("Link", "https://youtu.be/${item.id.videoId}", true);
-			embed.addField("${context.locale.YOUTUBE_CHANNEL.msgFormat()}", "${item.snippet.channelTitle}", true);
+			embed.addField("${context.locale["YOUTUBE_CHANNEL"]}", "${item.snippet.channelTitle}", true);
 			embed.setColor(Color(217, 66, 52));
 
 			// Criar novo embed!
 			msg.editMessage(embed.build()).complete();
 
+			context.metadata.put("currentItem", item);
+
 			if (context.config.musicConfig.isEnabled) {
 				// Se o sistema de músicas está ativado...
 				msg.addReaction("▶").complete(); // Vamos colocar um ícone para tocar!
-				context.metadata.put("playMusic", item);
 			}
+
+			msg.addReaction("\uD83D\uDCE5").complete(); // Adicionar ícone para baixar a música em MP3
 		}
 	}
 }
---
+++ src/main/java/com/mrpowergamerbr/loritta/utils/MiscUtils.kt
@@ -0,0 +1,118 @@
+package com.mrpowergamerbr.loritta.utils
+
+import com.github.kevinsawicki.http.HttpRequest
+import com.github.salomonbrys.kotson.obj
+import com.github.salomonbrys.kotson.string
+import com.google.gson.JsonParser
+import com.google.gson.stream.JsonReader
+import com.mrpowergamerbr.loritta.commands.CommandContext
+import java.io.StringReader
+import kotlin.concurrent.fixedRateTimer
+
+object MiscUtils {
+	fun sendYouTubeVideoMp3(context: CommandContext, videoUrl: String) {
+		var mensagem = context.sendMessage("💭 **|** " + context.getAsMention(true) + "${context.locale["PROCESSING"]}...");
+
+		var link = videoUrl
+		link = link.replace("https://www.youtube.com/watch?v=", "");
+		link = link.replace("https://youtube.com/watch?v=", "");
+		link = link.replace("https://m.youtube.com/watch?v=", "");
+		link = link.replace("https://www.youtu.be/", "");
+		link = link.replace("https://youtu.be/", "");
+		link = link.replace("https://m.youtu.be/", "");
+		link = link.replace("http://www.youtube.com/watch?v=", "");
+		link = link.replace("http://youtube.com/watch?v=", "");
+		link = link.replace("http://m.youtube.com/watch?v=", "");
+		link = link.replace("http://www.youtu.be/", "");
+		link = link.replace("http://youtu.be/", "");
+		link = link.replace("http://m.youtu.be/", "");
+
+		var videoId = link;
+		var callbackId = "lorittaCallback";
+
+		var checkResponse = HttpRequest.get("https://d.ymcdn.cc/check.php?callback=$callbackId&v=$videoId&f=mp3&_=1498314662109")
+				.userAgent(Constants.USER_AGENT)
+				.body()
+				.replace(callbackId, "")
+		checkResponse = checkResponse.removePrefix("(").removeSuffix(")");
+
+		val reader = StringReader(checkResponse)
+		val jsonReader = JsonReader(reader)
+		val checkJsonResponse = JsonParser().parse(jsonReader).obj // Base
+
+		var hash = checkJsonResponse["hash"].string
+		var title = checkJsonResponse["title"].asString
+
+		if (title == "none") {
+			mensagem.editMessage(Constants.ERROR + " **|** " + context.getAsMention(true) + context.locale["YOUTUBEMP3_INVALID_LINK"]).complete();
+			return;
+		}
+
+		var lastProgress = "0";
+		// create a fixed rate timer that prints hello world every 100ms
+		// after a 100ms delay
+		val fixedRateTimer = fixedRateTimer(name = "YTMP3 Progress Check Thread",
+				initialDelay = 0, period = 1000) {
+			var progressResponse = HttpRequest.get("https://d.ymcdn.cc/progress.php?callback=$callbackId&id=$hash&_=1498315402819")
+					.userAgent("Mozilla/5.0 (Windows NT 6.3; WOW64; rv:37.0) Gecko/20100101 Firefox/37.0")
+					.body()
+					.replace(callbackId, "")
+			progressResponse = progressResponse.removePrefix("(").removeSuffix(")");
+
+			val readerProgress = StringReader(progressResponse)
+			val progressJsonResponse = JsonParser().parse(readerProgress).obj // Base
+
+			val progress = progressJsonResponse.get("progress").string
+			if (progressJsonResponse.has("error") && progressJsonResponse["error"].string.isNotEmpty()) {
+				mensagem.editMessage(Constants.ERROR + " **|** " + context.getAsMention(true) + context.locale["YOUTUBEMP3_ERROR_WHEN_CONVERTING"]).complete()
+				this.cancel()
+				return@fixedRateTimer
+			}
+			if (progress == "1" && lastProgress != progress) {
+				mensagem.editMessage("💭 **|** " + context.getAsMention(true) + context.locale["YOUTUBEMP3_DOWNLOADING_VIDEO"]).complete()
+			}
+			if (progress == "2" && lastProgress != progress) {
+				mensagem.editMessage("💭 **|** " + context.getAsMention(true) + context.locale["YOUTUBEMP3_CONVERTING_VIDEO"]).complete()
+			}
+			if (progress == "3") {
+				var serverId = progressJsonResponse.get("sid").string;
+				var serverName = "";
+				when (serverId) {
+					"1" -> serverName = "odg"
+					"2" -> serverName = "ado"
+					"3" -> serverName = "jld"
+					"4" -> serverName = "tzg"
+					"5" -> serverName = "uuj"
+					"6" -> serverName = "bkl"
+					"7" -> serverName = "fnw"
+					"8" -> serverName = "eeq"
+					"9" -> serverName = "ebr"
+					"10" -> serverName = "asx"
+					"11" -> serverName = "ghn"
+					"12" -> serverName = "eal"
+					"13" -> serverName = "hrh"
+					"14" -> serverName = "quq"
+					"15" -> serverName = "zki"
+					"16" -> serverName = "tff"
+					"17" -> serverName = "aol"
+					"18" -> serverName = "eeu"
+					"19" -> serverName = "kkr"
+					"20" -> serverName = "yui"
+					"21" -> serverName = "yyd"
+					"22" -> serverName = "hdi"
+					"23" -> serverName = "ddb"
+					"24" -> serverName = "iir"
+					"25" -> serverName = "ihi"
+					"26" -> serverName = "heh"
+					"27" -> serverName = "xaa"
+					"28" -> serverName = "nim"
+					"29" -> serverName = "omp"
+					"30" -> serverName = "eez"
+				}
+				mensagem.editMessage("📥 **|** " + context.getAsMention(true) + context.locale["YOUTUBEMP3_FINISHED", "https://$serverName.ymcdn.cc/download.php?id=$hash"]).complete()
+				this.cancel()
+			}
+			lastProgress = progress;
+		}
+	}
+}
