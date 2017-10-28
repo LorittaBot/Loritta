6c433b8b6d32a3b64e31ee4f6cf5e6268fdef256
Maycoooonm <30837470+Maycoooonm@users.noreply.github.com>
2017-10-28 19:49:06 -0200

    Update CommandManager.kt

--- src/main/java/com/mrpowergamerbr/loritta/commands/CommandManager.kt
+++ src/main/java/com/mrpowergamerbr/loritta/commands/CommandManager.kt
@@ -79,6 +79,7 @@ import com.mrpowergamerbr.loritta.commands.vanilla.minecraft.SpigotMcCommand
 import com.mrpowergamerbr.loritta.commands.vanilla.misc.AjudaCommand
 import com.mrpowergamerbr.loritta.commands.vanilla.misc.AngelCommand
 import com.mrpowergamerbr.loritta.commands.vanilla.misc.EscolherCommand
+import com.mrpowergamerbr.loritta.commands.vanilla.misc.HesayCommand
 import com.mrpowergamerbr.loritta.commands.vanilla.misc.LanguageCommand
 import com.mrpowergamerbr.loritta.commands.vanilla.misc.PatreonCommand
 import com.mrpowergamerbr.loritta.commands.vanilla.misc.PingCommand
@@ -195,6 +196,7 @@ class CommandManager(isMusicOnly: Boolean) {
 		commandMap.add(EscolherCommand())
 		commandMap.add(LanguageCommand())
 		commandMap.add(PatreonCommand())
+		commandMap.add(HesayCommand())
 
 		// =======[ SOCIAL ]======
 		commandMap.add(PerfilCommand())
