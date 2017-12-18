9e80efbda4f0d6958b44d38c0a38c893a684c4ba
Leonardo Malaman <leonardomalaman@gmail.com>
2017-12-18 15:25:45 -0200

    :pencil: | Apenas deixar os event handlers ativados na guild da Loritta

--- src/main/java/com/mrpowergamerbr/loritta/listeners/DiscordListener.kt
+++ src/main/java/com/mrpowergamerbr/loritta/listeners/DiscordListener.kt
@@ -142,8 +142,10 @@ class DiscordListener(internal val loritta: Loritta) : ListenerAdapter() {
 					if (serverConfig.aminoConfig.isEnabled && serverConfig.aminoConfig.fixAminoImages)
 						AminoConverterModule.convertToImage(event)
 
-					for (eventHandler in serverConfig.nashornEventHandlers) {
-						eventHandler.handleMessageReceived(event)
+					if (event.guild.id == "297732013006389252") {
+						for (eventHandler in serverConfig.nashornEventHandlers) {
+							eventHandler.handleMessageReceived(event)
+						}
 					}
 
 					if (lorittaUser.hasPermission(LorittaPermission.IGNORE_COMMANDS))
