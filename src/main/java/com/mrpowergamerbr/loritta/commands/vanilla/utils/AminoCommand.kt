7e56534f523856b219333d352ad15f4d7699432e
MrPowerGamerBR <leonardomalaman@gmail.com>
2018-03-15 18:40:29 -0300

    :bug: | Detectar se a imagem "Amino" é uma GIF e, caso for, coloque a extensão de GIF

--- src/main/java/com/mrpowergamerbr/loritta/commands/vanilla/utils/AminoCommand.kt
+++ src/main/java/com/mrpowergamerbr/loritta/commands/vanilla/utils/AminoCommand.kt
@@ -10,7 +10,11 @@ import com.mrpowergamerbr.loritta.utils.LorittaUtils
 import com.mrpowergamerbr.loritta.utils.humanize
 import com.mrpowergamerbr.loritta.utils.locale.BaseLocale
 import net.dv8tion.jda.core.EmbedBuilder
+import org.apache.commons.io.IOUtils
 import java.awt.Color
+import java.io.ByteArrayInputStream
+import java.net.HttpURLConnection
+import java.net.URL
 import java.time.ZoneOffset
 
 
@@ -25,7 +29,7 @@ class AminoCommand : AbstractCommand("amino", category = CommandCategory.UTILS) 
 	}
 
 	override fun run(context: CommandContext, locale: BaseLocale) {
-		if (context.args.size > 0) {
+		if (context.args.isNotEmpty()) {
 			if (context.args.size > 1) {
 				if (context.args[0] == context.locale["SEARCH"]) {
 					// Pesquisar uma comunidade no Amino
@@ -62,17 +66,30 @@ class AminoCommand : AbstractCommand("amino", category = CommandCategory.UTILS) 
 			} else {
 				if (context.args[0] == context.locale["AMINO_CONVERT"]) { // Converter imagens .Amino para imagens normais
 					if (context.message.attachments.isNotEmpty()) {
-						val attachment = context.message.attachments[0]
+						val imageUrl = URL(context.message.attachments.first().url)
+						val connection = imageUrl.openConnection() as HttpURLConnection
+						connection.setRequestProperty("User-Agent",
+								Constants.USER_AGENT)
 
-						val imagem = LorittaUtils.downloadImage(attachment.url)
+						val byteArray = IOUtils.toByteArray(connection.inputStream)
+						// Nós não conseguimos detectar se a imagem é uma GIF a partir do content type...
+						val isGif = byteArray.let {
+							val byte0 = it[0].toInt()
+							val byte1 = it[1].toInt()
+							val byte2 = it[2].toInt()
+							val byte3 = it[3].toInt()
+							val byte4 = it[4].toInt()
+							val byte5 = it[5].toInt()
 
-						if (imagem != null) {
-							context.sendFile(imagem, "amino.png", "\uD83D\uDDBC **|** " + context.getAsMention(true) + context.locale["AMINO_YOUR_IMAGE", context.message.attachments[0].fileName])
-							return
+							byte0 == 0x47 && byte1 == 0x49 && byte2 == 0x46 && byte3 == 0x38 && byte4 == 0x39 && byte5 == 0x61 // GIF89a
 						}
+
+						val extension = if (isGif) "gif" else "png"
+						context.sendFile(byteArray.inputStream(), "amino.$extension", "\uD83D\uDDBC **|** " + context.getAsMention(true) + context.locale["AMINO_YOUR_IMAGE", context.message.attachments[0].fileName])
+						return
 					}
 					context.sendMessage(Constants.ERROR + " **|** " + context.getAsMention(true) + context.locale["AMINO_NO_IMAGE_FOUND"])
-					return;
+					return
 				}
 				context.explain()
 			}
--- src/main/java/com/mrpowergamerbr/loritta/utils/modules/AminoConverterModule.kt
+++ src/main/java/com/mrpowergamerbr/loritta/utils/modules/AminoConverterModule.kt
@@ -1,25 +1,42 @@
 package com.mrpowergamerbr.loritta.utils.modules
 
+import com.mrpowergamerbr.loritta.utils.Constants
 import com.mrpowergamerbr.loritta.utils.LorittaUtils
 import net.dv8tion.jda.core.MessageBuilder
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent
+import org.apache.commons.io.IOUtils
 import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
+import java.net.HttpURLConnection
+import java.net.URL
 import javax.imageio.ImageIO
 
 object AminoConverterModule {
 	fun convertToImage(event: MessageReceivedEvent) {
 		for (attachments in event.message.attachments) {
 			if (attachments.fileName.endsWith(".Amino") || attachments.fileName == "Amino") {
-				val bufferedImage = LorittaUtils.downloadImage(attachments.url)
+				val imageUrl = URL(attachments.url)
+				val connection = imageUrl.openConnection() as HttpURLConnection
+				connection.setRequestProperty("User-Agent",
+						Constants.USER_AGENT)
 
-				ByteArrayOutputStream().use {
-					ImageIO.write(bufferedImage, "png", it)
-					ByteArrayInputStream(it.toByteArray()).use { inputStream ->
-						event.textChannel.sendFile(inputStream, "amino.png", MessageBuilder().append("(Por " + event.member.asMention + ") **Link para o \".Amino\":** " + attachments.url).build()).complete()
-						event.message.delete().complete()
-					}
+				val byteArray = IOUtils.toByteArray(connection.inputStream)
+				// Nós não conseguimos detectar se a imagem é uma GIF a partir do content type...
+				val isGif = byteArray.let {
+					val byte0 = it[0].toInt()
+					val byte1 = it[1].toInt()
+					val byte2 = it[2].toInt()
+					val byte3 = it[3].toInt()
+					val byte4 = it[4].toInt()
+					val byte5 = it[5].toInt()
+
+					byte0 == 0x47 && byte1 == 0x49 && byte2 == 0x46 && byte3 == 0x38 && byte4 == 0x39 && byte5 == 0x61 // GIF89a
 				}
+
+				val extension = if (isGif) "gif" else "png"
+
+				event.textChannel.sendFile(byteArray.inputStream(), "amino.$extension", MessageBuilder().append("(Por " + event.member.asMention + ") **Link para o \".Amino\":** " + attachments.url).build()).complete()
+				event.message.delete().complete()
 			}
 		}
 	}
