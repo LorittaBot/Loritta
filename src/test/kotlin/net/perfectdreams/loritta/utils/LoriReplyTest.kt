2a36034b696381538aa59c1b910af3bc813b0aef
MrPowerGamerBR <leonardomalaman@gmail.com>
2018-07-07 11:17:06 -0300

    :white_check_mark: | Adicionar unit tests
    Para garantir a estabilidade da Lori ao mexer no código dela, vamos começar a criar alguns unit tests para evitar mudanças no código que causam problemas

---
+++ src/test/kotlin/net/perfectdreams/loritta/utils/LoriReplyTest.kt
@@ -0,0 +1,46 @@
+package net.perfectdreams.loritta.utils
+
+import com.mrpowergamerbr.loritta.utils.Constants
+import com.mrpowergamerbr.loritta.utils.LoriReply
+import io.mockk.every
+import io.mockk.mockk
+import net.dv8tion.jda.core.entities.User
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class LoriReplyTest {
+	val user = mockk<User>()
+	val userId = "123170274651668480"
+
+	init {
+		every { user.asMention } returns "<@$userId>"
+	}
+
+	@Test
+	fun `simple lori reply`() {
+		val loriReply = LoriReply(message = "Hello World!")
+
+		assertThat(loriReply.build(user)).isEqualTo(Constants.LEFT_PADDING + " **|** <@123170274651668480> Hello World!")
+	}
+
+	@Test
+	fun `lori reply with custom prefix`() {
+		val loriReply = LoriReply(message = "Hello World!", prefix = "<:whatdog:388420518107414528>")
+
+		assertThat(loriReply.build(user)).isEqualTo("<:whatdog:388420518107414528> **|** <@123170274651668480> Hello World!")
+	}
+
+	@Test
+	fun `lori reply without mention`() {
+		val loriReply = LoriReply(message = "Hello World!", mentionUser = false)
+
+		assertThat(loriReply.build(user)).isEqualTo(Constants.LEFT_PADDING + " **|** Hello World!")
+	}
+
+	@Test
+	fun `lori reply without mention and with custom prefix`() {
+		val loriReply = LoriReply(message = "Hello World!", mentionUser = false, prefix = "<:whatdog:388420518107414528>")
+
+		assertThat(loriReply.build(user)).isEqualTo("<:whatdog:388420518107414528> **|** Hello World!")
+	}
+}
