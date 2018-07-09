de0fc702472f7a17cf3504a738864862132983fd
MrPowerGamerBR <leonardomalaman@gmail.com>
2018-07-09 10:58:54 -0300

    :pencil: | Tente reiniciar de maneira "segura"

--- src/main/java/com/mrpowergamerbr/loritta/website/requests/routes/page/api/v1/callbacks/UpdateAvailableCallbackController.kt
+++ src/main/java/com/mrpowergamerbr/loritta/website/requests/routes/page/api/v1/callbacks/UpdateAvailableCallbackController.kt
@@ -1,6 +1,8 @@
 package com.mrpowergamerbr.loritta.website.requests.routes.page.api.v1.callbacks
 
+import com.github.kevinsawicki.http.HttpRequest
 import com.mrpowergamerbr.loritta.utils.logger
+import com.mrpowergamerbr.loritta.utils.loritta
 import com.mrpowergamerbr.loritta.utils.lorittaShards
 import com.mrpowergamerbr.loritta.website.LoriAuthLevel
 import com.mrpowergamerbr.loritta.website.LoriDoNotLocaleRedirect
@@ -9,6 +11,7 @@ import org.jooby.Request
 import org.jooby.Response
 import org.jooby.mvc.GET
 import org.jooby.mvc.Path
+import kotlin.concurrent.thread
 
 @Path("/api/v1/callbacks/update-available")
 class UpdateAvailableCallbackController {
@@ -31,7 +34,25 @@ class UpdateAvailableCallbackController {
 		}
 
 		logger.info("Recebi que um update está disponível no Jenkins! Irei reiniciar para aplicar as novas mudanças recebidas!!!")
-		System.exit(0)
+
+		thread(start = true) {
+			Thread.sleep(50)
+			loritta.website.stop()
+			lorittaShards.shards.forEach {
+				it.shutdownNow()
+			}
+			System.exit(0)
+		}
+
 		return "{}"
 	}
+}
+
+fun main(args: Array<String>) {
+	val body = HttpRequest.get("https://canary.loritta.website/api/v1/callbacks/update-available")
+			.userAgent("hello world")
+			.authorization("c5KHPD3HVNEBmmw9")
+			.body()
+
+	println(body)
 }
