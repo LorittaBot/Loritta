22fd344b8e5338d55ded60d4031e4668b8cf18c8
MrPowerGamerBR <leonardomalaman@gmail.com>
2018-07-11 15:48:44 -0300

    :pencil: | Desativar mensagens de suporte caso EnvironmentType == PRODUCTION

--- src/main/java/com/mrpowergamerbr/loritta/modules/ServerSupportModule.kt
+++ src/main/java/com/mrpowergamerbr/loritta/modules/ServerSupportModule.kt
@@ -1,15 +1,17 @@
 package com.mrpowergamerbr.loritta.modules
 
+import com.mrpowergamerbr.loritta.Loritta
 import com.mrpowergamerbr.loritta.events.LorittaMessageEvent
 import com.mrpowergamerbr.loritta.userdata.LorittaProfile
 import com.mrpowergamerbr.loritta.userdata.ServerConfig
 import com.mrpowergamerbr.loritta.utils.LorittaUser
+import com.mrpowergamerbr.loritta.utils.config.EnvironmentType
 import com.mrpowergamerbr.loritta.utils.locale.BaseLocale
 import com.mrpowergamerbr.loritta.utils.response.responses.*
 
 class ServerSupportModule : MessageReceivedModule {
 	override fun matches(event: LorittaMessageEvent, lorittaUser: LorittaUser, lorittaProfile: LorittaProfile, serverConfig: ServerConfig, locale: BaseLocale): Boolean {
-		return event.channel.id == "398987569485971466"
+		return event.channel.id == "398987569485971466" && Loritta.config.environment == EnvironmentType.PRODUCTION
 	}
 
 	override fun handle(event: LorittaMessageEvent, lorittaUser: LorittaUser, lorittaProfile: LorittaProfile, serverConfig: ServerConfig, locale: BaseLocale): Boolean {
