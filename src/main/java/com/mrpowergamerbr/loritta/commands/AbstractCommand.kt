33d3880f27c639de8171b3d67eaea06cdfa3e958
MrPowerGamerBR <leonardomalaman@gmail.com>
2018-07-19 15:31:12 -0300

    :bug: | Corrigir tempo de cooldown

--- src/main/java/com/mrpowergamerbr/loritta/commands/AbstractCommand.kt
+++ src/main/java/com/mrpowergamerbr/loritta/commands/AbstractCommand.kt
@@ -20,7 +20,9 @@ abstract class AbstractCommand(open val label: String, var aliases: List<String>
 		val logger = LoggerFactory.getLogger(AbstractCommand::class.java)
 	}
 
-	val cooldown = if (needsToUploadFiles()) 10000 else 5000
+	val cooldown: Int
+		get() = if (needsToUploadFiles()) 10000 else 5000
+	
 	var executedCount = 0
 
 	open fun getDescription(locale: BaseLocale): String {
