e5ce02c2037de35d4aa059699d33bccc7f633e33
MrPowerGamerBR <leonardomalaman@gmail.com>
2018-07-20 08:33:52 -0300

    :fire: | Isto não deveria estar aqui :eyes:

--- src/main/java/com/mrpowergamerbr/loritta/commands/AbstractCommand.kt
+++ src/main/java/com/mrpowergamerbr/loritta/commands/AbstractCommand.kt
@@ -381,7 +381,6 @@ abstract class AbstractCommand(open val label: String, var aliases: List<String>
 		val locale = context.locale
 
 		if (conf.explainOnCommandRun) {
-			context.userHandle.openPrivateChannel().complete()
 			val rawArguments = context.message.contentRaw.split(" ")
 			var commandLabel = rawArguments[0]
 			if (rawArguments.getOrNull(1) != null && (rawArguments[0] == "<@${Loritta.config.clientId}>" || rawArguments[0] == "<@!${Loritta.config.clientId}>")) {
