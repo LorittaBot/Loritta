d5fa9459978d869e4a9e3a56ec9d0573299b3c8d
MrPowerGamerBR <leonardomalaman@gmail.com>
2018-08-06 19:30:21 -0300

    Adicionar logging na hora que um usuário é punido

--- src/main/java/com/mrpowergamerbr/loritta/utils/networkbans/LorittaNetworkBanManager.kt
+++ src/main/java/com/mrpowergamerbr/loritta/utils/networkbans/LorittaNetworkBanManager.kt
@@ -76,6 +76,7 @@ class LorittaNetworkBanManager {
 
 	fun addBanEntry(entry: NetworkBanEntry) {
 		val userId = entry.id
+		logger.info { "Adicionando $userId na lista de usuários banidos na Loritta Network..." }
 		val user = lorittaShards.getUserById(entry.id) ?: run {
 			logger.error("$userId não é um usuário válido!")
 			return
