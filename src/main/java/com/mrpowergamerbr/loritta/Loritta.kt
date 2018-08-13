dc21cf4734509365a718a8c39119510ccfb3da1e
MrPowerGamerBR <leonardomalaman@gmail.com>
2018-08-13 19:20:11 -0300

    Não fazer cache do jogo que o usuário está jogando para economizar memória

--- src/main/java/com/mrpowergamerbr/loritta/Loritta.kt
+++ src/main/java/com/mrpowergamerbr/loritta/Loritta.kt
@@ -43,6 +43,7 @@ import kotlinx.coroutines.experimental.asCoroutineDispatcher
 import mu.KotlinLogging
 import net.dv8tion.jda.core.AccountType
 import net.dv8tion.jda.core.JDABuilder
+import net.dv8tion.jda.core.utils.cache.CacheFlag
 import org.bson.codecs.configuration.CodecRegistries
 import org.bson.codecs.pojo.PojoCodecProvider
 import org.slf4j.LoggerFactory
@@ -169,6 +170,7 @@ class Loritta(config: LorittaConfig) {
 				.setToken(Loritta.config.clientToken)
 				.setCorePoolSize(16)
 				.setBulkDeleteSplittingEnabled(false)
+				.setDisabledCacheFlags(EnumSet.of(CacheFlag.GAME))
 				.addEventListener(discordListener)
 				.addEventListener(eventLogListener)
 				.addEventListener(messageListener)
