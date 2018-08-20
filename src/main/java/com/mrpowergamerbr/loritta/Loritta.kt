60bb1f0573f81ecdd1cfc73ce1a635cdaa98ace9
MrPowerGamerBR <leonardomalaman@gmail.com>
2018-08-20 20:03:23 -0300

    Diminuir número de threads da core pool

--- src/main/java/com/mrpowergamerbr/loritta/Loritta.kt
+++ src/main/java/com/mrpowergamerbr/loritta/Loritta.kt
@@ -167,7 +167,7 @@ class Loritta(config: LorittaConfig) {
 		builder = JDABuilder(AccountType.BOT)
 				.setStatus(Loritta.config.userStatus)
 				.setToken(Loritta.config.clientToken)
-				.setCorePoolSize(128)
+				.setCorePoolSize(16)
 				.setBulkDeleteSplittingEnabled(false)
 				// .setDisabledCacheFlags(EnumSet.of(CacheFlag.GAME))
 				.addEventListener(discordListener)
