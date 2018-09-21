f0dd04d3ece23956e8288bf26fccdb3991df0894
MrPowerGamerBR <leonardomalaman@gmail.com>
2018-09-21 18:41:45 -0300

    Adicionar import

--- src/main/java/com/mrpowergamerbr/loritta/commands/CommandManager.kt
+++ src/main/java/com/mrpowergamerbr/loritta/commands/CommandManager.kt
@@ -2,6 +2,7 @@ package com.mrpowergamerbr.loritta.commands
 
 import com.mrpowergamerbr.loritta.Loritta
 import com.mrpowergamerbr.loritta.commands.vanilla.`fun`.*
+import com.mrpowergamerbr.loritta.commands.vanilla.actions.HugCommand
 import com.mrpowergamerbr.loritta.commands.vanilla.administration.*
 import com.mrpowergamerbr.loritta.commands.vanilla.discord.*
 import com.mrpowergamerbr.loritta.commands.vanilla.economy.*
