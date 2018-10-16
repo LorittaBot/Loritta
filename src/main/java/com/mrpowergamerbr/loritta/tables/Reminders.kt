f33ca873c60e7423072758adc4c59e72036b6b2d
MrPowerGamerBR <leonardomalaman@gmail.com>
2018-10-16 15:18:24 -0300

    Retirar override de IDs

--- src/main/java/com/mrpowergamerbr/loritta/tables/Reminders.kt
+++ src/main/java/com/mrpowergamerbr/loritta/tables/Reminders.kt
@@ -1,13 +1,8 @@
 package com.mrpowergamerbr.loritta.tables
 
-import org.jetbrains.exposed.dao.EntityID
 import org.jetbrains.exposed.dao.LongIdTable
-import org.jetbrains.exposed.sql.Column
 
 object Reminders : LongIdTable() {
-	override val id: Column<EntityID<Long>>
-		get() = long("id").primaryKey().autoIncrement().entityId()
-
 	val userId = long("user_id").index()
 	val channelId = long("channel_id")
 	val remindAt = long("remind_at")
--- src/main/java/com/mrpowergamerbr/loritta/tables/UsernameChanges.kt
+++ src/main/java/com/mrpowergamerbr/loritta/tables/UsernameChanges.kt
@@ -1,13 +1,8 @@
 package com.mrpowergamerbr.loritta.tables
 
-import org.jetbrains.exposed.dao.EntityID
 import org.jetbrains.exposed.dao.LongIdTable
-import org.jetbrains.exposed.sql.Column
 
 object UsernameChanges : LongIdTable() {
-	override val id: Column<EntityID<Long>>
-		get() = long("id").primaryKey().autoIncrement().entityId()
-
 	val userId = long("user_id").index()
 	val username = text("username")
 	val discriminator = text("discriminator")
