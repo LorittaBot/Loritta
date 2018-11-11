0049f5af5f3bd774688ce48c4fdb4cb27623f556
MrPowerGamerBR <leonardomalaman@gmail.com>
2018-11-11 18:00:56 -0200

    Apenas mostrar quem fez a fan art ao ver o avatar da Lori, e não de todos os usuários

--- src/main/java/com/mrpowergamerbr/loritta/commands/vanilla/discord/AvatarCommand.kt
+++ src/main/java/com/mrpowergamerbr/loritta/commands/vanilla/discord/AvatarCommand.kt
@@ -38,30 +38,31 @@ class AvatarCommand : AbstractCommand("avatar", category = CommandCategory.DISCO
 		embed.setColor(Constants.DISCORD_BLURPLE) // Cor do embed (Cor padrão do Discord)
 		embed.setDescription("**${context.locale["AVATAR_CLICKHERE", getAvatar.effectiveAvatarUrl + "?size=2048"]}**")
 
-		if (getAvatar.id == Loritta.config.clientId)
-			embed.appendDescription("\n*${context.locale["AVATAR_LORITTACUTE"]}* \uD83D\uDE0A")
 		if (getAvatar.id == "390927821997998081")
 			embed.appendDescription("\n*${context.locale["AVATAR_PantufaCute"]}* \uD83D\uDE0A")
 
-		val calendar = Calendar.getInstance()
-		val currentDay = calendar.get(Calendar.DAY_OF_WEEK)
+		if (getAvatar.id == Loritta.config.clientId) {
+			val calendar = Calendar.getInstance()
+			val currentDay = calendar.get(Calendar.DAY_OF_WEEK)
+			
+			embed.appendDescription("\n*${context.locale["AVATAR_LORITTACUTE"]}* \uD83D\uDE0A")
+			if (Loritta.config.fanArtExtravaganza && currentDay == Calendar.SUNDAY) {
+				val fanArt = UpdateStatusThread.currentFanArt
 
-		if (Loritta.config.fanArtExtravaganza && currentDay == Calendar.SUNDAY) {
-			val fanArt = UpdateStatusThread.currentFanArt
+				val user = lorittaShards.retrieveUserById(fanArt.artistId)
 
-			val user = lorittaShards.retrieveUserById(fanArt.artistId)
+				val displayName = fanArt.fancyName ?: user?.name
 
-			val displayName = fanArt.fancyName ?: user?.name
-
-			embed.appendDescription("\n\n**" + locale.format(displayName) { commands.fanarts.madeBy } + "**")
-			val artist = loritta.fanArtConfig.artists[fanArt.artistId]
-			if (artist != null) {
-				for (socialNetwork in artist.socialNetworks) {
-					var root = socialNetwork.display
-					if (socialNetwork.link != null) {
-						root = "[$root](${socialNetwork.link})"
+				embed.appendDescription("\n\n**" + locale.format(displayName) { commands.fanarts.madeBy } + "**")
+				val artist = loritta.fanArtConfig.artists[fanArt.artistId]
+				if (artist != null) {
+					for (socialNetwork in artist.socialNetworks) {
+						var root = socialNetwork.display
+						if (socialNetwork.link != null) {
+							root = "[$root](${socialNetwork.link})"
+						}
+						embed.appendDescription("\n**${socialNetwork.socialNetwork.fancyName}:** $root")
 					}
-					embed.appendDescription("\n**${socialNetwork.socialNetwork.fancyName}:** $root")
 				}
 			}
 		}
