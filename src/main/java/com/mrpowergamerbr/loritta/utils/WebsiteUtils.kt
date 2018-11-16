274b1b3fd3340021f106c908e6c404d0f36f7196
MrPowerGamerBR <leonardomalaman@gmail.com>
2018-11-16 16:18:11 -0200

    Apenas enviar o hash da imagem para o frontend

--- src/main/java/com/mrpowergamerbr/loritta/utils/WebsiteUtils.kt
+++ src/main/java/com/mrpowergamerbr/loritta/utils/WebsiteUtils.kt
@@ -176,7 +176,7 @@ object WebsiteUtils {
 					val simpleUserIdentification = SimpleUserIdentification(
 							user.name,
 							user.id,
-							user.effectiveAvatarUrl,
+							user.effectiveAvatarUrl.split("/")[5].split(".")[0],
 							user.discriminator
 					)
 
