f9ccc575b38a330f2d25d2bc5bdff3416cf40eb2
MrPowerGamerBR <leonardomalaman@gmail.com>
2018-11-29 16:03:34 -0200

    Adicionar rate limit na endpoint de "send-message"

--- src/main/java/com/mrpowergamerbr/loritta/website/LoriWebCode.kt
+++ src/main/java/com/mrpowergamerbr/loritta/website/LoriWebCode.kt
@@ -17,6 +17,7 @@ enum class LoriWebCode(val errorId: Int, val fancyName: String) {
 	CHANNEL_DOESNT_EXIST(722, "Channel doesn't exist"),
 	CANT_TALK_IN_CHANNEL(723, "Can't talk in channel"),
 	INVALID_MESSAGE(724, "Invalid message"),
+	RATE_LIMIT(799, "Rate limited!"),
 
 	MISSING_PAYLOAD_HANDLER(810, "Missing Payload Handler"),
 }
