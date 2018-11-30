32553ad2fac275c259eccdc9cf0b52c72bae29c4
MrPowerGamerBR <leonardomalaman@gmail.com>
2018-11-30 16:03:57 -0200

    Tirar alguns "complete()"

--- src/main/java/com/mrpowergamerbr/loritta/listeners/EventLogListener.kt
+++ src/main/java/com/mrpowergamerbr/loritta/listeners/EventLogListener.kt
@@ -425,13 +425,13 @@ class EventLogListener(internal val loritta: Loritta) : ListenerAdapter() {
 		if (DebugLog.cancelAllEvents)
 			return
 
-		loritta.executor.execute {
+		GlobalScope.launch(loritta.coroutineDispatcher) {
 			// Fazer relay de bans
 			if (event.guild.id == Constants.PORTUGUESE_SUPPORT_GUILD_ID) {
 				val relayTo = lorittaShards.getGuildById(Constants.ENGLISH_SUPPORT_GUILD_ID)
 
 				if (relayTo != null) {
-					if (relayTo.banList.complete().firstOrNull { it.user == event.user } == null) {
+					if (relayTo.banList.await().firstOrNull { it.user == event.user } == null) {
 						relayTo.controller.ban(event.user, 7, "Banned on LorittaLand (Brazilian Server)")?.queue()
 					}
 				}
@@ -440,7 +440,7 @@ class EventLogListener(internal val loritta: Loritta) : ListenerAdapter() {
 				val relayTo = lorittaShards.getGuildById(Constants.PORTUGUESE_SUPPORT_GUILD_ID)
 
 				if (relayTo != null) {
-					if (relayTo.banList.complete().firstOrNull { it.user == event.user } == null) {
+					if (relayTo.banList.await().firstOrNull { it.user == event.user } == null) {
 						relayTo.controller.ban(event.user, 7, "Banido na LorittaLand (English Server)")?.queue()
 					}
 				}
@@ -450,17 +450,17 @@ class EventLogListener(internal val loritta: Loritta) : ListenerAdapter() {
 			val eventLogConfig = serverConfig.eventLogConfig
 
 			if (eventLogConfig.isEnabled && eventLogConfig.memberBanned) {
-				val textChannel = event.guild.getTextChannelById(eventLogConfig.eventLogChannelId) ?: return@execute
+				val textChannel = event.guild.getTextChannelById(eventLogConfig.eventLogChannelId) ?: return@launch
 				val locale = loritta.getLocaleById(serverConfig.localeId)
 
 				if (!textChannel.canTalk())
-					return@execute
+					return@launch
 				if (!event.guild.selfMember.hasPermission(Permission.MESSAGE_EMBED_LINKS))
-					return@execute
+					return@launch
 				if (!event.guild.selfMember.hasPermission(Permission.VIEW_CHANNEL))
-					return@execute
+					return@launch
 				if (!event.guild.selfMember.hasPermission(Permission.MESSAGE_READ))
-					return@execute
+					return@launch
 
 				val embed = EmbedBuilder()
 				embed.setTimestamp(Instant.now())
@@ -482,7 +482,7 @@ class EventLogListener(internal val loritta: Loritta) : ListenerAdapter() {
 				embed.setFooter(locale["EVENTLOG_USER_ID", event.user.id], null)
 
 				textChannel.sendMessage(embed.build()).queue()
-				return@execute
+				return@launch
 			}
 		}
 	}
@@ -491,7 +491,7 @@ class EventLogListener(internal val loritta: Loritta) : ListenerAdapter() {
 		if (DebugLog.cancelAllEvents)
 			return
 
-		loritta.executor.execute {
+		GlobalScope.launch(loritta.coroutineDispatcher) {
 			// Fazer relay de unbans
 			if (event.guild.id == Constants.PORTUGUESE_SUPPORT_GUILD_ID) {
 				val relayTo = lorittaShards.getGuildById(Constants.ENGLISH_SUPPORT_GUILD_ID)
@@ -508,16 +508,16 @@ class EventLogListener(internal val loritta: Loritta) : ListenerAdapter() {
 			val eventLogConfig = serverConfig.eventLogConfig
 
 			if (eventLogConfig.isEnabled && eventLogConfig.memberUnbanned) {
-				val textChannel = event.guild.getTextChannelById(eventLogConfig.eventLogChannelId) ?: return@execute
+				val textChannel = event.guild.getTextChannelById(eventLogConfig.eventLogChannelId) ?: return@launch
 				val locale = loritta.getLocaleById(serverConfig.localeId)
 				if (!textChannel.canTalk())
-					return@execute
+					return@launch
 				if (!event.guild.selfMember.hasPermission(Permission.MESSAGE_EMBED_LINKS))
-					return@execute
+					return@launch
 				if (!event.guild.selfMember.hasPermission(Permission.VIEW_CHANNEL))
-					return@execute
+					return@launch
 				if (!event.guild.selfMember.hasPermission(Permission.MESSAGE_READ))
-					return@execute
+					return@launch
 
 				val embed = EmbedBuilder()
 				embed.setTimestamp(Instant.now())
@@ -527,7 +527,7 @@ class EventLogListener(internal val loritta: Loritta) : ListenerAdapter() {
 
 				if (event.guild.selfMember.hasPermission(Permission.VIEW_AUDIT_LOGS)) {
 					// Caso a Loritta consiga ver o audit log, vamos pegar quem baniu e o motivo do ban!
-					val auditLog = event.guild.auditLogs.complete().first()
+					val auditLog = event.guild.auditLogs.await().first()
 
 					if (auditLog.type == ActionType.UNBAN) {
 						message += "\n${locale["EVENTLOG_UnbannedBy", auditLog.user?.asMention ?: "???"]}"
@@ -539,7 +539,7 @@ class EventLogListener(internal val loritta: Loritta) : ListenerAdapter() {
 				embed.setFooter(locale["EVENTLOG_USER_ID", event.user.id], null)
 
 				textChannel.sendMessage(embed.build()).queue()
-				return@execute
+				return@launch
 			}
 		}
 	}
