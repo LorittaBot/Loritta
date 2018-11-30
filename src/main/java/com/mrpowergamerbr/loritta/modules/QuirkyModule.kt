58a0147a6b209612146a11a25803ca24fe85ff82
MrPowerGamerBR <leonardomalaman@gmail.com>
2018-11-30 10:32:31 -0200

    Verificar se a Lori tem permissão para enviar mensagens antes de realizar algo no QuirkyModule

--- src/main/java/com/mrpowergamerbr/loritta/modules/QuirkyModule.kt
+++ src/main/java/com/mrpowergamerbr/loritta/modules/QuirkyModule.kt
@@ -13,7 +13,7 @@ import net.dv8tion.jda.core.entities.MessageType
 
 class QuirkyModule : MessageReceivedModule {
 	override fun matches(event: LorittaMessageEvent, lorittaUser: LorittaUser, lorittaProfile: Profile, serverConfig: ServerConfig, locale: BaseLocale): Boolean {
-		return serverConfig.miscellaneousConfig.enableQuirky && event.guild?.selfMember?.hasPermission(event.textChannel, Permission.MESSAGE_ADD_REACTION, Permission.MESSAGE_EXT_EMOJI, Permission.MESSAGE_HISTORY) == true && event.message.type == MessageType.DEFAULT
+		return serverConfig.miscellaneousConfig.enableQuirky && event.guild?.selfMember?.hasPermission(event.textChannel, Permission.MESSAGE_ADD_REACTION, Permission.MESSAGE_EXT_EMOJI, Permission.MESSAGE_HISTORY, Permission.MESSAGE_WRITE) == true && event.message.type == MessageType.DEFAULT
 	}
 
 	override fun handle(event: LorittaMessageEvent, lorittaUser: LorittaUser, lorittaProfile: Profile, serverConfig: ServerConfig, locale: BaseLocale): Boolean {
