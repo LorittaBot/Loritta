cadec78aadedb47c4c050333881b539bdc44ba08
MrGaabriel <hello@mrgaabriel.space>
2018-12-04 11:47:42 -0200

    Identação

--- src/main/java/com/mrpowergamerbr/loritta/commands/vanilla/discord/ChannelInfoCommand.kt
+++ src/main/java/com/mrpowergamerbr/loritta/commands/vanilla/discord/ChannelInfoCommand.kt
@@ -12,34 +12,34 @@ import net.dv8tion.jda.core.EmbedBuilder
 import net.dv8tion.jda.core.entities.TextChannel
 
 class ChannelInfoCommand : AbstractCommand("channelinfo", listOf("channel"), CommandCategory.DISCORD) {
-    override fun getDescription(locale: BaseLocale): String {
-        return locale.commands.channelInfo.description
-    }
-
-    override fun canUseInPrivateChannel(): Boolean {
-        return false
-    }
-
-    override suspend fun run(context: CommandContext, locale: BaseLocale) {
-        val channel = if (context.args.isEmpty()) {
-            context.message.textChannel
-        } else {
+	override fun getDescription(locale: BaseLocale): String {
+		return locale.commands.channelInfo.description
+	}
+	
+	override fun canUseInPrivateChannel(): Boolean {
+		return false
+	}
+	
+	override suspend fun run(context: CommandContext, locale: BaseLocale) {
+		val channel = if (context.args.isEmpty()) {
+			context.message.textChannel
+		} else {
 			getTextChannel(context, context.rawArgs[0])
-        }
+		}
 		
-        if (channel == null) {
-            context.reply(
-                    LoriReply(
-                            message = locale.commands.channelInfo.channelNotFound,
-                            prefix = Constants.ERROR
-                    )
-            )
-            return
-        }
-
+		if (channel == null) {
+			context.reply(
+					LoriReply(
+							message = locale.commands.channelInfo.channelNotFound,
+							prefix = Constants.ERROR
+					)
+			)
+			return
+		}
+		
 		val channelCreatedDiff = DateUtils.formatDateDiff(channel.creationTime.toInstant().toEpochMilli(), context.locale)
 		
-        val builder = EmbedBuilder()
+		val builder = EmbedBuilder()
 		
 		builder.setColor(Constants.DISCORD_BLURPLE)
 		builder.setTitle("\uD83D\uDC81 ${locale["CHANNELINFO_ChannelInformation", "#${channel.name}"]}")
@@ -49,9 +49,9 @@ class ChannelInfoCommand : AbstractCommand("channelinfo", listOf("channel"), Com
 		builder.addField("\uD83D\uDD1E NSFW", if (channel.isNSFW) locale["LORITTA_Yes"] else locale["LORITTA_No"], true)
 		builder.addField("\uD83D\uDCC5 ${context.locale.commands.channelInfo.channelCreated}", channelCreatedDiff, true)
 		builder.addField("\uD83D\uDCD8 ${context.locale.commands.channelInfo.channelTopic}", if (channel.topic.isNullOrEmpty()) context.locale.commands.channelInfo.undefined else "```${channel.topic}```", true)
-
-        context.sendMessage(context.userHandle.asMention, builder.build())
-    }
+		
+		context.sendMessage(context.userHandle.asMention, builder.build())
+	}
 	
 	fun getTextChannel(context: CommandContext, input: String?): TextChannel? {
 		if (input == null)
