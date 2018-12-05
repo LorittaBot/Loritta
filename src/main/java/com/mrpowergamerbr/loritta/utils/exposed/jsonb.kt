6ba1ae01d22492f7b17073eb5361ca66ef27ec92
MrPowerGamerBR <leonardomalaman@gmail.com>
2018-12-05 20:53:43 -0200

    Debugging!

--- src/main/java/com/mrpowergamerbr/loritta/utils/exposed/jsonb.kt
+++ src/main/java/com/mrpowergamerbr/loritta/utils/exposed/jsonb.kt
@@ -14,6 +14,7 @@ class Json<out T : Any>(private val klass: Class<T>, private val jsonMapper: Gso
 	override fun sqlType() = "jsonb"
 
 	override fun setParameter(stmt: PreparedStatement, index: Int, value: Any?) {
+		println("Setting param $value!")
 		val obj = PGobject()
 		obj.type = "jsonb"
 		obj.value = value as String
@@ -21,9 +22,12 @@ class Json<out T : Any>(private val klass: Class<T>, private val jsonMapper: Gso
 	}
 
 	override fun valueFromDB(value: Any): Any {
+		println("valueFromDB is $value - ${value::class.java}")
 		if (value !is PGobject)
 			return value
 
+		println("Converting from JSON!!")
+		
 		return try {
 			jsonMapper.fromJson(value.value, klass)
 		} catch (e: Exception) {
@@ -33,10 +37,16 @@ class Json<out T : Any>(private val klass: Class<T>, private val jsonMapper: Gso
 	}
 
 	override fun notNullValueToDB(value: Any): Any {
+		println("notNullValueToDB is $value - ${value::class.java}")
 		if (value is String)
 			return value
+
+		println("Converting to JSON!! $value @ notNullValueToDB")
 		return jsonMapper.toJson(value)
 	}
 
-	override fun nonNullValueToString(value: Any): String = "'${jsonMapper.toJson(value)}'"
+	override fun nonNullValueToString(value: Any): String {
+		println("nonNullValueToString is $value - ${value::class.java}")
+		return "'${jsonMapper.toJson(value)}'"
+	}
 }
