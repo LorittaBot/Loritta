7815dbf7a1c5104f2985c52573c68db3104dd6e7
kaikecarlos <caheasta@gmail.com>
2018-12-10 20:51:26 -0300

    Create LockCommand.kt

---
+++ src/main/java/com/mrpowergamerbr/loritta/commands/vanilla/administration/LockCommand.kt
@@ -0,0 +1,10 @@
+package com.mrpowergamerbr.loritta.commands.vanilla.administration
+
+import com.mrpowergamerbr.loritta.commands.*
+import com.mrpowergamerbr.loritta.utils.*
+import net.dv8tion.jda.core.Permission
+import com.mrpowergamerbr.loritta.utils.locale.BaseLocale
+
+class LockCommand : AbscractCommand("lock", listOf("trancar"), CommandCategory.ADMIN){
+
+}
