6131a9612605ee9c2bf50bac904187ff7455d955
MrPowerGamerBR <leonardomalaman@gmail.com>
2018-12-11 14:19:12 -0200

    Caso mais de 20 pessoas saiam ao mesmo tempo, ignore e envie um arquivo com todas que sairam depois

--- src/main/java/com/mrpowergamerbr/loritta/modules/WelcomeModule.kt
+++ src/main/java/com/mrpowergamerbr/loritta/modules/WelcomeModule.kt
@@ -1,20 +1,68 @@
 package com.mrpowergamerbr.loritta.modules
 
+import com.github.benmanes.caffeine.cache.Caffeine
 import com.mrpowergamerbr.loritta.Loritta
 import com.mrpowergamerbr.loritta.LorittaLauncher.loritta
 import com.mrpowergamerbr.loritta.userdata.ServerConfig
+import com.mrpowergamerbr.loritta.utils.Emotes
 import com.mrpowergamerbr.loritta.utils.LorittaUtils
 import com.mrpowergamerbr.loritta.utils.MessageUtils
 import com.mrpowergamerbr.loritta.utils.extensions.await
 import com.mrpowergamerbr.loritta.utils.extensions.humanize
+import com.mrpowergamerbr.loritta.utils.lorittaShards
 import kotlinx.coroutines.delay
+import mu.KotlinLogging
+import net.dv8tion.jda.core.MessageBuilder
 import net.dv8tion.jda.core.Permission
 import net.dv8tion.jda.core.audit.ActionType
+import net.dv8tion.jda.core.entities.User
 import net.dv8tion.jda.core.events.guild.member.GuildMemberJoinEvent
 import net.dv8tion.jda.core.events.guild.member.GuildMemberLeaveEvent
+import org.apache.commons.io.IOUtils
+import java.nio.charset.Charset
+import java.util.concurrent.CopyOnWriteArrayList
 import java.util.concurrent.TimeUnit
 
 object WelcomeModule {
+	private val logger = KotlinLogging.logger {}
+
+	val leftMembersCache = Caffeine.newBuilder()
+			.expireAfterAccess(15, TimeUnit.SECONDS)
+			.removalListener { k1: Long?, v1: CopyOnWriteArrayList<User>?, removalCause ->
+				if (k1 != null && v1 != null) {
+					logger.info("Removendo left members cache de ${k1}... ${v1.size} membros tinham saído durante este período")
+
+					if (v1.size > 20) {
+						logger.info("Mais de 20 membros sairam em menos de 15 segundos em ${k1}! Que triste, né? Vamos enviar um arquivo com todos que sairam!")
+
+						val joinLeaveConfig = loritta.getServerConfigForGuild(v1.toString()).joinLeaveConfig
+						if (joinLeaveConfig.tellOnLeave && joinLeaveConfig.leaveMessage.isNotEmpty()) {
+							val guild = lorittaShards.getGuildById(k1) ?: return@removalListener
+
+							if (joinLeaveConfig.canalLeaveId != null) {
+								val textChannel = guild.getTextChannelById(joinLeaveConfig.canalLeaveId)
+
+								if (textChannel != null) {
+									if (textChannel.canTalk()) {
+										if (guild.selfMember.hasPermission(Permission.MESSAGE_ATTACH_FILES)) {
+											val lines = mutableListOf<String>()
+											for (user in v1) {
+												lines.add("${user.name}#${user.discriminator} - (${user.id})")
+											}
+											val targetStream = IOUtils.toInputStream(lines.joinToString("\n"), Charset.defaultCharset())
+											textChannel.sendFile(targetStream, "left-users.log", MessageBuilder().setContent("Quanta gente saindo! Para não encher o canal de mensagens, aqui está a lista de todos que sairam ${Emotes.LORI_OWO}").build())
+											logger.info("Enviado arquivo de texto em $k1 com todas as pessoas que sairam, yay!")
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+			.build<Long, CopyOnWriteArrayList<User>>()
+
+
 	suspend fun handleJoin(event: GuildMemberJoinEvent, serverConfig: ServerConfig) {
 		if (Loritta.config.ghostIds.contains(event.user.id)) // Ignorar ID do RevengeTakedown
 			return
@@ -65,6 +113,13 @@ object WelcomeModule {
 		val joinLeaveConfig = serverConfig.joinLeaveConfig
 		if (joinLeaveConfig.tellOnLeave && joinLeaveConfig.leaveMessage.isNotEmpty()) {
 			val guild = event.guild
+
+			val list = leftMembersCache.getIfPresent(event.guild.idLong) ?: CopyOnWriteArrayList()
+			list.add(event.user)
+			leftMembersCache.put(event.guild.idLong, list)
+
+			if (list.size > 20)
+				return
 
 			if (joinLeaveConfig.canalLeaveId != null) {
 				val textChannel = guild.getTextChannelById(joinLeaveConfig.canalLeaveId)
