fedaf5ae6df2e40ae87c9cc680ad5bae2e0acbff
kaikecarlos <caheasta@gmail.com>
2018-12-15 15:35:08 -0300

    Create UnlockCommand.kt

---
+++ src/main/java/com/mrpowergamerbr/loritta/commands/vanilla/administration/UnlockCommand.kt
@@ -0,0 +1,42 @@
+package com.mrpowergamerbr.loritta.commands.vanilla.administration
+
+import com.mrpowergamerbr.loritta.commands.*
+import com.mrpowergamerbr.loritta.utils.*
+import net.dv8tion.jda.core.Permission
+import com.mrpowergamerbr.loritta.utils.locale.BaseLocale
+
+class UnLockCommand : AbstractCommand("unlock", listOf("destrancar"), CommandCategory.ADMIN){
+	override fun getDescription(locale: BaseLocale): String {
+		return locale.format { commands.administration.unlock.description }
+	}
+	override fun getDiscordPermissions(): List<Permission> {
+		return listOf(Permission.MANAGE_SERVER)
+	}
+	override fun canUseInPrivateChannel(): Boolean {
+		return false
+	}
+	override fun getBotPermissions(): List<Permission> {
+		return listOf(Permission.MANAGE_CHANNEL, Permission.MANAGE_PERMISSIONS)
+	}
+	override suspend fun run(context: CommandContext,locale: BaseLocale) {
+		if (context.args.isNotEmpty()) {
+			val textChannel = context.event.textChannel
+			if (textChannel != null) {
+					val everyoneRole = context.guild.publicRole
+					val permissionOverride = textChannel.getPermissionOverride(everyoneRole)
+					if (permissionOverride == null) {
+						textChannel.createPermissionOverride(everyoneRole)
+						.setAllow(Permission.MESSAGE_WRITE)
+						.queue()
+						contex.reply(
+							LoriReply(
+								locale.format { commands.administration.unlock.success }
+							)
+						)
+					} 
+				}
+		} else {
+			context.explain()
+		}
+	}
+}
