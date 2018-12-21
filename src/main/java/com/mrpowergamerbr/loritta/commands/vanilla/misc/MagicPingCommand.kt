b2e8aa3474a069d817955723426aed36f8f7e8e6
MrPowerGamerBR <leonardomalaman@gmail.com>
2018-12-20 23:21:49 -0200

    CommandContext -> LorittaCommandContext

--- src/main/java/com/mrpowergamerbr/loritta/commands/vanilla/misc/MagicPingCommand.kt
+++ src/main/java/com/mrpowergamerbr/loritta/commands/vanilla/misc/MagicPingCommand.kt
@@ -1,17 +1,17 @@
 package com.mrpowergamerbr.loritta.commands.vanilla.misc
 
-import com.mrpowergamerbr.loritta.commands.CommandContext
 import com.mrpowergamerbr.loritta.utils.LoriReply
 import com.mrpowergamerbr.loritta.utils.locale.BaseLocale
 import net.dv8tion.jda.core.entities.User
 import net.perfectdreams.commands.annotation.Subcommand
 import net.perfectdreams.commands.loritta.LorittaCommand
+import net.perfectdreams.commands.loritta.LorittaCommandContext
 import net.perfectdreams.commands.loritta.notNull
 import kotlin.contracts.ExperimentalContracts
 
 class MagicPingCommand : LorittaCommand(arrayOf("magicping")) {
 	@Subcommand
-	suspend fun root(context: CommandContext, locale: BaseLocale) {
+	suspend fun root(context: LorittaCommandContext, locale: BaseLocale) {
 		context.reply(
 				LoriReply(
 						"Na verdade isto é só um comando para testes... yay?"
@@ -21,7 +21,7 @@ class MagicPingCommand : LorittaCommand(arrayOf("magicping")) {
 
 	@ExperimentalContracts
 	@Subcommand(["mention"])
-	suspend fun mentionUser(context: CommandContext, locale: BaseLocale, user: User?) {
+	suspend fun mentionUser(context: LorittaCommandContext, locale: BaseLocale, user: User?) {
 		notNull(user, "that ain't a user dawg")
 
 		context.reply(
