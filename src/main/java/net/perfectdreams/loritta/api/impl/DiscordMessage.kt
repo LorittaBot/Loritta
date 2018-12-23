aa7169b05b37216feb9c35cdbf5efbe80df6f91d
MrPowerGamerBR <leonardomalaman@gmail.com>
2018-12-23 17:36:17 -0200

    Adicionar DiscordMessage.author

--- src/main/java/net/perfectdreams/loritta/api/impl/DiscordMessage.kt
+++ src/main/java/net/perfectdreams/loritta/api/impl/DiscordMessage.kt
@@ -1,10 +1,8 @@
 package net.perfectdreams.loritta.api.impl
 
 import net.perfectdreams.loritta.api.entities.Message
-import net.perfectdreams.loritta.api.entities.User
 
 class DiscordMessage(val handle: net.dv8tion.jda.core.entities.Message) : Message {
-	override val author: User
-		get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+	override val author = DiscordUser(handle.author)
 	override val content = handle.contentRaw
 }
