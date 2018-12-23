83595649160375cbaba6ad4606b0205150b9a133
MrPowerGamerBR <leonardomalaman@gmail.com>
2018-12-23 13:08:44 -0200

    Adicionar sistema de plugins

---
+++ src/main/java/com/mrpowergamerbr/loritta/plugin/LorittaPlugin.kt
@@ -0,0 +1,25 @@
+package com.mrpowergamerbr.loritta.plugin
+
+import com.mrpowergamerbr.loritta.Loritta
+import com.mrpowergamerbr.loritta.utils.loritta
+import net.perfectdreams.commands.loritta.LorittaCommand
+import java.io.File
+import java.net.URLClassLoader
+
+open class LorittaPlugin(val name: String, val classLoader: URLClassLoader? = null) {
+	val commands = mutableListOf<LorittaCommand>()
+	val dataFolder = File(Loritta.FOLDER, "plugins/$name")
+
+	open fun onEnable() {
+
+	}
+
+	open fun onDisable() {
+
+	}
+
+	fun registerCommand(command: LorittaCommand) {
+		loritta.lorittaCommandManager.registerCommand(command)
+		commands.add(command)
+	}
+}
