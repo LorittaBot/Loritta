394539f2502e55014eb16d2f40bfeb8cbdddd447
MrPowerGamerBR <leonardomalaman@gmail.com>
2018-12-26 16:41:25 -0200

    Adicionar Platform Features

---
+++ src/main/java/net/perfectdreams/loritta/api/platform/PlatformType.kt
@@ -0,0 +1,35 @@
+package net.perfectdreams.loritta.api.platform
+
+enum class PlatformType(val supportedFeatures: Array<PlatformFeature>) {
+	DISCORD(
+			PlatformFeature.values()
+	),
+	TWITTER(
+			arrayOf(
+					PlatformFeature.FILE_UPLOAD,
+					PlatformFeature.IMAGE_UPLOAD
+			)
+	),
+	TELEGRAM(
+			arrayOf(
+					PlatformFeature.FILE_UPLOAD,
+					PlatformFeature.IMAGE_UPLOAD,
+					PlatformFeature.INLINE_KEYBOARD,
+					PlatformFeature.TYPING_STATUS
+			)
+	),
+	AMINO(
+			arrayOf(
+					PlatformFeature.FILE_UPLOAD,
+					PlatformFeature.IMAGE_UPLOAD,
+					PlatformFeature.TYPING_STATUS
+			)
+	),
+	UNKNOWN(
+			arrayOf()
+	);
+
+	fun supports(vararg features: PlatformFeature): Boolean {
+		return this.supportedFeatures.toMutableSet().containsAll(features.toMutableSet())
+	}
+}
