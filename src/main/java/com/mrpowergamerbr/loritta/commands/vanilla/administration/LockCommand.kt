c67540e8c8ea8eebf64b8d208c3756db61b28684
MrGaabriel <hello@mrgaabriel.space>
2018-12-27 21:08:13 -0200

    Re-escrever "+lock" e "+unlock"

--- src/main/java/com/mrpowergamerbr/loritta/commands/vanilla/administration/LockCommand.kt
+++ src/main/java/com/mrpowergamerbr/loritta/commands/vanilla/administration/LockCommand.kt
@@ -3,66 +3,86 @@ package com.mrpowergamerbr.loritta.commands.vanilla.administration
 import com.mrpowergamerbr.loritta.commands.AbstractCommand
 import net.perfectdreams.loritta.api.commands.CommandCategory
 import com.mrpowergamerbr.loritta.commands.CommandContext
+import com.mrpowergamerbr.loritta.utils.LoriReply
+import com.mrpowergamerbr.loritta.utils.isValidSnowflake
 import com.mrpowergamerbr.loritta.utils.locale.LegacyBaseLocale
+import com.mrpowergamerbr.loritta.utils.remove
 import net.dv8tion.jda.core.Permission
+import net.dv8tion.jda.core.entities.TextChannel
 
-class LockCommand : AbstractCommand("lock", listOf("trancar", "fechar"), CommandCategory.ADMIN){
+class LockCommand : AbstractCommand("lock", listOf("trancar", "fechar"), CommandCategory.ADMIN) {
+	
 	override fun getDescription(locale: LegacyBaseLocale): String {
 		return locale.format { commands.moderation.lock.description }
 	}
+	
 	override fun getDiscordPermissions(): List<Permission> {
 		return listOf(Permission.MANAGE_SERVER)
 	}
+	
 	override fun canUseInPrivateChannel(): Boolean {
 		return false
 	}
+	
 	override fun getBotPermissions(): List<Permission> {
 		return listOf(Permission.MANAGE_CHANNEL, Permission.MANAGE_PERMISSIONS)
 	}
+	
 	override suspend fun run(context: CommandContext, locale: LegacyBaseLocale) {
-		/* if (context.args.isNotEmpty()) {
-			var args = context.rawArgs
-			val channelId = context.rawArgs[0]
-
-			// Pegando canal de texto, via menções, ID ou nada
-			val textChannel = if (args.size >= 2) {
-				if (channelId.startsWith("<#") && channelId.endsWith(">")) {
-					try {
-						val ch = context.guild.getTextChannelById(channelId.substring(2, channelId.length - 1))
-						args = args.remove(0)
-						ch
-					} catch (e: Exception) {
-						null
-					}
-				} else {
-					try {
-						val ch = context.guild.getTextChannelById(channelId)
-						args = args.remove(0)
-						ch
-					} catch (e: Exception) {
-						null
-					}
-				}
-			} else { null } ?: context.event.textChannel!! // Text Channel sempre será not null
-
-			val everyoneRole = context.guild.publicRole
-			val permissionOverride = textChannel.getPermissionOverride(everyoneRole)
-
-			if (permissionOverride == null) {
-			} else {
-				    	textChannel.createPermissionOverride(everyoneRole)
+		if (context.args.isEmpty()) {
+			context.explain()
+			return
+		}
+		
+		val channel = getTextChannel(context, context.args[0]) ?: context.event.textChannel!! // Já que o comando não será executado via DM, podemos assumir que textChannel nunca será nulo
+		
+		val publicRole = context.guild.publicRole
+		val override = channel.getPermissionOverride(publicRole)
+		
+		if (override != null) {
+			if (Permission.MESSAGE_WRITE !in override.denied) {
+				channel.createPermissionOverride(publicRole)
 						.setDeny(Permission.MESSAGE_WRITE)
 						.queue()
-
-					context.reply(
-						LoriReply(
-								locale.format { commands.moderation.lock.denied }
-						)
-					)
-				}
 			}
 		} else {
-			context.explain()
-		} */
+			channel.createPermissionOverride(publicRole)
+					.setDeny(Permission.MESSAGE_WRITE)
+					.queue()
+		}
+		
+		context.reply(
+				LoriReply(
+						locale.format { commands.moderation.lock.denied },
+						"\uD83C\uDF89"
+				)
+		)
+	}
+	
+	fun getTextChannel(context: CommandContext, input: String?): TextChannel? {
+		if (input == null)
+			return null
+		
+		val guild = context.guild
+		
+		val channels = guild.getTextChannelsByName(input, false)
+		if (channels.isNotEmpty()) {
+			return channels[0]
+		}
+		
+		val id = input
+				.replace("<", "")
+				.replace("#", "")
+				.replace(">", "")
+		
+		if (!id.isValidSnowflake())
+			return null
+		
+		val channel = guild.getTextChannelById(id)
+		if (channel != null) {
+			return channel
+		}
+		
+		return null
 	}
 }
