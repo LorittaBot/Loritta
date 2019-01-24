e0dc97f18d3d4be241ab137df8081b0fd787c129
MrGaabriel <hello@mrgaabriel.space>
2019-01-23 22:07:06 -0200

    Verificar se há um convite no "+congaparrot"

--- src/main/java/com/mrpowergamerbr/loritta/commands/vanilla/fun/CongaParrotCommand.kt
+++ src/main/java/com/mrpowergamerbr/loritta/commands/vanilla/fun/CongaParrotCommand.kt
@@ -3,8 +3,11 @@ package com.mrpowergamerbr.loritta.commands.vanilla.`fun`
 import com.mrpowergamerbr.loritta.commands.AbstractCommand
 import net.perfectdreams.loritta.api.commands.CommandCategory
 import com.mrpowergamerbr.loritta.commands.CommandContext
+import com.mrpowergamerbr.loritta.modules.InviteLinkModule
 import com.mrpowergamerbr.loritta.utils.Constants
 import com.mrpowergamerbr.loritta.utils.LoriReply
+import com.mrpowergamerbr.loritta.utils.LorittaPermission
+import com.mrpowergamerbr.loritta.utils.MiscUtils
 import com.mrpowergamerbr.loritta.utils.locale.LegacyBaseLocale
 
 class CongaParrotCommand : AbstractCommand("congaparrot", category = CommandCategory.FUN) {
@@ -22,6 +25,22 @@ class CongaParrotCommand : AbstractCommand("congaparrot", category = CommandCate
 
 	override suspend fun run(context: CommandContext,locale: LegacyBaseLocale) {
 		var arg0 = context.args.getOrNull(0)
+
+		val inviteBlockerConfig = context.config.inviteBlockerConfig
+		val checkInviteLinks = inviteBlockerConfig.isEnabled && !inviteBlockerConfig.whitelistedChannels.contains(context.event.channel.id) && !context.lorittaUser.hasPermission(LorittaPermission.ALLOW_INVITES)
+
+		if (checkInviteLinks) {
+			val whitelisted = mutableListOf<String>()
+			whitelisted.addAll(context.config.inviteBlockerConfig.whitelistedIds)
+
+			InviteLinkModule.cachedInviteLinks[context.guild.id]?.forEach {
+				whitelisted.add(it)
+			}
+
+			if (MiscUtils.hasInvite(arg0, whitelisted)) {
+				return
+			}
+		}
 
 		if (arg0 == null) {
 			context.explain()
