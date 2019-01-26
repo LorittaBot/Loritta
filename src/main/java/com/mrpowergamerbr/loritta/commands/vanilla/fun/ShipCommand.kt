5d1931fa7c1709a08a28855917b4c35361bc27f2
MrPowerGamerBR <leonardomalaman@gmail.com>
2019-01-25 22:58:28 -0200

    Corrigir mensagem

--- src/main/java/com/mrpowergamerbr/loritta/commands/vanilla/fun/ShipCommand.kt
+++ src/main/java/com/mrpowergamerbr/loritta/commands/vanilla/fun/ShipCommand.kt
@@ -119,10 +119,10 @@ class ShipCommand : AbstractCommand("ship", listOf("shippar"), CommandCategory.F
 				}
 			}
 
-			if (Loritta.RANDOM.nextInt(0, 10) == 9 && context.lorittaUser.profile.money >= 3000) {
+			if (Loritta.RANDOM.nextInt(0, 50) == 9 && context.lorittaUser.profile.money >= 3000) {
 				context.reply(
 						LoriReply(
-								context.locale["commands.fun.ship.bribeLove", "${Loritta.config}user/@me/dashboard/ship-effects"]
+								context.locale["commands.fun.ship.bribeLove", "${Loritta.config.websiteUrl}user/@me/dashboard/ship-effects"]
 						)
 				)
 			}
