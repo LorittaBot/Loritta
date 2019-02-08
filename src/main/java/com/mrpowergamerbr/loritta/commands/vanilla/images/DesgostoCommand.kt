51293f47814b3f60a76a8e7c7d1996a882b46dae
BlueSlimee <33792776+BlueSlimee@users.noreply.github.com>
2019-02-08 13:46:23 -0400

    Arrumando erro aleatório e gerar overlay

--- src/main/java/com/mrpowergamerbr/loritta/commands/vanilla/images/DesgostoCommand.kt
+++ src/main/java/com/mrpowergamerbr/loritta/commands/vanilla/images/DesgostoCommand.kt
@@ -12,11 +12,11 @@ import java.awt.image.BufferedImage
 import java.io.File
 import java.util.*
 import javax.imageio.ImageIO
+import java.awt.Color
 
 class DesgostoCommand : AbstractCommand("desgosto", category = CommandCategory.IMAGES) {
 	companion object {
 		val TEMPLATE by lazy { ImageIO.read(File(Loritta.ASSETS + "desgosto/desgosto.png")) }
-		val OVERLAY by lazy { ImageIO.read(File(Loritta.ASSETS + "degosto/overlay.png")) }
 	}
 
 	override fun getDescription(locale: LegacyBaseLocale): String {
@@ -38,16 +38,23 @@ class DesgostoCommand : AbstractCommand("desgosto", category = CommandCategory.I
 		return true
 	}
 
-	override suspend fun run(context: CommandContext,locale: LegacyBaseLocale) {
-		val base = BufferedImage(412, 528, BufferedImage.TYPE_INT_ARGB)
-
+	override suspend fun run (context: CommandContext, locale: LegacyBaseLocale) {
 		val contextImage = context.getImageAt(0) ?: run { Constants.INVALID_IMAGE_REPLY.invoke(context); return; }
-		val scaled = contextImage.getScaledInstance(412, 371, BufferedImage.SCALE_SMOOTH).toBufferedImage()
+		val scaled = contextImage.getScaledInstance(412, 371, BufferedImage.SCALE_SMOOTH)
+
+		val base = BufferedImage(412, 528, BufferedImage.TYPE_INT_ARGB)
+		val tint = BufferedImage(412, 371, BufferedImage.TYPE_INT_ARGB)
+
+		val color = Color(255, 0, 0, 60)
+		tint.graphics.color = color
+		tint.graphics.fillRect(412, 371, tint.width, tint.height)
 
 		base.graphics.drawImage(TEMPLATE, 0, 0, null)
 		base.graphics.drawImage(scaled, 0, 0, null)
-		base.graphics.drawImage(OVERLAY, 0, 0, null)
+		base.graphics.drawImage(tint, 0, 0, null)
 
 		context.sendFile(base, "desgosto.png", context.getAsMention(true))
 	}
 }
+
+
