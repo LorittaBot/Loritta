0a81c397a26a97466c125a37cb3aac2ca263304e
MrPowerGamerBR <leonardomalaman@gmail.com>
2019-03-10 08:32:45 -0300

    Permitir usar "+undetalebox" via mensagem direta, permitir usar IDs de usuários no comando

--- src/main/java/com/mrpowergamerbr/loritta/commands/vanilla/undertale/UndertaleBoxCommand.kt
+++ src/main/java/com/mrpowergamerbr/loritta/commands/vanilla/undertale/UndertaleBoxCommand.kt
@@ -2,12 +2,12 @@ package com.mrpowergamerbr.loritta.commands.vanilla.undertale
 
 import com.mrpowergamerbr.loritta.Loritta
 import com.mrpowergamerbr.loritta.commands.AbstractCommand
-import net.perfectdreams.loritta.api.commands.CommandCategory
 import com.mrpowergamerbr.loritta.commands.CommandContext
 import com.mrpowergamerbr.loritta.utils.Constants
 import com.mrpowergamerbr.loritta.utils.ImageUtils
 import com.mrpowergamerbr.loritta.utils.LorittaUtils
 import com.mrpowergamerbr.loritta.utils.locale.LegacyBaseLocale
+import net.perfectdreams.loritta.api.commands.CommandCategory
 import java.awt.Color
 import java.awt.Font
 import java.awt.Image
@@ -36,15 +36,18 @@ class UndertaleBoxCommand : AbstractCommand("utbox", listOf("undertalebox"), Com
 	override suspend fun run(context: CommandContext,locale: LegacyBaseLocale) {
 		try {
 			if (context.args.size >= 1) {
-				var member = context.handle
-				if (context.message.mentionedUsers.size == 1) {
-					member = context.guild.getMember(context.message.mentionedUsers[0])
-				}
+				var user = context.userHandle
+				val mentionedUser = context.getUserAt(0)
 				var str = context.args.joinToString(" ") // Primeiro nós juntamos tudo
+
+				if (mentionedUser != null) {
+					user = mentionedUser
+					str = str.drop(1) // Remover a menção detectada
+				}
+
 				// Mas ok, ainda tem uma coisa chamada "nome do usuário mencionado"
 				// Sabe o que a gente faz com ele? Gambiarra!
 				// TODO: Menos gambiarra
-				str = str.replace("@" + member.effectiveName + " ", "")
 				val bi = ImageIO.read(File(Loritta.ASSETS + "undertale_dialogbox.png"))
 				val graph = bi.graphics
 
@@ -55,7 +58,7 @@ class UndertaleBoxCommand : AbstractCommand("utbox", listOf("undertalebox"), Com
 				// graph.getFontMetrics(determinationMono) tem problemas, a width do char é sempre 1 (bug?)
 				ImageUtils.drawTextWrap(str, 180, 56 + determinationMono.size, 578, 0, graph.fontMetrics, graph)
 
-				val avatarImg = LorittaUtils.downloadImage(member.user.effectiveAvatarUrl)!!.getScaledInstance(128, 128, Image.SCALE_SMOOTH)
+				val avatarImg = LorittaUtils.downloadImage(user.effectiveAvatarUrl)!!.getScaledInstance(128, 128, Image.SCALE_SMOOTH)
 
 				val blackWhite = BufferedImage(avatarImg.getWidth(null), avatarImg.getHeight(null), BufferedImage.TYPE_BYTE_GRAY)
 				val g2d = blackWhite.createGraphics()
@@ -69,12 +72,12 @@ class UndertaleBoxCommand : AbstractCommand("utbox", listOf("undertalebox"), Com
 					val x = 0
 					val y = 166
 					graph.color = Color.BLACK
-					graph.drawString(member.user.name + "#" + member.user.discriminator, x + 1, y + 12)
-					graph.drawString(member.user.name + "#" + member.user.discriminator, x + 1, y + 14)
-					graph.drawString(member.user.name + "#" + member.user.discriminator, x, y + 13)
-					graph.drawString(member.user.name + "#" + member.user.discriminator, x + 2, y + 13)
+					graph.drawString(user.name + "#" + user.discriminator, x + 1, y + 12)
+					graph.drawString(user.name + "#" + user.discriminator, x + 1, y + 14)
+					graph.drawString(user.name + "#" + user.discriminator, x, y + 13)
+					graph.drawString(user.name + "#" + user.discriminator, x + 2, y + 13)
 					graph.color = Color.WHITE
-					graph.drawString(member.user.name + "#" + member.user.discriminator, x + 1, y + 13)
+					graph.drawString(user.name + "#" + user.discriminator, x + 1, y + 13)
 				}
 
 				graph.drawImage(blackWhite, 20, 22, null)
