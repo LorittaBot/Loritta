9f23d3b2a7f2486685eb0c6ff57478feab33cb0c
MrPowerGamerBR <leonardomalaman@gmail.com>
2019-04-13 09:54:48 -0300

    Refatorar geração de linhas do bulk message delete

---
+++ src/main/java/net/perfectdreams/loritta/utils/DateUtils.kt
@@ -0,0 +1,7 @@
+package net.perfectdreams.loritta.utils
+
+import java.time.format.DateTimeFormatter
+
+object DateUtils {
+    val PRETTY_DATE_FORMAT = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss")
+}
