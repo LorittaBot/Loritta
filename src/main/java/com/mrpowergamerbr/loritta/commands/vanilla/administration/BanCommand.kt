f2655d67f1655ba0a79a485282dd35b1aa707ca0
BlueSlimee <33792776+BlueSlimee@users.noreply.github.com>
2019-05-29 22:35:20 -0400

    owo pls work

--- src/main/java/com/mrpowergamerbr/loritta/commands/vanilla/administration/BanCommand.kt
+++ src/main/java/com/mrpowergamerbr/loritta/commands/vanilla/administration/BanCommand.kt
@@ -90,9 +90,10 @@ class BanCommand : AbstractCommand("ban", listOf("banir", "hackban", "forceban")
 			}
 
 			val (reason, skipConfirmation, silent, delDays) = AdminUtils.getOptions(context) ?: return
+                        val fancyReason = reason + message.attachments.get(0)?.url ?: ""
 
 			val banCallback: suspend (Message?, Boolean) -> (Unit) = { message, isSilent ->
-				ban(context.config, context.guild, context.userHandle, locale, user, reason, isSilent, delDays)
+				ban(context.config, context.guild, context.userHandle, locale, user, fancyReason, isSilent, delDays)
 
 				message?.delete()?.queue()
 
