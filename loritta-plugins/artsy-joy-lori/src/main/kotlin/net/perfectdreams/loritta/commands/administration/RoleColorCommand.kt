73285b72550cb45d40fdddd6f69d09bf0dee180b
Nightdavisao <nightdavisao27@protonmail.com>
2019-06-15 18:40:04 -0300

    done

--- loritta-plugins/artsy-joy-lori/src/main/kotlin/net/perfectdreams/loritta/commands/administration/RoleColorCommand.kt
+++ loritta-plugins/artsy-joy-lori/src/main/kotlin/net/perfectdreams/loritta/commands/administration/RoleColorCommand.kt
@@ -22,9 +22,7 @@ class RoleColorCommand : LorittaDiscordCommand(arrayOf("rolecolor", "colorrole",
         }
     }
 
-    override fun getExamples(locale: BaseLocale): List<String> {
-        return listOf("351473717194522647 #000FFF", "cargolegal #000000")
-    }
+    override fun getExamples(locale: BaseLocale): List<String> = locale.getWithType("commands.moderation.rolecolor.examples")
 
     override val botPermissions: List<Permission> = listOf(Permission.MANAGE_ROLES)
     override val discordPermissions: List<Permission> = listOf(Permission.MANAGE_ROLES)
@@ -32,26 +30,27 @@ class RoleColorCommand : LorittaDiscordCommand(arrayOf("rolecolor", "colorrole",
 
     @Subcommand
     suspend fun root(context: DiscordCommandContext, locale: BaseLocale) {
-        if (2 > context.args.size) {
+        if (2 >= context.args.size) {
             context.explain()
             return
         }
-        val hexStr = context.args.drop(1).joinToString(" ")
-        val hexColor = ColorUtils.getColorFromString(hexStr)
+        val colorStr = context.args.drop(1).joinToString(" ")
+        val color = ColorUtils.getColorFromString(colorStr)
         val role = context.getRoleAt(0)
 
         if (role == null) {
-            context.reply(locale["commands.moderation.rolecolor.rolenotfound"])
+            context.reply(locale["commands.roleNotFound"])
             return
         }
-        if (hexColor == null) {
-            context.reply(locale["commands.moderation.rolecolor."])
+        if (color == null) {
+            context.reply(locale["commands.moderation.rolecolor.invalidColor"])
             return
         }
         // E finalmente vamos mudar a cor do cargo
         try {
             val manager = role.manager
-            manager.setColor(hexColor).queue()
+            manager.setColor(color).queue()
+            context.reply(locale["commands.moderation.rolecolor.success"], "\uD83C\uDFA8")
         } catch(e: Exception) {
             context.reply(locale["commands.moderation.rolecolor.cantChange"], Constants.ERROR)
         }
