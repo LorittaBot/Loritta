29ff0153a4119ba60ca8640bb0856410fe6c78da
MrPowerGamerBR <leonardomalaman@gmail.com>
2019-06-29 08:57:10 -0300

    Atualizar JDA
    Fixes #1284

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 val loriVersion   = "2019.07.05-SNAPSHOT"
 val kotlinVersion = "1.3.31"
 val ktorVersion   = "1.2.1"
-val jdaVersion    = "4.BETA.0_7"
+val jdaVersion    = "4.BETA.0_14"
 
 println("Compiling Loritta $loriVersion")
 println("Kotlin Version: $kotlinVersion")
