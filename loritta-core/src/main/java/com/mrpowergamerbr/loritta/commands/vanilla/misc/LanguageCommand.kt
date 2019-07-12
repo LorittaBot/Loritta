3839754a2d6bef0fad0a40e9c7a0ffcee07a3153
MrPowerGamerBR <leonardomalaman@gmail.com>
2019-07-12 16:36:28 -0300

    Adicionar "pt-furry", refatorar locales no website

--- loritta-core/src/main/java/com/mrpowergamerbr/loritta/commands/vanilla/misc/LanguageCommand.kt
+++ loritta-core/src/main/java/com/mrpowergamerbr/loritta/commands/vanilla/misc/LanguageCommand.kt
@@ -2,6 +2,7 @@ package com.mrpowergamerbr.loritta.commands.vanilla.misc
 
 import com.mrpowergamerbr.loritta.commands.AbstractCommand
 import com.mrpowergamerbr.loritta.commands.CommandContext
+import com.mrpowergamerbr.loritta.utils.extensions.edit
 import com.mrpowergamerbr.loritta.utils.extensions.isEmote
 import com.mrpowergamerbr.loritta.utils.locale.BaseLocale
 import com.mrpowergamerbr.loritta.utils.locale.LegacyBaseLocale
@@ -11,6 +12,7 @@ import com.mrpowergamerbr.loritta.utils.onReactionAddByAuthor
 import com.mrpowergamerbr.loritta.utils.save
 import net.dv8tion.jda.api.EmbedBuilder
 import net.dv8tion.jda.api.Permission
+import net.dv8tion.jda.api.entities.MessageEmbed
 import net.perfectdreams.loritta.api.commands.CommandCategory
 import java.awt.Color
 
@@ -35,12 +37,6 @@ class LanguageCommand : AbstractCommand("language", listOf("linguagem", "speak")
 						"\uD83C\uDDE7\uD83C\uDDF7"
 				),
 				LocaleWrapper(
-						"Português-Funk",
-						loritta.getLocaleById("pt-funk"),
-						loritta.getLegacyLocaleById("pt-funk"),
-						"<:loritta_quebrada:338679008210190336>"
-				),
-				LocaleWrapper(
 						"Português-Portugal",
 						loritta.getLocaleById("pt-pt"),
 						loritta.getLegacyLocaleById("pt-pt"),
@@ -57,52 +53,89 @@ class LanguageCommand : AbstractCommand("language", listOf("linguagem", "speak")
 						loritta.getLocaleById("es-es"),
 						loritta.getLegacyLocaleById("es-es"),
 						"\uD83C\uDDEA\uD83C\uDDF8"
+				),
+				LocaleWrapper(
+						"Português-Funk",
+						loritta.getLocaleById("pt-funk"),
+						loritta.getLegacyLocaleById("pt-funk"),
+						"<:loritta_quebrada:338679008210190336>"
+				),
+				LocaleWrapper(
+						"Português-Furry",
+						loritta.getLocaleById("pt-furry"),
+						loritta.getLegacyLocaleById("default"),
+						"\uD83D\uDC3E"
 				)
 		)
 
-		embed.setTitle("\uD83C\uDF0E " + locale.toNewLocale()["commands.miscellaneous.language.pleaseSelectYourLanguage"], "")
+		val message = context.sendMessage(
+				context.getAsMention(true),
+				buildLanguageEmbed(
+						locale.toNewLocale(),
+						validLanguages.subList(0, 4)
+				)
+		)
 
-		for (wrapper in validLanguages) {
+		message.onReactionAddByAuthor(context) {
+			if (it.reactionEmote.isEmote("426183783008698391")) {
+				message.edit(
+						" ",
+						buildLanguageEmbed(
+								locale.toNewLocale(),
+								validLanguages.subList(4, validLanguages.size)
+						),
+						true
+				)
+
+				for (wrapper in validLanguages.subList(4, validLanguages.size)) {
+					// O "replace" é necessário já que a gente usa emojis personalizados para algumas linguagens
+					message.addReaction(wrapper.emoteName.replace("<", "").replace(">", "")).queue()
+				}
+				return@onReactionAddByAuthor
+			}
+
+			var localeId = "default"
+
+			val newLanguage = validLanguages.firstOrNull { language -> it.reactionEmote.isEmote(language.emoteName) }
+			localeId = newLanguage?.locale?.id ?: localeId
+
+			context.config.localeId = localeId
+			loritta save context.config
+
+			val newLocale = loritta.getLocaleById(localeId)
+			if (localeId == "default") {
+				localeId = "pt-br" // Já que nós já salvamos, vamos trocar o localeId para algo mais "decente"
+			}
+
+			context.reply(newLocale["commands.miscellaneous.language.languageChanged", "`$localeId`"], "\uD83C\uDFA4")
+			message.delete().queue()
+		}
+
+		for (wrapper in validLanguages.subList(0, 4)) {
+			// O "replace" é necessário já que a gente usa emojis personalizados para algumas linguagens
+			message.addReaction(wrapper.emoteName.replace("<", "").replace(">", "")).queue()
+		}
+
+		message.addReaction("lori_ok_hand:426183783008698391").queue()
+	}
+
+	private fun buildLanguageEmbed(locale: BaseLocale, languages: List<LocaleWrapper>): MessageEmbed {
+		val embed = EmbedBuilder()
+		embed.setColor(Color(0, 193, 223))
+
+		embed.setTitle("\uD83C\uDF0E " + locale["commands.miscellaneous.language.pleaseSelectYourLanguage"])
+
+		for (wrapper in languages) {
 			val translators = wrapper.locale.getWithType<List<String>>("loritta.translationAuthors").mapNotNull { lorittaShards.getUserById(it) }
 
 			embed.addField(
 					wrapper.emoteName + " " + wrapper.name,
-					"**${locale.toNewLocale()["commands.miscellaneous.language.translatedBy"]}:** ${translators.joinToString(transform = { "`${it.name}`" })}",
+					"**${locale["commands.miscellaneous.language.translatedBy"]}:** ${translators.joinToString(transform = { "`${it.name}`" })}",
 					true
 			)
 		}
 
-		val message = context.sendMessage(context.getAsMention(true), embed.build())
-
-		message.onReactionAddByAuthor(context) {
-			var localeId = "default"
-			if (it.reactionEmote.isEmote("loritta_quebrada")) {
-				localeId = "pt-funk"
-			}
-			if (it.reactionEmote.isEmote("\uD83C\uDDFA\uD83C\uDDF8")) {
-				localeId = "en-us"
-			}
-			if (it.reactionEmote.isEmote("\uD83C\uDDF5\uD83C\uDDF9")) {
-				localeId = "pt-pt"
-			}
-			if (it.reactionEmote.isEmote("\uD83C\uDDEA\uD83C\uDDF8")) {
-				localeId = "es-es"
-			}
-
-			context.config.localeId = localeId
-			loritta save context.config
-			val newLocale = loritta.getLegacyLocaleById(localeId)
-			if (localeId == "default") {
-				localeId = "pt-br" // Já que nós já salvamos, vamos trocar o localeId para algo mais "decente"
-			}
-			context.reply(newLocale.toNewLocale()["commands.miscellaneous.language.languageChanged", "`$localeId`"], "\uD83C\uDFA4")
-			message.delete().queue()
-		}
-
-		for (wrapper in validLanguages) {
-			// O "replace" é necessário já que a gente usa emojis personalizados para algumas linguagens
-			message.addReaction(wrapper.emoteName.replace("<", "").replace(">", "")).queue()
-		}
+		return embed.build()
 	}
 
 	private class LocaleWrapper(
