66148047bbecacc8364f5fe85fd3c08776058b9f
kaikecarlos <caheasta@gmail.com>
2019-07-21 19:42:46 -0300

    Update GuildBannerCommand.kt

--- loritta-plugins/artsy-joy-lori/src/main/kotlin/net/perfectdreams/loritta/commands/discord/GuildBannerCommand.kt
+++ loritta-plugins/artsy-joy-lori/src/main/kotlin/net/perfectdreams/loritta/commands/discord/GuildBannerCommand.kt
@@ -8,6 +8,7 @@ import net.perfectdreams.loritta.api.commands.*
 import net.perfectdreams.loritta.platform.discord.entities.DiscordCommandContext
 
 class GuildBannerCommand : LorittaCommand(arrayOf("guildbanner", "serverbanner"), CommandCategory.DISCORD) {
+
     override fun getDescription(locale: BaseLocale): String? {
         return locale["commands.discord.guildbanner.description"]
     }
@@ -19,19 +20,24 @@ class GuildBannerCommand : LorittaCommand(arrayOf("guildbanner", "serverbanner")
             argument(ArgumentType.TEXT) {}
         }
     }
+
     @Subcommand
     suspend fun root(context: DiscordCommandContext, locale: BaseLocale) {
         val discordGuild = context.discordGuild
         val guildBanner = discordGuild!!.bannerUrl
+
         // Verificar se a guild tem banner
-        if(guildBanner != null) {
+        if(guildBanner == null) {
             context.reply(locale["commands.discord.guildbanner.noBanner"], Constants.ERROR)
+
         } else {
             val embed = EmbedBuilder()
 
             embed.setTitle("🖼 ${discordGuild.name}")
+            embed.setColor(Constants.DISCORD_BLURPLE)
             embed.setDescription(locale["loritta.clickHere", guildBanner])
             embed.setImage(guildBanner)
+
             context.sendMessage(context.getAsMention(true), embed.build())
         }
     }
