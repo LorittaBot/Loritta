441f4049356d35d2b4d52963d14edb68038b120f
MrPowerGamerBR <leonardomalaman@gmail.com>
2019-08-03 20:09:46 -0300

    Arrumar testes

--- loritta-core/src/test/java/net/perfectdreams/loritta/utils/CalendarTest.kt
+++ loritta-core/src/test/java/net/perfectdreams/loritta/utils/CalendarTest.kt
@@ -9,21 +9,23 @@ class CalendarTest {
 	fun `reset calendar to start of the day`() {
 		val calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
 		calendar.set(Calendar.DAY_OF_MONTH, 11)
-		calendar.set(Calendar.MONTH, 10)
+		calendar.set(Calendar.MONTH, 7)
 		calendar.set(Calendar.YEAR, 2019)
 
 		CalendarUtils.resetToBeginningOfTheDay(calendar)
-		assertThat(calendar.toString()).isEqualTo("java.util.GregorianCalendar[time=?,areFieldsSet=false,areAllFieldsSet=true,lenient=true,zone=sun.util.calendar.ZoneInfo[id=\"UTC\",offset=0,dstSavings=0,useDaylight=false,transitions=0,lastRule=null],firstDayOfWeek=1,minimalDaysInFirstWeek=1,ERA=1,YEAR=2019,MONTH=10,WEEK_OF_YEAR=30,WEEK_OF_MONTH=4,DAY_OF_MONTH=11,DAY_OF_YEAR=204,DAY_OF_WEEK=3,DAY_OF_WEEK_IN_MONTH=4,AM_PM=0,HOUR=11,HOUR_OF_DAY=0,MINUTE=0,SECOND=0,MILLISECOND=0,ZONE_OFFSET=0,DST_OFFSET=0]")
+		println(calendar)
+		assertThat(calendar.toString()).isEqualTo("java.util.GregorianCalendar[time=?,areFieldsSet=false,areAllFieldsSet=true,lenient=true,zone=sun.util.calendar.ZoneInfo[id=\"UTC\",offset=0,dstSavings=0,useDaylight=false,transitions=0,lastRule=null],firstDayOfWeek=1,minimalDaysInFirstWeek=1,ERA=1,YEAR=2019,MONTH=7,WEEK_OF_YEAR=31,WEEK_OF_MONTH=1,DAY_OF_MONTH=11,DAY_OF_YEAR=215,DAY_OF_WEEK=7,DAY_OF_WEEK_IN_MONTH=1,AM_PM=1,HOUR=11,HOUR_OF_DAY=0,MINUTE=0,SECOND=0,MILLISECOND=0,ZONE_OFFSET=0,DST_OFFSET=0]")
 	}
 
 	@Test
 	fun `reset calendar to end of the day`() {
 		val calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
 		calendar.set(Calendar.DAY_OF_MONTH, 11)
-		calendar.set(Calendar.MONTH, 10)
+		calendar.set(Calendar.MONTH, 7)
 		calendar.set(Calendar.YEAR, 2019)
 
 		CalendarUtils.resetToEndOfTheDay(calendar)
-		assertThat(calendar.toString()).isEqualTo("java.util.GregorianCalendar[time=?,areFieldsSet=false,areAllFieldsSet=true,lenient=true,zone=sun.util.calendar.ZoneInfo[id=\"Etc/UTC\",offset=0,dstSavings=0,useDaylight=false,transitions=0,lastRule=null],firstDayOfWeek=1,minimalDaysInFirstWeek=1,ERA=1,YEAR=2019,MONTH=10,WEEK_OF_YEAR=30,WEEK_OF_MONTH=4,DAY_OF_MONTH=11,DAY_OF_YEAR=204,DAY_OF_WEEK=3,DAY_OF_WEEK_IN_MONTH=4,AM_PM=0,HOUR=11,HOUR_OF_DAY=23,MINUTE=59,SECOND=59,MILLISECOND=999,ZONE_OFFSET=0,DST_OFFSET=0]")
+		println(calendar)
+		assertThat(calendar.toString()).isEqualTo("java.util.GregorianCalendar[time=?,areFieldsSet=false,areAllFieldsSet=true,lenient=true,zone=sun.util.calendar.ZoneInfo[id=\"UTC\",offset=0,dstSavings=0,useDaylight=false,transitions=0,lastRule=null],firstDayOfWeek=1,minimalDaysInFirstWeek=1,ERA=1,YEAR=2019,MONTH=7,WEEK_OF_YEAR=31,WEEK_OF_MONTH=1,DAY_OF_MONTH=11,DAY_OF_YEAR=215,DAY_OF_WEEK=7,DAY_OF_WEEK_IN_MONTH=1,AM_PM=1,HOUR=11,HOUR_OF_DAY=23,MINUTE=59,SECOND=59,MILLISECOND=999,ZONE_OFFSET=0,DST_OFFSET=0]")
 	}
 }
