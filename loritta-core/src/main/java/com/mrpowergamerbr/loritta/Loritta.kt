662c7930f310f52be340800ffac47671d7866462
MrPowerGamerBR <leonardomalaman@gmail.com>
2019-08-21 22:08:09 -0300

    Corrigir verificação da confiabilidade de domínios implementando sistema de SLDs

--- loritta-core/src/main/java/com/mrpowergamerbr/loritta/Loritta.kt
+++ loritta-core/src/main/java/com/mrpowergamerbr/loritta/Loritta.kt
@@ -75,7 +75,9 @@ import org.slf4j.LoggerFactory
 import java.io.File
 import java.io.FileNotFoundException
 import java.util.*
-import java.util.concurrent.*
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 import kotlin.concurrent.thread
 
 /**
