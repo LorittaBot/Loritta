ff001db28e3ccbb88bfb9fca957d54b8da5fedbd
MrPowerGamerBR <leonardomalaman@gmail.com>
2019-08-24 22:12:09 -0300

    Externalizar perfis da Lori no plugin "profile-designs"
    Assim é possível colocar novos designs sem precisar reiniciar a Lori

---
+++ loritta-plugins/profile-designs/src/main/kotlin/net/perfectdreams/loritta/plugin/profiles/ProfileDesigns.kt
@@ -0,0 +1,43 @@
+package net.perfectdreams.loritta.plugin.profiles
+
+import com.mrpowergamerbr.loritta.plugin.LorittaPlugin
+import com.mrpowergamerbr.loritta.profile.ProfileDesign
+import net.perfectdreams.loritta.plugin.profiles.designs.DebugProfileCreator
+import net.perfectdreams.loritta.plugin.profiles.designs.MonicaAtaProfileCreator
+
+class ProfileDesigns : LorittaPlugin() {
+    val registeredProfiles = mutableListOf<ProfileDesign>()
+
+    override fun onEnable() {
+        registeredProfiles.add(
+                ProfileDesign(
+                        false,
+                        DebugProfileCreator::class.java,
+                        "debug",
+                        0.0,
+                        listOf()
+                )
+        )
+        registeredProfiles.add(
+                ProfileDesign(
+                        true,
+                        MonicaAtaProfileCreator::class.java,
+                        "monica_ata",
+                        10000.0,
+                        listOf(321821618177310721L)
+                )
+        )
+
+        registeredProfiles.forEach {
+            loritta.profileDesignManager.registerDesign(it)
+        }
+    }
+
+    override fun onDisable() {
+        super.onDisable()
+
+        registeredProfiles.forEach {
+            loritta.profileDesignManager.unregisterDesign(it)
+        }
+    }
+}
