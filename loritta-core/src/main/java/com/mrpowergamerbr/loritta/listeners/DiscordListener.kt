6ca6da0dfdf7b05bd5536eaf306d4ad11a652d97
MrPowerGamerBR <leonardomalaman@gmail.com>
2019-10-18 18:12:03 -0300

    Esperar o cargo ser dado antes de criar o role removal

--- loritta-core/src/main/java/com/mrpowergamerbr/loritta/listeners/DiscordListener.kt
+++ loritta-core/src/main/java/com/mrpowergamerbr/loritta/listeners/DiscordListener.kt
@@ -425,7 +425,7 @@ class DiscordListener(internal val loritta: Loritta) : ListenerAdapter() {
 					val locale = loritta.getLegacyLocaleById(conf.localeId)
 					val muteRole = MuteCommand.getMutedRole(event.guild, loritta.getLegacyLocaleById(conf.localeId)) ?: return@launch
 
-					event.guild.addRoleToMember(event.member, muteRole).queue()
+					event.guild.addRoleToMember(event.member, muteRole).await()
 
 					if (mute.isTemporary)
 						MuteCommand.spawnRoleRemovalThread(event.guild, locale, event.user, mute.expiresAt!!)
