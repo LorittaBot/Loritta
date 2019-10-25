6e0d29d400d8ef4acda4d8b08f81505e0a3afa44
MrPowerGamerBR <leonardomalaman@gmail.com>
2019-10-25 13:27:28 -0300

    Registrar "+randomtiktok" apenas na Canary
    Os TikToks que aparecem nem tem tanta graça assim, então vou deixar apenas na Canary atualmente

--- loritta-plugins/funny-lori/src/main/kotlin/net/perfectdreams/loritta/FunnyLori.kt
+++ loritta-plugins/funny-lori/src/main/kotlin/net/perfectdreams/loritta/FunnyLori.kt
@@ -1,12 +1,14 @@
 package net.perfectdreams.loritta
 
 import com.mrpowergamerbr.loritta.plugin.LorittaPlugin
+import com.mrpowergamerbr.loritta.utils.config.EnvironmentType
 import net.perfectdreams.loritta.commands.`fun`.RandomSAMCommand
 import net.perfectdreams.loritta.commands.`fun`.RandomTikTokCommand
 
 class FunnyLori : LorittaPlugin() {
     override fun onEnable() {
         registerCommand(RandomSAMCommand())
-        registerCommand(RandomTikTokCommand())
+        if (loritta.config.loritta.environment == EnvironmentType.CANARY)
+            registerCommand(RandomTikTokCommand())
     }
 }
