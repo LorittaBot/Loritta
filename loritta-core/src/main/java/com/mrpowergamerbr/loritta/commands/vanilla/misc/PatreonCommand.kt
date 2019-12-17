7ae981b9f251ac39d9a66716daef89938744cb97
MrPowerGamerBR <leonardomalaman@gmail.com>
2019-12-17 12:10:21 -0300

    A correção bem tosca do "+patreon"

--- loritta-core/src/main/java/com/mrpowergamerbr/loritta/commands/vanilla/misc/PatreonCommand.kt
+++ loritta-core/src/main/java/com/mrpowergamerbr/loritta/commands/vanilla/misc/PatreonCommand.kt
@@ -2,17 +2,11 @@ package com.mrpowergamerbr.loritta.commands.vanilla.misc
 
 import com.mrpowergamerbr.loritta.commands.AbstractCommand
 import com.mrpowergamerbr.loritta.commands.CommandContext
-import com.mrpowergamerbr.loritta.network.Databases
 import com.mrpowergamerbr.loritta.utils.locale.LegacyBaseLocale
 import com.mrpowergamerbr.loritta.utils.loritta
-import com.mrpowergamerbr.loritta.utils.lorittaShards
 import net.dv8tion.jda.api.EmbedBuilder
 import net.perfectdreams.loritta.api.commands.CommandCategory
-import net.perfectdreams.loritta.tables.Payments
 import net.perfectdreams.loritta.utils.Emotes
-import net.perfectdreams.loritta.utils.payments.PaymentReason
-import org.jetbrains.exposed.sql.*
-import org.jetbrains.exposed.sql.transactions.transaction
 import java.awt.Color
 
 class PatreonCommand : AbstractCommand("donator", listOf("donators", "patreons", "patreon", "doadores", "doador", "apoiador", "apoiadores", "contribuidores", "contribuidor", "doar", "donate"), category = CommandCategory.MISC) {
@@ -21,37 +15,7 @@ class PatreonCommand : AbstractCommand("donator", listOf("donators", "patreons",
 	}
 
 	override suspend fun run(context: CommandContext,locale: LegacyBaseLocale) {
-		var patrons = ""
-
-		val moneySumId = Payments.money.sum()
-		val mostPayingUsers = transaction(Databases.loritta) {
-			Payments.slice(Payments.userId, moneySumId)
-					.select {
-						Payments.paidAt.isNotNull() and
-								(Payments.reason eq PaymentReason.DONATION) or (Payments.reason eq PaymentReason.SPONSORED) and
-								(Payments.expiresAt greaterEq System.currentTimeMillis())
-					}
-					.groupBy(Payments.userId)
-					.orderBy(moneySumId, SortOrder.DESC)
-					.toMutableList()
-		}
-
-		patrons = mostPayingUsers.map {
-			val money = it[moneySumId]?.toDouble() ?: 0.0
-			val isBold = money >= 59.99
-
-			logger.info { "ID is ${it[Payments.userId]}" }
-
-			val user = lorittaShards.retrieveUserById(it[Payments.userId])
-
-			if (user != null) {
-				var name = "`${user.name}#${user.discriminator}`"
-				if (isBold) {
-					name = "**$name**"
-				}
-				name
-			} else { "???" }
-		}.joinToString(", ")
+		val patrons = "Veja todos os doadores em https://loritta.website/donate (tem tantos doadores que não cabe nesta mensagem! ${Emotes.LORI_CRYING})"
 
 		val embed = EmbedBuilder().apply {
 			setThumbnail("https://loritta.website/assets/img/fanarts/Loritta_-_Heathecliff.png")
