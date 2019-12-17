85c91f4763a0625ae53f642278c9baa1e65c2e8b
MrPowerGamerBR <leonardomalaman@gmail.com>
2019-12-17 12:01:46 -0300

    Corrigir lista de doadores ativos no website

--- loritta-core/src/main/java/com/mrpowergamerbr/loritta/website/requests/routes/page/DonateController.kt
+++ loritta-core/src/main/java/com/mrpowergamerbr/loritta/website/requests/routes/page/DonateController.kt
@@ -1,16 +1,20 @@
 package com.mrpowergamerbr.loritta.website.requests.routes.page
 
+import com.mrpowergamerbr.loritta.network.Databases
 import com.mrpowergamerbr.loritta.oauth2.SimpleUserIdentification
-import com.mrpowergamerbr.loritta.utils.Constants
 import com.mrpowergamerbr.loritta.utils.extensions.getOrNull
 import com.mrpowergamerbr.loritta.utils.locale.BaseLocale
 import com.mrpowergamerbr.loritta.utils.lorittaShards
 import com.mrpowergamerbr.loritta.website.LoriRequiresVariables
 import com.mrpowergamerbr.loritta.website.evaluate
 import com.mrpowergamerbr.loritta.website.evaluateKotlin
+import kotlinx.coroutines.runBlocking
 import kotlinx.html.div
 import kotlinx.html.stream.appendHTML
-import net.dv8tion.jda.api.entities.Member
+import net.perfectdreams.loritta.tables.Payments
+import net.perfectdreams.loritta.utils.payments.PaymentReason
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.transaction
 import org.jooby.Request
 import org.jooby.Response
 import org.jooby.mvc.GET
@@ -22,21 +26,24 @@ class DonateController {
 	@GET
 	@LoriRequiresVariables(true)
 	fun handle(req: Request, res: Response, @Local locale: BaseLocale, @Local variables: MutableMap<String, Any?>) {
-		val lorittaGuild = lorittaShards.getGuildById(Constants.PORTUGUESE_SUPPORT_GUILD_ID)
-
-		val activeDonators = mutableListOf<Member>()
-		val inactiveDonators = mutableListOf<Member>()
-
-		if (lorittaGuild != null) {
-			val rolePatreons = lorittaGuild.getRoleById("364201981016801281") // Pagadores de Aluguel
-			val roleDonators = lorittaGuild.getRoleById("435856512787677214") // Doadores
-			activeDonators.addAll(lorittaGuild.getMembersWithRoles(rolePatreons))
-			inactiveDonators.addAll(lorittaGuild.getMembersWithRoles(roleDonators))
+		val moneySumId = Payments.money.sum()
+		val mostPayingUsers = transaction(Databases.loritta) {
+			Payments.slice(Payments.userId, moneySumId)
+					.select {
+						Payments.paidAt.isNotNull() and
+								(Payments.reason eq PaymentReason.DONATION) or (Payments.reason eq PaymentReason.SPONSORED) and
+								(Payments.expiresAt greaterEq System.currentTimeMillis())
+					}
+					.groupBy(Payments.userId)
+					.orderBy(moneySumId, SortOrder.DESC)
+					.toMutableList()
 		}
+
+		val activeDonators = mostPayingUsers.mapNotNull { runBlocking { lorittaShards.retrieveUserById(it[Payments.userId]) } }
 
 		val userIdentification = req.ifGet<SimpleUserIdentification>("userIdentification").getOrNull()
 
-		val result = evaluateKotlin("donate.kts", "onLoad", locale, userIdentification, activeDonators, inactiveDonators)
+		val result = evaluateKotlin("donate.kts", "onLoad", locale, userIdentification, activeDonators)
 		val builder = StringBuilder()
 		builder.appendHTML().div { result.invoke(this) }
 
