5d628af89981bb471c6ed96bdd4185f84418b20e
MrPowerGamerBR <leonardomalaman@gmail.com>
2020-01-14 10:54:17 -0300

    Corrigir locale keys do "+unwarn"

--- loritta-core/src/main/java/com/mrpowergamerbr/loritta/commands/vanilla/administration/UnwarnCommand.kt
+++ loritta-core/src/main/java/com/mrpowergamerbr/loritta/commands/vanilla/administration/UnwarnCommand.kt
@@ -70,7 +70,7 @@ class UnwarnCommand : AbstractCommand("unwarn", listOf("desavisar"), CommandCate
 			if (warns.isEmpty()) {
 				context.reply(
 						LoriReply(
-								context.locale["$LOCALE_PREFIX.warn.noWarnsFound"],
+								context.locale["$LOCALE_PREFIX.unwarn.noWarnsFound"],
 								Constants.ERROR
 						)
 				)
@@ -86,7 +86,7 @@ class UnwarnCommand : AbstractCommand("unwarn", listOf("desavisar"), CommandCate
 
 			context.reply(
 					LoriReply(
-							context.locale["$LOCALE_PREFIX.warn.warnRemoved"] + " ${Emotes.LORI_HMPF}",
+							context.locale["$LOCALE_PREFIX.unwarn.warnRemoved"] + " ${Emotes.LORI_HMPF}",
 							"\uD83C\uDF89"
 					)
 			)
