60e3559338b611ad4d39200a264a5cc084bec137
MrPowerGamerBR <git@mrpowergamerbr.com>
2020-02-07 11:03:33 -0300

    Remover "+discriminator"

--- loritta-core/src/main/java/com/mrpowergamerbr/loritta/commands/CommandManager.kt
+++ loritta-core/src/main/java/com/mrpowergamerbr/loritta/commands/CommandManager.kt
@@ -124,7 +124,6 @@ class CommandManager {
 		commandMap.add(PerfilCommand())
 		commandMap.add(BackgroundCommand())
 		commandMap.add(SobreMimCommand())
-		commandMap.add(DiscriminatorCommand())
 		commandMap.add(RepCommand())
 		commandMap.add(RankCommand())
 		commandMap.add(EditarXPCommand())
