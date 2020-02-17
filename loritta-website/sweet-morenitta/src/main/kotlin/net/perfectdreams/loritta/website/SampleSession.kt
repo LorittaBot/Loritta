b2c9c3f1bc51417c2b2cddef75fa975703490430
MrPowerGamerBR <git@mrpowergamerbr.com>
2020-02-17 06:56:39 -0300

    Remover projeto antigo feito para tentar migrar o website para Ktor

--- loritta-website/sweet-morenitta/src/main/kotlin/net/perfectdreams/loritta/website/SampleSession.kt
+++
@@ -1,9 +0,0 @@
-package net.perfectdreams.loritta.website
-
-import java.util.*
-
-data class SampleSession(
-    val uniqueId: UUID,
-    val discordId: String?,
-    val serializedDiscordAuth: String?
-)
--- loritta-website/sweet-morenitta/src/main/kotlin/net/perfectdreams/loritta/website/utils/identification/SimpleUserIdentification.kt
+++
@@ -1,7 +0,0 @@
-package net.perfectdreams.loritta.website.utils.identification
-
-class SimpleUserIdentification(
-    val id: String,
-    val name: String,
-    val avatar: String?
-)
