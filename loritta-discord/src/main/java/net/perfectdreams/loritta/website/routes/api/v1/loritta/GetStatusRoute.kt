87563b8497248f95d80a04dda14dfe48606c5aad
MrPowerGamerBR <git@mrpowergamerbr.com>
2020-02-24 00:14:57 -0300

    Colocar mais informações sobre a Lori na route de status da Loritta

--- loritta-discord/src/main/java/net/perfectdreams/loritta/website/routes/api/v1/loritta/GetStatusRoute.kt
+++ loritta-discord/src/main/java/net/perfectdreams/loritta/website/routes/api/v1/loritta/GetStatusRoute.kt
@@ -10,15 +10,57 @@ import net.perfectdreams.loritta.platform.discord.LorittaDiscord
 import net.perfectdreams.loritta.website.routes.BaseRoute
 import net.perfectdreams.loritta.website.utils.extensions.respondJson
 import java.lang.management.ManagementFactory
+import java.util.jar.Attributes
+import java.util.jar.JarFile
 
 class GetStatusRoute(loritta: LorittaDiscord) : BaseRoute(loritta, "/api/v1/loritta/status") {
 	override suspend fun onRequest(call: ApplicationCall) {
 		loritta as Loritta
 		val currentShard = loritta.lorittaCluster
 
+		val path = this::class.java.protectionDomain.codeSource.location.path
+		val jar = JarFile(path)
+		val mf = jar.manifest
+		val mattr = mf.mainAttributes
+
+		val lorittaVersion = mattr[Attributes.Name("Loritta-Version")] as String
+		val buildNumber = mattr[Attributes.Name("Build-Number")] as String
+		val commitHash = mattr[Attributes.Name("Commit-Hash")] as String
+		val gitBranch = mattr[Attributes.Name("Git-Branch")] as String
+		val compiledAt = mattr[Attributes.Name("Compiled-At")] as String
+		val kotlinVersion = mattr[Attributes.Name("Kotlin-Version")] as String
+		val jdaVersion = mattr[Attributes.Name("JDA-Version")] as String
+
+		val mb = 1024 * 1024
+		val runtime = Runtime.getRuntime()
+		val usedMemory = (runtime.totalMemory() - runtime.freeMemory()) / mb
+		val freeMemory = runtime.freeMemory() / mb
+		val maxMemory = runtime.maxMemory() / mb
+		val totalMemory = runtime.totalMemory() / mb
+
 		val jsonObject = jsonObject(
 				"id" to currentShard.id,
 				"name" to currentShard.name,
+				"versions" to jsonObject(
+						"kotlin" to kotlinVersion,
+						"java" to System.getProperty("java.version"),
+						"jda" to jdaVersion
+				),
+				"build" to jsonObject(
+						"version" to lorittaVersion,
+						"buildNumber" to buildNumber,
+						"commitHash" to commitHash,
+						"gitBranch" to gitBranch,
+						"compiledAt" to compiledAt,
+						"environment" to com.mrpowergamerbr.loritta.utils.loritta.config.loritta.environment.name
+				),
+				"memory" to jsonObject(
+						"used" to usedMemory,
+						"free" to freeMemory,
+						"max" to maxMemory,
+						"total" to totalMemory
+				),
+				"threadCount" to ManagementFactory.getThreadMXBean().threadCount,
 				"globalRateLimitHits" to loritta.bucketedController?.getGlobalRateLimitHitsInTheLastMinute(),
 				"isIgnoringRequests" to loritta.rateLimitChecker.checkIfRequestShouldBeIgnored(),
 				"minShard" to currentShard.minShard,
