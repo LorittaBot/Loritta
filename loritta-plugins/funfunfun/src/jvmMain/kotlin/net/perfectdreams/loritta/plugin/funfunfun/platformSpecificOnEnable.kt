d78d5eab4f0b8a88a4b657cb5af25b6d07352c0d
MrPowerGamerBR <git@mrpowergamerbr.com>
2020-02-26 10:49:44 -0300

    Remover "+randomsam" porque parece que o Instagram bloqueou o IP da Loritta, whoops
    Ao tentar acessar, o "ProfilePage" não existe em algumas máquinas

--- loritta-plugins/funfunfun/src/jvmMain/kotlin/net/perfectdreams/loritta/plugin/funfunfun/platformSpecificOnEnable.kt
+++ loritta-plugins/funfunfun/src/jvmMain/kotlin/net/perfectdreams/loritta/plugin/funfunfun/platformSpecificOnEnable.kt
@@ -1,7 +1,4 @@
 package net.perfectdreams.loritta.plugin.funfunfun
 
-import net.perfectdreams.loritta.plugin.funfunfun.commands.RandomSAMCommand
-
 actual fun FunFunFunPlugin.platformSpecificOnEnable() {
-	registerCommand(RandomSAMCommand.command(this.loritta))
 }
