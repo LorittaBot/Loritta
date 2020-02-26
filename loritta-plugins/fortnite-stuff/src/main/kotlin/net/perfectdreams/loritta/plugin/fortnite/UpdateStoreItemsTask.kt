5573180e26d3d6cac09c8ddbe2495b9b39ae7b73
MrPowerGamerBR <git@mrpowergamerbr.com>
2020-02-25 21:18:31 -0300

    Derp

--- loritta-plugins/fortnite-stuff/src/main/kotlin/net/perfectdreams/loritta/plugin/fortnite/UpdateStoreItemsTask.kt
+++ loritta-plugins/fortnite-stuff/src/main/kotlin/net/perfectdreams/loritta/plugin/fortnite/UpdateStoreItemsTask.kt
@@ -115,7 +115,7 @@ class UpdateStoreItemsTask(val m: FortniteStuff) {
 			val month = instantAtOffset.month.toString().padStart(2, '0')
 			val day = instantAtOffset.dayOfMonth.toString().padStart(2, '0')
 
-			logger.info { "Last shop update for $apiLocaleId was at ${shopData["updateAt"].long} ($year_$month_$day) New updated at = $newUpdatedAt"}
+			logger.info { "Last shop update for $apiLocaleId was at ${shopData["updateAt"].long} (${year}_${month}_${day}) New updated at = $newUpdatedAt"}
 
 			val fileName = "${locale.id}-${year}_${month}_${day}.png"
 
