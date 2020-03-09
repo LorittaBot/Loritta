4d2c062751ac7e5f181c5166d25fa3bb138cbb32
MrPowerGamerBR <git@mrpowergamerbr.com>
2020-03-09 14:56:12 -0300

    Mudar página do daily para kts

--- loritta-discord/src/main/java/net/perfectdreams/loritta/website/routes/DailyRoute.kt
+++ loritta-discord/src/main/java/net/perfectdreams/loritta/website/routes/DailyRoute.kt
@@ -1,15 +1,28 @@
 package net.perfectdreams.loritta.website.routes
 
 import com.mrpowergamerbr.loritta.utils.locale.BaseLocale
-import com.mrpowergamerbr.loritta.website.evaluate
 import io.ktor.application.ApplicationCall
+import io.ktor.http.ContentType
+import io.ktor.request.path
+import io.ktor.response.respondText
 import net.perfectdreams.loritta.platform.discord.LorittaDiscord
-import net.perfectdreams.loritta.website.utils.extensions.legacyVariables
-import net.perfectdreams.loritta.website.utils.extensions.respondHtml
+import net.perfectdreams.loritta.website.LorittaWebsite
+import net.perfectdreams.loritta.website.utils.ScriptingUtils
+import java.io.File
 
 class DailyRoute(loritta: LorittaDiscord) : LocalizedRoute(loritta, "/daily") {
 	override suspend fun onLocalizedRequest(call: ApplicationCall, locale: BaseLocale) {
-		val variables = call.legacyVariables(locale)
-		call.respondHtml(evaluate("daily.html", variables))
+		val html = ScriptingUtils.evaluateWebPageFromTemplate(
+				File(
+						"${LorittaWebsite.INSTANCE.config.websiteFolder}/views/daily.kts"
+				),
+				mapOf(
+						"path" to call.request.path().split("/").drop(2).joinToString("/"),
+						"websiteUrl" to LorittaWebsite.INSTANCE.config.websiteUrl,
+						"locale" to locale
+				)
+		)
+
+		call.respondText(html, ContentType.Text.Html)
 	}
 }
