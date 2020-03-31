245975fb607c2f42df11b4d25762c3ae570b8ad9
MrPowerGamerBR <git@mrpowergamerbr.com>
2020-03-31 20:44:47 -0300

    Redimensionar badges para 256x256 caso elas sejam maiores que isto

--- loritta-discord/src/main/java/net/perfectdreams/loritta/website/routes/api/v1/guild/PatchUpdateServerConfigBadgeRoute.kt
+++ loritta-discord/src/main/java/net/perfectdreams/loritta/website/routes/api/v1/guild/PatchUpdateServerConfigBadgeRoute.kt
@@ -5,11 +5,13 @@ import com.github.salomonbrys.kotson.jsonObject
 import com.github.salomonbrys.kotson.string
 import com.google.gson.JsonParser
 import com.mrpowergamerbr.loritta.Loritta
+import com.mrpowergamerbr.loritta.utils.toBufferedImage
 import io.ktor.application.ApplicationCall
 import io.ktor.request.receiveText
 import net.perfectdreams.loritta.platform.discord.LorittaDiscord
 import net.perfectdreams.loritta.website.routes.api.v1.RequiresAPIAuthenticationRoute
 import net.perfectdreams.loritta.website.utils.extensions.respondJson
+import java.awt.image.BufferedImage
 import java.io.ByteArrayInputStream
 import java.io.File
 import java.util.*
@@ -24,8 +26,14 @@ class PatchUpdateServerConfigBadgeRoute(loritta: LorittaDiscord) : RequiresAPIAu
 		val imageBytes = Base64.getDecoder().decode(base64Image)
 		val img = ImageIO.read(ByteArrayInputStream(imageBytes))
 
-		if (img != null)
-			ImageIO.write(img, "png", File(Loritta.ASSETS, "badges/custom/${call.parameters["guildId"]}.png"))
+		if (img != null) {
+			var finalImage = img
+			if (finalImage.width > 256 && finalImage.height > 256)
+				finalImage = finalImage.getScaledInstance(256, 256, BufferedImage.SCALE_SMOOTH)
+						.toBufferedImage()
+
+			ImageIO.write(finalImage, "png", File(Loritta.ASSETS, "badges/custom/${call.parameters["guildId"]}.png"))
+		}
 
 		call.respondJson(jsonObject())
 	}
