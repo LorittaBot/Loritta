c2ada5b231595d97e00f9a0de61ec99e86f12d03
MrPowerGamerBR <git@mrpowergamerbr.com>
2020-04-07 09:49:33 -0300

    Adicionar logging de erros ao enviar reminders, corrigir bug em que lembretes somem sozinhos

--- loritta-discord/src/main/java/com/mrpowergamerbr/loritta/threads/RemindersThread.kt
+++ loritta-discord/src/main/java/com/mrpowergamerbr/loritta/threads/RemindersThread.kt
@@ -5,6 +5,7 @@ import com.mrpowergamerbr.loritta.network.Databases
 import com.mrpowergamerbr.loritta.tables.Reminders
 import com.mrpowergamerbr.loritta.utils.loritta
 import com.mrpowergamerbr.loritta.utils.lorittaShards
+import mu.KotlinLogging
 import org.jetbrains.exposed.sql.deleteWhere
 import org.jetbrains.exposed.sql.transactions.transaction
 
@@ -12,6 +13,10 @@ import org.jetbrains.exposed.sql.transactions.transaction
  * Thread que atualiza o status da Loritta a cada 1s segundos
  */
 class RemindersThread : Thread("Reminders Thread") {
+	companion object {
+		private val logger = KotlinLogging.logger {}
+	}
+
 	override fun run() {
 		super.run()
 
@@ -19,32 +24,41 @@ class RemindersThread : Thread("Reminders Thread") {
 			try {
 				checkReminders()
 			} catch (e: Exception) {
-				e.printStackTrace()
+				logger.warn(e) { "Something went wrong while checking reminders!"}
 			}
-			Thread.sleep(5000)
+			sleep(5000)
 		}
 	}
 
 	fun checkReminders() {
 		val reminders = transaction(Databases.loritta) {
-			Reminder.find { Reminders.remindAt.lessEq(System.currentTimeMillis()) }.toMutableList()
+			Reminder.find { Reminders.remindAt.lessEq(System.currentTimeMillis()) }
+					.toList()
 		}
 
+		val notifiedReminders = mutableListOf<Reminder>()
+
 		for (reminder in reminders) {
-			val channel = lorittaShards.getTextChannelById(reminder.channelId.toString())
+			try {
+				val channel = lorittaShards.getTextChannelById(reminder.channelId.toString())
 
-			if (channel != null && channel.canTalk()) {
-				if (loritta.isMainAccountOnlineAndWeAreNotTheMainAccount(channel.guild))
-					return
+				if (channel != null && channel.canTalk()) {
+					if (loritta.isMainAccountOnlineAndWeAreNotTheMainAccount(channel.guild))
+						continue
 
-				channel.sendMessage("<a:lori_notification:394165039227207710> | <@" + reminder.userId + "> Lembrete! `" + reminder.content + "`").queue()
-
-				transaction(Databases.loritta) {
-					Reminders.deleteWhere { Reminders.remindAt.lessEq(System.currentTimeMillis()) }
+					channel.sendMessage("<a:lori_notification:394165039227207710> | <@" + reminder.userId + "> Lembrete! `" + reminder.content + "`").queue()
+					notifiedReminders.add(reminder)
+				} else {
+					// TODO: Enviar na DM do usuário
 				}
-			} else {
-				// TODO: Enviar na DM do usuário
+			} catch (e: Exception) {
+				logger.warn(e) { "Something went wrong while trying to notify ${reminder.userId} about ${reminder.content} at channel ${reminder.channelId}" }
 			}
+		}
+
+		// Apenas delete os lembretes NOTIFICADOS, as vezes lembretes podem ser de canais em outros clusters, e a gente não deve deletá-los
+		transaction(Databases.loritta) {
+			Reminders.deleteWhere { Reminders.id inList notifiedReminders.map { it.id } }
 		}
 	}
 }
