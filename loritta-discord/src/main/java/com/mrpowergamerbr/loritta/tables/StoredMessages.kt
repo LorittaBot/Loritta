134cd6d47c0ee120ca357f6360b169a4b5fd4fe1
MrPowerGamerBR <git@mrpowergamerbr.com>
2020-05-07 13:31:37 -0300

    Add index for the created_at field
    Sometimes it can take up to 60s just to purge the old stored messages!

--- loritta-discord/src/main/java/com/mrpowergamerbr/loritta/tables/StoredMessages.kt
+++ loritta-discord/src/main/java/com/mrpowergamerbr/loritta/tables/StoredMessages.kt
@@ -8,7 +8,7 @@ object StoredMessages : LongIdTable() {
 	val authorId = long("author_id").index()
 	val channelId = long("channel_id")
 	val content = text("content")
-	val createdAt = long("created_at")
+	val createdAt = long("created_at").index()
 	val editedAt = long("edited_at").nullable()
 	val storedAttachments = array<String>("stored_attachments", TextColumnType())
 	val initializationVector = text("initialization_vector")
