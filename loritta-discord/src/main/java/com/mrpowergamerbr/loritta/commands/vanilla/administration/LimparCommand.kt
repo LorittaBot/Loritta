9f79962289104361a471c249cfdc54a93407ceea
Luke <42049518+IDjinn@users.noreply.github.com>
2020-05-13 17:39:42 -0300

    Don't delete pinned messages in Clear Command (#2096)
    * Don't delete pinned messages in Clear Command
    
    Pinned messages will be deleted only if user use the force flag.
    
    * Fix filter and new locales Clear Command
    
    * Change sendMessage to LoriReply Clear Command

--- loritta-discord/src/main/java/com/mrpowergamerbr/loritta/commands/vanilla/administration/LimparCommand.kt
+++ loritta-discord/src/main/java/com/mrpowergamerbr/loritta/commands/vanilla/administration/LimparCommand.kt
@@ -3,6 +3,7 @@ package com.mrpowergamerbr.loritta.commands.vanilla.administration
 import com.mrpowergamerbr.loritta.commands.AbstractCommand
 import com.mrpowergamerbr.loritta.commands.CommandContext
 import com.mrpowergamerbr.loritta.utils.Constants
+import com.mrpowergamerbr.loritta.utils.LoriReply
 import com.mrpowergamerbr.loritta.utils.extensions.await
 import com.mrpowergamerbr.loritta.utils.locale.LegacyBaseLocale
 import net.dv8tion.jda.api.Permission
@@ -14,7 +15,7 @@ import net.perfectdreams.loritta.api.commands.arguments
 
 class LimparCommand : AbstractCommand("clean", listOf("limpar", "clear"), CommandCategory.ADMIN) {
 	override fun getDescription(locale: LegacyBaseLocale): String {
-		return locale["LIMPAR_DESCRIPTION"]
+		return locale.toNewLocale()["commands.moderation.clear.description"]
 	}
 
 	override fun getUsage(locale: LegacyBaseLocale): CommandArguments {
@@ -22,16 +23,14 @@ class LimparCommand : AbstractCommand("clean", listOf("limpar", "clear"), Comman
 			argument(ArgumentType.NUMBER) {
 				optional = false
 			}
+			argument(ArgumentType.USER){
+				optional = true
+			}
 		}
 	}
 
-
-	override fun getUsage(): String {
-		return "QuantasMensagens"
-	}
-
-	override fun getExamples(): List<String> {
-		return listOf("10", "25", "7 @Tsugami", "50 @Tsugami @Tsumari")
+	override fun getExamples(locale: LegacyBaseLocale): List<String> {
+		return locale.toNewLocale().getWithType("commands.moderation.clear.examples")
 	}
 
 	override fun getDiscordPermissions(): List<Permission> {
@@ -51,52 +50,63 @@ class LimparCommand : AbstractCommand("clean", listOf("limpar", "clear"), Comman
 			val toClear = context.args[0].toIntOrNull()
 
 			if (toClear == null) {
-				context.sendMessage("${Constants.ERROR} **|** ${context.getAsMention(true)}${context.legacyLocale["INVALID_NUMBER", context.args[0]]}")
+				context.reply(
+						LoriReply(
+								"${context.legacyLocale["INVALID_NUMBER", context.args[0]]}",
+								Constants.ERROR
+						)
+				)
 				return
 			}
 
 			if (toClear !in 2..100) {
-				context.sendMessage("${Constants.ERROR} **|** ${context.getAsMention(true)}${context.legacyLocale["LIMPAR_INVALID_RANGE"]}")
+				context.reply(
+						LoriReply(
+								"${context.locale["commands.moderation.clear.invalidClearRange"]}",
+								Constants.ERROR
+						)
+				)
 				return
 			}
 
 			// Primeiros iremos deletar a mensagem do comando que o usuário enviou
-			try { context.message.delete().await() } catch (e: Exception) {}
+			try { context.message.delete().await() } catch (e: Exception) { }
 
-			var hasTooOldMessages = false
 			val messages = context.event.textChannel!!.history.retrievePast(toClear).await()
-			val allowedMessages = messages.asSequence().filter {
-				if (context.message.mentionedUsers.isNotEmpty()) {
-					context.message.mentionedUsers.contains(it.author)
-				} else {
-					true
-				}
-			}.filter {
-				val twoWeeksAgo = System.currentTimeMillis() - 14 * 24 * 60 * 60 * 1000 - Constants.DISCORD_EPOCH shl Constants.TIMESTAMP_OFFSET.toInt()
-				val isTooOld = MiscUtil.parseSnowflake(it.id) > twoWeeksAgo
-				if (isTooOld) {
-					hasTooOldMessages = true
-				}
-				isTooOld
-			}.toList()
+			val oldMessages = messages.filter { (System.currentTimeMillis() / 1000) - it.timeCreated.toEpochSecond() > 14 * 24 * 60 * 60 }
+			val pinnedMessages = messages.filter { it.isPinned }
+			val allowedMessages  = messages.filter {(context.message.mentionedUsers.isEmpty() || (context.message.mentionedUsers.isNotEmpty() && context.message.mentionedUsers.contains(it.author))) && !oldMessages.contains(it) && !pinnedMessages.contains(it) }
 
 			if (allowedMessages.isEmpty()) {
-				context.sendMessage("${Constants.ERROR} **|** ${context.userHandle.asMention} ${context.legacyLocale["LIMPAR_COUDLNT_FIND_MESSAGES"]}")
+				context.reply(
+						LoriReply(
+								"${context.locale["commands.moderation.clear.couldNotFindMessages"]}",
+								Constants.ERROR
+						)
+				)
 				return
 			}
 
 			if (allowedMessages.size !in 2..100) {
-				context.sendMessage("${Constants.ERROR} **|** ${context.userHandle.asMention} ${context.legacyLocale["LIMPAR_COUDLNT_FIND_MESSAGES"]}")
+				context.reply(
+						LoriReply(
+								"${context.locale["commands.moderation.clear.couldNotFindMessages"]}",
+								Constants.ERROR
+						)
+				)
 				return
 			}
 
 			// E agora realmente iremos apagar as mensagens!
 			context.message.textChannel.deleteMessages(allowedMessages).await()
-
-			if (allowedMessages.size == messages.size) {
-				context.sendMessage(context.legacyLocale["LIMPAR_SUCCESS", context.userHandle.asMention])
-			} else if (hasTooOldMessages) {
-				context.sendMessage(context.legacyLocale["LIMPAR_SUCCESS_IGNORED_TOO_OLD", context.userHandle.asMention, messages.size - allowedMessages.size])
+			if (oldMessages.size > 0 && pinnedMessages.size > 0) {
+				context.sendMessage(context.locale["commands.moderation.clear.ignoredTooOldAndPinnedMessages", context.userHandle.asMention, oldMessages.size, pinnedMessages.size])
+			} else if (oldMessages.size > 0) {
+				context.sendMessage(context.locale["commands.moderation.clear.ignoredTooOldMessages", context.userHandle.asMention, oldMessages.size])
+			} else if (pinnedMessages.size > 0) {
+				context.sendMessage(context.locale["commands.moderation.clear.ignoredPinnedMessages", context.userHandle.asMention, pinnedMessages.size])
+			} else {
+				context.sendMessage(context.locale["commands.moderation.clear.sucess", context.userHandle.asMention])
 			}
 		} else {
 			this.explain(context)
