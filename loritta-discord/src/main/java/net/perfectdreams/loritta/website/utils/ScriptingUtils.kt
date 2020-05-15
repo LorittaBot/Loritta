af47c1f57c944ffa1547fcb85d66035e2a8c751b
MrPowerGamerBR <git@mrpowergamerbr.com>
2020-05-15 10:57:36 -0300

    Fix derpy indentation

--- loritta-discord/src/main/java/net/perfectdreams/loritta/website/utils/ScriptingUtils.kt
+++ loritta-discord/src/main/java/net/perfectdreams/loritta/website/utils/ScriptingUtils.kt
@@ -35,7 +35,7 @@ object ScriptingUtils {
         val argTypes = modifiedArgs.map {
             it.key to it.value.run {
                 if (this is WebsiteArgumentType) {
-					val clazzName = this.overrideClassName ?: (this.kType.classifier as KClass<*>).qualifiedName
+                    val clazzName = this.overrideClassName ?: (this.kType.classifier as KClass<*>).qualifiedName
 
                     var str = clazzName
 
@@ -122,8 +122,8 @@ object ScriptingUtils {
                 $code
             """.trimIndent()
 
-	val generatedViewsFolder = File("${LorittaWebsite.INSTANCE.config.websiteFolder}/generated_views/")
-	generatedViewsFolder.mkdirs()
+        val generatedViewsFolder = File("${LorittaWebsite.INSTANCE.config.websiteFolder}/generated_views/")
+        generatedViewsFolder.mkdirs()
         File(generatedViewsFolder, file.name).writeText(editedCode)
 
         logger.info { "Compiling ${file.name}..." }
