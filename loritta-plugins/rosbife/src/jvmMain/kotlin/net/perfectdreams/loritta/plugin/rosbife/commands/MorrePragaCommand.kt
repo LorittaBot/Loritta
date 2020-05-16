b73983adc7dcdf38b10c31225358e5f4bf47c7b2
David Paiva <pillgp@icloud.com>
2020-05-16 13:37:39 +0100

    +morrepraga Command (#2123)
    * +morrepraga Command
    
    * Moving +morrepraga from AbstractCommand to DSLCommand
    
    * Removing unnecessary semicolons
    
    * Fixing indentation

---
+++ loritta-plugins/rosbife/src/jvmMain/kotlin/net/perfectdreams/loritta/plugin/rosbife/commands/MorrePragaCommand.kt
@@ -0,0 +1,67 @@
+package net.perfectdreams.loritta.plugin.rosbife.commands
+
+import com.mrpowergamerbr.loritta.Loritta
+import com.mrpowergamerbr.loritta.utils.ImageUtils
+import net.perfectdreams.loritta.api.LorittaBot
+import net.perfectdreams.loritta.api.commands.ArgumentType
+import net.perfectdreams.loritta.api.utils.extensions.enableFontAntiAliasing
+import net.perfectdreams.loritta.api.utils.image.JVMImage
+import net.perfectdreams.loritta.plugin.rosbife.commands.base.DSLCommandBase
+import java.awt.Color
+import java.awt.Font
+import java.awt.Graphics
+import java.awt.Rectangle
+import java.awt.image.BufferedImage
+import java.io.File
+
+object MorrePragaCommand : DSLCommandBase {
+	override fun command(loritta: LorittaBot) = create(
+			loritta,
+			listOf("dieplague", "morrepraga")
+	) {
+		description { it["commands.images.morrepraga.description"] }
+
+		usage {
+			argument(ArgumentType.IMAGE) {}
+		}
+
+		needsToUploadFiles = true
+
+		executes {
+			// TODO: Multiplatform
+			val mppImage = validate(image(0))
+			mppImage as JVMImage
+			val mppMorrePraga = loritta.assets.loadImage("morre_praga.png", loadFromCache = false)
+			val template = (mppMorrePraga as JVMImage).handle as BufferedImage
+
+			val contextImage = mppImage.handle as BufferedImage
+			val graphics = template.createGraphics()
+
+			val scaled = contextImage.getScaledInstance(312, 312, BufferedImage.SCALE_SMOOTH)
+			
+			graphics.enableFontAntiAliasing()
+			val lato = Font.createFont(Font.TRUETYPE_FONT, File(Loritta.ASSETS, "fonts/Cambria-Italic.ttf"))
+			val font = lato.deriveFont(150f)
+			graphics.color = Color.BLACK
+			graphics.font = font
+
+			ImageUtils.drawCenteredString(
+				graphics,
+				locale["commands.images.morrepraga.topText"],
+				Rectangle(25, 38, 502, 132),
+				graphics.font
+			)
+
+			ImageUtils.drawCenteredString(
+				graphics,
+				locale["commands.images.morrepraga.bottomText"],
+				Rectangle(43, 480, 468, 139),
+				graphics.font
+			)
+			
+			graphics.drawImage(scaled, 115, 183, null)
+
+			sendImage(JVMImage(template), "morre_praga.png")
+		}
+	}
+}
--- loritta-plugins/rosbife/src/jvmMain/kotlin/net/perfectdreams/loritta/plugin/rosbife/platformSpecificOnEnable.kt
+++ loritta-plugins/rosbife/src/jvmMain/kotlin/net/perfectdreams/loritta/plugin/rosbife/platformSpecificOnEnable.kt
@@ -3,11 +3,13 @@ package net.perfectdreams.loritta.plugin.rosbife
 // import net.perfectdreams.loritta.plugin.rosbife.commands.AtendenteCommand
 import net.perfectdreams.loritta.plugin.rosbife.commands.TerminatorCommand
 import net.perfectdreams.loritta.plugin.rosbife.commands.ToBeContinuedCommand
+import net.perfectdreams.loritta.plugin.rosbife.commands.MorrePragaCommand
 
 actual fun RosbifePlugin.platformSpecificOnEnable() {
 	registerCommands(
 			// AtendenteCommand.command(loritta),
 			ToBeContinuedCommand.command(loritta),
-			TerminatorCommand.command(loritta)
+			TerminatorCommand.command(loritta),
+			MorrePragaCommand.command(loritta)
 	)
 }
