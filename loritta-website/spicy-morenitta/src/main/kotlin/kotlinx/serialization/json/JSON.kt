efb5a3a83ce98f5304fbef860e0255134c58dd1d
MrPowerGamerBR <git@mrpowergamerbr.com>
2020-05-19 16:25:23 -0300

    Lenient due to bad serialization

--- loritta-website/spicy-morenitta/src/main/kotlin/kotlinx/serialization/json/JSON.kt
+++ loritta-website/spicy-morenitta/src/main/kotlin/kotlinx/serialization/json/JSON.kt
@@ -2,5 +2,5 @@ package kotlinx.serialization.json
 
 object JSON {
 	// Workaround because I'm too lazy to fix old code
-	val nonstrict = Json(JsonConfiguration.Default.copy(ignoreUnknownKeys = true))
+	val nonstrict = Json(JsonConfiguration.Default.copy(ignoreUnknownKeys = true, isLenient = true))
 }
