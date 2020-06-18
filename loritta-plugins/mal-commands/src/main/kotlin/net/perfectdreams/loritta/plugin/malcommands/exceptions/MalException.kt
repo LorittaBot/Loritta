23aa276ae7b201e966b486d087afa8089bfbc080
Nightdavisao <nightdavisao27@protonmail.com>
2020-06-18 15:59:50 -0300

    what happenend git

--- loritta-plugins/mal-commands/src/main/kotlin/net/perfectdreams/loritta/plugin/malcommands/exceptions/MalException.kt
+++ loritta-plugins/mal-commands/src/main/kotlin/net/perfectdreams/loritta/plugin/malcommands/exceptions/MalException.kt
@@ -1,7 +1,3 @@
 package net.perfectdreams.loritta.plugin.malcommands.exceptions
 
-<<<<<<< HEAD
-open class MalException(override val message: String?): Exception()
-=======
 open class MalException: Exception()
->>>>>>> 50d31a5... Improve some things about throwing/handling errors
