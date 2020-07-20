7c3feb15ead61dfa239e45482046f7b18808616c
MrPowerGamerBR <git@mrpowergamerbr.com>
2020-07-20 12:24:27 -0300

    Fix kotlinx.html repository

--- build.gradle.kts
+++ build.gradle.kts
@@ -86,6 +86,7 @@ allprojects {
 
         maven("https://dl.bintray.com/kotlin/kotlin-dev/")
         maven("https://dl.bintray.com/kotlin/kotlin-eap/")
+        maven("https://dl.bintray.com/kotlin/kotlinx.html")
         maven("https://jcenter.bintray.com")
         maven("https://oss.sonatype.org/content/repositories/snapshots/")
         maven("https://repo.perfectdreams.net/")
