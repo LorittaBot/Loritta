fcf6f149d302bdcd6142ec9ed7444615fb1b44b4
MrPowerGamerBR <git@mrpowergamerbr.com>
2020-07-24 20:04:43 -0300

    Add (staff only) command to add backgrounds

--- loritta-plugins/lori-guild-stuff/src/main/kotlin/net/perfectdreams/loritta/plugin/loriguildstuff/LoriGuildStuffPlugin.kt
+++ loritta-plugins/lori-guild-stuff/src/main/kotlin/net/perfectdreams/loritta/plugin/loriguildstuff/LoriGuildStuffPlugin.kt
@@ -3,13 +3,17 @@ package net.perfectdreams.loritta.plugin.loriguildstuff
 import net.perfectdreams.loritta.api.LorittaBot
 import net.perfectdreams.loritta.platform.discord.LorittaDiscord
 import net.perfectdreams.loritta.platform.discord.plugin.LorittaDiscordPlugin
+import net.perfectdreams.loritta.plugin.loriguildstuff.commands.AddBackgroundCommand
+import net.perfectdreams.loritta.plugin.loriguildstuff.commands.NotifyBackgroundsCommand
 import net.perfectdreams.loritta.plugin.loriguildstuff.commands.NotifyCommand
 import net.perfectdreams.loritta.plugin.loriguildstuff.modules.AddReactionsToChannelsModule
 
 class LoriGuildStuffPlugin(name: String, loritta: LorittaBot) : LorittaDiscordPlugin(name, loritta) {
     override fun onEnable() {
         loritta as LorittaDiscord
+        registerCommand(NotifyBackgroundsCommand.create(loritta))
         registerCommand(NotifyCommand.create(loritta))
+        registerCommand(AddBackgroundCommand.create(loritta))
 
         addMessageReceivedModules(AddReactionsToChannelsModule(this))
     }
