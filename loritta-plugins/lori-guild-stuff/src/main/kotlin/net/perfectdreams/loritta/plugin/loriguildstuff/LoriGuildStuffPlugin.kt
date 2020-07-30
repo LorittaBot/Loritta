31303b7826fd85362c4bbb17c3f89a4ea60daead
MrPowerGamerBR <git@mrpowergamerbr.com>
2020-07-29 21:08:54 -0300

    Stuff

--- loritta-plugins/lori-guild-stuff/src/main/kotlin/net/perfectdreams/loritta/plugin/loriguildstuff/LoriGuildStuffPlugin.kt
+++ loritta-plugins/lori-guild-stuff/src/main/kotlin/net/perfectdreams/loritta/plugin/loriguildstuff/LoriGuildStuffPlugin.kt
@@ -7,8 +7,12 @@ import net.perfectdreams.loritta.plugin.loriguildstuff.commands.AddBackgroundCom
 import net.perfectdreams.loritta.plugin.loriguildstuff.commands.NotifyBackgroundsCommand
 import net.perfectdreams.loritta.plugin.loriguildstuff.commands.NotifyCommand
 import net.perfectdreams.loritta.plugin.loriguildstuff.modules.AddReactionsToChannelsModule
+import net.perfectdreams.loritta.plugin.loriguildstuff.modules.BlockBadWordsModule
+import java.io.File
 
 class LoriGuildStuffPlugin(name: String, loritta: LorittaBot) : LorittaDiscordPlugin(name, loritta) {
+    val badWords by lazy { File(dataFolder, "bad-words.txt").readLines() }
+
     override fun onEnable() {
         loritta as LorittaDiscord
         registerCommand(NotifyBackgroundsCommand.create(loritta))
@@ -16,5 +20,6 @@ class LoriGuildStuffPlugin(name: String, loritta: LorittaBot) : LorittaDiscordPl
         registerCommand(AddBackgroundCommand.create(loritta))
 
         addMessageReceivedModules(AddReactionsToChannelsModule(this))
+        addMessageReceivedModules(BlockBadWordsModule(this))
     }
 }
