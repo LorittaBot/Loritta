55256b6b770f24fd3f469534330aade960c73985
hechfx <60321993+hechfx@users.noreply.github.com>
2020-07-31 18:32:54 -0300

    ColorCommand is bacc (#2177)

--- loritta-plugins/lori-guild-stuff/src/main/kotlin/net/perfectdreams/loritta/plugin/loriguildstuff/LoriGuildStuffPlugin.kt
+++ loritta-plugins/lori-guild-stuff/src/main/kotlin/net/perfectdreams/loritta/plugin/loriguildstuff/LoriGuildStuffPlugin.kt
@@ -4,6 +4,7 @@ import net.perfectdreams.loritta.api.LorittaBot
 import net.perfectdreams.loritta.platform.discord.LorittaDiscord
 import net.perfectdreams.loritta.platform.discord.plugin.LorittaDiscordPlugin
 import net.perfectdreams.loritta.plugin.loriguildstuff.commands.AddBackgroundCommand
+import net.perfectdreams.loritta.plugin.loriguildstuff.commands.ColorCommand
 import net.perfectdreams.loritta.plugin.loriguildstuff.commands.NotifyBackgroundsCommand
 import net.perfectdreams.loritta.plugin.loriguildstuff.commands.NotifyCommand
 import net.perfectdreams.loritta.plugin.loriguildstuff.modules.AddReactionsToChannelsModule
@@ -15,9 +16,12 @@ class LoriGuildStuffPlugin(name: String, loritta: LorittaBot) : LorittaDiscordPl
 
     override fun onEnable() {
         loritta as LorittaDiscord
-        registerCommand(NotifyBackgroundsCommand.create(loritta))
-        registerCommand(NotifyCommand.create(loritta))
-        registerCommand(AddBackgroundCommand.create(loritta))
+        registerCommands(
+                NotifyBackgroundsCommand.create(loritta),
+                NotifyCommand.create(loritta),
+                AddBackgroundCommand.create(loritta),
+                ColorCommand.create(loritta)
+        )
 
         addMessageReceivedModules(AddReactionsToChannelsModule(this))
         addMessageReceivedModules(BlockBadWordsModule(this))
---
+++ loritta-plugins/lori-guild-stuff/src/main/kotlin/net/perfectdreams/loritta/plugin/loriguildstuff/commands/ColorCommand.kt
@@ -0,0 +1,83 @@
+package net.perfectdreams.loritta.plugin.loriguildstuff.commands
+
+import com.mrpowergamerbr.loritta.utils.Constants
+import com.mrpowergamerbr.loritta.utils.extensions.await
+import net.perfectdreams.loritta.api.commands.CommandCategory
+import net.perfectdreams.loritta.platform.discord.LorittaDiscord
+import net.perfectdreams.loritta.platform.discord.commands.discordCommand
+import net.perfectdreams.loritta.api.messages.LorittaReply
+
+object ColorCommand {
+
+    private val colors = hashMapOf(
+            "azul claro" to 373539846620315648L,
+            "vermelho escuro" to 373540076095012874L,
+            "verde escuro" to 374613624536170500L,
+            "rosa choque" to 411235044842012674L,
+            "rosa escuro" to 374614002707333120L,
+            "azul escuro" to 373539894259351553L,
+            "rosa claro" to 374613958608551936L,
+            "vermelho" to 373540030053875713L,
+            "amarelo" to 373539918863007745L,
+            "dourado" to 373539973984550912L,
+            "verde" to 374613592185634816L
+    )
+
+    fun create(loritta: LorittaDiscord) = discordCommand(loritta, listOf("cor", "color"), CommandCategory.MISC) {
+        this.hideInHelp = true
+        this.commandCheckFilter { lorittaMessageEvent, _, _, _, _ ->
+            lorittaMessageEvent.guild?.idLong == 297732013006389252L
+        }
+
+        executesDiscord {
+            val donatorRole = guild.getRoleById(364201981016801281L)!! //364201981016801281L - role original
+            val member = this.member!!
+
+            if (!member.roles.contains(donatorRole)) {
+                reply(
+                        LorittaReply(
+                                "Este comando é apenas para doadores, se você quer me ajudar a comprar um :custard:, então vire um doador! https://loritta.website/donate",
+                                Constants.ERROR
+                        )
+                )
+            } else {
+                val selection = args.joinToString(" ")
+
+                val c = colors[selection]
+
+                if (c != null) {
+                    val role = guild.getRoleById(c)!!
+
+                    if (member.roles.contains(role)) {
+                        guild.removeRoleFromMember(member, role).await()
+                        reply(
+                                LorittaReply(
+                                        "Cor removida!",
+                                        "\uD83C\uDFA8"
+                                )
+                        )
+                    } else {
+                        guild.addRoleToMember(member, role).await()
+                        reply(
+                                LorittaReply(
+                                        "Cor adicionada!",
+                                        "\uD83C\uDFA8"
+                                )
+                        )
+                    }
+                }
+
+                val list = colors.keys.joinToString(", ")
+
+                if (args.isEmpty()) {
+                    reply(
+                            LorittaReply(
+                                    "Cores disponíveis: `$list`",
+                                    "\uD83C\uDFA8"
+                            )
+                    )
+                }
+            }
+        }
+    }
+}
