e0bbeee708f01a8947717562cc6a8ad222668349
hechfx <60321993+hechfx@users.noreply.github.com>
2020-07-30 22:03:07 -0300

    Added ColorCommand

--- loritta-plugins/lori-guild-stuff/src/main/kotlin/net/perfectdreams/loritta/plugin/loriguildstuff/LoriGuildStuffPlugin.kt
+++ loritta-plugins/lori-guild-stuff/src/main/kotlin/net/perfectdreams/loritta/plugin/loriguildstuff/LoriGuildStuffPlugin.kt
@@ -3,11 +3,15 @@ package net.perfectdreams.loritta.plugin.loriguildstuff
 import net.perfectdreams.loritta.api.LorittaBot
 import net.perfectdreams.loritta.platform.discord.LorittaDiscord
 import net.perfectdreams.loritta.platform.discord.plugin.LorittaDiscordPlugin
+import net.perfectdreams.loritta.plugin.loriguildstuff.commands.ColorCommand
 import net.perfectdreams.loritta.plugin.loriguildstuff.commands.NotifyCommand
+import net.perfectdreams.loritta.plugin.loriguildstuff.modules.AddReactionsToChannelsModule
 
 class LoriGuildStuffPlugin(name: String, loritta: LorittaBot) : LorittaDiscordPlugin(name, loritta) {
     override fun onEnable() {
         loritta as LorittaDiscord
-        registerCommand(NotifyCommand.create(loritta))
+        registerCommands(NotifyCommand.create(loritta), ColorCommand.create(loritta))
+
+        addMessageReceivedModules(AddReactionsToChannelsModule(this))
     }
 }
