2f51449b241b004f0a722f5f51d2a166935ac6b3
hechfx <60321993+hechfx@users.noreply.github.com>
2020-08-01 20:15:07 -0300

    Update SendFanartCommand.kt

--- loritta-plugins/lori-guild-stuff/src/main/kotlin/net/perfectdreams/loritta/plugin/loriguildstuff/commands/SendFanartCommand.kt
+++ loritta-plugins/lori-guild-stuff/src/main/kotlin/net/perfectdreams/loritta/plugin/loriguildstuff/commands/SendFanartCommand.kt
@@ -16,26 +16,37 @@ object SendFanartCommand {
 
         executesDiscord {
             val role = guild.getRoleById(589159900379086868L)!!
-            val firstFanartChannel = guild.getGuildChannelById(589159642160955402L)!!
+            val firstFanartChannel = guild.getTextChannelById(589159642160955402L)!!
             val member = this.member!!
 
             if (member.roles.contains(role)) {
-                if (message.channel == firstFanartChannel && discordMessage.attachments.isEmpty()) {
-                    reply(
-                            LorittaReply(
-                                    "Apenas envie sua fanart, não precisa usar o comando novamente!",
-                                    "<a:lori_pat:706263175892566097>"
-                            )
-                    )
-                } else if (message.channel !== firstFanartChannel && discordMessage.attachments.isNotEmpty()) {
-                    reply(
-                            LorittaReply(
-                                    "Você deve enviar sua fanart em <#589159642160955402>!",
-                                    "<:lori_ameno:673868465433477126>"
-                            )
-                    )
-                } else if (message.channel == firstFanartChannel && discordMessage.attachments.isNotEmpty()) {
-                    doNothing()
+                if (discordMessage.textChannel == firstFanartChannel) {
+                    if (discordMessage.attachments.isEmpty()) {
+                        reply(
+                                LorittaReply(
+                                        "Apenas envie sua fanart, não precisa usar o comando novamente!",
+                                        "<a:lori_pat:706263175892566097>"
+                                )
+                        )
+                    }
+                }
+                if (discordMessage.textChannel != firstFanartChannel) {
+                    if (discordMessage.attachments.isNotEmpty()) {
+                        reply(
+                                LorittaReply(
+                                        "Você deve enviar sua fanart em <#589159642160955402>!",
+                                        "<:lori_ameno:673868465433477126>"
+                                )
+                        )
+                    } else {
+                        guild.removeRoleFromMember(member, role).await()
+                        reply(
+                                LorittaReply(
+                                        "Que pena, pensei que você queria enviar a sua fan art para mim!",
+                                        "<a:bongo_lori_triste:524894216510373888>"
+                                )
+                        )
+                    }
                 }
             } else {
                 guild.addRoleToMember(member, role).await()
