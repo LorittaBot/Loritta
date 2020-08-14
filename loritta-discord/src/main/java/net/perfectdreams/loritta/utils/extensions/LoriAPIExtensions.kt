fbc4604872e0e57cf20148c34616069c6025385b
MrPowerGamerBR <git@mrpowergamerbr.com>
2020-08-13 21:16:57 -0300

    Fix server-support replies compilation

--- loritta-discord/src/main/java/net/perfectdreams/loritta/utils/extensions/LoriAPIExtensions.kt
+++ loritta-discord/src/main/java/net/perfectdreams/loritta/utils/extensions/LoriAPIExtensions.kt
@@ -41,3 +41,11 @@ fun LorittaReply.build(context: CommandContext) = this.build(JDAUser(context.use
  * @return        the reply as a string
  */
 fun LorittaReply.build(context: DiscordCommandContext) = this.build(JDAUser(context.userHandle))
+
+/**
+ * Builds a reply based on the user [user]
+ *
+ * @param context the current context
+ * @return        the reply as a string
+ */
+fun LorittaReply.build(user: net.dv8tion.jda.api.entities.User) = this.build(JDAUser(user))
