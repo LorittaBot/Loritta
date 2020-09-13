faa386bd328518ffa495a0ee16e354de25baea9d
MrPowerGamerBR <git@mrpowergamerbr.com>
2020-09-13 16:57:25 -0300

    Add "+petpet"

--- loritta-plugins/rosbife/src/jvmMain/kotlin/net/perfectdreams/loritta/plugin/rosbife/platformSpecificOnEnable.kt
+++ loritta-plugins/rosbife/src/jvmMain/kotlin/net/perfectdreams/loritta/plugin/rosbife/platformSpecificOnEnable.kt
@@ -1,10 +1,7 @@
 package net.perfectdreams.loritta.plugin.rosbife
 
 // import net.perfectdreams.loritta.plugin.rosbife.commands.AtendenteCommand
-import net.perfectdreams.loritta.plugin.rosbife.commands.CarlyAaahCommand
-import net.perfectdreams.loritta.plugin.rosbife.commands.TerminatorCommand
-import net.perfectdreams.loritta.plugin.rosbife.commands.ToBeContinuedCommand
-import net.perfectdreams.loritta.plugin.rosbife.commands.MorrePragaCommand
+import net.perfectdreams.loritta.plugin.rosbife.commands.*
 
 actual fun RosbifePlugin.platformSpecificOnEnable() {
 	registerCommands(
@@ -12,6 +9,7 @@ actual fun RosbifePlugin.platformSpecificOnEnable() {
 			ToBeContinuedCommand.command(loritta),
 			TerminatorCommand.command(loritta),
 			MorrePragaCommand.command(loritta),
-			CarlyAaahCommand.command(loritta)
+			CarlyAaahCommand.command(loritta),
+			PetPetCommand.command(loritta)
 	)
 }
