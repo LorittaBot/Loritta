7b92e64add3b37bb3bd75cb06b86ae5061e5d1d1
Nightdavisao <33351880+Nightdavisao@users.noreply.github.com>
2020-09-24 20:21:42 -0300

    Fix +anime scrapping (#2208)
    * Fix scrapping and use an endpoint for anime query
    
    * fixtures and refactors
    
    * also remove useless constant
    
    * refactor²

--- loritta-plugins/mal-commands/src/main/kotlin/net/perfectdreams/loritta/plugin/malcommands/exceptions/MalSearchException.kt
+++
@@ -1,4 +0,0 @@
-package net.perfectdreams.loritta.plugin.malcommands.exceptions
-
-class MalSearchException(override val cause: Throwable? = null):
-        MalException("Failed at searching/querying anime")
