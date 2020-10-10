1fa78f1df7465cd320626bf51b8706224ecb5acb
MrPowerGamerBR <git@mrpowergamerbr.com>
2020-10-10 16:05:37 -0300

    Add CoinFlipBetXCommand and TestCommand... just for testing purposes

--- loritta-api/src/commonMain/kotlin/net/perfectdreams/loritta/api/plugin/LorittaPlugin.kt
+++ loritta-api/src/commonMain/kotlin/net/perfectdreams/loritta/api/plugin/LorittaPlugin.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import net.perfectdreams.loritta.api.LorittaBot
+import net.perfectdreams.loritta.api.commands.AbstractCommandBase
 import net.perfectdreams.loritta.api.commands.Command
 import net.perfectdreams.loritta.api.commands.CommandContext
 
@@ -21,6 +22,16 @@ abstract class LorittaPlugin(val name: String, val loritta: LorittaBot) {
 
 	fun registerCommand(command: Command<CommandContext>) {
 		loritta.commandMap.register(command)
+		registeredCommands.add(command)
+	}
+
+	fun registerCommands(vararg commands: AbstractCommandBase<*, *>) {
+		commands.forEach { registerCommand(it) }
+	}
+
+	fun registerCommand(commandBase: AbstractCommandBase<*, *>) {
+		val command = commandBase.command()
+		loritta.commandMap.register(command as Command<CommandContext>)
 		registeredCommands.add(command)
 	}
 
--- loritta-plugins/helping-hands/src/main/kotlin/net/perfectdreams/loritta/plugin/helpinghands/HelpingHandsPlugin.kt
+++ loritta-plugins/helping-hands/src/main/kotlin/net/perfectdreams/loritta/plugin/helpinghands/HelpingHandsPlugin.kt
@@ -4,9 +4,7 @@ import mu.KotlinLogging
 import net.perfectdreams.loritta.api.LorittaBot
 import net.perfectdreams.loritta.platform.discord.LorittaDiscord
 import net.perfectdreams.loritta.platform.discord.plugin.LorittaDiscordPlugin
-import net.perfectdreams.loritta.plugin.helpinghands.commands.CoinFlipBetCommand
-import net.perfectdreams.loritta.plugin.helpinghands.commands.DailyInactivityTaxExecutor
-import net.perfectdreams.loritta.plugin.helpinghands.commands.RepListCommand
+import net.perfectdreams.loritta.plugin.helpinghands.commands.*
 import net.perfectdreams.loritta.plugin.helpinghands.utils.DailyInactivityTaxUtils
 
 class HelpingHandsPlugin(name: String, loritta: LorittaBot) : LorittaDiscordPlugin(name, loritta) {
@@ -15,12 +13,15 @@ class HelpingHandsPlugin(name: String, loritta: LorittaBot) : LorittaDiscordPlug
 
 		registerCommand(CoinFlipBetCommand.command(this, loritta))
 		registerCommand(RepListCommand.create(loritta))
+		registerCommands(
+				TestCommand(this),
+				CoinFlipBetXCommand(this)
+		)
 
 		if (loritta.isMaster)
 			launch(DailyInactivityTaxUtils.createAutoInactivityTask())
 		this.loriToolsExecutors += DailyInactivityTaxExecutor
 	}
-
 
 	override fun onDisable() {
 		super.onDisable()
