c239fd7882947372d9382cdcc7df3bc38a1d200e
MrPowerGamerBR <git@mrpowergamerbr.com>
2020-10-15 17:03:28 -0300

    Fix BobBurningFireCommand not working at all

--- loritta-plugins/rosbife/src/commonMain/kotlin/net/perfectdreams/loritta/plugin/rosbife/commands/BobBurningPaperCommand.kt
+++ loritta-plugins/rosbife/src/commonMain/kotlin/net/perfectdreams/loritta/plugin/rosbife/commands/BobBurningPaperCommand.kt
@@ -8,10 +8,24 @@ class BobBurningPaperCommand(m: RosbifePlugin) : BasicSkewedImageCommand(
 		listOf("bobburningpaper", "bobpaperfire", "bobfire", "bobpapelfogo", "bobfogo"),
 		"commands.images.bobfire.description",
 		"bobfire.png",
-		Corners(
-				21f, 373f,
-				14f, 353f,
-				48f, 334f,
-				82f, 354f
+		listOf(
+				Corners(
+						21f, 373f,
+
+						14f, 353f,
+
+						48f, 334f,
+
+						82f, 354f
+				),
+				Corners(
+						24f, 32f,
+
+						138f, 33f,
+
+						137f, 177f,
+
+						20f, 175f
+				)
 		)
 )
