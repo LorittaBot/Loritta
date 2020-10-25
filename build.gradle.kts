be9dfd07adb043034f04d31b7e78d634b236f9a9
MrPowerGamerBR <git@mrpowergamerbr.com>
2020-10-24 23:24:15 -0300

    Change source/target to 14

--- build.gradle.kts
+++ build.gradle.kts
@@ -122,8 +122,8 @@ publishing {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_14
+    targetCompatibility = JavaVersion.VERSION_14
 }
 
 subprojects {
