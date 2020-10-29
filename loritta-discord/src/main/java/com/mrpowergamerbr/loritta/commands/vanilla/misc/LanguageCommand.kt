ad7cc6e30c8ffe51033d2b2a05a9e26f8d31955d
Nightdavisao <33351880+Nightdavisao@users.noreply.github.com>
2020-10-29 09:30:07 -0300

    Implement resetting/removal option of personal language (#2242)
    * Implement resetting option of personal language
    
    (when executing +language in DM)
    
    * Some changes, added locales and reformatted code
    
    * Add a tip that personal language *actually* exists
    
    * reformatting again

--- loritta-discord/src/main/java/com/mrpowergamerbr/loritta/commands/vanilla/misc/LanguageCommand.kt
+++ loritta-discord/src/main/java/com/mrpowergamerbr/loritta/commands/vanilla/misc/LanguageCommand.kt
@@ -2,6 +2,7 @@ package com.mrpowergamerbr.loritta.commands.vanilla.misc
 
 import com.mrpowergamerbr.loritta.commands.AbstractCommand
 import com.mrpowergamerbr.loritta.commands.CommandContext
+import com.mrpowergamerbr.loritta.dao.Profile
 import com.mrpowergamerbr.loritta.utils.extensions.edit
 import com.mrpowergamerbr.loritta.utils.extensions.isEmote
 import com.mrpowergamerbr.loritta.utils.locale.BaseLocale
@@ -13,188 +14,220 @@ import net.dv8tion.jda.api.EmbedBuilder
 import net.dv8tion.jda.api.Permission
 import net.dv8tion.jda.api.entities.MessageEmbed
 import net.perfectdreams.loritta.api.commands.CommandCategory
+import net.perfectdreams.loritta.api.messages.LorittaReply
 import java.awt.Color
 
 class LanguageCommand : AbstractCommand("language", listOf("linguagem", "speak"), category = CommandCategory.MISC) {
-	override fun getDescription(locale: LegacyBaseLocale): String {
-		return locale.toNewLocale()["commands.misc.language.description", "\uD83D\uDE0A"]
-	}
+    private val resetPersonalLanguageEmote = "\uD83D\uDE45"
 
-	override fun getDiscordPermissions(): List<Permission> {
-		return listOf(Permission.MANAGE_SERVER)
-	}
+    override fun getDescription(locale: LegacyBaseLocale): String {
+        return locale.toNewLocale()["commands.misc.language.description", "\uD83D\uDE0A"]
+    }
 
-	override suspend fun run(context: CommandContext, locale: LegacyBaseLocale) {
-		val embed = EmbedBuilder()
-		embed.setColor(Color(0, 193, 223))
+    override fun getDiscordPermissions(): List<Permission> {
+        return listOf(Permission.MANAGE_SERVER)
+    }
 
-		val validLanguages = listOf(
-				LocaleWrapper(
-						"Português-Brasil",
-						loritta.getLocaleById("default"),
-						loritta.getLegacyLocaleById("default"),
-						"\uD83C\uDDE7\uD83C\uDDF7"
-				),
-				/* LocaleWrapper(
-						"Português-Portugal",
-						loritta.getLocaleById("pt-pt"),
-						loritta.getLegacyLocaleById("pt-pt"),
-						"\uD83C\uDDF5\uD83C\uDDF9"
-				), */
-				LocaleWrapper(
-						"English (United States)",
-						loritta.getLocaleById("en-us"),
-						loritta.getLegacyLocaleById("en-us"),
-						"\uD83C\uDDFA\uD83C\uDDF8"
-				),
-				/* LocaleWrapper(
-						"Español",
-						loritta.getLocaleById("es-es"),
-						loritta.getLegacyLocaleById("es-es"),
-						"\uD83C\uDDEA\uD83C\uDDF8"
-				), */
-				LocaleWrapper(
-						"Português-Funk",
-						loritta.getLocaleById("pt-funk"),
-						loritta.getLegacyLocaleById("pt-funk"),
-						"<:loritta_quebrada:338679008210190336>"
-				),
-				LocaleWrapper(
-						"Português-Furry",
-						loritta.getLocaleById("pt-furry"),
-						loritta.getLegacyLocaleById("default"),
-						"\uD83D\uDC3E"
-				),
-				LocaleWrapper(
-						"English-Furry",
-						loritta.getLocaleById("en-furry"),
-						loritta.getLegacyLocaleById("default"),
-						"\uD83D\uDC31"
-				)
-		)
+    override suspend fun run(context: CommandContext, locale: LegacyBaseLocale) {
+        val profile = loritta.getOrCreateLorittaProfile(context.userHandle.idLong)
+        val hasPersonalLanguage = loritta.newSuspendedTransaction {
+            profile.settings.language
+        } != null && context.isPrivateChannel
 
-		if (context.rawArgs.getOrNull(0) == "br-debug") {
-			activateLanguage(
-					context,
-					LocaleWrapper(
-							"Auto-PT-BR-Debug",
-							loritta.getLocaleById("br-debug"),
-							loritta.getLegacyLocaleById("default"),
-							"\uD83D\uDC31"
-					)
-			)
-			return
-		}
+        val embed = EmbedBuilder()
+        embed.setColor(Color(0, 193, 223))
 
-		if (context.rawArgs.getOrNull(0) == "en-debug") {
-			activateLanguage(
-					context,
-					LocaleWrapper(
-							"Auto-EN-Debug",
-							loritta.getLocaleById("en-debug"),
-							loritta.getLegacyLocaleById("default"),
-							"\uD83D\uDC31"
-					)
-			)
-			return
-		}
+        val validLanguages = listOf(
+                LocaleWrapper(
+                        "Português-Brasil",
+                        loritta.getLocaleById("default"),
+                        loritta.getLegacyLocaleById("default"),
+                        "\uD83C\uDDE7\uD83C\uDDF7"
+                ),
+                /* LocaleWrapper(
+                        "Português-Portugal",
+                        loritta.getLocaleById("pt-pt"),
+                        loritta.getLegacyLocaleById("pt-pt"),
+                        "\uD83C\uDDF5\uD83C\uDDF9"
+                ), */
+                LocaleWrapper(
+                        "English (United States)",
+                        loritta.getLocaleById("en-us"),
+                        loritta.getLegacyLocaleById("en-us"),
+                        "\uD83C\uDDFA\uD83C\uDDF8"
+                ),
+                /* LocaleWrapper(
+                        "Español",
+                        loritta.getLocaleById("es-es"),
+                        loritta.getLegacyLocaleById("es-es"),
+                        "\uD83C\uDDEA\uD83C\uDDF8"
+                ), */
+                LocaleWrapper(
+                        "Português-Funk",
+                        loritta.getLocaleById("pt-funk"),
+                        loritta.getLegacyLocaleById("pt-funk"),
+                        "<:loritta_quebrada:338679008210190336>"
+                ),
+                LocaleWrapper(
+                        "Português-Furry",
+                        loritta.getLocaleById("pt-furry"),
+                        loritta.getLegacyLocaleById("default"),
+                        "\uD83D\uDC3E"
+                ),
+                LocaleWrapper(
+                        "English-Furry",
+                        loritta.getLocaleById("en-furry"),
+                        loritta.getLegacyLocaleById("default"),
+                        "\uD83D\uDC31"
+                )
+        )
 
-		val message = context.sendMessage(
-				context.getAsMention(true),
-				buildLanguageEmbed(
-						locale.toNewLocale(),
-						validLanguages.subList(0, 2),
-						context.isPrivateChannel
-				)
-		)
+        if (context.rawArgs.getOrNull(0) == "br-debug") {
+            activateLanguage(
+                    context,
+                    profile,
+                    LocaleWrapper(
+                            "Auto-PT-BR-Debug",
+                            loritta.getLocaleById("br-debug"),
+                            loritta.getLegacyLocaleById("default"),
+                            "\uD83D\uDC31"
+                    )
+            )
+            return
+        }
 
-		message.onReactionAddByAuthor(context) {
-			if (it.reactionEmote.isEmote("426183783008698391")) {
-				message.edit(
-						" ",
-						buildLanguageEmbed(
-								locale.toNewLocale(),
-								validLanguages.subList(2, validLanguages.size),
-								context.isPrivateChannel
-						),
-						true
-				)
+        if (context.rawArgs.getOrNull(0) == "en-debug") {
+            activateLanguage(
+                    context,
+                    profile,
+                    LocaleWrapper(
+                            "Auto-EN-Debug",
+                            loritta.getLocaleById("en-debug"),
+                            loritta.getLegacyLocaleById("default"),
+                            "\uD83D\uDC31"
+                    )
+            )
+            return
+        }
 
-				for (wrapper in validLanguages.subList(2, validLanguages.size)) {
-					// O "replace" é necessário já que a gente usa emojis personalizados para algumas linguagens
-					message.addReaction(wrapper.emoteName.replace("<", "").replace(">", "")).queue()
-				}
-				return@onReactionAddByAuthor
-			}
+        val message = context.sendMessage(
+                context.getAsMention(true),
+                buildLanguageEmbed(
+                        locale.toNewLocale(),
+                        validLanguages.subList(0, 2),
+                        context.isPrivateChannel,
+                        hasPersonalLanguage
+                )
+        )
 
-			val newLanguage = validLanguages.firstOrNull { language ->
-				if (language.emoteName.startsWith("<")) {
-					it.reactionEmote.isEmote(language.emoteName.split(":")[2].removeSuffix(">"))
-				} else {
-					it.reactionEmote.isEmote(language.emoteName)
-				}
-			}
+        message.onReactionAddByAuthor(context) {
+            if (it.reactionEmote.isEmote("426183783008698391")) {
+                message.edit(
+                        " ",
+                        buildLanguageEmbed(
+                                locale.toNewLocale(),
+                                validLanguages.subList(2, validLanguages.size),
+                                context.isPrivateChannel,
+                                hasPersonalLanguage
+                        ),
+                        true
+                )
 
-			message.delete().queue()
-			activateLanguage(context, newLanguage ?: validLanguages.first { it.locale.id == "default" }, context.isPrivateChannel)
-		}
+                for (wrapper in validLanguages.subList(2, validLanguages.size)) {
+                    // O "replace" é necessário já que a gente usa emojis personalizados para algumas linguagens
+                    message.addReaction(wrapper.emoteName.replace("<", "").replace(">", "")).queue()
+                }
+                return@onReactionAddByAuthor
+            }
 
-		for (wrapper in validLanguages.subList(0, 2)) {
-			// O "replace" é necessário já que a gente usa emojis personalizados para algumas linguagens
-			message.addReaction(wrapper.emoteName.replace("<", "").replace(">", "")).queue()
-		}
+            // This removes/resets user's personal language (when you choose a language in DM)
+            if (it.reactionEmote.isEmote(resetPersonalLanguageEmote) && hasPersonalLanguage) {
+                loritta.newSuspendedTransaction {
+                    profile.settings.language = null
+                }
+                context.reply(
+                        LorittaReply(
+                                locale.toNewLocale()["commands.misc.language.removedPersonalLanguage"]
+                        )
+                )
+                return@onReactionAddByAuthor
+            }
 
-		message.addReaction("lori_ok_hand:426183783008698391").queue()
-	}
+            val newLanguage = validLanguages.firstOrNull { language ->
+                if (language.emoteName.startsWith("<")) {
+                    it.reactionEmote.isEmote(language.emoteName.split(":")[2].removeSuffix(">"))
+                } else {
+                    it.reactionEmote.isEmote(language.emoteName)
+                }
+            }
 
-	private suspend fun activateLanguage(context: CommandContext, newLanguage: LocaleWrapper, isPrivateChannel: Boolean = false) {
-		var localeId = newLanguage.locale.id
+            message.delete().queue()
+            activateLanguage(context, profile, newLanguage
+                    ?: validLanguages.first { it.locale.id == "default" }, context.isPrivateChannel)
+        }
 
-		val profile = loritta.getOrCreateLorittaProfile(context.userHandle.idLong)
-		loritta.newSuspendedTransaction {
-			if (isPrivateChannel) // If command was executed in DM channel, will be set only to user
-				profile.settings.language = localeId
-			else
-				context.config.localeId = localeId
-		}
+        for (wrapper in validLanguages.subList(0, 2)) {
+            // O "replace" é necessário já que a gente usa emojis personalizados para algumas linguagens
+            message.addReaction(wrapper.emoteName.replace("<", "").replace(">", "")).queue()
+        }
 
-		val newLocale = loritta.getLocaleById(localeId)
-		if (localeId == "default") {
-			localeId = "pt-br" // Já que nós já salvamos, vamos trocar o localeId para algo mais "decente"
-		}
+        message.addReaction("lori_ok_hand:426183783008698391").queue()
 
-		if(isPrivateChannel)
-			context.reply(newLocale["commands.misc.language.languageChanged", "`${localeId}`"], "\uD83C\uDFA4")
-		else
-			context.reply(newLocale["commands.misc.language.serverLanguageChanged", "`${localeId}`"], "\uD83C\uDFA4")
-	}
+        if (hasPersonalLanguage) message.addReaction(resetPersonalLanguageEmote).queue()
+    }
 
-	private suspend fun buildLanguageEmbed(locale: BaseLocale, languages: List<LocaleWrapper>, isPrivateChannel: Boolean): MessageEmbed {
-		val embed = EmbedBuilder()
-		embed.setColor(Color(0, 193, 223))
-		embed.setTitle("\uD83C\uDF0E " + locale["commands.misc.language.pleaseSelectYourLanguage"])
-		if (isPrivateChannel) {
-			embed.setDescription(locale["commands.misc.language.changeLanguageDescription"])
-		} else {
-			embed.setDescription(locale["commands.misc.language.changeServerLanguageDescription"])
-		}
+    private suspend fun activateLanguage(context: CommandContext, profile: Profile, newLanguage: LocaleWrapper, isPrivateChannel: Boolean = false) {
+        var localeId = newLanguage.locale.id
 
-		for (wrapper in languages) {
-			val translators = wrapper.locale.getList("loritta.translationAuthors").mapNotNull { lorittaShards.retrieveUserInfoById(it.toLong()) }
-			
-      embed.addField(
-					wrapper.emoteName + " " + wrapper.name,
-					"**${locale["commands.misc.language.translatedBy"]}:** ${translators.joinToString(transform = { "`${it.name}`" })}",
-					true
-			)
-		}
-		return embed.build()
-	}
+        loritta.newSuspendedTransaction {
+            if (isPrivateChannel) // If command was executed in DM channel, will be set only to user
+                profile.settings.language = localeId
+            else
+                context.config.localeId = localeId
+        }
 
-	private class LocaleWrapper(
-			val name: String,
-			val locale: BaseLocale,
-			val legacyLocale: LegacyBaseLocale,
-			val emoteName: String
-	)
+        val newLocale = loritta.getLocaleById(localeId)
+        if (localeId == "default") {
+            localeId = "pt-br" // Já que nós já salvamos, vamos trocar o localeId para algo mais "decente"
+        }
+
+        if (isPrivateChannel)
+            context.reply(newLocale["commands.misc.language.languageChanged", "`${localeId}`"], "\uD83C\uDFA4")
+        else
+            context.reply(newLocale["commands.misc.language.serverLanguageChanged", "`${localeId}`"], "\uD83C\uDFA4")
+    }
+
+    private suspend fun buildLanguageEmbed(locale: BaseLocale, languages: List<LocaleWrapper>, isPrivateChannel: Boolean, hasPersonalLanguage: Boolean): MessageEmbed {
+        val embed = EmbedBuilder()
+        embed.setColor(Color(0, 193, 223))
+        embed.setTitle("\uD83C\uDF0E " + locale["commands.misc.language.pleaseSelectYourLanguage"])
+
+        if (isPrivateChannel) {
+            embed.setDescription(locale["commands.misc.language.changeLanguageDescription"])
+        } else {
+            embed.setDescription(locale["commands.misc.language.changeServerLanguageDescription"])
+            embed.setFooter(locale["commands.misc.language.personalLanguageTip"])
+        }
+
+        if (hasPersonalLanguage)
+            embed.setFooter(locale["commands.misc.language.personalLanguageRemovalTip", resetPersonalLanguageEmote])
+
+        for (wrapper in languages) {
+            val translators = wrapper.locale.getList("loritta.translationAuthors").mapNotNull { lorittaShards.retrieveUserInfoById(it.toLong()) }
+
+            embed.addField(
+                    wrapper.emoteName + " " + wrapper.name,
+                    "**${locale["commands.misc.language.translatedBy"]}:** ${translators.joinToString(transform = { "`${it.name}`" })}",
+                    true
+            )
+        }
+        return embed.build()
+    }
+
+    private class LocaleWrapper(
+            val name: String,
+            val locale: BaseLocale,
+            val legacyLocale: LegacyBaseLocale,
+            val emoteName: String
+    )
 }
