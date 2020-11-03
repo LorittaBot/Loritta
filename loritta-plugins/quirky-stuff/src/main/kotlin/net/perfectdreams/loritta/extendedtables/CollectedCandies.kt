3aab0d3cb3d6e8c39720391d5881a892e4ee1e7f
MrPowerGamerBR <git@mrpowergamerbr.com>
2020-11-03 20:56:10 -0300

    Yet another thing that should be index, but isn't
    Big W for performance improvements

--- loritta-plugins/quirky-stuff/src/main/kotlin/net/perfectdreams/loritta/extendedtables/CollectedCandies.kt
+++ loritta-plugins/quirky-stuff/src/main/kotlin/net/perfectdreams/loritta/extendedtables/CollectedCandies.kt
@@ -4,7 +4,7 @@ import com.mrpowergamerbr.loritta.tables.Profiles
 import org.jetbrains.exposed.dao.id.LongIdTable
 
 object CollectedCandies : LongIdTable() {
-	val user = reference("user", Profiles)
+	val user = reference("user", Profiles).index()
 	val guildId = long("guild")
 	val channelId = long("channel")
 	val messageId = long("message")
