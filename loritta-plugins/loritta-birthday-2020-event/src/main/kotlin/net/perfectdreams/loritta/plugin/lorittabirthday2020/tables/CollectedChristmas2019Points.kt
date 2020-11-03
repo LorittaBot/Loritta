7867d6b5770bba4ead8109bdbc7fc18eee5b7fd0
MrPowerGamerBR <git@mrpowergamerbr.com>
2020-11-03 19:55:41 -0300

    Add index to the CollectedChristmas2019Points#user field
    This is using a lot of time to query that table when someone uses "+profile", probably because there isn't any index on it

--- loritta-plugins/loritta-birthday-2020-event/src/main/kotlin/net/perfectdreams/loritta/plugin/lorittabirthday2020/tables/CollectedChristmas2019Points.kt
+++ loritta-plugins/loritta-birthday-2020-event/src/main/kotlin/net/perfectdreams/loritta/plugin/lorittabirthday2020/tables/CollectedChristmas2019Points.kt
@@ -4,7 +4,7 @@ import com.mrpowergamerbr.loritta.tables.Profiles
 import org.jetbrains.exposed.dao.id.LongIdTable
 
 object CollectedChristmas2019Points : LongIdTable() {
-	val user = reference("user", Profiles)
+	val user = reference("user", Profiles).index()
 	val guildId = long("guild")
 	val channelId = long("channel")
 	val messageId = long("message")
