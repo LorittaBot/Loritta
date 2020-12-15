6c0b4217d0530029a7f920791bd37c76c4d510e4
iiAmFilippi <68162426+iiAmFilippi@users.noreply.github.com>
2020-12-15 13:07:20 -0300

    Send DM when divorce (#2292)
    Fixes #2289
    
    * Send DM when divorce
    
    * Added an embed in place of the old message
    
    * Fixed somethings
    
    Fixed a few things that Gaabriel suggested.
    
    * whoops
    
    * Whoops² omg sorry for the problems

--- loritta-discord/src/main/java/com/mrpowergamerbr/loritta/commands/vanilla/social/DivorceCommand.kt
+++ loritta-discord/src/main/java/com/mrpowergamerbr/loritta/commands/vanilla/social/DivorceCommand.kt
@@ -4,12 +4,16 @@ import com.mrpowergamerbr.loritta.commands.AbstractCommand
 import com.mrpowergamerbr.loritta.commands.CommandContext
 import com.mrpowergamerbr.loritta.tables.Profiles
 import com.mrpowergamerbr.loritta.utils.Constants
+import com.mrpowergamerbr.loritta.utils.extensions.await
 import net.perfectdreams.loritta.api.messages.LorittaReply
 import com.mrpowergamerbr.loritta.utils.extensions.isEmote
 import com.mrpowergamerbr.loritta.utils.locale.BaseLocale
 import com.mrpowergamerbr.loritta.utils.loritta
+import com.mrpowergamerbr.loritta.utils.lorittaShards
 import com.mrpowergamerbr.loritta.utils.onReactionAddByAuthor
+import net.dv8tion.jda.api.EmbedBuilder
 import net.perfectdreams.loritta.api.commands.CommandCategory
+import net.perfectdreams.loritta.profile.ProfileUtils 
 import net.perfectdreams.loritta.utils.Emotes
 import org.jetbrains.exposed.sql.update
 
@@ -17,6 +21,7 @@ class DivorceCommand : AbstractCommand("divorce", listOf("divorciar"), CommandCa
 	companion object {
 	    const val LOCALE_PREFIX = "commands.social.divorce"
 		const val DIVORCE_REACTION_EMOJI = "\uD83D\uDC94"
+		const val DIVORCE_EMBED_URI = "https://cdn.discordapp.com/emojis/556524143281963008.png?size=2048"
 	}
 
 	override fun getDescription(locale: BaseLocale): String {
@@ -24,9 +29,13 @@ class DivorceCommand : AbstractCommand("divorce", listOf("divorciar"), CommandCa
 	}
 
 	override suspend fun run(context: CommandContext,locale: BaseLocale) {
-		val marriage = loritta.newSuspendedTransaction { context.lorittaUser.profile.marriage }
+		val userProfile = loritta.newSuspendedTransaction { context.lorittaUser.profile }
+		val marriage = ProfileUtils.getMarriageInfo(userProfile) ?: return
+		val userMarriage = loritta.newSuspendedTransaction { context.lorittaUser.profile.marriage }
+		val marriagePartner = marriage.partner                            
+		val user = lorittaShards.getUserById(marriagePartner.id) ?: return
 
-		if (marriage != null) {
+		if (userMarriage != null) {
 			val message = context.reply(
                     LorittaReply(
                             locale["$LOCALE_PREFIX.prepareToDivorce", Emotes.LORI_CRYING],
@@ -42,10 +51,10 @@ class DivorceCommand : AbstractCommand("divorce", listOf("divorciar"), CommandCa
 				if (it.reactionEmote.isEmote(DIVORCE_REACTION_EMOJI)) {
 					// depois
 					loritta.newSuspendedTransaction {
-						Profiles.update({ Profiles.marriage eq marriage.id }) {
+						Profiles.update({ Profiles.marriage eq userMarriage.id }) {
 							it[Profiles.marriage] = null
 						}
-						marriage.delete()
+						userMarriage.delete()
 					}
 
 					message.delete().queue()
@@ -55,6 +64,19 @@ class DivorceCommand : AbstractCommand("divorce", listOf("divorciar"), CommandCa
                                     locale["$LOCALE_PREFIX.divorced", Emotes.LORI_HUG]
                             )
 					)
+					
+					try {
+						val userPrivateChannel = user.openPrivateChannel().await() ?: return@onReactionAddByAuthor
+
+						userPrivateChannel.sendMessage(
+							EmbedBuilder()
+								.setTitle(locale["$LOCALE_PREFIX.divorcedTitle"])
+								.setDescription(locale["$LOCALE_PREFIX.divorcedDescription", context.userHandle.name])
+								.setThumbnail(DIVORCE_EMBED_URI)
+								.setColor(Constants.LORITTA_AQUA)
+								.build()
+						).queue()
+					} catch (e: Exception) {}
 				}
 			}
 
