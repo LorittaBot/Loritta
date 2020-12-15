6892037f09cf806da135d926b923db20a6bf4ae9
ArthurProgrammer <42424131+ArthurProgrammer@users.noreply.github.com>
2020-12-14 21:30:42 -0300

    Make possible to see others guild's banner via ID
    Examples: 
    `+guildbanner ID`
    `+guildbanner 297732013006389252`

--- loritta-plugins/artsy-joy-lori/src/main/kotlin/net/perfectdreams/loritta/commands/discord/GuildBannerCommand.kt
+++ loritta-plugins/artsy-joy-lori/src/main/kotlin/net/perfectdreams/loritta/commands/discord/GuildBannerCommand.kt
@@ -1,10 +1,14 @@
 package net.perfectdreams.loritta.commands.discord
 
 import com.mrpowergamerbr.loritta.utils.Constants
+import com.mrpowergamerbr.loritta.utils.isValidSnowflake
+import com.mrpowergamerbr.loritta.utils.lorittaShards
 import net.dv8tion.jda.api.EmbedBuilder
+import net.dv8tion.jda.api.entities.Guild
 import net.perfectdreams.loritta.api.commands.ArgumentType
 import net.perfectdreams.loritta.api.commands.CommandCategory
 import net.perfectdreams.loritta.api.commands.arguments
+import net.perfectdreams.loritta.api.messages.LorittaReply
 import net.perfectdreams.loritta.platform.discord.LorittaDiscord
 import net.perfectdreams.loritta.platform.discord.commands.DiscordAbstractCommandBase
 
@@ -21,14 +25,34 @@ class GuildBannerCommand(loritta: LorittaDiscord) : DiscordAbstractCommandBase(l
         canUseInPrivateChannel = false
 
         executesDiscord {
-            val discordGuild = guild
+            val context = this
+
+            var guild: Guild? = null
+
+            if (context.args.isNotEmpty()) {
+                val id = context.args.first()
+                if (id.isValidSnowflake()) {
+                    guild = lorittaShards.getGuildById(context.args[0])
+                }
+            } else {
+                guild = lorittaShards.getGuildById(context.guild.idLong)
+            }
+            // Verificar se a guild existe
+            if (guild == null) {
+                context.reply(
+                        LorittaReply(
+                                message = context.locale["commands.discord.serverinfo.unknownGuild", context.args[0]],
+                                prefix = Constants.ERROR
+                        )
+                )
+            }
 
             // Verificar se a guild tem banner
-            val guildBanner = discordGuild.bannerUrl ?: fail(locale["commands.discord.guildbanner.noBanner"])
+            val guildBanner = guild!!.bannerUrl ?: fail(locale["commands.discord.guildbanner.noBanner"])
 
             val embed = EmbedBuilder()
 
-            embed.setTitle("🖼 ${discordGuild.name}")
+            embed.setTitle("🖼 ${guild.name}")
             embed.setColor(Constants.DISCORD_BLURPLE)
             embed.setDescription(locale["loritta.clickHere", "$guildBanner?size=2048"])
             embed.setImage("$guildBanner?size=2048")
