b0e32cc2b7eaa9c8084c87da0f349bfdc4e3d59b
MrPowerGamerBR <git@mrpowergamerbr.com>
2020-12-17 15:54:17 -0300

    Add a way to query the user's donation keys

--- loritta-discord/src/main/java/net/perfectdreams/loritta/website/routes/api/v1/user/GetSelfInfoRoute.kt
+++ loritta-discord/src/main/java/net/perfectdreams/loritta/website/routes/api/v1/user/GetSelfInfoRoute.kt
@@ -7,7 +7,11 @@ import com.google.gson.JsonParser
 import com.mrpowergamerbr.loritta.Loritta
 import com.mrpowergamerbr.loritta.dao.Background
 import com.mrpowergamerbr.loritta.dao.ProfileDesign
+import com.mrpowergamerbr.loritta.dao.UserDonationKey
+import com.mrpowergamerbr.loritta.tables.DonationKeys
+import com.mrpowergamerbr.loritta.tables.UserDonationKeys
 import com.mrpowergamerbr.loritta.utils.Constants
+import com.mrpowergamerbr.loritta.utils.lorittaShards
 import com.mrpowergamerbr.loritta.utils.networkbans.ApplyBansTask
 import com.mrpowergamerbr.loritta.website.LoriWebCode
 import com.mrpowergamerbr.loritta.website.WebsiteAPIException
@@ -18,7 +22,11 @@ import kotlinx.serialization.json.Json
 import mu.KotlinLogging
 import net.perfectdreams.loritta.platform.discord.LorittaDiscord
 import net.perfectdreams.loritta.serializable.UserIdentification
-import net.perfectdreams.loritta.tables.*
+import net.perfectdreams.loritta.tables.BackgroundPayments
+import net.perfectdreams.loritta.tables.Backgrounds
+import net.perfectdreams.loritta.tables.BannedIps
+import net.perfectdreams.loritta.tables.ProfileDesigns
+import net.perfectdreams.loritta.tables.ProfileDesignsPayments
 import net.perfectdreams.loritta.website.routes.BaseRoute
 import net.perfectdreams.loritta.website.session.LorittaJsonWebSession
 import net.perfectdreams.loritta.website.utils.WebsiteUtils
@@ -26,10 +34,7 @@ import net.perfectdreams.loritta.website.utils.extensions.respondJson
 import net.perfectdreams.loritta.website.utils.extensions.trueIp
 import org.jetbrains.exposed.sql.and
 import org.jetbrains.exposed.sql.select
-import kotlin.collections.firstOrNull
-import kotlin.collections.map
 import kotlin.collections.set
-import kotlin.collections.toSet
 
 class GetSelfInfoRoute(loritta: LorittaDiscord) : BaseRoute(loritta, "/api/v1/users/@me/{sections?}") {
 	companion object {
@@ -116,6 +121,23 @@ class GetSelfInfoRoute(loritta: LorittaDiscord) : BaseRoute(loritta, "/api/v1/us
 				payload["donations"] = jsonObject(
 						"value" to loritta.getActiveMoneyFromDonationsAsync(userIdentification.id.toLong())
 				)
+			}
+
+			if ("userdonationkeys" in sections) {
+				val userDonationKeys = loritta.newSuspendedTransaction {
+					UserDonationKey.find { UserDonationKeys.userId eq userIdentification.id.toLong() and (DonationKeys.expiresAt greaterEq System.currentTimeMillis()) }
+							.toList()
+				}
+
+				payload["userDonationKeys"] = userDonationKeys.map {
+					jsonObject(
+							"id" to it.id.value,
+							"value" to it.value,
+							"expiresAt" to it.expiresAt,
+							"user" to WebsiteUtils.transformToJson(lorittaShards.retrieveUserById(it.userId)!!),
+							"activeIn" to loritta.newSuspendedTransaction { it.activeIn }
+					)
+				}.toJsonArray()
 			}
 
 			if ("settings" in sections) {
