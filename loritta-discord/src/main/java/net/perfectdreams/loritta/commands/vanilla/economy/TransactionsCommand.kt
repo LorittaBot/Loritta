f9a4b3c786a6e6704df539378481e06b3c987419
hechfx <60321993+hechfx@users.noreply.github.com>
2020-12-28 20:52:03 -0300

    Added pagination system for `+transactions`
    Based on `+fanarts` & `+atm top`

--- loritta-discord/src/main/java/net/perfectdreams/loritta/commands/vanilla/economy/TransactionsCommand.kt
+++ loritta-discord/src/main/java/net/perfectdreams/loritta/commands/vanilla/economy/TransactionsCommand.kt
@@ -1,15 +1,23 @@
 package net.perfectdreams.loritta.commands.vanilla.economy
 
 import com.mrpowergamerbr.loritta.utils.Constants
+import com.mrpowergamerbr.loritta.utils.extensions.*
+import com.mrpowergamerbr.loritta.utils.locale.BaseLocale
 import com.mrpowergamerbr.loritta.utils.lorittaShards
+import com.mrpowergamerbr.loritta.utils.onReactionAddByAuthor
 import net.dv8tion.jda.api.EmbedBuilder
+import net.dv8tion.jda.api.entities.Message
+import net.dv8tion.jda.api.entities.User
 import net.perfectdreams.loritta.api.commands.ArgumentType
 import net.perfectdreams.loritta.api.commands.CommandCategory
 import net.perfectdreams.loritta.api.commands.arguments
+import net.perfectdreams.loritta.api.messages.LorittaReply
 import net.perfectdreams.loritta.platform.discord.LorittaDiscord
 import net.perfectdreams.loritta.platform.discord.commands.DiscordAbstractCommandBase
+import net.perfectdreams.loritta.platform.discord.commands.DiscordCommandContext
 import net.perfectdreams.loritta.tables.SonhosTransaction
 import net.perfectdreams.loritta.utils.Emotes
+import net.perfectdreams.loritta.utils.RankingGenerator.isValidRankingPage
 import net.perfectdreams.loritta.utils.SonhosPaymentReason
 import org.jetbrains.exposed.sql.SortOrder
 import org.jetbrains.exposed.sql.or
@@ -18,8 +26,12 @@ import java.time.Instant
 import java.time.ZoneId
 
 class TransactionsCommand(loritta: LorittaDiscord) : DiscordAbstractCommandBase(loritta, listOf("transactions", "transações"), CommandCategory.ECONOMY) {
+	companion object {
+		private const val LOCALE_PREFIX = "commands.economy.transactions"
+	}
+
 	override fun command() = create {
-		localizedDescription("commands.economy.transactions.description")
+		localizedDescription("$LOCALE_PREFIX.description")
 
 		arguments {
 			argument(ArgumentType.USER) {
@@ -30,94 +42,190 @@ class TransactionsCommand(loritta: LorittaDiscord) : DiscordAbstractCommandBase(
 		executesDiscord {
 			val user = user(0)?.handle ?: user
 
-			val transactions = loritta.newSuspendedTransaction {
+			var customPage = args.getOrNull(1)?.toLongOrNull() ?: args.getOrNull(0)?.toLongOrNull()
+
+			if (customPage != null && !isValidRankingPage(customPage)) {
+				reply(
+						LorittaReply(
+								locale["$LOCALE_PREFIX.pageDoesNotExist"],
+								Constants.ERROR
+						)
+				)
+				return@executesDiscord
+			}
+
+			if (customPage == null) {
+				customPage = 0
+			} else {
+				customPage -= 1
+			}
+
+			val allTransactions = loritta.newSuspendedTransaction {
 				SonhosTransaction.select {
 					SonhosTransaction.givenBy eq user.idLong or (SonhosTransaction.receivedBy eq user.idLong)
 				}.orderBy(SonhosTransaction.givenAt, SortOrder.DESC)
-						.limit(20)
-						.toMutableList()
+						.toList()
 			}
 
-			val description = buildString {
-				for (transaction in transactions) {
-					val receivedSonhos = transaction[SonhosTransaction.receivedBy] == user.idLong
-
-					val givenAtTime = Instant.ofEpochMilli(transaction[SonhosTransaction.givenAt])
-							.atZone(ZoneId.systemDefault())
-
-					val day = givenAtTime.dayOfMonth.toString().padStart(2, '0')
-					val month = givenAtTime.monthValue.toString().padStart(2, '0')
-					val year = givenAtTime.year
-
-					val hour = givenAtTime.hour.toString().padStart(2, '0')
-					val minute = givenAtTime.minute.toString().padStart(2, '0')
-
-					this.append("`[$day/$month/$year $hour:$minute]` ")
-
-					val emoji = if (receivedSonhos)
-						"\uD83D\uDCB5"
-					else
-						"\uD83D\uDCB8"
-
-					this.append(emoji)
-
-					this.append(" ")
-
-					if (transaction[SonhosTransaction.reason] == SonhosPaymentReason.PAYMENT) {
-						val receivedByUserId = if (receivedSonhos) {
-							transaction[SonhosTransaction.givenBy]
-						} else {
-							transaction[SonhosTransaction.receivedBy]
-						}
-
-						val receivedByUser = lorittaShards.retrieveUserInfoById(receivedByUserId)
-
-						val name = ("${receivedByUser?.name}#${receivedByUser?.discriminator} ($receivedByUserId)")
-
-						if (receivedSonhos) {
-							this.append(locale["commands.economy.transactions.receivedMoneySonhos", transaction[SonhosTransaction.quantity], "`$name`"])
-						} else {
-							this.append(locale["commands.economy.transactions.sentMoneySonhos", transaction[SonhosTransaction.quantity], "`$name`"])
-						}
-					} else if (transaction[SonhosTransaction.reason] == SonhosPaymentReason.PAYMENT_TAX) {
-						this.append(locale["commands.economy.transactions.sentMoneySonhosTax", transaction[SonhosTransaction.quantity]])
-					} else {
-						val type = transaction[SonhosTransaction.reason].name
-								.toLowerCase()
-								.replace("_", " ")
-								.split(" ")
-								.map {
-									it.capitalize()
-								}
-								.joinToString("")
-								.toCharArray().apply {
-									this[0] = this[0].toLowerCase()
-								}
-								.joinToString("")
-
-						val genericTypeName = locale["commands.economy.transactions.types.${type}"]
-
-						if (receivedSonhos)
-							this.append(locale["commands.economy.transactions.genericReceived", transaction[SonhosTransaction.quantity], genericTypeName])
-						else
-							this.append(locale["commands.economy.transactions.genericSent", transaction[SonhosTransaction.quantity], genericTypeName])
-					}
-					this.append("\n")
-				}
+			if (allTransactions.isEmpty()) {
+				reply(
+						LorittaReply(
+								locale["$LOCALE_PREFIX.unknownTransactions"]
+						)
+				)
+				return@executesDiscord
 			}
 
-			val embed = EmbedBuilder()
-					.setTitle(
-							"${Emotes.LORI_RICH} " +
-									if (user != this.user)
-										locale["commands.economy.transactions.otherUserTransactions", user.asTag]
-									else
-										locale["commands.economy.transactions.title"]
-					)
-					.setColor(Constants.LORITTA_AQUA)
-					.setDescription(description)
-
-			sendMessage(getUserMention(true), embed.build())
+			sendTransactionEmbed(
+					this,
+					locale,
+					customPage,
+					null,
+					user,
+			)
 		}
+	}
+
+	suspend fun sendTransactionEmbed(context: DiscordCommandContext, locale: BaseLocale, item: Long?, currentMessage: Message?, user: User) {
+
+		var page = item
+
+		if (page == null) page = 0
+
+		val transactions = loritta.newSuspendedTransaction {
+			SonhosTransaction.select {
+				SonhosTransaction.givenBy eq user.idLong or (SonhosTransaction.receivedBy eq user.idLong)
+			}.orderBy(SonhosTransaction.givenAt, SortOrder.DESC)
+					.limit(20, page * 20)
+					.toList()
+		}
+
+		val allTransactions = loritta.newSuspendedTransaction {
+			SonhosTransaction.select {
+				SonhosTransaction.givenBy eq user.idLong or (SonhosTransaction.receivedBy eq user.idLong)
+			}.orderBy(SonhosTransaction.givenAt, SortOrder.DESC)
+					.toList()
+		}
+
+		val description = buildString {
+			for (transaction in transactions) {
+				val receivedSonhos = transaction[SonhosTransaction.receivedBy] == user.idLong
+
+				val givenAtTime = Instant.ofEpochMilli(transaction[SonhosTransaction.givenAt])
+						.atZone(ZoneId.systemDefault())
+
+				val day = givenAtTime.dayOfMonth.toString().padStart(2, '0')
+				val month = givenAtTime.monthValue.toString().padStart(2, '0')
+				val year = givenAtTime.year
+
+				val hour = givenAtTime.hour.toString().padStart(2, '0')
+				val minute = givenAtTime.minute.toString().padStart(2, '0')
+
+				this.append("`[$day/$month/$year $hour:$minute]` ")
+
+				val emoji = if (receivedSonhos)
+					"\uD83D\uDCB5"
+				else
+					"\uD83D\uDCB8"
+
+				this.append(emoji)
+
+				this.append(" ")
+
+				if (transaction[SonhosTransaction.reason] == SonhosPaymentReason.PAYMENT) {
+					val receivedByUserId = if (receivedSonhos) {
+						transaction[SonhosTransaction.givenBy]
+					} else {
+						transaction[SonhosTransaction.receivedBy]
+					}
+
+					val receivedByUser = lorittaShards.retrieveUserInfoById(receivedByUserId)
+
+					val name = ("${receivedByUser?.name}#${receivedByUser?.discriminator} ($receivedByUserId)")
+
+					if (receivedSonhos) {
+						this.append(locale["$LOCALE_PREFIX.receivedMoneySonhos", transaction[SonhosTransaction.quantity], "`$name`"])
+					} else {
+						this.append(locale["$LOCALE_PREFIX.sentMoneySonhos", transaction[SonhosTransaction.quantity], "`$name`"])
+					}
+				} else if (transaction[SonhosTransaction.reason] == SonhosPaymentReason.PAYMENT_TAX) {
+					this.append(locale["$LOCALE_PREFIX.sentMoneySonhosTax", transaction[SonhosTransaction.quantity]])
+				} else {
+					val type = transaction[SonhosTransaction.reason].name
+							.toLowerCase()
+							.replace("_", " ")
+							.split(" ")
+							.map {
+								it.capitalize()
+							}
+							.joinToString("")
+							.toCharArray().apply {
+								this[0] = this[0].toLowerCase()
+							}
+							.joinToString("")
+
+					val genericTypeName = locale["$LOCALE_PREFIX.types.${type}"]
+
+					if (receivedSonhos)
+						this.append(locale["$LOCALE_PREFIX.genericReceived", transaction[SonhosTransaction.quantity], genericTypeName])
+					else
+						this.append(locale["$LOCALE_PREFIX.genericSent", transaction[SonhosTransaction.quantity], genericTypeName])
+				}
+				this.append("\n")
+			}
+		}
+
+		val embed = EmbedBuilder().apply {
+			setTitle(
+					"${Emotes.LORI_RICH} " +
+							if (user != user)
+								"${locale["$LOCALE_PREFIX.otherUserTransactions", user.asTag]} — ${locale["$LOCALE_PREFIX.page"]} $page"
+							else
+								"${locale["$LOCALE_PREFIX.title"]} — ${locale["$LOCALE_PREFIX.page"]} ${page + 1}"
+			)
+			setColor(Constants.LORITTA_AQUA)
+			setDescription(description)
+			setFooter("${locale["$LOCALE_PREFIX.totalTransactions"]}: ${allTransactions.count()}")
+		}
+
+		var message = currentMessage?.edit(context.getUserMention(true), embed.build(), clearReactions = false) ?: context.sendMessage(context.getUserMention(true), embed.build())
+
+		val allowForward = allTransactions.count() >= (page + 1) * 20
+		val allowBack = page != 0L
+
+		if ((!allowForward && message.reactions.any { it.reactionEmote.isEmote("⏩") }) || (!allowBack && message.reactions.any { it.reactionEmote.isEmote("⏪") })) {
+			message.clearReactions().await()
+			message = message.refresh().await()
+		}
+
+		message.onReactionAddByAuthor(context) {
+			if (allowForward && it.reactionEmote.isEmote("⏩")) {
+				sendTransactionEmbed(
+							context,
+							locale,
+							page + 1,
+							message,
+							user,
+				)
+			}
+			if (allowBack && it.reactionEmote.isEmote("⏪")) {
+				sendTransactionEmbed(
+							context,
+							locale,
+							page - 1,
+							message,
+							user,
+				)
+			}
+		}
+
+		val emotes = mutableListOf<String>()
+
+		if (allowBack)
+			emotes.add("⏪")
+		if (allowForward)
+			emotes.add("⏩")
+
+		message.doReactions(*emotes.toTypedArray())
 	}
 }
