76b8ba55ead3876ab63c2a5294b6fa89cc1a3590
MrPowerGamerBR <git@mrpowergamerbr.com>
2021-01-16 15:13:17 -0300

    Small and simple tool to migrate the locale keys

---
+++ loritta-discord/src/main/java/com/mrpowergamerbr/loritta/commands/MoveToLocaleKey.kt
@@ -0,0 +1,52 @@
+package com.mrpowergamerbr.loritta.commands
+
+import java.io.File
+
+fun main() {
+    analyze(
+            File("C:\\Users\\Leonardo\\Documents\\IdeaProjects\\LorittaBot\\Loritta\\loritta-discord\\src\\main\\java\\com\\mrpowergamerbr\\loritta\\commands\\vanilla")
+                    .listFiles()
+    )
+}
+
+fun analyze(files: Array<File>) {
+    for (file in files) {
+        if (file.isDirectory) {
+            analyze(file.listFiles())
+        } else if (file.extension == "kt") {
+            val lines = file.readLines()
+
+            val indexOfLine = lines.indexOfFirst { it.contains("getDescription(") && it.contains("BaseLocale") }
+            if (indexOfLine != -1) {
+                println(file)
+
+                // Find the description if possible
+                val regex = Regex("return locale\\[\"([A-z0-9\\.]+)\\\"]")
+                val probablyTheLocale = (lines[indexOfLine + 1])
+                val probablyTheEndOfTheBlock = (lines[indexOfLine + 2])
+
+                println(probablyTheLocale)
+                println(probablyTheEndOfTheBlock)
+
+                val findMatch = regex.find(probablyTheLocale)
+
+                if (findMatch != null && probablyTheEndOfTheBlock == "\t}") {
+                    println(findMatch.groupValues)
+
+                    // And, if it is found, try to rewrite the file with the new values
+                    val newContent = lines.toMutableList()
+                    // Remove three lines
+                    newContent.removeAt(indexOfLine)
+                    newContent.removeAt(indexOfLine)
+                    newContent.removeAt(indexOfLine)
+
+                    newContent.add(indexOfLine, "\toverride fun getDescriptionKey() = LocaleKeyData(\"${findMatch.groupValues[1]}\")")
+
+                    println(newContent.joinToString("\n"))
+
+                    file.writeText(newContent.joinToString("\n"))
+                }
+            }
+        }
+    }
+}
--- loritta-discord/src/main/java/com/mrpowergamerbr/loritta/commands/vanilla/administration/BanCommand.kt
+++ loritta-discord/src/main/java/com/mrpowergamerbr/loritta/commands/vanilla/administration/BanCommand.kt
@@ -6,6 +6,7 @@ import com.mrpowergamerbr.loritta.utils.MessageUtils
 import com.mrpowergamerbr.loritta.utils.extensions.isEmote
 import com.mrpowergamerbr.loritta.utils.extensions.retrieveMemberOrNull
 import com.mrpowergamerbr.loritta.utils.locale.BaseLocale
+import com.mrpowergamerbr.loritta.utils.locale.LocaleKeyData
 import com.mrpowergamerbr.loritta.utils.onReactionAddByAuthor
 import net.dv8tion.jda.api.Permission
 import net.dv8tion.jda.api.entities.Guild
@@ -18,9 +19,7 @@ import net.perfectdreams.loritta.api.commands.arguments
 import net.perfectdreams.loritta.utils.PunishmentAction
 
 class BanCommand : AbstractCommand("ban", listOf("banir", "hackban", "forceban"), CommandCategory.ADMIN) {
-	override fun getDescription(locale: BaseLocale): String {
-		return locale["commands.moderation.ban.description"]
-	}
+	override fun getDescriptionKey() = LocaleKeyData("commands.moderation.ban.description")
 
 	override fun getUsage(): CommandArguments {
 		return arguments {
