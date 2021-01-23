01445efa77aac180b16010b4167d3efef5b79a4f
MrPowerGamerBR <git@mrpowergamerbr.com>
2021-01-23 18:28:00 -0300

    Thanks IDEA very cool

--- loritta-plugins/helping-hands/src/main/kotlin/net/perfectdreams/loritta/plugin/helpinghands/HelpingHandsPlugin.kt
+++ loritta-plugins/helping-hands/src/main/kotlin/net/perfectdreams/loritta/plugin/helpinghands/HelpingHandsPlugin.kt
@@ -2,7 +2,13 @@ package net.perfectdreams.loritta.plugin.helpinghands
 
 import net.perfectdreams.loritta.platform.discord.LorittaDiscord
 import net.perfectdreams.loritta.platform.discord.plugin.LorittaDiscordPlugin
-import net.perfectdreams.loritta.plugin.helpinghands.commands.*
+import net.perfectdreams.loritta.plugin.helpinghands.commands.CoinFlipBetCommand
+import net.perfectdreams.loritta.plugin.helpinghands.commands.CoinFlipBetStatsCommand
+import net.perfectdreams.loritta.plugin.helpinghands.commands.DailyInactivityTaxExecutor
+import net.perfectdreams.loritta.plugin.helpinghands.commands.EmojiFightBetCommand
+import net.perfectdreams.loritta.plugin.helpinghands.commands.EmojiFightCommand
+import net.perfectdreams.loritta.plugin.helpinghands.commands.GuessNumberCommand
+import net.perfectdreams.loritta.plugin.helpinghands.commands.RepListCommand
 import net.perfectdreams.loritta.plugin.helpinghands.utils.DailyInactivityTaxUtils
 
 class HelpingHandsPlugin(name: String, loritta: LorittaDiscord) : LorittaDiscordPlugin(name, loritta) {
