6157b8060cbe06185df91a5d17168d62b62edd6f
MrPowerGamerBR <git@mrpowergamerbr.com>
2021-02-07 09:01:20 -0300

    Nice typo

--- loritta-discord/src/main/java/com/mrpowergamerbr/loritta/commands/vanilla/images/LavaReversoCommand.kt
+++ loritta-discord/src/main/java/com/mrpowergamerbr/loritta/commands/vanilla/images/LavaReversoCommand.kt
@@ -22,7 +22,7 @@ import java.io.File
 
 class LavaReversoCommand : AbstractCommand("lavareverse", listOf("lavareverso", "reverselava"), CommandCategory.IMAGES) {
 	override fun getDescriptionKey() = LocaleKeyData("commands.command.reverselava.description")
-	override fun getExamplesKey() = LocaleKeyData("commands.command.reverselavaqq\\.examples")
+	override fun getExamplesKey() = LocaleKeyData("commands.command.reverselava.examples")
 
 	override fun getUsage() = arguments {
 		argument(ArgumentType.IMAGE) {}
