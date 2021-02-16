3191c13b31c9a2a4edfd4238ba48f92a4c0f9d06
MrPowerGamerBR <git@mrpowergamerbr.com>
2021-02-16 14:45:12 -0300

    Add taking sonhos from users that did a chargeback

--- loritta-discord/src/main/java/net/perfectdreams/loritta/website/routes/api/v1/callbacks/PostPerfectPaymentsCallbackRoute.kt
+++ loritta-discord/src/main/java/net/perfectdreams/loritta/website/routes/api/v1/callbacks/PostPerfectPaymentsCallbackRoute.kt
@@ -1,6 +1,7 @@
 package net.perfectdreams.loritta.website.routes.api.v1.callbacks
 
 import com.github.salomonbrys.kotson.*
+import com.google.gson.JsonObject
 import com.google.gson.JsonParser
 import com.mrpowergamerbr.loritta.Loritta
 import com.mrpowergamerbr.loritta.dao.DonationKey
@@ -11,6 +12,10 @@ import io.ktor.application.*
 import io.ktor.http.*
 import io.ktor.request.*
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import mu.KotlinLogging
 import net.perfectdreams.loritta.dao.Payment
@@ -18,16 +23,29 @@ import net.perfectdreams.loritta.platform.discord.LorittaDiscord
 import net.perfectdreams.loritta.tables.BannedUsers
 import net.perfectdreams.loritta.tables.Payments
 import net.perfectdreams.loritta.tables.SonhosBundles
+import net.perfectdreams.loritta.utils.PaymentUtils
 import net.perfectdreams.loritta.utils.payments.PaymentReason
-import net.perfectdreams.sequins.ktor.BaseRoute
 import net.perfectdreams.loritta.website.utils.extensions.respondJson
+import net.perfectdreams.sequins.ktor.BaseRoute
 import org.jetbrains.exposed.sql.*
 import java.util.*
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.atomic.AtomicLong
 
 class PostPerfectPaymentsCallbackRoute(val loritta: LorittaDiscord) : BaseRoute("/api/v1/callbacks/perfect-payments") {
 	companion object {
 		private val logger = KotlinLogging.logger {}
 	}
+
+	/**
+	 * Stores the chargeback quantity made by the user
+	 */
+	private val chargebackedQuantity = ConcurrentHashMap<Long, AtomicLong>()
+
+	/**
+	 * Stores the current active job of the chargeback process, used for cancellation
+	 */
+	private val jobs = ConcurrentHashMap<Long, Job>()
 
 	override suspend fun onRequest(call: ApplicationCall) {
 		loritta as Loritta
@@ -65,6 +83,61 @@ class PostPerfectPaymentsCallbackRoute(val loritta: LorittaDiscord) : BaseRoute(
 			// User charged back the payment, let's ban him!
 			logger.warn { "User ${internalPayment.userId} charged back the payment! Let's ban him >:(" }
 
+			val metadata = internalPayment.metadata
+			if (metadata != null) {
+				if (internalPayment.reason == PaymentReason.SONHOS_BUNDLE) {
+					val metadataAsObj = metadata.obj
+
+					val bundleType = metadataAsObj["bundleType"].nullString
+
+					if (bundleType == "dreams") {
+						// LORI-BUNDLE-InternalTransactionId
+						val bundle = retrieveSonhosBundleFromMetadata(metadataAsObj)
+
+						if (bundle != null) {
+							// If it is a sonhos bundle, we need to remove all the sonhos from the bundle to the user
+							// We are going to execute it in a separate task, to avoid hanging the request!
+							//
+							// However we need to keep in mind that users can buy multiple sonhos packages and then charging back all at once, so we need to have a delay waiting to get all the quantity and then process the sonhos removal.
+							jobs[internalPayment.userId]?.cancel()
+
+							// We use a AtomicLong to avoid concurrency issues
+							chargebackedQuantity[internalPayment.userId] = chargebackedQuantity.getOrPut(internalPayment.userId) { AtomicLong() }
+									.also { it.addAndGet(bundle[SonhosBundles.sonhos]) }
+
+							jobs[internalPayment.userId] = GlobalScope.launch(loritta.coroutineDispatcher) {
+								delay(300_000L) // Five minutes
+
+								// And then dispatch to a separate job, just to avoid any other cancellations causing issues after we already started removing the sonhos
+								GlobalScope.launch(loritta.coroutineDispatcher) {
+									val quantity = chargebackedQuantity[internalPayment.userId]
+											?.get()
+
+									chargebackedQuantity.remove(internalPayment.userId)
+
+									if (quantity != null) {
+										logger.info { "Starting a chargeback sonhos job for ${internalPayment.userId}, chargeback quantity: $quantity" }
+
+										PaymentUtils.removeSonhosDueToChargeback(
+												loritta,
+												internalPayment.userId,
+												quantity,
+												removeSonhos = true,
+												notifyChargebackUser = false,
+												notifyUsers = true
+										)
+									} else {
+										logger.warn { "Tried starting a chargeback sonhos job for ${internalPayment.userId}, but the chargeback quantity is null!" }
+									}
+								}
+							}
+						} else {
+							logger.warn { "PerfectPayments Payment with Reference ID: $referenceId ($internalTransactionId) does not have a valid bundle! We are still going to ban the user due to chargeback..." }
+						}
+					}
+				}
+			}
+
 			loritta.newSuspendedTransaction {
 				BannedUsers.insert {
 					it[userId] = internalPayment.userId
@@ -97,16 +170,9 @@ class PostPerfectPaymentsCallbackRoute(val loritta: LorittaDiscord) : BaseRoute(
 
 				if (bundleType == "dreams") {
 					// LORI-BUNDLE-InternalTransactionId
-					val bundleId = metadataAsObj["bundleId"].long
+					val bundle = retrieveSonhosBundleFromMetadata(metadataAsObj)
 
-					val bundle = loritta.newSuspendedTransaction {
-						SonhosBundles.select {
-							// Before we checked if the bundle was active, but what if we want to add new bundles while taking out old bundles?
-							// If someone bought and the bundle was deactivated, when the payment was approved, the user wouldn't receive the bundle!
-							// So we don't care if the bundle is not active anymore.
-							SonhosBundles.id eq bundleId
-						}.firstOrNull()
-					} ?: run {
+					if (bundle == null) {
 						logger.warn { "PerfectPayments Payment with Reference ID: $referenceId ($internalTransactionId) does not have a valid bundle!" }
 						call.respondJson(jsonObject())
 						return
@@ -172,5 +238,29 @@ class PostPerfectPaymentsCallbackRoute(val loritta: LorittaDiscord) : BaseRoute(
 		}
 
 		call.respondJson(jsonObject())
+	}
+
+	private suspend fun retrieveSonhosBundleFromMetadata(metadata: JsonObject): ResultRow? {
+		val metadataAsObj = metadata.obj
+
+		val bundleType = metadataAsObj["bundleType"].nullString
+
+		if (bundleType == "dreams") {
+			// LORI-BUNDLE-InternalTransactionId
+			val bundleId = metadataAsObj["bundleId"].long
+
+			val bundle = loritta.newSuspendedTransaction {
+				SonhosBundles.select {
+					// Before we checked if the bundle was active, but what if we want to add new bundles while taking out old bundles?
+					// If someone bought and the bundle was deactivated, when the payment was approved, the user wouldn't receive the bundle!
+					// So we don't care if the bundle is not active anymore.
+					SonhosBundles.id eq bundleId
+				}.firstOrNull()
+			} ?: return null
+
+			return bundle
+		}
+
+		return null
 	}
 }
