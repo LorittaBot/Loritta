8f29b8d84d6559baad403d83962a8e09fe9108af
MrPowerGamerBR <git@mrpowergamerbr.com>
2021-03-29 18:25:22 -0300

    Add "/localedebug"

--- loritta-interactions/src/main/kotlin/net/perfectdreams/loritta/interactions/LorittaInteractions.kt
+++ loritta-interactions/src/main/kotlin/net/perfectdreams/loritta/interactions/LorittaInteractions.kt
@@ -4,8 +4,11 @@ import dev.kord.common.entity.Snowflake
 import kotlinx.coroutines.runBlocking
 import net.perfectdreams.discordinteraktions.InteractionsServer
 import net.perfectdreams.loritta.interactions.commands.vanilla.AnagramCommand
+import net.perfectdreams.loritta.interactions.commands.vanilla.LocaleDebugCommand
 import net.perfectdreams.loritta.interactions.commands.vanilla.PingCommand
 import net.perfectdreams.loritta.interactions.utils.config.DiscordConfig
+import net.perfectdreams.loritta.utils.locale.LocaleManager
+import java.io.File
 
 class LorittaInteractions(val discordConfig: DiscordConfig) {
     val server = InteractionsServer(
@@ -13,12 +16,16 @@ class LorittaInteractions(val discordConfig: DiscordConfig) {
         discordConfig.publicKey,
         discordConfig.token
     )
+    val localeManager = LocaleManager(File("L:\\RandomProjects\\LorittaInteractions\\locales"))
 
     fun start() {
+        localeManager.loadLocales()
+
         runBlocking {
             server.commandManager.registerAll(
                 PingCommand(),
-                AnagramCommand()
+                AnagramCommand(),
+                LocaleDebugCommand(this@LorittaInteractions)
             )
 
             server.commandManager.updateAllCommandsInGuild(
