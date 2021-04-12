f234a44b31bd0d4e1112b689b583f7918b322deb
MrPowerGamerBR <git@mrpowergamerbr.com>
2021-04-12 13:58:27 -0300

    Add BaseLocale and LorittaBot classes, add unit tests for BaseLocale

---
+++ common/src/commonMain/kotlin/net/perfectdreams/loritta/common/LorittaBot.kt
@@ -0,0 +1,9 @@
+package net.perfectdreams.loritta.common
+
+/**
+ * Represents a Loritta Morenitta implementation.
+ *
+ * This should be extended by plataform specific Lori's :3
+ */
+abstract class LorittaBot {
+}
