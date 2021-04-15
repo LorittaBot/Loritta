bcad8dd494cebd6050d23957490e878e27a07503
MrPowerGamerBR <git@mrpowergamerbr.com>
2021-04-15 15:47:54 -0300

    Add file upload support, add Mania Title Card

--- common/src/commonMain/kotlin/net/perfectdreams/loritta/common/entities/LorittaMessage.kt
+++ common/src/commonMain/kotlin/net/perfectdreams/loritta/common/entities/LorittaMessage.kt
@@ -2,5 +2,6 @@ package net.perfectdreams.loritta.common.entities
 
 class LorittaMessage(
     val content: String,
-    val embed: LorittaEmbed?
+    val embed: LorittaEmbed?,
+    val files: Map<String, ByteArray>
 )
