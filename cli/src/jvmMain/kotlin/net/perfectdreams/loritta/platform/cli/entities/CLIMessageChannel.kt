47501d62429acf8369a416ec1411431d0354107d
Gabriel <gabriel@lunala.xyz>
2021-04-15 15:01:16 -0300

    Add support to embeds to CLI and Gateway modules (#2394)
    * Add support to embeds to CLI and Gateway modules
    
    * Add an embed builder on common
    
    * Fix bugs and implement builder on ayaya command
    
    * Solve conflicts
    
    Co-authored-by: xGabrielDEV <58668092+xGabrielDEV@users.noreply.github.com>

--- cli/src/jvmMain/kotlin/net/perfectdreams/loritta/platform/cli/entities/CLIMessageChannel.kt
+++ cli/src/jvmMain/kotlin/net/perfectdreams/loritta/platform/cli/entities/CLIMessageChannel.kt
@@ -1,11 +1,22 @@
 package net.perfectdreams.loritta.platform.cli.entities
 
 import net.perfectdreams.loritta.common.builder.MessageBuilder
+import net.perfectdreams.loritta.common.entities.LorittaEmbed
 import net.perfectdreams.loritta.common.entities.LorittaMessage
 import net.perfectdreams.loritta.common.entities.MessageChannel
 
 class CLIMessageChannel : MessageChannel {
     override suspend fun sendMessage(message: LorittaMessage) {
         println(message.content)
+        println(translateEmbed(message.embed ?: return))
+    }
+
+    private fun translateEmbed(embed: LorittaEmbed): String = buildString {
+        append("Title: ${embed.title}\n")
+        append("Description: ${embed.description}\n")
+
+        embed.fields.forEach {
+            append("${it.name}: ${it.value}\n")
+        }
     }
 }
--- common/src/commonMain/kotlin/net/perfectdreams/loritta/common/entities/LorittaMessage.kt
+++ common/src/commonMain/kotlin/net/perfectdreams/loritta/common/entities/LorittaMessage.kt
@@ -1,5 +1,6 @@
 package net.perfectdreams.loritta.common.entities
 
 class LorittaMessage(
-    val content: String
+    val content: String,
+    val embed: LorittaEmbed?
 )
