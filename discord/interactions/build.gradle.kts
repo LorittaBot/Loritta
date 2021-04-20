ef801417bf7ae887c6fe99d3b9f6e62255bfdd21
MrPowerGamerBR <git@mrpowergamerbr.com>
2021-04-20 09:46:25 -0300

    Track metrics with Prometheus

--- discord/interactions/build.gradle.kts
+++ discord/interactions/build.gradle.kts
@@ -21,7 +21,21 @@ kotlin {
                 implementation(project(":common"))
                 implementation(project(":commands"))
                 implementation(project(":discord:common"))
+
+                // Sequins
+                api("net.perfectdreams.sequins.ktor:base-route:1.0.2")
+
+                // Base Route uses a more up to date impl of Ktor compared to Discord InteraKTions
+                // (because Kord Core does not work with Ktor 1.5.X yet)
+                api("io.ktor:ktor-server-core:1.4.1")
+                api("io.ktor:ktor-server-netty:1.4.1")
+
                 implementation("net.perfectdreams.discordinteraktions:core:0.0.4-SNAPSHOT")
+
+                // Prometheus
+                api("io.prometheus:simpleclient:0.10.0")
+                api("io.prometheus:simpleclient_hotspot:0.10.0")
+                api("io.prometheus:simpleclient_common:0.10.0")
             }
         }
     }
---
+++ discord/interactions/src/jvmMain/kotlin/net/perfectdreams/loritta/platform/interaktions/utils/metrics/Prometheus.kt
@@ -0,0 +1,32 @@
+package net.perfectdreams.loritta.platform.interaktions.utils.metrics
+
+import io.prometheus.client.Counter
+import io.prometheus.client.Histogram
+import java.time.Duration
+
+/**
+ * Used to hold Prometheus instrumentations
+ */
+object Prometheus {
+    val UPDATE_PERIOD = Duration.ofSeconds(5L)
+
+    val EXECUTED_COMMAND_LATENCY_COUNT = Histogram.build()
+        .name("executed_command_latency")
+        .help("Executed Command Latency")
+        .labelNames("command", "executor")
+        .buckets(0.5, 1.0, 2.0, 3.0, 4.0, 5.0, 7.5, 10.0, 15.0, 30.0)
+        .create()
+
+    val AUTOMATICALLY_DEFERRED_COUNT = Counter.build()
+        .name("automatically_deferred_count")
+        .help("Count of requests that were automatically deferred")
+        .labelNames("command", "executor")
+        .create()
+
+    fun register() {
+        JFRExports.register()
+
+        EXECUTED_COMMAND_LATENCY_COUNT.register<Counter>()
+        AUTOMATICALLY_DEFERRED_COUNT.register<Counter>()
+    }
+}
