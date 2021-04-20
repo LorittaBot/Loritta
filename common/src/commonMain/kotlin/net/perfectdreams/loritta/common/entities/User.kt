29fd474bd1504c2a46e3e1f017c7d619c039ac9b
MrPowerGamerBR <git@mrpowergamerbr.com>
2021-04-20 14:43:34 -0300

    Add user avatar support
    Inspired by Discord InteraKTions... which is inspired by Kord

--- common/src/commonMain/kotlin/net/perfectdreams/loritta/common/entities/User.kt
+++ common/src/commonMain/kotlin/net/perfectdreams/loritta/common/entities/User.kt
@@ -3,5 +3,5 @@ package net.perfectdreams.loritta.common.entities
 interface User : Mentionable {
     val id: Long
     val name: String
-    val avatarUrl: String
+    val avatar: UserAvatar
 }
--- discord/interactions/src/jvmMain/kotlin/net/perfectdreams/loritta/platform/interaktions/entities/InteraKTionsUser.kt
+++ discord/interactions/src/jvmMain/kotlin/net/perfectdreams/loritta/platform/interaktions/entities/InteraKTionsUser.kt
@@ -1,13 +1,19 @@
 package net.perfectdreams.loritta.platform.interaktions.entities
 
 import net.perfectdreams.loritta.common.entities.User
+import net.perfectdreams.loritta.discord.DiscordUserAvatar
 
 class InteraKTionsUser(val user: net.perfectdreams.discordinteraktions.api.entities.User) : User {
     override val id: Long
         get() = user.id.value
-    override val name by user::username
-    override val avatarUrl
-        get() = TODO()
+    override val name by user::user
+
+    override val avatar = DiscordUserAvatar(
+        user.id.value,
+        user.discriminator.toInt(),
+        user.avatar.avatarId
+    )
+
     override val asMention: String
         get() = "<@${id}>"
 }
