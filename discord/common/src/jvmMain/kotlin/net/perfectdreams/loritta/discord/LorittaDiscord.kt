b8df64e42e03206d9d910f53120afb60a63c383e
MrPowerGamerBR <git@mrpowergamerbr.com>
2021-04-20 19:14:13 -0300

    Refactors

--- discord/common/src/jvmMain/kotlin/net/perfectdreams/loritta/discord/LorittaDiscord.kt
+++ discord/common/src/jvmMain/kotlin/net/perfectdreams/loritta/discord/LorittaDiscord.kt
@@ -2,8 +2,11 @@ package net.perfectdreams.loritta.discord
 
 import net.perfectdreams.loritta.common.LorittaBot
 import net.perfectdreams.loritta.common.utils.config.LorittaConfig
+import net.perfectdreams.loritta.common.utils.minecraft.MinecraftMojangAPI
 
 abstract class LorittaDiscord(
     config: LorittaConfig,
     val discordConfig: LorittaDiscordConfig
-): LorittaBot(config)
+): LorittaBot(config) {
+    val mojangApi = MinecraftMojangAPI()
+}
--- discord/interactions/src/jvmMain/kotlin/net/perfectdreams/loritta/platform/interaktions/LorittaInteraKTionsLauncher.kt
+++ discord/interactions/src/jvmMain/kotlin/net/perfectdreams/loritta/platform/interaktions/LorittaInteraKTionsLauncher.kt
@@ -1,9 +1,41 @@
 package net.perfectdreams.loritta.platform.interaktions
 
+import com.typesafe.config.ConfigFactory
+import mu.KotlinLogging
+import net.perfectdreams.loritta.common.emotes.Emotes
+import net.perfectdreams.loritta.common.locale.LocaleManager
 import net.perfectdreams.loritta.common.utils.ConfigUtils
 import net.perfectdreams.loritta.discord.parseDiscordConfig
+import net.perfectdreams.loritta.platform.interaktions.emotes.DiscordEmoteManager
+import net.perfectdreams.loritta.platform.interaktions.utils.metrics.Prometheus
+import java.io.File
 
-fun main() {
-    val loritta = LorittaInteraKTions(ConfigUtils.parseConfig(), ConfigUtils.parseDiscordConfig())
-    loritta.start()
+object LorittaInteraKTionsLauncher {
+    private val logger = KotlinLogging.logger {}
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val emotes = loadEmotes()
+        logger.info { "Loaded ${emotes.size} emotes" }
+
+        Prometheus.register()
+        logger.info { "Registered Prometheus Metrics" }
+
+        val loritta = LorittaInteraKTions(
+            ConfigUtils.parseConfig(),
+            ConfigUtils.parseDiscordConfig(),
+            Emotes(DiscordEmoteManager(emotes))
+        )
+
+        loritta.start()
+    }
+
+    private fun loadEmotes(): Map<String, String> {
+        val emotesFile = File("./emotes.conf")
+        val fileConfig = ConfigFactory.parseFile(emotesFile)
+
+        return fileConfig.entrySet().map {
+            it.key to it.value.unwrapped() as String
+        }.toMap()
+    }
 }
