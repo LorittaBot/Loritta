e46b00911334ff8f7965db3ffd815f3d64ec60cb
MrPowerGamerBR <git@mrpowergamerbr.com>
2021-04-20 18:05:57 -0300

    Fix build

--- discord/interactions/src/jvmMain/kotlin/net/perfectdreams/loritta/platform/interaktions/entities/InteraKTionsUser.kt
+++ discord/interactions/src/jvmMain/kotlin/net/perfectdreams/loritta/platform/interaktions/entities/InteraKTionsUser.kt
@@ -6,7 +6,7 @@ import net.perfectdreams.loritta.discord.DiscordUserAvatar
 class InteraKTionsUser(val user: net.perfectdreams.discordinteraktions.api.entities.User) : User {
     override val id: Long
         get() = user.id.value
-    override val name by user::user
+    override val name by user::name
 
     override val avatar = DiscordUserAvatar(
         user.id.value,
