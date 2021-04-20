d861164201f9dc7bd48950e4bc7092e5e0bbe9b9
MrPowerGamerBR <git@mrpowergamerbr.com>
2021-04-19 22:32:32 -0300

    Move Discord common module to :discord:common

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,18 @@ buildscript {
 }
 
 allprojects {
-    group = "net.perfectdreams.loritta"
+    // Example:
+    // ":discord:interactions" will be transformed into "discord"
+    // We need to do this because we have the ":discord:common" module and, if you don't change the
+    // group coordinates, Gradle will detect it as a circular reference (but it isn't!)
+    val splittedPath = this.path.split(":")
+    group = if (splittedPath.size >= 3) {
+        // No need for the dot because the first entry in the "splittedPath" is a empty string
+        "net.perfectdreams.loritta${splittedPath.dropLast(1).joinToString(".")}"
+    } else {
+        "net.perfectdreams.loritta"
+    }
+
     version = Versions.LORITTA
 
     repositories {
