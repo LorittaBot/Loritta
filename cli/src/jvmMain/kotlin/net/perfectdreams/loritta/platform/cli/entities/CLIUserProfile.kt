491d9ad8938500239338b245eede945bed9f9cce
MrPowerGamerBR <git@mrpowergamerbr.com>
2021-04-26 10:13:43 -0300

    Update CLI module to remove old UserProfile impl

--- cli/src/jvmMain/kotlin/net/perfectdreams/loritta/platform/cli/entities/CLIUserProfile.kt
+++
@@ -1,8 +0,0 @@
-package net.perfectdreams.loritta.platform.cli.entities
-
-import net.perfectdreams.loritta.common.entities.UserProfile
-
-class CLIUserProfile(override val id: Long) : UserProfile {
-    override val money: Long
-        get() = TODO("Not yet implemented")
-}
