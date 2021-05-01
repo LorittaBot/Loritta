3ce1e111834f38f16133760aaf9760112736d6fa
MrPowerGamerBR <git@mrpowergamerbr.com>
2021-05-01 16:08:39 -0300

    I hope it works now, take 2

--- platforms/cli/src/jvmMain/kotlin/net/perfectdreams/loritta/platform/cli/LorittaCLILauncher.kt
+++ platforms/cli/src/jvmMain/kotlin/net/perfectdreams/loritta/platform/cli/LorittaCLILauncher.kt
@@ -1,10 +1,15 @@
 package net.perfectdreams.loritta.platform.cli
 
-import net.perfectdreams.loritta.common.LorittaBot
+import kotlinx.coroutines.runBlocking
 import net.perfectdreams.loritta.common.utils.ConfigUtils
 
-suspend fun main(args: Array<String>) {
-    val loritta = LorittaCLI(ConfigUtils.parseConfig())
-    loritta.start()
-    loritta.runArgs(args)
+object LorittaCLILauncher {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val loritta = LorittaCLI(ConfigUtils.parseConfig())
+        loritta.start()
+        runBlocking {
+            loritta.runArgs(args)
+        }
+    }
 }
--- platforms/cli/src/jvmMain/kotlin/net/perfectdreams/loritta/platform/cli/LorittaREPLLauncher.kt
+++ platforms/cli/src/jvmMain/kotlin/net/perfectdreams/loritta/platform/cli/LorittaREPLLauncher.kt
@@ -1,8 +1,14 @@
 package net.perfectdreams.loritta.platform.cli
 
+import kotlinx.coroutines.runBlocking
 import net.perfectdreams.loritta.common.utils.ConfigUtils
 
-suspend fun main() {
-    val loritta = LorittaREPL(ConfigUtils.parseConfig())
-    loritta.start()
+object LorittaREPLLauncher {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val loritta = LorittaREPL(ConfigUtils.parseConfig())
+        runBlocking {
+            loritta.start()
+        }
+    }
 }
