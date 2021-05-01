cb1d529a145ad258179b8c4d9ede859d48607914
MrPowerGamerBR <git@mrpowergamerbr.com>
2021-05-01 16:57:15 -0300

    Add more Docker containers (and I hope they work)

--- platforms/discord/interactions/build.gradle.kts
+++ platforms/discord/interactions/build.gradle.kts
@@ -1,7 +1,7 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
-    kotlin("multiplatform") version Versions.KOTLIN
+    kotlin("jvm") version Versions.KOTLIN
     kotlin("plugin.serialization") version Versions.KOTLIN
     id("com.github.johnrengelman.shadow") version "5.2.0"
 }
@@ -12,44 +12,34 @@ repositories {
     mavenLocal()
 }
 
-kotlin {
-    jvm {
-        compilations.all {
-            kotlinOptions.jvmTarget = "1.8"
-        }
-        withJava()
-    }
+dependencies {
+    implementation(project(":common"))
+    implementation(project(":commands"))
+    implementation(project(":services:pudding"))
+    implementation(project(":platforms:discord:common"))
+    implementation(project(":platforms:discord:commands"))
 
-    sourceSets {
-        commonMain {
-            dependencies {
-                implementation(project(":common"))
-                implementation(project(":commands"))
-                implementation(project(":services:pudding"))
-                implementation(project(":platforms:discord:common"))
-                implementation(project(":platforms:discord:commands"))
+    implementation("net.perfectdreams.discordinteraktions:core:0.0.4-SNAPSHOT")
 
-                implementation("net.perfectdreams.discordinteraktions:core:0.0.4-SNAPSHOT")
+    // Sequins
+    api("net.perfectdreams.sequins.ktor:base-route:1.0.2")
 
-                // Sequins
-                api("net.perfectdreams.sequins.ktor:base-route:1.0.2")
+    // Prometheus
+    api("io.prometheus:simpleclient:0.10.0")
+    api("io.prometheus:simpleclient_hotspot:0.10.0")
+    api("io.prometheus:simpleclient_common:0.10.0")
 
-                // Prometheus
-                api("io.prometheus:simpleclient:0.10.0")
-                api("io.prometheus:simpleclient_hotspot:0.10.0")
-                api("io.prometheus:simpleclient_common:0.10.0")
-            }
-        }
-        jvm().compilations["main"].defaultSourceSet {
-            dependencies {
-                implementation("dev.kord:kord-rest:0.7.x-SNAPSHOT")
-            }
-        }
+    implementation("dev.kord:kord-rest:0.7.x-SNAPSHOT")
+}
+
+tasks.withType<ShadowJar> {
+    manifest {
+        attributes["Main-Class"] = "net.perfectdreams.loritta.platform.interaktions.LorittaInteraKTionsLauncher"
     }
 }
 
-tasks.withType<ShadowJar>() {
-    manifest {
-        attributes["Main-Class"] = "net.perfectdreams.loritta.platform.interaktions.LorittaInteraKTionsLauncher"
+tasks {
+    build {
+        dependsOn(shadowJar)
     }
 }
--- platforms/twitter/build.gradle.kts
+++ platforms/twitter/build.gradle.kts
@@ -1,7 +1,7 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
-    kotlin("multiplatform") version Versions.KOTLIN
+    kotlin("jvm") version Versions.KOTLIN
     kotlin("plugin.serialization") version Versions.KOTLIN
     id("com.github.johnrengelman.shadow") version "5.2.0"
 }
@@ -12,30 +12,23 @@ repositories {
     mavenLocal()
 }
 
-kotlin {
-    jvm {
-        compilations.all {
-            kotlinOptions.jvmTarget = "1.8"
-        }
-        withJava()
-    }
+dependencies {
+    implementation(project(":common"))
+    implementation(project(":commands"))
+    implementation(project(":services:memory"))
 
-    sourceSets {
-        commonMain {
-            dependencies {
-                implementation(project(":common"))
-                implementation(project(":commands"))
-                implementation(project(":services:memory"))
-
-                implementation("blue.starry:penicillin:6.1.0")
-                implementation("io.ktor:ktor-client-cio:1.5.4")
-            }
-        }
-    }
+    implementation("blue.starry:penicillin:6.1.0")
+    implementation("io.ktor:ktor-client-cio:1.5.4")
 }
 
 tasks.withType<ShadowJar> {
     manifest {
         attributes["Main-Class"] = "net.perfectdreams.loritta.platform.twitter.LorittaTwitterLauncher"
+    }
+}
+
+tasks {
+    build {
+        dependsOn(shadowJar)
     }
 }
