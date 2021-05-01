fe26dc0e6340fe2634348409de3e1513b024c33d
MrPowerGamerBR <git@mrpowergamerbr.com>
2021-05-01 15:12:14 -0300

    More workflow and script changes

--- docker/build_and_push_all_containers.sh
+++ docker/build_and_push_all_containers.sh
@@ -1,1 +1,4 @@
+# Login into Docker Registry
+echo "${{ secrets.GITHUB_TOKEN }}" | docker login ghcr.io -u ${{ github.actor }} --password-stdin
+
 sh ${0%/*}/build_and_push_container.sh cinnamon-cli ${0%/*}/cli
