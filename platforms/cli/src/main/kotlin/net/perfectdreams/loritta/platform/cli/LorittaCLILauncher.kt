369dd55d8c6a4efc5bf75c1e00cc6bdc1199c136
MrPowerGamerBR <git@mrpowergamerbr.com>
2021-05-05 09:14:44 -0300

    Add default config copying if the config does not exist

--- platforms/cli/src/main/kotlin/net/perfectdreams/loritta/platform/cli/LorittaCLILauncher.kt
+++ platforms/cli/src/main/kotlin/net/perfectdreams/loritta/platform/cli/LorittaCLILauncher.kt
@@ -10,7 +10,7 @@ object LorittaCLILauncher {
 
     @JvmStatic
     fun main(args: Array<String>) {
-        val rootConfig = ConfigUtils.loadAndParseConfig<RootConfig>("./loritta.conf")
+        val rootConfig = ConfigUtils.loadAndParseConfigOrCopyFromJarAndExit<RootConfig>(LorittaCLI::class, "loritta.conf")
         logger.info { "Loaded Loritta's configuration file" }
 
         val loritta = LorittaCLI(rootConfig.loritta, rootConfig.services.gabrielaImageServer)
