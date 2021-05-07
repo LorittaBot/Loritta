99a58ad27429d4ab2744ffbf6da3ba9ded565f6e
MrPowerGamerBR <git@mrpowergamerbr.com>
2021-05-07 12:27:32 -0300

    Whoops, forgot to update the common module

--- common/src/commonMain/kotlin/net/perfectdreams/loritta/common/utils/embed/LorittaColor.kt
+++ common/src/commonMain/kotlin/net/perfectdreams/loritta/common/utils/embed/LorittaColor.kt
@@ -1,14 +1,16 @@
 package net.perfectdreams.loritta.common.utils.embed
 
-sealed class LorittaColor(val rgb: Int)  {
+class LorittaColor(val rgb: Int) {
+    companion object {
+        val DISCORD_BLURPLE = LorittaColor(114, 137, 218)
+        val LORITTA_AQUA = LorittaColor(26, 160, 254)
+        val ROBLOX_RED = LorittaColor(226, 35, 26)
+    }
+
     constructor(r: Int, g: Int, b: Int) : this(
             rgb = 255 and 0xFF shl 24 or
                     (r and 0xFF shl 16) or
                     (g and 0xFF shl 8) or
                     (b and 0xFF shl 0)
         )
-
-    object DISCORD_BLURPLE: LorittaColor(114, 137, 218)
-    object LORITTA_AQUA: LorittaColor(26, 160, 254)
-    object ROBLOX_RED: LorittaColor(226, 35, 26)
 }
