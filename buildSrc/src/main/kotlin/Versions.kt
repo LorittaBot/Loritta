216ae7aa821b593b6c4040ef44f9c2d6cc83a49e
MrPowerGamerBR <git@mrpowergamerbr.com>
2021-05-23 16:00:39 -0300

    Downgrade Kotlin Coroutines

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -4,6 +4,6 @@ object Versions {
     const val KTOR = "1.5.2"
     const val JDA = "4.2.1_261"
     const val KOTLIN_SERIALIZATION = "1.1.0"
-    const val KOTLIN_COROUTINES = "1.5.0"
+    const val KOTLIN_COROUTINES = "1.4.3" // Only upgrade after Loritta is updated to Kotlin 1.5.0!
     const val EXPOSED = "0.31.1"
 }
