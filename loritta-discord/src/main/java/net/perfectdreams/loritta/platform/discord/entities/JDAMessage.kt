c4b59967ae6524892d8b659b3e6d2a989245400d
MrPowerGamerBR <git@mrpowergamerbr.com>
2021-05-24 08:57:07 -0300

    Refactor VemDeZapCommand to move variables to a companion object
    Code so bad it breaks Kotlin's 1.5.0 compiler

--- loritta-discord/src/main/java/net/perfectdreams/loritta/platform/discord/entities/JDAMessage.kt
+++ loritta-discord/src/main/java/net/perfectdreams/loritta/platform/discord/entities/JDAMessage.kt
@@ -3,5 +3,6 @@ package net.perfectdreams.loritta.platform.discord.entities
 import net.perfectdreams.loritta.common.entities.Message
 
 class JDAMessage(private val message: net.dv8tion.jda.api.entities.Message) : Message {
-    override val id by message::idLong
+    override val id: Long
+        get() = message.idLong
 }
--- loritta-discord/src/main/java/net/perfectdreams/loritta/platform/discord/entities/JDAUser.kt
+++ loritta-discord/src/main/java/net/perfectdreams/loritta/platform/discord/entities/JDAUser.kt
@@ -3,9 +3,12 @@ package net.perfectdreams.loritta.platform.discord.entities
 import net.perfectdreams.loritta.common.entities.User
 
 class JDAUser(internal val user: net.dv8tion.jda.api.entities.User) : User {
-    override val id by user::idLong
-    override val name by user::name
-    override val asMention by user::asMention
+    override val id: Long
+        get() = user.idLong
+    override val name: String
+        get() = user.name
+    override val asMention: String
+        get() = user.asMention
     override val avatar
         get() = TODO()
 }
