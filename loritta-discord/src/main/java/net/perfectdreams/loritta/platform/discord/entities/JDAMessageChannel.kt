7d09b849a5cc60c62b80820205cb1726d2f08114
MrPowerGamerBR <git@mrpowergamerbr.com>
2021-05-24 08:29:48 -0300

    Fix allowed mentions

--- loritta-discord/src/main/java/net/perfectdreams/loritta/platform/discord/entities/JDAMessageChannel.kt
+++ loritta-discord/src/main/java/net/perfectdreams/loritta/platform/discord/entities/JDAMessageChannel.kt
@@ -2,6 +2,7 @@ package net.perfectdreams.loritta.platform.discord.entities
 
 import com.mrpowergamerbr.loritta.utils.extensions.await
 import net.dv8tion.jda.api.MessageBuilder
+import net.dv8tion.jda.api.entities.Message
 import net.perfectdreams.loritta.common.entities.LorittaMessage
 import net.perfectdreams.loritta.common.entities.MessageChannel
 
@@ -21,7 +22,16 @@ class JDAMessageChannel(internal val channel: net.dv8tion.jda.api.entities.Messa
             builder.append(reply.content)
         }
 
-        builder.mentionUsers(*message.allowedMentions.users.map { it.id }.toLongArray())
+        // Deny everything!
+        val allowedMentionTypes = mutableSetOf<Message.MentionType>()
+
+        if (message.allowedMentions.users.isNotEmpty()) {
+            // Okay let's allow
+            // We don't need to mention the user! According to Discord's docs, you need to use "USER" *or* user IDs
+            builder.mentionUsers(*message.allowedMentions.users.map { it.id }.toLongArray())
+        }
+
+        builder.setAllowedMentions(allowedMentionTypes)
 
         channel.sendMessage(builder.build())
             .apply {
