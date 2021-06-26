1c622745c33196134b20358ccf73abb81970fd0e
Vitor Paulo <vitorblog12@gmail.com>
2021-06-26 17:01:50 -0300

    Solving message limit error.

--- platforms/discord/legacy/src/main/java/net/perfectdreams/loritta/utils/GenericReplies.kt
+++ platforms/discord/legacy/src/main/java/net/perfectdreams/loritta/utils/GenericReplies.kt
@@ -3,17 +3,17 @@ package net.perfectdreams.loritta.utils
 import com.mrpowergamerbr.loritta.commands.CommandContext
 import com.mrpowergamerbr.loritta.utils.stripCodeMarks
 import net.perfectdreams.loritta.api.messages.LorittaReply
-import net.perfectdreams.loritta.platform.discord.legacy.commands.DiscordCommandContext
+import net.perfectdreams.loritta.platform.discord.commands.DiscordCommandContext
 
 object GenericReplies {
 	suspend fun invalidNumber(context: CommandContext, value: String) {
 		context.reply(
                 LorittaReply(
-                        context.locale["commands.invalidNumber", value.stripCodeMarks()] + " ${Emotes.LORI_CRYING}",
+                        context.locale["commands.invalidNumber", value.stripCodeMarks().take(8)] + " ${Emotes.LORI_CRYING}",
                         Emotes.LORI_HM
                 )
 		)
 	}
 
-	fun invalidNumber(context: DiscordCommandContext, value: String): Nothing = context.fail(context.locale["commands.invalidNumber", value] + " ${Emotes.LORI_CRYING}", Emotes.LORI_HM)
+	fun invalidNumber(context: DiscordCommandContext, value: String): Nothing = context.fail(context.locale["commands.invalidNumber", value.take(8)] + " ${Emotes.LORI_CRYING}", Emotes.LORI_HM)
 }
