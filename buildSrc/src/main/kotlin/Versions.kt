80270ca9aa199b617ee6e2fb65a45d6be68c386c
MrPowerGamerBR <git@mrpowergamerbr.com>
2021-06-27 18:56:24 -0300

    Update JDA

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,7 +2,7 @@ object Versions {
     const val LORITTA = "2021-SNAPSHOT"
     const val KOTLIN = "1.5.0"
     const val KTOR = "1.5.2"
-    const val JDA = "4.2.1_261"
+    const val JDA = "4.3.0_285"
     const val KOTLIN_SERIALIZATION = "1.1.0"
     const val KOTLIN_COROUTINES = "1.5.0"
     const val EXPOSED = "0.31.1"
