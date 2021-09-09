60269168865d41837fbbb380340b8c43c0d799ff
MrPowerGamerBR <git@mrpowergamerbr.com>
2021-09-09 10:34:33 -0300

    Remove unused platforms

--- platforms/cli/src/main/kotlin/net/perfectdreams/loritta/platform/cli/utils/config/LorittaDataConfig.kt
+++
@@ -1,9 +0,0 @@
-package net.perfectdreams.loritta.cinnamon.platform.cli.utils.config
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class LorittaDataConfig(
-    val type: String,
-    val pudding: PuddingConfig?
-)
--- platforms/twitter/src/main/kotlin/net/perfectdreams/loritta/platform/twitter/utils/config/ServicesConfig.kt
+++
@@ -1,9 +0,0 @@
-package net.perfectdreams.loritta.cinnamon.platform.twitter.utils.config
-
-import kotlinx.serialization.Serializable
-import net.perfectdreams.loritta.cinnamon.common.utils.config.GabrielaImageServerConfig
-
-@Serializable
-class ServicesConfig(
-    val gabrielaImageServer: GabrielaImageServerConfig
-)
