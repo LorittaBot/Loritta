02e22dad06068f165b71f5b99abcbb08ed56303d
MrPowerGamerBR <git@mrpowergamerbr.com>
2021-09-09 10:34:33 -0300

    Remove unused platforms

--- platforms/cli/src/main/kotlin/net/perfectdreams/loritta/platform/cli/utils/config/PuddingConfig.kt
+++
@@ -1,9 +0,0 @@
-package net.perfectdreams.loritta.cinnamon.platform.cli.utils.config
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class PuddingConfig(
-    val url: String,
-    val authorization: String
-)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,15 +19,8 @@ include(":commands")
 include(":services:memory")
 include(":services:pudding")
 
-//// ===[ PLATFORMS ]===
-// ===[ CLI ]===
-// include(":platforms:cli")
-
 // ===[ DISCORD ]===
 include(":platforms:discord:common")
 // include(":platforms:discord:gateway")
 include(":platforms:discord:commands")
 include(":platforms:discord:interactions")
-
-// ===[ TWITTER ]===
-// include(":platforms:twitter")
