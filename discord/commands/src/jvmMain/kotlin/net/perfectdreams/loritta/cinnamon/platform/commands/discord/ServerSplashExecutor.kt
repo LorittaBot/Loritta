1b4ebc762fbcc44fab3e4dcf9184ce5bf3b8182e
Luís <nonlyfx@gmail.com>
2021-09-28 07:44:35 -0300

    Resolving conflicts

--- discord/commands/src/jvmMain/kotlin/net/perfectdreams/loritta/cinnamon/platform/commands/discord/ServerSplashExecutor.kt
+++ discord/commands/src/jvmMain/kotlin/net/perfectdreams/loritta/cinnamon/platform/commands/discord/ServerSplashExecutor.kt
@@ -4,7 +4,6 @@ import dev.kord.common.Color
 import dev.kord.rest.service.RestClient
 import net.perfectdreams.discordinteraktions.common.builder.message.actionRow
 import net.perfectdreams.discordinteraktions.common.builder.message.embed
-import net.perfectdreams.discordinteraktions.common.context.commands.GuildApplicationCommandContext
 import net.perfectdreams.loritta.cinnamon.common.emotes.Emotes
 import net.perfectdreams.loritta.cinnamon.i18n.I18nKeysData
 import net.perfectdreams.loritta.cinnamon.platform.commands.*
@@ -14,11 +13,11 @@ class ServerSplashExecutor(val rest: RestClient) : CommandExecutor() {
     companion object : CommandExecutorDeclaration(ServerSplashExecutor::class)
 
     override suspend fun execute(context: ApplicationCommandContext, args: CommandArguments) {
-        if (context.interaKTionsContext !is GuildApplicationCommandContext) throw CommandException {
+        if (context !is GuildApplicationCommandContext) throw CommandException {
             content = context.i18nContext.get(I18nKeysData.Commands.CommandOnlyAvailableInGuilds)
         }
 
-        val guild = rest.guild.getGuild(context.guildId!!)
+        val guild = rest.guild.getGuild(context.guildId)
 
         val splashId = guild.splash.value ?: context.failEphemerally {
             styled(
