5c965f56a92ac51fc5a07b0f1abcba92fbbcb9c5
MrPowerGamerBR <git@mrpowergamerbr.com>
2021-10-03 17:32:27 -0300

    Add Gabriela's Image Server exception handling

--- discord/commands/src/jvmMain/kotlin/net/perfectdreams/loritta/cinnamon/platform/commands/images/gabrielaimageserver/GabrielaImageServerClientExtensions.kt
+++ discord/commands/src/jvmMain/kotlin/net/perfectdreams/loritta/cinnamon/platform/commands/images/gabrielaimageserver/GabrielaImageServerClientExtensions.kt
@@ -1,7 +1,11 @@
 package net.perfectdreams.loritta.cinnamon.platform.commands.images.gabrielaimageserver
 
 import net.perfectdreams.gabrielaimageserver.client.GabrielaImageServerClient
+import net.perfectdreams.gabrielaimageserver.exceptions.ContentLengthTooLargeException
 import net.perfectdreams.gabrielaimageserver.exceptions.ImageNotFoundException
+import net.perfectdreams.gabrielaimageserver.exceptions.ImageTooLargeException
+import net.perfectdreams.gabrielaimageserver.exceptions.StreamExceedsLimitException
+import net.perfectdreams.gabrielaimageserver.exceptions.UntrustedURLException
 import net.perfectdreams.loritta.cinnamon.common.emotes.Emotes
 import net.perfectdreams.loritta.cinnamon.i18n.I18nKeysData
 import net.perfectdreams.loritta.cinnamon.platform.InteractionContext
@@ -9,8 +13,13 @@ import net.perfectdreams.loritta.cinnamon.platform.InteractionContext
 suspend fun <R> GabrielaImageServerClient.handleExceptions(context: InteractionContext, block: suspend net.perfectdreams.gabrielaimageserver.client.GabrielaImageServerClient.() -> (R)): R {
     return try {
         block.invoke(this)
-    } catch (e: ImageNotFoundException) { // This is called if the image wasn't found
-        context.fail(context.i18nContext.get(I18nKeysData.Commands.NoValidImageFound), Emotes.LoriSob)
+    } catch (e: Exception) { // This is called if the image wasn't found
+        when (e) {
+            is ImageNotFoundException -> context.fail(context.i18nContext.get(I18nKeysData.Commands.NoValidImageFound), Emotes.LoriSob)
+            is UntrustedURLException -> context.fail(context.i18nContext.get(I18nKeysData.Commands.ImageUrlIsUntrusted), Emotes.LoriSob)
+            is ImageTooLargeException, is StreamExceedsLimitException, is ContentLengthTooLargeException -> context.fail(context.i18nContext.get(I18nKeysData.Commands.SentImageIsTooLarge), Emotes.LoriSob)
+            else -> throw e // Propagate it
+        }
     }
     // TODO: More exception handling
 }
