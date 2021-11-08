6cc89773659e5201e714f1cd069555ad6ba1a2e0
MrPowerGamerBR <git@mrpowergamerbr.com>
2021-11-08 12:12:01 -0300

    Update Ktor in the frontend, hopefully the workaround is not needed anymore

--- loritta-website/spicy-morenitta/build.gradle.kts
+++ loritta-website/spicy-morenitta/build.gradle.kts
@@ -21,21 +21,8 @@ kotlin {
 		implementation(project(":loritta-website:embed-editor-crosswindow"))
 		implementation("org.jetbrains.kotlinx:kotlinx-html-js:0.7.2")
 		api("org.jetbrains.kotlinx:kotlinx-serialization-json:${Versions.KOTLIN_SERIALIZATION}")
-
-		// ===[ KOTLIN 1.5.0 WORKAROUNDS ]===
-		// Because Kotlin 1.5.0 has too many issues omg
-		// Workaround for https://kotlinlang.slack.com/archives/C0B8L3U69/p1621863081132900
-		// :kotlin_intensifies:
-		api("io.ktor:ktor-client-js:1.4.3") {
-			version {
-				strictly("1.4.3")
-			}
-		}
-		implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:1.4.3") {
-			version {
-				strictly("1.4.3")
-			}
-		}
+		api("io.ktor:ktor-client-js:${Versions.KTOR}")
+		implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:1.5.2")
 
 		// Yes, deprecated... but we need this because if we don't add this, DCE will fail :(
 		api(npm("text-encoding", "0.7.0"))
