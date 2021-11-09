b195c49b7b1a4ca58e2f45f302c94109f8b65561
MrPowerGamerBR <git@mrpowergamerbr.com>
2021-11-08 21:43:19 -0300

    Update Kotlin, Ktor and Kotlin Coroutines

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,9 +1,9 @@
 object Versions {
     const val LORITTA = "2021-SNAPSHOT"
-    const val KOTLIN = "1.5.0"
-    const val KTOR = "1.5.2"
+    const val KOTLIN = "1.5.31"
+    const val KTOR = "1.6.5"
     const val JDA = "4.3.0_324"
     const val KOTLIN_SERIALIZATION = "1.1.0"
-    const val KOTLIN_COROUTINES = "1.5.0"
+    const val KOTLIN_COROUTINES = "1.5.2"
     const val EXPOSED = "0.36.1"
 }
