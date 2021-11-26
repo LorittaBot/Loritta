6f712561351eeb5b30cfe1e266e7277a0c075f5e
MrPowerGamerBR <git@mrpowergamerbr.com>
2021-11-26 12:55:09 -0300

    Change sidebar entries to be a link instead of a div

--- web/dashboard/src/jsMain/kotlin/net/perfectdreams/loritta/spicymorenitta/dashboard/components/SidebarEntry.kt
+++ web/dashboard/src/jsMain/kotlin/net/perfectdreams/loritta/spicymorenitta/dashboard/components/SidebarEntry.kt
@@ -3,13 +3,13 @@ package net.perfectdreams.loritta.spicymorenitta.dashboard.components
 import androidx.compose.runtime.Composable
 import net.perfectdreams.loritta.spicymorenitta.dashboard.styles.AppStylesheet
 import net.perfectdreams.loritta.spicymorenitta.dashboard.utils.SVGIconManager
+import org.jetbrains.compose.web.dom.A
 import org.jetbrains.compose.web.dom.Div
-import org.jetbrains.compose.web.dom.Img
 import org.jetbrains.compose.web.dom.Text
 
 @Composable
 fun SidebarEntry(name: String) {
-    Div(attrs = { classes(AppStylesheet.leftSidebarEntry) }) {
+    A(attrs = { classes(AppStylesheet.leftSidebarEntry, AppStylesheet.resetLinkStyle) }) {
         UIIcon(SVGIconManager.sparkles) {
             classes(AppStylesheet.leftSidebarEntryIcon)
         }
