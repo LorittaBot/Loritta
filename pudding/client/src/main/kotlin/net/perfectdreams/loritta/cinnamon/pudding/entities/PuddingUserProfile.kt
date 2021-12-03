f5d48b4415262cb139f32d2b177bb70f7eb21465
Luis <nonlyfx@gmail.com>
2021-12-03 20:52:21 -0300

    Add PuddingUserSettings (#2465)
    * Added a PuddingUserSetting
    
    * Update GenderExecutor.kt
    
    * Updated UserSetting name to ProfileSettings
    
    * Added some functions

--- pudding/client/src/main/kotlin/net/perfectdreams/loritta/cinnamon/pudding/entities/PuddingUserProfile.kt
+++ pudding/client/src/main/kotlin/net/perfectdreams/loritta/cinnamon/pudding/entities/PuddingUserProfile.kt
@@ -2,11 +2,9 @@ package net.perfectdreams.loritta.cinnamon.pudding.entities
 
 import kotlinx.datetime.Instant
 import net.perfectdreams.loritta.cinnamon.common.achievements.AchievementType
-import net.perfectdreams.loritta.cinnamon.common.utils.Gender
 import net.perfectdreams.loritta.cinnamon.pudding.Pudding
 import net.perfectdreams.loritta.cinnamon.pudding.data.UserProfile
 import net.perfectdreams.loritta.cinnamon.pudding.tables.Profiles
-import net.perfectdreams.loritta.cinnamon.pudding.tables.UserSettings
 import org.jetbrains.exposed.sql.update
 
 class PuddingUserProfile(
@@ -49,9 +47,7 @@ class PuddingUserProfile(
         }
     }
 
-    suspend fun setGender(gender: Gender) = pudding.transaction {
-        UserSettings.update({ UserSettings.id eq this@PuddingUserProfile.id.value.toLong() }) {
-            it[UserSettings.gender] = gender
-        }
-    }
+    suspend fun getOrCreateProfileSettings() = pudding.users.getOrCreateProfileSettings(id)
+
+    suspend fun getProfileSettings() = pudding.users.getProfileSettings(id)
 }
