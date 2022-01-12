0a71024316b3a2372e62f31a06043bee97134e4d
MrPowerGamerBR <git@mrpowergamerbr.com>
2022-01-12 01:15:58 -0300

    Pin Discord InteraKTions version to see if Gradle will refresh the SNAPSHOT dependency

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -8,7 +8,7 @@ object Versions {
     const val EXPOSED = "0.36.2"
     const val PROMETHEUS = "0.12.0"
     const val KTOR = "1.6.7"
-    const val DISCORD_INTERAKTIONS = "0.0.11-SNAPSHOT"
+    const val DISCORD_INTERAKTIONS = "0.0.11-20220112.035808-8"
     const val I18N_HELPER = "0.0.2-SNAPSHOT"
     const val KOTLINX_DATE_TIME = "0.3.1"
 }
