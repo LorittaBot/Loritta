b61f8e60dc38b8174d2c9aaa4cc4d899b7f9f6be
MrPowerGamerBR <git@mrpowergamerbr.com>
2022-01-15 19:30:57 -0300

    Initial implementation of the tables and classes, but not finished yet

---
+++ common/src/commonMain/kotlin/net/perfectdreams/loritta/cinnamon/common/utils/SparklyPowerLSXTransactionEntryAction.kt
@@ -0,0 +1,6 @@
+package net.perfectdreams.loritta.cinnamon.common.utils
+
+enum class SparklyPowerLSXTransactionEntryAction {
+    TRANSFERRED_TO_SPARKLYPOWER,
+    TRANSFERRED_FROM_SPARKLYPOWER
+}
---
+++ pudding/client/src/main/kotlin/net/perfectdreams/loritta/cinnamon/pudding/tables/SparklyPowerLSXSonhosTransactionsLog.kt
@@ -0,0 +1,13 @@
+package net.perfectdreams.loritta.cinnamon.pudding.tables
+
+import net.perfectdreams.loritta.cinnamon.common.utils.SparklyPowerLSXTransactionEntryAction
+import net.perfectdreams.loritta.cinnamon.pudding.utils.exposed.postgresEnumeration
+import org.jetbrains.exposed.dao.id.LongIdTable
+
+object SparklyPowerLSXSonhosTransactionsLog : LongIdTable() {
+    val timestampLog = reference("timestamp_log", SonhosTransactionsLog)
+    val action = postgresEnumeration<SparklyPowerLSXTransactionEntryAction>("action")
+    val sonhos = long("sonhos")
+    val playerName = text("player_name")
+    val playerUniqueId = uuid("player_unique_id")
+}
