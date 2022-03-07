ff6b9cbbda9e626623d7f67d0d8d1560ba3c5c21
MrPowerGamerBR <git@mrpowergamerbr.com>
2022-03-06 21:49:16 -0300

    Change daily collector and warner to READ_COMMITED isolation level to avoid errors due to concurrent access

--- microservices/daily-tax/src/main/kotlin/net/perfectdreams/loritta/cinnamon/microservices/dailytax/utils/DailyTaxTasks.kt
+++ microservices/daily-tax/src/main/kotlin/net/perfectdreams/loritta/cinnamon/microservices/dailytax/utils/DailyTaxTasks.kt
@@ -42,7 +42,7 @@ class DailyTaxTasks(private val m: DailyTax) {
 
         // at midnight + notify about the user about taxes
         scheduleEveryDayAtSpecificHour(
-            LocalTime.MIDNIGHT.plusMinutes(30), // TODO: woo tests please remove this later
+            LocalTime.MIDNIGHT.plusMinutes(60), // TODO: woo tests please remove this later
             dailyTaxCollector
         )
 
--- microservices/daily-tax/src/main/kotlin/net/perfectdreams/loritta/cinnamon/microservices/dailytax/utils/DailyTaxWarner.kt
+++ microservices/daily-tax/src/main/kotlin/net/perfectdreams/loritta/cinnamon/microservices/dailytax/utils/DailyTaxWarner.kt
@@ -8,6 +8,7 @@ import net.perfectdreams.loritta.cinnamon.pudding.data.UserDailyTaxWarnDirectMes
 import net.perfectdreams.loritta.cinnamon.pudding.data.UserId
 import net.perfectdreams.loritta.cinnamon.pudding.tables.DailyTaxPendingDirectMessages
 import org.jetbrains.exposed.sql.deleteAll
+import java.sql.Connection
 import java.time.LocalDateTime
 import java.time.ZoneOffset
 
@@ -31,7 +32,9 @@ class DailyTaxWarner(val m: DailyTax) : RunnableCoroutineWrapper() {
                 .toInstant(ZoneOffset.UTC)
                 .toKotlinInstant()
 
-            m.services.transaction {
+            // We need to use Read Commited to avoid "Culd not serialize access due to concurrent update"
+            // This is more "unsafe" because we may make someone be in the negative sonhos, but there isn't another good alterative, so yeah...
+            m.services.transaction(Connection.TRANSACTION_READ_COMMITTED) {
                 // Delete all pending direct messages because we will replace with newer messages
                 DailyTaxPendingDirectMessages.deleteAll()
 
