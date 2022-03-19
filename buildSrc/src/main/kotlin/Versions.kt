0220c1aad287f6006667208b1622bc9918f2265f
MrPowerGamerBR <git@mrpowergamerbr.com>
2022-03-19 13:33:17 -0300

    Update Kord and Discord InteraKTions

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -8,8 +8,8 @@ object Versions {
     const val EXPOSED = "0.36.2"
     const val PROMETHEUS = "0.12.0"
     const val KTOR = "1.6.7"
-    const val DISCORD_INTERAKTIONS = "0.0.12-20220219.144301-19"
-    const val KORD = "0.8.x-lori-fork-20220209.020415-3" // Should match Discord InteraKTions
+    const val DISCORD_INTERAKTIONS = "0.0.12-20220319.162224-21"
+    const val KORD = "0.8.x-20220315.083129-149" // Should match Discord InteraKTions
     const val I18N_HELPER = "0.0.4-SNAPSHOT"
     const val KOTLINX_DATE_TIME = "0.3.1"
     const val HIKARICP = "5.0.1"
