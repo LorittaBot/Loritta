d4e011c38e2963fae23aeb891a4ffe0cf6700bd0
MrPowerGamerBR <git@mrpowergamerbr.com>
2022-03-19 11:20:14 -0300

    Whoops, forgot this

--- common/src/commonMain/kotlin/net/perfectdreams/loritta/cinnamon/common/utils/UserPremiumPlans.kt
+++ common/src/commonMain/kotlin/net/perfectdreams/loritta/cinnamon/common/utils/UserPremiumPlans.kt
@@ -3,6 +3,7 @@ package net.perfectdreams.loritta.cinnamon.common.utils
 interface UserPremiumPlans {
     val cost: Double
     val coinFlipRewardTax: Double
+    val hasDailyInactivityTax: Boolean
 
     val isCoinFlipBetRewardTaxed: Boolean
         get() = coinFlipRewardTax != 0.0
@@ -19,20 +20,24 @@ interface UserPremiumPlans {
     object Free : UserPremiumPlans {
         override val cost = 0.0
         override val coinFlipRewardTax = 0.05
+        override val hasDailyInactivityTax = true
     }
 
     object Essential : UserPremiumPlans {
         override val cost = 19.99
         override val coinFlipRewardTax = 0.05
+        override val hasDailyInactivityTax = true
     }
 
     object Recommended : UserPremiumPlans {
         override val cost = 39.99
         override val coinFlipRewardTax = 0.0
+        override val hasDailyInactivityTax = true
     }
 
     object Complete : UserPremiumPlans {
         override val cost = 99.99
         override val coinFlipRewardTax = 0.0
+        override val hasDailyInactivityTax = false
     }
 }
