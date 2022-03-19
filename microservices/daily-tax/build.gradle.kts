3e1636467fb31c5980376353e614dcc9f2aab6c1
MrPowerGamerBR <git@mrpowergamerbr.com>
2022-03-19 11:55:01 -0300

    Add unit test for daily tax service

--- microservices/daily-tax/build.gradle.kts
+++ microservices/daily-tax/build.gradle.kts
@@ -22,6 +22,12 @@ dependencies {
 
     // Logback GELF, used for Graylog logging
     implementation("de.siegmar:logback-gelf:3.0.0")
+
+    testImplementation("ch.qos.logback:logback-classic:1.3.0-alpha14")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+    testImplementation("org.testcontainers:testcontainers:1.16.3")
+    testImplementation("org.testcontainers:junit-jupiter:1.16.3")
+    testImplementation("org.testcontainers:postgresql:1.16.3")
 }
 
 tasks.test {
@@ -30,6 +36,12 @@ tasks.test {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = Versions.JVM_TARGET
+}
+
+tasks.withType<Test> {
+    // Required for tests, if this is missing then Gradle will throw
+    // "No tests found for given includes: [***Test](filter.includeTestsMatching)"
+    useJUnitPlatform()
 }
 
 jib {
