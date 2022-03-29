7c6f55ab76e8e55a5fed4a519571acde7404e83c
MrPowerGamerBR <git@mrpowergamerbr.com>
2022-03-29 11:27:05 -0300

    Change blog post view list design to use the wobbly patterns

--- web/showtime/backend/src/main/kotlin/net/perfectdreams/showtime/backend/views/BlogView.kt
+++ web/showtime/backend/src/main/kotlin/net/perfectdreams/showtime/backend/views/BlogView.kt
@@ -6,7 +6,6 @@ import kotlinx.html.a
 import kotlinx.html.classes
 import kotlinx.html.div
 import kotlinx.html.h1
-import kotlinx.html.hr
 import kotlinx.html.unsafe
 import net.perfectdreams.dokyo.WebsiteTheme
 import net.perfectdreams.i18nhelper.core.I18nContext
@@ -36,64 +35,66 @@ class BlogView(
     override fun DIV.generateContent() {
         val languageId = showtimeBackend.languageManager.getIdByI18nContext(i18nContext)
 
-        innerContent {
-            div(classes = "odd-wrapper") {
-                div(classes = "media") {
-                    div(classes = "media-body") {
-                        showtimeBackend.loadSourceContentsFromFolder("blog")
-                            .sortedByDescending { it.metadata.parsedDate }
-                            .filterNot { it.metadata.hidden }
-                            .forEach {
-                                div {
-                                    val localizedContent = it.getLocalizedVersion(languageId)
+        val posts = showtimeBackend.loadSourceContentsFromFolder("blog")
+            .sortedByDescending { it.metadata.parsedDate }
+            .filterNot { it.metadata.hidden }
 
-                                    a(href = "/${locale.path}${it.path}") {
-                                        h1 {
-                                            +localizedContent.metadata.title
-                                        }
+        innerContent {
+            posts.forEachIndexed { index, it ->
+                div(classes = if (index % 2 == 0) "odd-wrapper" else "even-wrapper") {
+                    if (index != 0)
+                    classes = classes + "wobbly-bg"
+
+                    div(classes = "media") {
+                        div(classes = "media-body") {
+                            div {
+                                val localizedContent = it.getLocalizedVersion(languageId)
+
+                                a(href = "/${locale.path}${it.path}") {
+                                    h1 {
+                                        +localizedContent.metadata.title
+                                    }
+                                }
+
+                                /* val time = it.metadata.parsedDate?.toInstant()?.atZone(ZoneId.of("America/Sao_Paulo"))
+                                if (time != null) {
+                                    span {
+                                        +"${time.dayOfMonth.toString().padStart(2, '0')}/${
+                                            time.monthValue.toString().padStart(2, '0')
+                                        }/${time.year}"
+                                    }
+                                } */
+
+                                div {
+                                    unsafe {
+                                        raw(
+                                            BlogPostView.parseContent(
+                                                showtimeBackend,
+                                                locale,
+                                                localizedContent.content
+                                                    .substringBefore("{{ read_more }}")
+                                            )
+                                        )
                                     }
 
-                                    /* val time = it.metadata.parsedDate?.toInstant()?.atZone(ZoneId.of("America/Sao_Paulo"))
-                                    if (time != null) {
-                                        span {
-                                            +"${time.dayOfMonth.toString().padStart(2, '0')}/${
-                                                time.monthValue.toString().padStart(2, '0')
-                                            }/${time.year}"
-                                        }
-                                    } */
-
-                                    div {
-                                        unsafe {
-                                            raw(
-                                                BlogPostView.parseContent(
-                                                    showtimeBackend,
-                                                    locale,
-                                                    localizedContent.content
-                                                        .substringBefore("{{ read_more }}")
-                                                )
-                                            )
-                                        }
-
-                                        if (localizedContent.content.contains("{{ read_more }}")) {
-                                            div(classes = "read-more") {
-                                                imgSrcSetFromResources(
-                                                    "/v3/assets/img/emotes/lori-zap.png",
-                                                    "1.5em"
-                                                ) {
-                                                    classes = setOf("inline-emoji")
-                                                }
+                                    if (localizedContent.content.contains("{{ read_more }}")) {
+                                        div(classes = "read-more") {
+                                            imgSrcSetFromResources(
+                                                "/v3/assets/img/emotes/lori-zap.png",
+                                                "1.5em"
+                                            ) {
+                                                classes = setOf("inline-emoji")
+                                            }
 
 
-                                                a(href = "/${locale.path}${it.path}") {
-                                                    +" ${i18nContext.get(I18nKeysData.Website.Blog.KeepReading)} »"
-                                                }
+                                            a(href = "/${locale.path}${it.path}") {
+                                                +" ${i18nContext.get(I18nKeysData.Website.Blog.KeepReading)} »"
                                             }
                                         }
                                     }
                                 }
-
-                                hr {}
                             }
+                        }
                     }
                 }
             }
