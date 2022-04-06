a3bde37aa8ee12f0338e8569dd787595f007b7d0
MrPowerGamerBR <git@mrpowergamerbr.com>
2022-04-06 00:40:13 -0300

    Looks like this is not required anymore in Gradle 7.4+

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,8 +8,6 @@ pluginManagement {
 
 rootProject.name = "cinnamon-parent"
 
-enableFeaturePreview("VERSION_CATALOGS")
-
 dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
