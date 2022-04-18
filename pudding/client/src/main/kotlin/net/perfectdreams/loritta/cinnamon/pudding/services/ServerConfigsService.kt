d69e72aa8686f4710051f7af5493c40237060afd
MrPowerGamerBR <git@mrpowergamerbr.com>
2022-04-18 12:17:41 -0300

    Fix "No transaction in context"

--- pudding/client/src/main/kotlin/net/perfectdreams/loritta/cinnamon/pudding/services/ServerConfigsService.kt
+++ pudding/client/src/main/kotlin/net/perfectdreams/loritta/cinnamon/pudding/services/ServerConfigsService.kt
@@ -10,13 +10,13 @@ import net.perfectdreams.loritta.cinnamon.pudding.tables.StarboardConfigs
 import org.jetbrains.exposed.sql.select
 
 class ServerConfigsService(private val pudding: Pudding) : Service(pudding) {
-    suspend fun getServerConfigRoot(guildId: ULong): PuddingServerConfigRoot? = _getServerConfigRoot(guildId)
+    suspend fun getServerConfigRoot(guildId: ULong): PuddingServerConfigRoot? = pudding.transaction { _getServerConfigRoot(guildId) }
 
     suspend fun _getServerConfigRoot(guildId: ULong): PuddingServerConfigRoot? = ServerConfigs.select {
         ServerConfigs.id eq guildId.toLong()
     }.firstOrNull()?.let { PuddingServerConfigRoot.fromRow(it) }
 
-    suspend fun getStarboardConfigById(id: Long): StarboardConfig? = _getStarboardConfigById(id)
+    suspend fun getStarboardConfigById(id: Long): StarboardConfig? = pudding.transaction {  _getStarboardConfigById(id) }
 
     suspend fun _getStarboardConfigById(id: Long): StarboardConfig? = StarboardConfigs.select {
         StarboardConfigs.id eq id
