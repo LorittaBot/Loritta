fec38ce264d71d6fd6f34b4a90252ba8681e25a9
MrPowerGamerBR <git@mrpowergamerbr.com>
2022-05-12 13:40:55 -0300

    Relay all events to RabbitMQ
    This may explode everything, but let's see what will happen ;)

--- platforms/discord/legacy/src/main/java/com/mrpowergamerbr/loritta/listeners/GatewayEventRelayerListener.kt
+++ platforms/discord/legacy/src/main/java/com/mrpowergamerbr/loritta/listeners/GatewayEventRelayerListener.kt
@@ -1,33 +1,14 @@
 package com.mrpowergamerbr.loritta.listeners
 
 import com.mrpowergamerbr.loritta.Loritta
-import com.rabbitmq.client.BuiltinExchangeType
 import com.rabbitmq.client.Channel
 import com.rabbitmq.client.ConnectionFactory
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import net.dv8tion.jda.api.events.RawGatewayEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
-import net.perfectdreams.loritta.cinnamon.pudding.tables.DiscordGatewayEvents
-import org.jetbrains.exposed.sql.insert
 
 class GatewayEventRelayerListener(val m: Loritta) : ListenerAdapter() {
-    companion object {
-        private val RELAYED_EVENTS = listOf(
-            "CHANNEL_CREATE",
-            "CHANNEL_UPDATE",
-            "CHANNEL_DELETE",
-            "GUILD_MEMBER_ADD",
-            "GUILD_MEMBER_REMOVE",
-            "GUILD_MEMBER_UPDATE",
-            "USER_UPDATE",
-            "MESSAGE_REACTION_ADD",
-            "MESSAGE_REACTION_REMOVE",
-            "MESSAGE_REACTION_REMOVE_EMOJI",
-            "MESSAGE_REACTION_REMOVE_ALL"
-        )
-    }
-
     val channel: Channel
     init {
         val factory = ConnectionFactory()
@@ -42,9 +23,6 @@ class GatewayEventRelayerListener(val m: Loritta) : ListenerAdapter() {
     }
 
     override fun onRawGateway(event: RawGatewayEvent) {
-        if (event.type !in RELAYED_EVENTS)
-            return
-
         GlobalScope.launch(m.coroutineDispatcher) {
             val routingKey = "event.${event.type.replace("_", "-").lowercase()}"
 
