87339ffcec2b037c21ea0984faa15dfb7a962a7b
MrPowerGamerBR <git@mrpowergamerbr.com>
2022-05-13 01:32:21 -0300

    Workaround to avoid a "java.lang.OutOfMemoryError: unable to create native thread: possibly out of memory or process/resource limits reached"

--- platforms/discord/legacy/src/main/java/com/mrpowergamerbr/loritta/listeners/GatewayEventRelayerListener.kt
+++ platforms/discord/legacy/src/main/java/com/mrpowergamerbr/loritta/listeners/GatewayEventRelayerListener.kt
@@ -5,11 +5,14 @@ import com.rabbitmq.client.Channel
 import com.rabbitmq.client.ConnectionFactory
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Semaphore
+import kotlinx.coroutines.sync.withPermit
 import net.dv8tion.jda.api.events.RawGatewayEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 
 class GatewayEventRelayerListener(val m: Loritta) : ListenerAdapter() {
     val channel: Channel
+
     init {
         val factory = ConnectionFactory()
         factory.host = m.config.rabbitMQ.host
@@ -22,17 +25,22 @@ class GatewayEventRelayerListener(val m: Loritta) : ListenerAdapter() {
         channel = connection.createChannel()
     }
 
+    private val semaphore = Semaphore(4)
+
     override fun onRawGateway(event: RawGatewayEvent) {
         GlobalScope.launch(m.coroutineDispatcher) {
-            val routingKey = "event.${event.type.replace("_", "-").lowercase()}"
+            // Just to avoid a LOT of threads being created at the same time, causing a "java.lang.OutOfMemoryError: unable to create native thread: possibly out of memory or process/resource limits reached"
+            semaphore.withPermit {
+                val routingKey = "event.${event.type.replace("_", "-").lowercase()}"
 
-            // Publish the event to the events exchange
-            channel.basicPublish(
-                "discord-gateway-events",
-                routingKey,
-                null,
-                event.`package`.toString().toByteArray(Charsets.UTF_8)
-            )
+                // Publish the event to the events exchange
+                channel.basicPublish(
+                    "discord-gateway-events",
+                    routingKey,
+                    null,
+                    event.`package`.toString().toByteArray(Charsets.UTF_8)
+                )
+            }
         }
     }
 }
