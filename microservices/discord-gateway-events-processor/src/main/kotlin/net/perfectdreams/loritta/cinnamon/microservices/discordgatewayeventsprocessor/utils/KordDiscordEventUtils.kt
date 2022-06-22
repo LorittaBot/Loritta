391e3ffed7f575e87e09f8a3b94dc2e0d64d2ce6
MrPowerGamerBR <git@mrpowergamerbr.com>
2022-06-22 13:53:04 -0300

    If something goes wrong while trying to deserialize the event, log and return null

--- microservices/discord-gateway-events-processor/src/main/kotlin/net/perfectdreams/loritta/cinnamon/microservices/discordgatewayeventsprocessor/utils/KordDiscordEventUtils.kt
+++ microservices/discord-gateway-events-processor/src/main/kotlin/net/perfectdreams/loritta/cinnamon/microservices/discordgatewayeventsprocessor/utils/KordDiscordEventUtils.kt
@@ -1,10 +1,18 @@
 package net.perfectdreams.loritta.cinnamon.microservices.discordgatewayeventsprocessor.utils
 
 import dev.kord.gateway.Event
-import kotlinx.serialization.json.*
+import kotlinx.serialization.SerializationException
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
+import kotlinx.serialization.json.longOrNull
+import kotlinx.serialization.json.put
+import mu.KotlinLogging
 
 object KordDiscordEventUtils {
     private val json = Json { ignoreUnknownKeys = true }
+    private val logger = KotlinLogging.logger {}
 
     fun parseEventFromJsonString(gatewayPayload: String): Event? {
         // TODO: Ktor doesn't deserialize this well because it relies on the order
@@ -12,25 +20,30 @@ object KordDiscordEventUtils {
             .jsonObject
 
         // Using decodeFromJsonElement crashes with "Index -1 out of bounds for length 0", why?
-        return json.decodeFromString(
-            Event.DeserializationStrategy,
-            buildJsonObject {
-                gatewayPayloadStuff["op"]?.let {
-                    put("op", it.jsonPrimitive.longOrNull)
-                }
+        try {
+            return json.decodeFromString(
+                Event.DeserializationStrategy,
+                buildJsonObject {
+                    gatewayPayloadStuff["op"]?.let {
+                        put("op", it.jsonPrimitive.longOrNull)
+                    }
 
-                gatewayPayloadStuff["t"]?.let {
-                    put("t", it)
-                }
+                    gatewayPayloadStuff["t"]?.let {
+                        put("t", it)
+                    }
 
-                gatewayPayloadStuff["s"]?.let {
-                    put("s", it)
-                }
+                    gatewayPayloadStuff["s"]?.let {
+                        put("s", it)
+                    }
 
-                gatewayPayloadStuff["d"]?.let {
-                    put("d", it)
-                }
-            }.toString()
-        )
+                    gatewayPayloadStuff["d"]?.let {
+                        put("d", it)
+                    }
+                }.toString()
+            )
+        } catch (e: SerializationException) {
+            logger.warn(e) { "Something went wrong while trying to deserialize $gatewayPayload" }
+            return null
+        }
     }
 }
