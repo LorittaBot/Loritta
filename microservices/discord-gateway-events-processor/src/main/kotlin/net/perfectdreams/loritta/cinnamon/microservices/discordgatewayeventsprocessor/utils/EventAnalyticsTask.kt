41fd6342b58a77326a2b97e51fea7012894765b1
MrPowerGamerBR <git@mrpowergamerbr.com>
2022-06-22 15:33:16 -0300

    Okay now we already know the issue

--- microservices/discord-gateway-events-processor/src/main/kotlin/net/perfectdreams/loritta/cinnamon/microservices/discordgatewayeventsprocessor/utils/EventAnalyticsTask.kt
+++ microservices/discord-gateway-events-processor/src/main/kotlin/net/perfectdreams/loritta/cinnamon/microservices/discordgatewayeventsprocessor/utils/EventAnalyticsTask.kt
@@ -1,7 +1,6 @@
 package net.perfectdreams.loritta.cinnamon.microservices.discordgatewayeventsprocessor.utils
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.debug.DebugProbes
 import mu.KotlinLogging
 import net.perfectdreams.loritta.cinnamon.microservices.discordgatewayeventsprocessor.DiscordGatewayEventsProcessor
 import net.perfectdreams.loritta.cinnamon.microservices.discordgatewayeventsprocessor.modules.ProcessDiscordEventsModule
@@ -13,7 +12,6 @@ class EventAnalyticsTask(private val m: DiscordGatewayEventsProcessor) : Runnabl
 
     var lastEventCountCheck = 0
 
-    @OptIn(ExperimentalCoroutinesApi::class)
     override fun run() {
         val totalEventsProcessed = m.tasks.processDiscordGatewayEvents.totalEventsProcessed
         logger.info { "Total Discord Events processed: $totalEventsProcessed; (+${totalEventsProcessed - lastEventCountCheck})" }
@@ -21,7 +19,6 @@ class EventAnalyticsTask(private val m: DiscordGatewayEventsProcessor) : Runnabl
         printStats(m.starboardModule)
         printStats(m.addFirstToNewChannelsModule)
         printStats(m.discordCacheModule)
-        DebugProbes.dumpCoroutines()
     }
 
     private fun printStats(module: ProcessDiscordEventsModule) {
