04445ab71db35bcaf6cd522dc07d36d0532a197f
MrPowerGamerBR <git@mrpowergamerbr.com>
2022-06-30 21:38:39 -0300

    Whoops, the job was never added to the pending events list

--- microservices/discord-gateway-events-processor/src/main/kotlin/net/perfectdreams/loritta/cinnamon/microservices/discordgatewayeventsprocessor/modules/ProcessDiscordEventsModule.kt
+++ microservices/discord-gateway-events-processor/src/main/kotlin/net/perfectdreams/loritta/cinnamon/microservices/discordgatewayeventsprocessor/modules/ProcessDiscordEventsModule.kt
@@ -37,6 +37,8 @@ abstract class ProcessDiscordEventsModule {
             block = block
         )
 
+        activeEvents.add(job)
+        
         // Yes, the order matters, since sometimes the invokeOnCompletion would be invoked before the job was
         // added to the list, causing leaks.
         // invokeOnCompletion is also invoked even if the job was already completed at that point, so no worries!
