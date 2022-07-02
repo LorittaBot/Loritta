984b33004dfe74ee5ab11af1f9842e59428d3685
MrPowerGamerBR <git@mrpowergamerbr.com>
2022-07-01 23:48:44 -0300

    Store when the gateway event was received and what shard received it

--- microservices/discord-gateway-events-processor/src/main/kotlin/net/perfectdreams/loritta/cinnamon/microservices/discordgatewayeventsprocessor/tables/DiscordGatewayEvents.kt
+++ microservices/discord-gateway-events-processor/src/main/kotlin/net/perfectdreams/loritta/cinnamon/microservices/discordgatewayeventsprocessor/tables/DiscordGatewayEvents.kt
@@ -1,9 +1,12 @@
 package net.perfectdreams.loritta.cinnamon.microservices.discordgatewayeventsprocessor.tables
 
+import net.perfectdreams.exposedpowerutils.sql.javatime.timestampWithTimeZone
 import net.perfectdreams.exposedpowerutils.sql.jsonb
 import org.jetbrains.exposed.dao.id.LongIdTable
 
 object DiscordGatewayEvents : LongIdTable() {
     val type = text("type").index()
+    val receivedAt = timestampWithTimeZone("received_at")
+    val shard = integer("shard")
     val payload = jsonb("payload")
 }
