89b7fab959801203d8424fe24dd5615b1fc10479
MrPowerGamerBR <git@mrpowergamerbr.com>
2022-07-04 17:11:51 -0300

    This is unused

--- microservices/discord-gateway-events-processor/src/main/kotlin/net/perfectdreams/loritta/cinnamon/microservices/discordgatewayeventsprocessor/utils/config/RootConfig.kt
+++ microservices/discord-gateway-events-processor/src/main/kotlin/net/perfectdreams/loritta/cinnamon/microservices/discordgatewayeventsprocessor/utils/config/RootConfig.kt
@@ -10,6 +10,5 @@ data class RootConfig(
     val discord: LorittaDiscordConfig,
     val replicas: ReplicasConfig,
     val gatewayProxies: List<GatewayProxyConfig>,
-    val pudding: PuddingConfig,
-    val queueDatabase: PuddingConfig
+    val pudding: PuddingConfig
 )
