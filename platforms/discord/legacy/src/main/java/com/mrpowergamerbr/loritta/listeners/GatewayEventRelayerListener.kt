4c7add67150f31a8115b899ef07531a5762b52ae
MrPowerGamerBR <git@mrpowergamerbr.com>
2022-07-04 09:49:58 -0300

    Enable relaying again

--- platforms/discord/legacy/src/main/java/com/mrpowergamerbr/loritta/listeners/GatewayEventRelayerListener.kt
+++ platforms/discord/legacy/src/main/java/com/mrpowergamerbr/loritta/listeners/GatewayEventRelayerListener.kt
@@ -1,15 +1,22 @@
 package com.mrpowergamerbr.loritta.listeners
 
 import com.mrpowergamerbr.loritta.Loritta
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.sync.Semaphore
+import kotlinx.coroutines.sync.withPermit
 import net.dv8tion.jda.api.events.RawGatewayEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
+import net.perfectdreams.loritta.tables.DiscordGatewayEvents
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.transactions.transaction
+import java.time.Instant
 
 class GatewayEventRelayerListener(val m: Loritta) : ListenerAdapter() {
     private val semaphore = Semaphore(4)
 
     override fun onRawGateway(event: RawGatewayEvent) {
-        /* val receivedAt = Instant.now()
+        val receivedAt = Instant.now()
 
         GlobalScope.launch(m.coroutineDispatcher) {
             // Just to avoid a LOT of threads being created at the same time, causing a "java.lang.OutOfMemoryError: unable to create native thread: possibly out of memory or process/resource limits reached"
@@ -23,6 +30,6 @@ class GatewayEventRelayerListener(val m: Loritta) : ListenerAdapter() {
                     }
                 }
             }
-        } */
+        }
     }
 }
