6bf6270e410c46f40dd69a22df29f665769a2001
MrPowerGamerBR <git@mrpowergamerbr.com>
2022-07-20 17:54:57 -0300

    Whoops

--- discord/discord-common/src/main/kotlin/net/perfectdreams/loritta/cinnamon/platform/utils/metrics/KordMetrics.kt
+++ discord/discord-common/src/main/kotlin/net/perfectdreams/loritta/cinnamon/platform/utils/metrics/KordMetrics.kt
@@ -2,6 +2,6 @@ package net.perfectdreams.loritta.cinnamon.platform.utils.metrics
 
 object KordMetrics : PrometheusMetrics() {
     val requests = createCounterWithLabels("kord_requests_count", "How many HTTP requests were made by Kord") {
-        labels("route", "http_method")
+        labelNames("route", "http_method")
     }
 }
