a2082d01e0d54fb9f7ac3f60e967fa10b4bdf0d2
MrPowerGamerBR <git@mrpowergamerbr.com>
2022-07-24 11:19:37 -0300

    Automatically shutdown the voice connection if there isn't anything playing for more than 5 minutes

--- microservices/discord-gateway-events-processor/src/main/kotlin/net/perfectdreams/loritta/cinnamon/microservices/discordgatewayeventsprocessor/voice/LorittaVoiceConnection.kt
+++ microservices/discord-gateway-events-processor/src/main/kotlin/net/perfectdreams/loritta/cinnamon/microservices/discordgatewayeventsprocessor/voice/LorittaVoiceConnection.kt
@@ -5,10 +5,12 @@ import dev.kord.common.entity.Snowflake
 import dev.kord.gateway.Gateway
 import dev.kord.gateway.UpdateVoiceStatus
 import dev.kord.voice.VoiceConnection
+import kotlinx.atomicfu.atomic
 import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.Channel
 import mu.KotlinLogging
 import java.util.concurrent.LinkedBlockingQueue
+import kotlin.time.Duration.Companion.minutes
 
 @OptIn(KordVoice::class)
 data class LorittaVoiceConnection(
@@ -25,6 +27,7 @@ data class LorittaVoiceConnection(
 
     private val audioClips = Channel<AudioClipInfo>(Channel.UNLIMITED)
     private val scope = CoroutineScope(SupervisorJob())
+    private var detachJob: Job? by atomic(null)
 
     fun isPlaying() = !audioProvider.audioFramesInOpusFormatQueue.isEmpty
 
@@ -54,9 +57,21 @@ data class LorittaVoiceConnection(
         for (notification in audioClipProviderNotificationChannel) {
             // So you want new audio frames, huh? Okaay :3
             logger.info { "Received a \"moar framez!! :3\" request on the audio channel, let's try getting more audio clips (if possible...)" }
+
+            // Initialize automatic shutdown after 5 minutes of inactivity
+            detachJob?.cancel()
+            detachJob = scope.launch {
+                delay(5.minutes)
+                logger.info { "Shutting down connection $voiceConnection due to inactivity... Bye!" }
+                shutdown() // Technically the connection will be cleaned up from the voice connections map after the voice connection is shutdown...
+            }
+
             val audioClipInfo = audioClips.receive() // This will suspend if there isn't any request audio clips in the channel
+            detachJob?.cancel()
+
             if (channelId != audioClipInfo.channelId)
                 switchChannel(audioClipInfo.channelId) // Audio Clip was queued on a different channel, switch to it!
+
             audioProvider.queue(audioClipInfo.frames)
         }
     }
