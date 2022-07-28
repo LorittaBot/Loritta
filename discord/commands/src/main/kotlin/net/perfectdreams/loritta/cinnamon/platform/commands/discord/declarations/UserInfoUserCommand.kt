b32ed225931e51fd33faa1a94ef96442a17e0bd9
MrPowerGamerBR <git@mrpowergamerbr.com>
2022-07-28 15:28:21 -0300

    Remove commands module folder

--- discord/commands/src/main/kotlin/net/perfectdreams/loritta/cinnamon/platform/commands/discord/declarations/UserInfoUserCommand.kt
+++
@@ -1,9 +0,0 @@
-package net.perfectdreams.loritta.cinnamon.discord.interactions.vanilla.discord.declarations
-
-import net.perfectdreams.loritta.cinnamon.locale.LanguageManager
-import net.perfectdreams.loritta.cinnamon.discord.interactions.commands.CinnamonUserCommandDeclarationWrapper
-import net.perfectdreams.loritta.cinnamon.discord.interactions.vanilla.discord.info.UserInfoUserExecutor
-
-class UserInfoUserCommand(languageManager: LanguageManager) : CinnamonUserCommandDeclarationWrapper(languageManager) {
-    override fun declaration() = userCommand(UserCommand.I18N_PREFIX.Info.ViewUserInfo, { UserInfoUserExecutor(it, it.http) })
-}
