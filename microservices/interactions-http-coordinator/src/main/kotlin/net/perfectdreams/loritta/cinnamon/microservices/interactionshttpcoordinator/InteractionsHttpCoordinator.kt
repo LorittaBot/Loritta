c162fbd0a71a1e277bc83b55e00eef9483d3a3be
MrPowerGamerBR <git@mrpowergamerbr.com>
2022-07-29 01:56:15 -0300

    Don't try parsing ping events, just passthru it directly to the first instance

--- microservices/interactions-http-coordinator/src/main/kotlin/net/perfectdreams/loritta/cinnamon/microservices/interactionshttpcoordinator/InteractionsHttpCoordinator.kt
+++ microservices/interactions-http-coordinator/src/main/kotlin/net/perfectdreams/loritta/cinnamon/microservices/interactionshttpcoordinator/InteractionsHttpCoordinator.kt
@@ -1,6 +1,7 @@
 package net.perfectdreams.loritta.cinnamon.microservices.interactionshttpcoordinator
 
 import dev.kord.common.entity.DiscordInteraction
+import dev.kord.common.entity.InteractionType
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.request.*
@@ -10,7 +11,7 @@ import io.ktor.server.netty.*
 import io.ktor.server.request.*
 import io.ktor.server.routing.*
 import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.*
 import mu.KotlinLogging
 import net.perfectdreams.loritta.cinnamon.microservices.interactionshttpcoordinator.config.InteractionsHttpCoordinatorConfig
 import kotlin.time.ExperimentalTime
@@ -39,21 +40,37 @@ class InteractionsHttpCoordinator(private val config: InteractionsHttpCoordinato
                 post("/") {
                     try {
                         val body = call.receiveText()
-                        val event = JsonIgnoreUnknownKeys.decodeFromString<DiscordInteraction>(body)
+                        val parse = JsonIgnoreUnknownKeys.parseToJsonElement(body)
+                            .jsonObject
 
-                        // Let's coordinate!
-                        val guildId = event.guildId.value
+                        val id = parse["id"]!!.jsonPrimitive.long
+                        val type = parse["type"]!!.jsonPrimitive.int
 
-                        val instance = if (guildId == null) {
-                            // Guild ID not set, forward to first registered instance!
+                        val instance = if (type == InteractionType.Ping.type) {
+                            // Ping request, relay to first instance
+                            logger.info { "Request $id is a ping request!" }
+
                             config.instances.first()
                         } else {
-                            val shardId = getShardIdFromGuildId(guildId.value.toLong())
+                            val event = JsonIgnoreUnknownKeys.decodeFromString<DiscordInteraction>(body)
 
-                            config.instances.firstOrNull { shardId in it.minShard..it.maxShard } ?: error("I couldn't find a matching instance for $it! Was Loritta resharded and you forgot to reconfigure the shard ranges on the configuration?")
+                            // Let's coordinate!
+                            val guildId = event.guildId.value
+
+                            logger.info { "Request $id has guild ID $guildId!" }
+
+                            if (guildId == null) {
+                                // Guild ID not set, forward to first registered instance!
+                                config.instances.first()
+                            } else {
+                                val shardId = getShardIdFromGuildId(guildId.value.toLong())
+
+                                config.instances.firstOrNull { shardId in it.minShard..it.maxShard }
+                                    ?: error("I couldn't find a matching instance for $it! Was Loritta resharded and you forgot to reconfigure the shard ranges on the configuration?")
+                            }
                         }
 
-                        logger.info { "Forwarding ${event.id} request to ${instance.url}! Guild ID: $guildId" }
+                        logger.info { "Forwarding $id request to ${instance.url}! Type: $type" }
 
                         // Forward the request as is
                         val duration = measureTime {
@@ -66,7 +83,7 @@ class InteractionsHttpCoordinator(private val config: InteractionsHttpCoordinato
                             }
                         }
 
-                        logger.info { "Request ${event.id} was successfully forwarded to ${instance.url}! Guild ID: $guildId - Took $duration" }
+                        logger.info { "Request $id was successfully forwarded to ${instance.url}! - Took $duration" }
                     } catch (e: Exception) {
                         logger.warn(e) { "Something went wrong while trying to forward the request!" }
                     }
