6110314bcd65ba1a97eb47e1fdda30172521f480
MrPowerGamerBR <git@mrpowergamerbr.com>
2022-08-27 15:05:10 -0300

    Improve "DateUtils.formatDateDiff(...)"

--- discord/discord-common/src/main/kotlin/net/perfectdreams/loritta/cinnamon/discord/utils/DateUtils.kt
+++ discord/discord-common/src/main/kotlin/net/perfectdreams/loritta/cinnamon/discord/utils/DateUtils.kt
@@ -3,30 +3,39 @@ package net.perfectdreams.loritta.cinnamon.discord.utils
 import net.perfectdreams.i18nhelper.core.I18nContext
 import net.perfectdreams.i18nhelper.core.keydata.StringI18nData
 import net.perfectdreams.loritta.cinnamon.i18n.I18nKeysData
-import kotlin.time.Duration.Companion.milliseconds
+import java.time.*
 
 object DateUtils {
-    fun formatDateDiff(i18nContext: I18nContext, fromDate: Long, toDate: Long): String {
-        val diff = toDate - fromDate
-        val duration = diff.milliseconds
+    fun formatDateDiff(i18nContext: I18nContext, fromEpochMilli: Long, toEpochMilli: Long): String {
+        // https://stackoverflow.com/a/59119149/7271796
+        val fromDateTime = LocalDateTime.ofInstant(Instant.ofEpochMilli(fromEpochMilli), ZoneId.of("America/Sao_Paulo"))
+        val toDateTime = LocalDateTime.ofInstant(Instant.ofEpochMilli(toEpochMilli), ZoneId.of("America/Sao_Paulo"))
+
+        // get the calendar period between the times (years, months & days)
+        var period: Period = Period.between(fromDateTime.toLocalDate(), toDateTime.toLocalDate())
+
+        // make sure to get the floor of the number of days
+        period = period.minusDays(if (toDateTime.toLocalTime() >= fromDateTime.toLocalTime()) 0 else 1)
+
+        // get the remainder as a duration (hours, minutes, etc.)
+        var duration: Duration = Duration.between(fromDateTime, toDateTime)
+
+        // remove days, already counted in the period
+        duration = duration.minusDays(duration.toDaysPart())
 
         val parts = mutableListOf<String>()
 
-        fun createAndAdd(value: Long, key: (Long) -> (StringI18nData)) {
+        fun createAndAdd(value: Int, key: (Int) -> (StringI18nData)) {
             if (value >= 1L)
                 parts.add(i18nContext.get(key.invoke(value)))
         }
 
-        duration.toComponents { wholeDays, hours, minutes, seconds, _ ->
-            val years = wholeDays / 365
-            val days = wholeDays % 365
-
-            createAndAdd(years) { I18nKeysData.Time.Years(it) }
-            createAndAdd(days) { I18nKeysData.Time.Days(it) }
-            createAndAdd(hours.toLong()) { I18nKeysData.Time.Hours(it) }
-            createAndAdd(minutes.toLong()) { I18nKeysData.Time.Minutes(it) }
-            createAndAdd(seconds.toLong()) { I18nKeysData.Time.Seconds(it) }
-        }
+        createAndAdd(period.years) { I18nKeysData.Time.Years(it) }
+        createAndAdd(period.months) { I18nKeysData.Time.Months(it) }
+        createAndAdd(period.days) { I18nKeysData.Time.Days(it) }
+        createAndAdd(duration.toHoursPart()) { I18nKeysData.Time.Hours(it) }
+        createAndAdd(duration.toMinutesPart()) { I18nKeysData.Time.Minutes(it) }
+        createAndAdd(duration.toSecondsPart()) { I18nKeysData.Time.Seconds(it) }
 
         return if (parts.isEmpty())
             i18nContext.get(I18nKeysData.Time.AFewMilliseconds)
