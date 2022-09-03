e74327306b50cbf6b2fd662b1ea88ad2a92b1521
MrPowerGamerBR <git@mrpowergamerbr.com>
2022-09-02 22:44:45 -0300

    Decrease timeout from 60s to 50s

--- platforms/discord/legacy/src/main/java/net/perfectdreams/loritta/utils/ProcessDiscordGatewayCommands.kt
+++ platforms/discord/legacy/src/main/java/net/perfectdreams/loritta/utils/ProcessDiscordGatewayCommands.kt
@@ -38,7 +38,7 @@ class ProcessDiscordGatewayCommands(
                     // Why the timeout can't be infinite: https://lettuce.io/core/release/reference/#faq.timeout.blpop
                     // The reason it is 60s is because Lettuce's default timeout is also 60s, and if you don't set a timeout here,
                     // the command will still be in Lettuce's command queue, so Lettuce will execute the command and just discard the response.
-                    60,
+                    50.0,
                     LMPopArgs.Builder.left().count(1),
                     *keys
                 )
