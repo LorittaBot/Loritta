3ee3b70c9d2d4b5b17b2b06f0cfcd4486f51f1ab
MrPowerGamerBR <git@mrpowergamerbr.com>
2022-09-26 15:12:06 -0300

    Remove unused CommandDeclaration stuff
    Once again, not sure why they exist...?

--- discord/loritta-bot-common/src/main/kotlin/net/perfectdreams/loritta/morenitta/commands/declarations/CommandExecutorDeclaration.kt
+++
@@ -1,8 +0,0 @@
-package net.perfectdreams.loritta.morenitta.commands.declarations
-
-import net.perfectdreams.loritta.common.commands.options.CommandOptions
-import kotlin.reflect.KClass
-
-open class CommandExecutorDeclaration(val parent: KClass<*>) {
-    open val options: CommandOptions = CommandOptions.NO_OPTIONS
-}
