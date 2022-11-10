93dd90e39ec1373c49f4288895bcd2dac0d950de
MrPowerGamerBR <git@mrpowergamerbr.com>
2022-11-10 11:51:03 -0300

    Should be retrieveMemberOrNullById here

--- discord/loritta-bot-common/src/main/kotlin/net/perfectdreams/loritta/morenitta/website/routes/dashboard/DashboardRoute.kt
+++ discord/loritta-bot-common/src/main/kotlin/net/perfectdreams/loritta/morenitta/website/routes/dashboard/DashboardRoute.kt
@@ -56,7 +56,7 @@ class DashboardRoute(loritta: LorittaBot) : RequiresDiscordLoginLocalizedRoute(l
         val guilds = userGuilds.filter {
             val guild = loritta.lorittaShards.getGuildById(it.id)
             if (guild != null) {
-                val member = guild.retrieveMemberById(lorittaProfile.userId)
+                val member = guild.retrieveMemberOrNullById(lorittaProfile.userId)
                 val config = serverConfigs.firstOrNull { config -> config.guildId.toString() == it.id }
                 if (member != null && config != null) { // As vezes member == null, então vamos verificar se não é null antes de verificar as permissões
                     val lorittaUser = GuildLorittaUser(
