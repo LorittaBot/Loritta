1b5c82928f4b75414ed6bc7ac314697468f2b562
MrPowerGamerBR <git@mrpowergamerbr.com>
2022-12-12 22:45:59 -0300

    More LoriTuber ideas and stuff

--- discord/loritta-bot-common/src/main/kotlin/net/perfectdreams/loritta/morenitta/interactions/vanilla/lorituber/LoriTuberCommand.kt
+++ discord/loritta-bot-common/src/main/kotlin/net/perfectdreams/loritta/morenitta/interactions/vanilla/lorituber/LoriTuberCommand.kt
@@ -1,16 +1,14 @@
 package net.perfectdreams.loritta.morenitta.interactions.vanilla.lorituber
 
-import net.dv8tion.jda.api.interactions.components.ActionRow
+import dev.minn.jda.ktx.messages.MessageEdit
+import net.dv8tion.jda.api.entities.User
+import net.dv8tion.jda.api.interactions.InteractionHook
 import net.dv8tion.jda.api.interactions.components.buttons.ButtonStyle
-import net.dv8tion.jda.api.interactions.components.selections.SelectMenu
-import net.dv8tion.jda.api.interactions.components.selections.StringSelectMenu
-import net.dv8tion.jda.api.interactions.components.text.TextInputStyle
-import net.perfectdreams.loritta.cinnamon.discord.interactions.commands.styled
 import net.perfectdreams.loritta.common.commands.CommandCategory
 import net.perfectdreams.loritta.i18n.I18nKeysData
 import net.perfectdreams.loritta.morenitta.LorittaBot
 import net.perfectdreams.loritta.morenitta.interactions.commands.*
-import net.perfectdreams.loritta.morenitta.interactions.modals.options.modalString
+import net.perfectdreams.loritta.morenitta.utils.extensions.await
 
 class LoriTuberCommand(val loritta: LorittaBot) : SlashCommandDeclarationWrapper {
     companion object {
@@ -18,137 +16,95 @@ class LoriTuberCommand(val loritta: LorittaBot) : SlashCommandDeclarationWrapper
     }
 
     override fun command() = slashCommand(I18N_PREFIX.Label, I18N_PREFIX.Description, CommandCategory.ECONOMY) {
-        subcommandGroup(I18N_PREFIX.Character.Label, I18N_PREFIX.Character.Description) {
-            subcommand(I18N_PREFIX.Character.Create.Label, I18N_PREFIX.Character.Create.Description) {
-                executor = LoriTuberCharacterCreatorExecutor()
-            }
+        executor = LoriTuberExecutor()
+    }
 
-            subcommand(I18N_PREFIX.Character.Change.Label, I18N_PREFIX.Character.Change.Description) {
-                executor = LoriTuberCharacterChangerExecutor()
-            }
-        }
+    inner class LoriTuberExecutor : LorittaSlashCommandExecutor() {
+        override suspend fun execute(context: ApplicationCommandContext, args: SlashCommandArguments) {
+            val hook = context.deferChannelMessage()
 
-        subcommandGroup(I18N_PREFIX.Channel.Label, I18N_PREFIX.Channel.Description) {
-            subcommand(I18N_PREFIX.Channel.Create.Label, I18N_PREFIX.Channel.Create.Description) {
-                executor = LoriTuberChannelCreateExecutor()
-            }
-
-            subcommand(I18N_PREFIX.Channel.Manage.Label, I18N_PREFIX.Channel.Manage.Description) {
-                executor = LoriTuberChannelManagerExecutor()
-            }
+            createUI(context.user, hook, LoriTuberScreen.ViewChannel)
         }
     }
 
-    inner class LoriTuberCharacterCreatorExecutor : LorittaSlashCommandExecutor() {
-        override suspend fun execute(context: ApplicationCommandContext, args: SlashCommandArguments) {
-            context.deferChannelMessage()
-
-            context.reply(false) {
-                content = "Bem-vindx a DreamLand!"
-
-                actionRow(
-                    loritta.interactivityManager.buttonForUser(
-                        context.user,
-                        ButtonStyle.PRIMARY,
-                        "Criar Personagem"
-                    ) {
-                        val characterNameOption = modalString("Nome do Personagem", TextInputStyle.SHORT)
-
-                        it.sendModal(
-                            "Criação de Personagem",
-                            listOf(ActionRow.of(characterNameOption.toJDA()))
-                        ) { it, args ->
-                            val result = args[characterNameOption]
-
-                            it.reply(false) {
-                                content = "Nome: $result"
-                            }
-                        }
-                    }
-                )
-            }
-        }
-    }
-
-    inner class LoriTuberCharacterChangerExecutor : LorittaSlashCommandExecutor() {
-        override suspend fun execute(context: ApplicationCommandContext, args: SlashCommandArguments) {
-            context.reply(false) {
-                styled("Atualmente não é possível ter mais de um personagem no LoriTuber!")
-            }
-        }
-    }
-
-    inner class LoriTuberChannelCreateExecutor : LorittaSlashCommandExecutor() {
-        override suspend fun execute(context: ApplicationCommandContext, args: SlashCommandArguments) {
-            context.reply(false) {
-                content = "É a hora de começar a grande jornada da Loritta Morenitta virar uma estrela de sucesso na DreamLand!"
-
-                actionRow(
-                    loritta.interactivityManager.buttonForUser(
-                        context.user,
-                        ButtonStyle.PRIMARY,
-                        "Criar Canal"
-                    ) {
-                        val channelNameOption = modalString("Nome do Canal", TextInputStyle.SHORT)
-
-                        it.sendModal(
-                            "Criação do Canal",
-                            listOf(ActionRow.of(channelNameOption.toJDA()))
-                        ) { it, args ->
-                            val result = args[channelNameOption]
-
-                            it.reply(false) {
-                                content = "Nome do Canal: $result"
-                            }
-                        }
-                    }
-                )
-            }
-        }
-    }
-
-    inner class LoriTuberChannelManagerExecutor : LorittaSlashCommandExecutor() {
-        override suspend fun execute(context: ApplicationCommandContext, args: SlashCommandArguments) {
-            context.reply(false) {
-                embed {
-                    title = "Canal MrBeast6000"
-
-                    description = "Olá Loritta Morenitta!\n\n\uD83D\uDCC8 1.000 views\n\uD83E\uDD29 1.000 inscritos"
+    suspend fun createUI(user: User, hook: InteractionHook, screen: LoriTuberScreen) {
+        when (screen) {
+            is LoriTuberScreen.ViewChannel -> {
+                val button = loritta.interactivityManager.buttonForUser(
+                    user,
+                    ButtonStyle.PRIMARY,
+                    "Switch to Motives",
+                ) {
+                    createUI(user, it.deferEdit(), LoriTuberScreen.ViewMotives)
                 }
 
-                actionRow(
-                    loritta.interactivityManager.buttonForUser(
-                        context.user,
-                        ButtonStyle.PRIMARY,
-                        "Criar um Vídeo"
-                    ) {
-                        it.reply(false) {
-                            embed {
-                                description = "Qual será o tema do vídeo?\n\nVocê pode ver a popularidade de cada tema em *insira comando aqui*\n\nLembre-se de manter um tema em comum para o seu canal!"
-                            }
+                hook.editOriginal(
+                    MessageEdit {
+                        embed {
+                            title = "Sobre o seu Canal MrBeast6000"
 
-                            actionRow(
-                                StringSelectMenu.create("dummy")
-                                    .addOption("Gameplay", "gameplay")
-                                    .addOption("Educacional", "educacional")
-                                    .addOption("Política", "politica")
-                                    .addOption("Vlog", "vlog")
-                                    .build(),
-                            )
+                            field("ayaya", "yay!!!")
                         }
-                    },
 
-                    /* loritta.interactivityManager.buttonForUser(
-                        context.user,
-                        ButtonStyle.PRIMARY,
-                        "Livestream"
-                    ) {
-                        it.reply(false) {
-                            content = "Livestreams são um ótimo jeito de conseguir fãs leais e fiéis ao seu conteúdo!\n\nEntretanto, você terá um retorno pequeno se fizer uma livestream enquanto não tem um público grande."
+                        actionRow(button)
+                    }
+                ).await()
+            }
+
+            LoriTuberScreen.ViewMotives -> {
+                val button = loritta.interactivityManager.buttonForUser(
+                    user,
+                    ButtonStyle.PRIMARY,
+                    "Switch to Channel",
+                ) {
+                    createUI(user, it.deferEdit(), LoriTuberScreen.ViewChannel)
+                }
+
+                val sleep = loritta.interactivityManager.buttonForUser(
+                    user,
+                    ButtonStyle.PRIMARY,
+                    "Dormir",
+                ) {
+                    createUI(user, it.deferEdit(), LoriTuberScreen.Sleeping)
+                }
+
+                hook.editOriginal(
+                    MessageEdit {
+                        embed {
+                            title = "Loritta Morenitta"
+
+                            field("Energia", "[XXXXX]")
                         }
-                    } */
-                )
+
+                        actionRow(button)
+                        actionRow(sleep)
+                    }
+                ).await()
+            }
+
+            LoriTuberScreen.Sleeping -> {
+                val wakeUp = loritta.interactivityManager.buttonForUser(
+                    user,
+                    ButtonStyle.PRIMARY,
+                    "Acordar!",
+                ) {
+                    createUI(user, it.deferEdit(), LoriTuberScreen.ViewMotives)
+                }
+
+                hook.editOriginal(
+                    MessageEdit {
+                        content = "A mimir..."
+
+                        actionRow(wakeUp)
+                    }
+                ).setReplace(true).await()
             }
         }
+    }
+
+    sealed class LoriTuberScreen {
+        object ViewChannel : LoriTuberScreen()
+        object ViewMotives : LoriTuberScreen()
+        object Sleeping : LoriTuberScreen()
     }
 }
