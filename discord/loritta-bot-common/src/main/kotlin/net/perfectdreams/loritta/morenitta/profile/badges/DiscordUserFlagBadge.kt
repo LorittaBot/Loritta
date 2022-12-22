e466a749d0e9a0fd0276e7a74e79fbb01ad2484f
MrPowerGamerBR <git@mrpowergamerbr.com>
2022-12-22 11:21:00 -0300

    Fix Discord badge path

--- discord/loritta-bot-common/src/main/kotlin/net/perfectdreams/loritta/morenitta/profile/badges/DiscordUserFlagBadge.kt
+++ discord/loritta-bot-common/src/main/kotlin/net/perfectdreams/loritta/morenitta/profile/badges/DiscordUserFlagBadge.kt
@@ -6,13 +6,13 @@ import net.perfectdreams.loritta.morenitta.profile.Badge
 import net.perfectdreams.loritta.morenitta.profile.ProfileUserInfoData
 
 open class DiscordUserFlagBadge(val flag: UserFlag, badgeName: String) : Badge(badgeName, 50) {
-	class DiscordBraveryHouseBadge : DiscordUserFlagBadge(UserFlag.HouseBravery, "badges/discord_bravery.png")
-	class DiscordBrillanceHouseBadge : DiscordUserFlagBadge(UserFlag.HouseBrilliance, "badges/discord_brillance.png")
-	class DiscordBalanceHouseBadge : DiscordUserFlagBadge(UserFlag.HouseBalance, "badges/discord_balance.png")
-	class DiscordEarlySupporterBadge : DiscordUserFlagBadge(UserFlag.EarlySupporter,"badges/discord_early_supporter.png")
-	class DiscordPartnerBadge : DiscordUserFlagBadge(UserFlag.DiscordPartner,"badges/discord_partner.png")
-	class DiscordHypesquadEventsBadge : DiscordUserFlagBadge(UserFlag.HypeSquad,"badges/hypesquad_events.png")
-	class DiscordVerifiedDeveloperBadge : DiscordUserFlagBadge(UserFlag.VerifiedBotDeveloper,"badges/verified_developer.png")
+	class DiscordBraveryHouseBadge : DiscordUserFlagBadge(UserFlag.HouseBravery, "discord_bravery.png")
+	class DiscordBrillanceHouseBadge : DiscordUserFlagBadge(UserFlag.HouseBrilliance, "discord_brillance.png")
+	class DiscordBalanceHouseBadge : DiscordUserFlagBadge(UserFlag.HouseBalance, "discord_balance.png")
+	class DiscordEarlySupporterBadge : DiscordUserFlagBadge(UserFlag.EarlySupporter,"discord_early_supporter.png")
+	class DiscordPartnerBadge : DiscordUserFlagBadge(UserFlag.DiscordPartner,"discord_partner.png")
+	class DiscordHypesquadEventsBadge : DiscordUserFlagBadge(UserFlag.HypeSquad,"hypesquad_events.png")
+	class DiscordVerifiedDeveloperBadge : DiscordUserFlagBadge(UserFlag.VerifiedBotDeveloper,"verified_developer.png")
 
 	override suspend fun checkIfUserDeservesBadge(user: ProfileUserInfoData, profile: Profile, mutualGuilds: Set<Long>) = user.flags.contains(flag)
 }
