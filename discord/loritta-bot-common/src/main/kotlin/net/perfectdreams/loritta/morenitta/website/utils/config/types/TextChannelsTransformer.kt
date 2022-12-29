cf583a70364e1641ebb7dfaf9404a4b1787412f1
MrPowerGamerBR <git@mrpowergamerbr.com>
2022-12-29 16:08:09 -0300

    To keep consistency of how this worked in pre-JDA v4, use StandardGuildMessageChannel instead of TextChannels

--- discord/loritta-bot-common/src/main/kotlin/net/perfectdreams/loritta/morenitta/website/utils/config/types/TextChannelsTransformer.kt
+++ discord/loritta-bot-common/src/main/kotlin/net/perfectdreams/loritta/morenitta/website/utils/config/types/TextChannelsTransformer.kt
@@ -6,6 +6,7 @@ import com.google.gson.JsonElement
 import com.google.gson.JsonObject
 import net.perfectdreams.loritta.morenitta.dao.ServerConfig
 import net.dv8tion.jda.api.entities.Guild
+import net.dv8tion.jda.api.entities.channel.middleman.StandardGuildMessageChannel
 
 object TextChannelsTransformer : ConfigTransformer {
     override val payloadType: String = "textchannels"
@@ -16,13 +17,15 @@ object TextChannelsTransformer : ConfigTransformer {
     }
 
     override suspend fun toJson(guild: Guild, serverConfig: ServerConfig): JsonElement {
-        return guild.textChannels.map {
-            jsonObject(
+        return guild.channels
+            .filterIsInstance<StandardGuildMessageChannel>()
+            .map {
+                jsonObject(
                     "id" to it.idLong,
                     "canTalk" to it.canTalk(),
                     "name" to it.name,
                     "topic" to it.topic
-            )
-        }.toJsonArray()
+                )
+            }.toJsonArray()
     }
 }
