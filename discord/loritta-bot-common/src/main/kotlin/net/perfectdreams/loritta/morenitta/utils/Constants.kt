cdaf73d472c1d28c97270a36e58b2c2e7eca10ab
MrPowerGamerBR <git@mrpowergamerbr.com>
2023-02-07 22:26:30 -0300

    Change LORITTA_AQUA color to Loritta's new (not really new lmao) blue color

--- discord/loritta-bot-common/src/main/kotlin/net/perfectdreams/loritta/morenitta/utils/Constants.kt
+++ discord/loritta-bot-common/src/main/kotlin/net/perfectdreams/loritta/morenitta/utils/Constants.kt
@@ -118,7 +118,7 @@ object Constants {
 
 	// ===[ COLORS ]===
 	val DISCORD_BLURPLE = Color(114, 137, 218)
-	val LORITTA_AQUA = Color(26, 160, 254)
+	val LORITTA_AQUA = Color(41, 166, 254)
 	val ROBLOX_RED = Color(226, 35, 26)
 	val IMAGE_FALLBACK by lazy { ImageIO.read(File(LorittaBot.ASSETS, "avatar0.png")) }
 	val URL_PATTERN = Pattern.compile("[-a-zA-Z0-9@:%._+~#=]{2,256}\\.[A-z]{2,7}\\b([-a-zA-Z0-9@:%_+.~#?&/=]*)")
