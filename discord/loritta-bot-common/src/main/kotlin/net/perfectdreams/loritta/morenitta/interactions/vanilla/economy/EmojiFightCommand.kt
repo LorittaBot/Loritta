97436a2f5a3b7b1af26f17cd2b55b5a40ad9a0f0
MrPowerGamerBR <git@mrpowergamerbr.com>
2023-02-26 22:05:11 -0300

    Change "/emojifight" to "/emojifight iniciar", add "/emojifight emoji"

--- discord/loritta-bot-common/src/main/kotlin/net/perfectdreams/loritta/morenitta/interactions/vanilla/economy/EmojiFightCommand.kt
+++ discord/loritta-bot-common/src/main/kotlin/net/perfectdreams/loritta/morenitta/interactions/vanilla/economy/EmojiFightCommand.kt
@@ -1,39 +1,49 @@
 package net.perfectdreams.loritta.morenitta.interactions.vanilla.economy
 
+import net.dv8tion.jda.api.entities.emoji.CustomEmoji
+import net.dv8tion.jda.api.entities.emoji.Emoji
 import net.perfectdreams.loritta.cinnamon.discord.interactions.commands.styled
 import net.perfectdreams.loritta.common.commands.CommandCategory
 import net.perfectdreams.loritta.common.utils.Emotes
 import net.perfectdreams.loritta.common.utils.GACampaigns
+import net.perfectdreams.loritta.common.utils.UserPremiumPlans
 import net.perfectdreams.loritta.i18n.I18nKeysData
 import net.perfectdreams.loritta.morenitta.LorittaBot
 import net.perfectdreams.loritta.morenitta.commands.vanilla.economy.EmojiFight
 import net.perfectdreams.loritta.morenitta.interactions.CommandContextCompat
 import net.perfectdreams.loritta.morenitta.interactions.commands.*
 import net.perfectdreams.loritta.morenitta.interactions.commands.options.ApplicationCommandOptions
-import net.perfectdreams.loritta.morenitta.messages.LorittaReply
-import net.perfectdreams.loritta.morenitta.utils.*
+import net.perfectdreams.loritta.morenitta.utils.AccountUtils
+import net.perfectdreams.loritta.morenitta.utils.Constants
+import net.perfectdreams.loritta.morenitta.utils.NumberUtils
 
 class EmojiFightCommand(val loritta: LorittaBot) : SlashCommandDeclarationWrapper {
-    override fun command() = slashCommand(I18N_PREFIX.Label, I18N_PREFIX.Description, CommandCategory.ECONOMY) {
-        isGuildOnly = true
-
-        executor = BanInfoExecutor()
-    }
-
     companion object {
         private val I18N_PREFIX = I18nKeysData.Commands.Command.Emojifight
     }
 
-    inner class BanInfoExecutor : LorittaSlashCommandExecutor() {
+    override fun command() = slashCommand(I18N_PREFIX.Label, I18N_PREFIX.Description, CommandCategory.ECONOMY) {
+        isGuildOnly = true
+
+        subcommand(I18N_PREFIX.Start.Label, I18N_PREFIX.Start.Description) {
+            executor = EmojiFightStartExecutor()
+        }
+
+        subcommand(I18N_PREFIX.Emoji.Label, I18N_PREFIX.Emoji.Description) {
+            executor = EmojiFightChangeEmojiExecutor()
+        }
+    }
+
+    inner class EmojiFightStartExecutor : LorittaSlashCommandExecutor() {
         inner class Options : ApplicationCommandOptions() {
             val sonhos = optionalString(
                 "sonhos",
-                I18N_PREFIX.Options.Sonhos.Text
+                I18N_PREFIX.Start.Options.Sonhos.Text
             )
 
             val maxPlayers = optionalLong(
                 "max_players",
-                I18N_PREFIX.Options.MaxPlayers.Text,
+                I18N_PREFIX.Start.Options.MaxPlayers.Text,
                 requiredRange = 2..EmojiFight.DEFAULT_MAX_PLAYER_COUNT.toLong()
             )
         }
@@ -87,15 +97,13 @@ class EmojiFightCommand(val loritta: LorittaBot) : SlashCommandDeclarationWrappe
                 }
 
                 // Only allow users to participate in a emoji fight bet if the user got their daily reward today
-                val todayDailyReward = AccountUtils.getUserTodayDailyReward(loritta, selfUserProfile)
-                if (todayDailyReward == null) {
-                    context.fail(true) {
+                AccountUtils.getUserTodayDailyReward(loritta, selfUserProfile)
+                    ?: context.fail(true) {
                         styled(
                             context.locale["commands.youNeedToGetDailyRewardBeforeDoingThisAction", context.config.commandPrefix],
                             Constants.ERROR
                         )
                     }
-                }
 
                 // Self user check
                 run {
@@ -122,6 +130,79 @@ class EmojiFightCommand(val loritta: LorittaBot) : SlashCommandDeclarationWrappe
             )
 
             emojiFight.start()
+        }
+    }
+
+    inner class EmojiFightChangeEmojiExecutor : LorittaSlashCommandExecutor() {
+        inner class Options : ApplicationCommandOptions() {
+            val emoji = optionalString("emoji", I18N_PREFIX.Emoji.Options.Emoji.Text)
+        }
+
+        override val options = Options()
+
+        override suspend fun execute(context: ApplicationCommandContext, args: SlashCommandArguments) {
+            val canUseCustomEmojis = loritta.newSuspendedTransaction {
+                UserPremiumPlans.getPlanFromValue(loritta._getActiveMoneyFromDonations(context.user.idLong)).customEmojisInEmojiFight
+            }
+
+            if (!canUseCustomEmojis) {
+                context.fail(true) {
+                    styled(
+                        "Apenas usuários com plano premium \"Recomendado\" ou superior podem colocar emojis personalizados no emoji fight!",
+                    )
+                }
+            }
+
+            val newEmojiAsString = args[options.emoji]
+
+            if (newEmojiAsString == null) {
+                loritta.newSuspendedTransaction {
+                    loritta.getOrCreateLorittaProfile(context.user.idLong)
+                        .settings
+                        .emojiFightEmoji = null
+                }
+
+                context.reply(true) {
+                    styled(
+                        "Emoji personalizado removido!"
+                    )
+                }
+                return
+            }
+
+            val discordEmoji = Emoji.fromFormatted(newEmojiAsString) as? CustomEmoji
+
+            val newEmoji = if (discordEmoji != null) {
+                discordEmoji.asMention
+            } else {
+                val match = loritta.unicodeEmojiManager.regex.find(newEmojiAsString)
+                    ?: context.fail(true) {
+                        styled(
+                            "Não encontrei nenhum emoji na sua mensagem..."
+                        )
+                    }
+
+                match.value
+            }
+
+            loritta.newSuspendedTransaction {
+                loritta.getOrCreateLorittaProfile(context.user.idLong)
+                    .settings
+                    .emojiFightEmoji = newEmojiAsString
+            }
+
+            if (discordEmoji == null)
+                context.reply(true) {
+                    styled(
+                        "Emoji alterado! Nas próximas rinhas de emoji, o $newEmoji irá te acompanhar nas suas incríveis batalhas cativantes."
+                    )
+                }
+            else
+                context.reply(true) {
+                    styled("Emoji alterado! Nas próximas rinhas de emoji, o $newEmoji irá te acompanhar nas suas incríveis batalhas cativantes.")
+                    styled("Lembre-se que eu preciso estar no servidor onde o emoji está para eu conseguir usar o emoji!")
+                    styled("Observação: Você será banido de usar a Loritta caso você coloque emojis sugestivos ou NSFW. Tenha bom senso e não atrapalhe os servidores dos outros com bobagens!")
+                }
         }
     }
 }
