6ca85b938cda6e742f285fa3f42f01173017abe4
MrPowerGamerBR <git@mrpowergamerbr.com>
2023-03-19 23:09:17 -0300

    Whoops

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -69,7 +69,7 @@ include(":web:embed-editor:embed-renderer")
 // ===[ SHOWTIME ]===
 include(":web:showtime:web-common")
 // TODO: Disabled for now because it depends on Cinnamon stuff that isn't present yet on loritta-bot
-// include(":web:showtime:backend")
+include(":web:showtime:backend")
 include(":web:showtime:showtime-frontend")
 
 // ===[ DASHBOARD ]===
