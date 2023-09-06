766c5c073e1c339374da8ce1d2d3779a79023c2b
MrPowerGamerBR <git@mrpowergamerbr.com>
2023-09-05 21:49:39 -0300

    This is a optional reference, not required

--- pudding/client/src/main/kotlin/net/perfectdreams/loritta/cinnamon/pudding/tables/CollectedCandies.kt
+++ pudding/client/src/main/kotlin/net/perfectdreams/loritta/cinnamon/pudding/tables/CollectedCandies.kt
@@ -3,7 +3,7 @@ package net.perfectdreams.loritta.cinnamon.pudding.tables
 import org.jetbrains.exposed.dao.id.LongIdTable
 
 object CollectedCandies : LongIdTable() {
-	val user = reference("user", Profiles).index()
+	val user = optReference("user", Profiles).index()
 	val guildId = long("guild")
 	val channelId = long("channel")
 	val messageId = long("message")
