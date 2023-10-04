729b9406859dee8aa076594703db6da30b2d62d2
MrPowerGamerBR <git@mrpowergamerbr.com>
2023-10-03 21:28:13 -0300

    Add activeIn and userId indexes to the DonationKeys table

--- pudding/client/src/main/kotlin/net/perfectdreams/loritta/cinnamon/pudding/tables/DonationKeys.kt
+++ pudding/client/src/main/kotlin/net/perfectdreams/loritta/cinnamon/pudding/tables/DonationKeys.kt
@@ -5,8 +5,8 @@ import net.perfectdreams.loritta.cinnamon.pudding.tables.servers.ServerConfigs
 import org.jetbrains.exposed.dao.id.LongIdTable
 
 object DonationKeys : LongIdTable() {
-	val activeIn = optReference("active_in", ServerConfigs)
-	val userId = long("user")
+	val activeIn = optReference("active_in", ServerConfigs).index()
+	val userId = long("user").index()
 	val value = double("value")
 	val expiresAt = long("expires_at")
 	val metadata = jsonb("metadata").nullable()
