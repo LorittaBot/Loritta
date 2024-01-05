c98dc1dc093a9e6162d809b23249637aa502332a
MrPowerGamerBR <git@mrpowergamerbr.com>
2024-01-05 20:36:08 -0300

    Add missing index on remind_at for Reminders

--- pudding/client/src/main/kotlin/net/perfectdreams/loritta/cinnamon/pudding/tables/Reminders.kt
+++ pudding/client/src/main/kotlin/net/perfectdreams/loritta/cinnamon/pudding/tables/Reminders.kt
@@ -6,6 +6,6 @@ object Reminders : LongIdTable() {
 	val userId = long("user_id").index()
 	val guildId = long("guild_id").nullable().index() // This is new, that's why it is nullable
 	val channelId = long("channel_id")
-	val remindAt = long("remind_at")
+	val remindAt = long("remind_at").index()
 	val content = text("content")
 }
