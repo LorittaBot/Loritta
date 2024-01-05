0fe69ff8a2073fcc8d321a524ba82d838d643ac3
MrPowerGamerBR <git@mrpowergamerbr.com>
2024-01-05 20:33:46 -0300

    Add missing index in LevelAnnouncementConfigs

--- pudding/client/src/main/kotlin/net/perfectdreams/loritta/cinnamon/pudding/tables/servers/moduleconfigs/LevelAnnouncementConfigs.kt
+++ pudding/client/src/main/kotlin/net/perfectdreams/loritta/cinnamon/pudding/tables/servers/moduleconfigs/LevelAnnouncementConfigs.kt
@@ -4,7 +4,7 @@ import net.perfectdreams.loritta.serializable.levels.LevelUpAnnouncementType
 import org.jetbrains.exposed.dao.id.LongIdTable
 
 object LevelAnnouncementConfigs : LongIdTable() {
-    val levelConfig = reference("config", LevelConfigs)
+    val levelConfig = reference("config", LevelConfigs).index()
     val type = enumeration("level_up_announcement_type", LevelUpAnnouncementType::class)
     val channelId = long("channel").nullable()
     val onlyIfUserReceivedRoles = bool("only_if_user_received_roles").default(false)
