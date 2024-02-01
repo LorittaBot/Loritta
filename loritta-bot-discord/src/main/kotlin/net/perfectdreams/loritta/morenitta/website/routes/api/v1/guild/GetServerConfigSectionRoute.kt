614e552209a201dc2305cc25970de1578ebe3bb9
MrPowerGamerBR <git@mrpowergamerbr.com>
2024-01-31 23:31:11 -0300

    Log payload responses

--- loritta-bot-discord/src/main/kotlin/net/perfectdreams/loritta/morenitta/website/routes/api/v1/guild/GetServerConfigSectionRoute.kt
+++ loritta-bot-discord/src/main/kotlin/net/perfectdreams/loritta/morenitta/website/routes/api/v1/guild/GetServerConfigSectionRoute.kt
@@ -2,20 +2,26 @@ package net.perfectdreams.loritta.morenitta.website.routes.api.v1.guild
 
 import com.github.salomonbrys.kotson.jsonObject
 import com.github.salomonbrys.kotson.set
-import net.perfectdreams.loritta.morenitta.dao.ServerConfig
-import io.ktor.server.application.ApplicationCall
+import io.ktor.server.application.*
+import mu.KotlinLogging
 import net.dv8tion.jda.api.entities.Guild
 import net.perfectdreams.loritta.morenitta.LorittaBot
+import net.perfectdreams.loritta.morenitta.dao.ServerConfig
 import net.perfectdreams.loritta.morenitta.website.LorittaWebsite
 import net.perfectdreams.loritta.morenitta.website.routes.api.v1.RequiresAPIGuildAuthRoute
-import net.perfectdreams.loritta.temmiewebsession.LorittaJsonWebSession
 import net.perfectdreams.loritta.morenitta.website.utils.extensions.respondJson
+import net.perfectdreams.loritta.morenitta.website.utils.extensions.trueIp
+import net.perfectdreams.loritta.temmiewebsession.LorittaJsonWebSession
 import net.perfectdreams.temmiediscordauth.TemmieDiscordAuth
 
 class GetServerConfigSectionRoute(
 	loritta: LorittaBot,
 	val website: LorittaWebsite
 ) : RequiresAPIGuildAuthRoute(loritta, "/config/{sections}") {
+	companion object {
+		private val logger = KotlinLogging.logger {}
+	}
+
 	override suspend fun onGuildAuthenticatedRequest(call: ApplicationCall, discordAuth: TemmieDiscordAuth, userIdentification: LorittaJsonWebSession.UserIdentification, guild: Guild, serverConfig: ServerConfig) {
 		val sections = call.parameters["sections"]!!.split(",").toSet()
 
@@ -27,6 +33,8 @@ class GetServerConfigSectionRoute(
 			if (transformer != null)
 				payload[transformer.configKey] = transformer.toJson(userIdentification, guild, serverConfig)
 		}
+
+		logger.info { "Response for ${call.request.trueIp} (guild: ${guild.idLong} / user: ${userIdentification.id}) is $payload" }
 
 		call.respondJson(payload)
 	}
