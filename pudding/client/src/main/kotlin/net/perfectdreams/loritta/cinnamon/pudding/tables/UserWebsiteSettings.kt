673f4d800bc0cf9eb5a3f83bc62938bd18d7e489
MrPowerGamerBR <git@mrpowergamerbr.com>
2024-05-14 20:53:12 -0300

    Whoops, forgot about this

--- pudding/client/src/main/kotlin/net/perfectdreams/loritta/cinnamon/pudding/tables/UserWebsiteSettings.kt
+++ pudding/client/src/main/kotlin/net/perfectdreams/loritta/cinnamon/pudding/tables/UserWebsiteSettings.kt
@@ -1,7 +1,10 @@
 package net.perfectdreams.loritta.cinnamon.pudding.tables
 
-import net.perfectdreams.exposedpowerutils.sql.jsonb
+import net.perfectdreams.exposedpowerutils.sql.javatime.timestampWithTimeZone
+import net.perfectdreams.exposedpowerutils.sql.postgresEnumeration
+import net.perfectdreams.loritta.serializable.ColorTheme
 
 object UserWebsiteSettings : SnowflakeTable() {
-    val favoritedGuilds = jsonb("favorited_guilds").nullable()
+    val dashboardColorThemePreference = postgresEnumeration<ColorTheme>("dashboard_color_theme_preference").nullable()
+    val dashboardColorThemePreferenceUpdatedAt = timestampWithTimeZone("dashboard_color_theme_preference_updated_at").nullable()
 }
