b0b5e372311412a3e64ef583c7dbcc9c48adbe8f
MrPowerGamerBR <git@mrpowergamerbr.com>
2024-06-05 22:36:18 -0300

    Fix sticker fan badge

--- loritta-bot-discord/src/main/kotlin/net/perfectdreams/loritta/morenitta/profile/badges/StickerFanBadge.kt
+++ loritta-bot-discord/src/main/kotlin/net/perfectdreams/loritta/morenitta/profile/badges/StickerFanBadge.kt
@@ -1,15 +1,15 @@
 package net.perfectdreams.loritta.morenitta.profile.badges
 
 import net.perfectdreams.loritta.cinnamon.pudding.Pudding
-import net.perfectdreams.loritta.cinnamon.pudding.tables.UserAchievements
+import net.perfectdreams.loritta.cinnamon.pudding.tables.loricoolcards.LoriCoolCardsEvents
 import net.perfectdreams.loritta.cinnamon.pudding.tables.loricoolcards.LoriCoolCardsFinishedAlbumUsers
-import net.perfectdreams.loritta.common.achievements.AchievementType
 import net.perfectdreams.loritta.morenitta.dao.Profile
 import net.perfectdreams.loritta.morenitta.profile.Badge
 import net.perfectdreams.loritta.morenitta.profile.ProfileDesignManager
 import net.perfectdreams.loritta.morenitta.profile.ProfileUserInfoData
+import org.jetbrains.exposed.sql.SortOrder
 import org.jetbrains.exposed.sql.and
-import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.selectAll
 import java.util.*
 
 class StickerFanBadge(val pudding: Pudding) : Badge.LorittaBadge(
@@ -21,7 +21,14 @@ class StickerFanBadge(val pudding: Pudding) : Badge.LorittaBadge(
 ) {
 	override suspend fun checkIfUserDeservesBadge(user: ProfileUserInfoData, profile: Profile, mutualGuilds: Set<Long>): Boolean {
 		return pudding.transaction {
-			LoriCoolCardsFinishedAlbumUsers.select { LoriCoolCardsFinishedAlbumUsers.user eq user.id }
+			// Get the latest finished album
+			val latestEvent = LoriCoolCardsEvents.select(LoriCoolCardsEvents.id)
+				.orderBy(LoriCoolCardsEvents.endsAt, SortOrder.DESC)
+				.limit(1)
+				.firstOrNull() ?: return@transaction false // There hasn't had any cool cards event yet...
+
+			// Have we finished that event?
+			LoriCoolCardsFinishedAlbumUsers.selectAll().where { LoriCoolCardsFinishedAlbumUsers.user eq user.id and (LoriCoolCardsFinishedAlbumUsers.event eq latestEvent[LoriCoolCardsEvents.id]) }
 				.count() == 1L
 		}
 	}
