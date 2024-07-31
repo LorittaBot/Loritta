90e662cbfac8868f7e2bb8e139d7d5c638088866
MrPowerGamerBR <git@mrpowergamerbr.com>
2024-07-31 16:31:58 -0300

    Use multiple message renderers

--- discord-chat-message-renderer-server/src/main/kotlin/net/perfectdreams/loritta/discordchatmessagerendererserver/DiscordChatMessageRendererServer.kt
+++ discord-chat-message-renderer-server/src/main/kotlin/net/perfectdreams/loritta/discordchatmessagerendererserver/DiscordChatMessageRendererServer.kt
@@ -12,22 +12,32 @@ import mu.KotlinLogging
 import net.perfectdreams.loritta.discordchatmessagerenderer.DiscordMessageRendererManager
 import net.perfectdreams.loritta.discordchatmessagerenderer.savedmessage.SavedMessage
 import java.time.ZoneId
+import java.util.concurrent.LinkedBlockingQueue
+import kotlin.time.measureTimedValue
 
 class DiscordChatMessageRendererServer {
     private val logger = KotlinLogging.logger {}
-    private val rendererManager = DiscordMessageRendererManager(
-        ZoneId.of("America/Sao_Paulo"),
-        setOf(
-            "image/gif",
-            "image/jpeg",
-            "image/bmp",
-            "image/png"
+    private val rendererManagers = (0 until 4).map {
+        DiscordMessageRendererManager(
+            ZoneId.of("America/Sao_Paulo"),
+            setOf(
+                "image/gif",
+                "image/jpeg",
+                "image/bmp",
+                "image/png"
+            )
         )
-    )
+    }
+    private val availableRenderers = LinkedBlockingQueue<DiscordMessageRendererManager>()
     private var successfulRenders = 0
     private var failedRenders = 0
 
     fun start() {
+        logger.info { "Using ${rendererManagers.size} renderers" }
+        for (rendererManager in rendererManagers) {
+            availableRenderers.put(rendererManager)
+        }
+
         val http = embeddedServer(Netty, port = 8080) {
             routing {
                 post("/generate-message") {
@@ -35,8 +45,15 @@ class DiscordChatMessageRendererServer {
 
                     val savedMessage = Json.decodeFromString<SavedMessage>(body)
 
+                    logger.info { "Attempting to get a available renderer... Available renderers: ${availableRenderers.size}" }
+                    
+                    val rendererManager = measureTimedValue { availableRenderers.take() }.also {
+                        logger.info { "Took ${it.duration} to get an available renderer for ${savedMessage.id}" }
+                    }.value
+
                     try {
                         val image = rendererManager.renderMessage(savedMessage, null)
+                        availableRenderers.add(rendererManager)
 
                         call.respondBytes(
                             image,
@@ -53,6 +70,9 @@ class DiscordChatMessageRendererServer {
                         )
                         failedRenders++
                         logger.info { "Successfully rendered message ${savedMessage.id}! Successful renders: $successfulRenders; Failed renders: $failedRenders" }
+                    } finally {
+                        logger.info { "Putting $rendererManager back into the available renderers queue" }
+                        availableRenderers.add(rendererManager)
                     }
                 }
             }
