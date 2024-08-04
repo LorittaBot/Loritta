1ed553a895967e50c103484808658fe3aaa6c6af
MrPowerGamerBR <git@mrpowergamerbr.com>
2024-08-04 15:41:30 -0300

    Split the render process to wait for DOMContentLoaded and asset loading individually, add proper timeouts, stop asset loading after load event has been invoked

--- discord-chat-message-renderer-server/src/main/kotlin/net/perfectdreams/loritta/discordchatmessagerenderer/DiscordMessageRendererManager.kt
+++ discord-chat-message-renderer-server/src/main/kotlin/net/perfectdreams/loritta/discordchatmessagerenderer/DiscordMessageRendererManager.kt
@@ -1,6 +1,8 @@
 package net.perfectdreams.loritta.discordchatmessagerenderer
 
 import com.microsoft.playwright.*
+import com.microsoft.playwright.options.LoadState
+import com.microsoft.playwright.options.ScreenshotType
 import com.microsoft.playwright.options.WaitUntilState
 import mu.KotlinLogging
 import java.io.Closeable
@@ -46,46 +48,86 @@ class DiscordMessageRendererManager : Closeable {
             logger.info { "DOMContentLoaded event for $messageId (render request: $savedMessageUniqueId)" }
         }
 
+        page.onConsoleMessage {
+            logger.info { "Page $messageId (render request: $savedMessageUniqueId): ${it.text()}" }
+        }
+
         var writeImageForDebug = false
 
         try {
-            logger.info { "Starting to render message $messageId! - Open pages: ${browserContext.pages().size} (render request: $savedMessageUniqueId)" }
+            val timedValueScreenshot = measureTimedValue {
+                logger.info { "Starting to render message $messageId! - Open pages: ${browserContext.pages().size} (render request: $savedMessageUniqueId)" }
 
-            val timedValueLoadPage = measureTimedValue {
-                logger.info { "Loading message preview page for $messageId! (render request: $savedMessageUniqueId)" }
+                // If this throws an error we are already fucked anyway, there isn't a good fallback here
+                val timedValueDOMContentLoadedPage = measureTimedValue {
+                    logger.info { "Loading message preview page for $messageId! (render request: $savedMessageUniqueId)" }
 
-                try {
                     page.navigate(
                         // We navigate to an URL to avoid any memory leaks that may cause by reusing the same page
                         "http://127.0.0.1:8080/internal/message-preview?message=$savedMessageUniqueId",
-                        // By using LOAD, we force the browser to actually wait the entire page to be loaded
-                        Page.NavigateOptions().setWaitUntil(WaitUntilState.LOAD),
+                        // By using DOMCONTENTLOADED, we force the browser to wait for DOMContentLoaded, we DON'T want to wait for assets here
+                        Page.NavigateOptions().setWaitUntil(WaitUntilState.DOMCONTENTLOADED),
                     )
-
-                    // We don't need to wait anything to load, because Playwright makes sure that everything is loaded when using "WaitUntilState.LOAD" :3
-                } catch (e: TimeoutError) {
-                    logger.warn(e) { "Took too long to load message preview page for $messageId (render request: $savedMessageUniqueId), maybe an image wasn't completely loaded? - We are going to skip the load and attempt to render the message anyway..." }
-                    writeImageForDebug = true
                 }
+
+                logger.info { "Took ${timedValueDOMContentLoadedPage.duration} to DOMContentLoaded the message preview page for $messageId! (render request: $savedMessageUniqueId)" }
+
+                val timedValueLoadPage = measureTimedValue {
+                    logger.info { "Waiting for all assets of the message preview page of $messageId to be loaded! (render request: $savedMessageUniqueId)" }
+
+                    // We will wait at MOST 2.5s
+                    try {
+                        page.waitForLoadState(LoadState.LOAD, Page.WaitForLoadStateOptions().setTimeout(2_500.0))
+                    } catch (e: TimeoutError) {
+                        logger.warn(e) { "Took too long to wait for assets of the message preview page for $messageId (render request: $savedMessageUniqueId), maybe an image wasn't completely loaded? - We are going to skip the load and attempt to render the message anyway..." }
+                        writeImageForDebug = true
+                    }
+                }
+
+                logger.info { "Took ${timedValueLoadPage.duration} to wait for all the message preview page assets of $messageId! (render request: $savedMessageUniqueId)" }
+
+                // STOP! Wait a minute!
+                // Stop all asset loading to avoid any non-loaded assets causing things to shift when taking screenshots
+                // While we could use "window.stop()", it doesn't work that well for images for some reason, sometimes Playwright just keeps loading the image
+                // So, to handle this, we will disable all images that aren't fully loaded
+                page.evaluate("""
+                    // Get all image elements on the page
+                    const images = document.querySelectorAll("img");
+                
+                    // Loop through each image element
+                    images.forEach(function(image) {
+                        // Check if the image is completely loaded
+                        if (!image.complete) {
+                            console.log("Replacing image " + image.src + " with a broken image");
+                            // Replace the src attribute with an invalid image
+                            image.setAttribute("src", "data:image/jpeg;charset=utf-8;base64,");
+                        }
+                    });
+                """.trimIndent())
+
+                val timedValueTakingScreenshot = measureTimedValue {
+                    logger.info { "Taking screenshot of message $messageId! (render request: $savedMessageUniqueId)" }
+
+                    page.querySelector("#wrapper").screenshot(
+                        ElementHandle.ScreenshotOptions()
+                            .setType(ScreenshotType.PNG)
+                    )
+                }
+
+                logger.info { "Took ${timedValueTakingScreenshot.duration} to generate a message screenshot for $messageId! (render request: $savedMessageUniqueId)" }
+
+                if (writeImageForDebug) {
+                    // If a timeout happens, let's write the image to the disk to see what is happening
+                    logger.info { "Writing screenshot of message $messageId (render request: $savedMessageUniqueId) to disk for debugging purposes..." }
+                    File("/debug/$messageId-$savedMessageUniqueId.png").writeBytes(timedValueTakingScreenshot.value)
+                }
+
+                return@measureTimedValue timedValueTakingScreenshot.value
             }
 
-            logger.info { "Took ${timedValueLoadPage.duration} to load the message preview page for $messageId! (render request: $savedMessageUniqueId)" }
+            logger.info { "Took ${timedValueScreenshot.duration} to generate everything related to $messageId! (render request: $savedMessageUniqueId)" }
 
-            val timedValueTakingScreenshot = measureTimedValue {
-                logger.info { "Taking screenshot of message $messageId! (render request: $savedMessageUniqueId)" }
-
-                page.querySelector("#wrapper").screenshot(ElementHandle.ScreenshotOptions())
-            }
-
-            logger.info { "Took ${timedValueTakingScreenshot.duration} to generate a message screenshot for $messageId! (render request: $savedMessageUniqueId)" }
-
-            if (writeImageForDebug) {
-                // If a timeout happens, let's write the image to the disk to see what is happening
-                logger.info { "Writing screenshot of message $messageId (render request: $savedMessageUniqueId) to disk for debugging purposes..." }
-                File("/debug/$messageId-$savedMessageUniqueId.png").writeBytes(timedValueTakingScreenshot.value)
-            }
-
-            return timedValueTakingScreenshot.value
+            return timedValueScreenshot.value
         } finally {
             page.close()
             browserContext.close()
