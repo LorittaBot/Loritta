df6b888307ab3dacf959403671dc9193bf20a974
MrPowerGamerBR <git@mrpowergamerbr.com>
2024-08-04 01:44:56 -0300

    Always create a new page to avoid memory leaks due to Playwright storing all request/responses

--- discord-chat-message-renderer-server/src/main/kotlin/net/perfectdreams/loritta/discordchatmessagerenderer/DiscordMessageRendererManager.kt
+++ discord-chat-message-renderer-server/src/main/kotlin/net/perfectdreams/loritta/discordchatmessagerenderer/DiscordMessageRendererManager.kt
@@ -20,16 +20,13 @@ class DiscordMessageRendererManager : Closeable {
     private val deviceScale = 2.0
     private val maxDimensionsOfImages = (16_384 / deviceScale).toInt()
     private val browserContext = browser.newContext(Browser.NewContextOptions().setDeviceScaleFactor(deviceScale))
-    private var page = createPage()
 
     private fun createPage(): Page {
         val newPage = browserContext.newPage()
         newPage.onCrash {
             // The reason we don't attempt to withPage lock it, is because this seems to create a deadlock because the onCrash handler is triggered within the rendering call
             // Failsafe if a page crashes
-            logger.error { "Page $it crashed! - Closing page..." }
-            page.close()
-            page = createPage()
+            logger.error { "Whoops, page $it crashed!" }
         }
         return newPage
     }
@@ -40,32 +37,35 @@ class DiscordMessageRendererManager : Closeable {
         savedMessageUniqueId: UUID,
         qrCodeImageAsByteArray: ByteArray?
     ): ByteArray {
-        logger.info { "Starting to render message $messageId! - Open pages: ${browserContext.pages().size} (render request: $savedMessageUniqueId)" }
+        // We always create a new page because Playwright leaks memory by keeping all request/responses stored in the "Connection.objects" HashMap
+        createPage().use { page ->
+            logger.info { "Starting to render message $messageId! - Open pages: ${browserContext.pages().size} (render request: $savedMessageUniqueId)" }
 
-        val timedValueLoadPage = measureTimedValue {
-            logger.info { "Loading message preview page for $messageId! (render request: $savedMessageUniqueId)" }
+            val timedValueLoadPage = measureTimedValue {
+                logger.info { "Loading message preview page for $messageId! (render request: $savedMessageUniqueId)" }
 
-            page.navigate(
-                // We navigate to an URL to avoid any memory leaks that may cause by reusing the same page
-                "http://127.0.0.1:8080/internal/message-preview?message=$savedMessageUniqueId",
-                // By using LOAD, we force the browser to actually wait the entire page to be loaded
-                Page.NavigateOptions().setWaitUntil(WaitUntilState.LOAD),
-            )
+                page.navigate(
+                    // We navigate to an URL to avoid any memory leaks that may cause by reusing the same page
+                    "http://127.0.0.1:8080/internal/message-preview?message=$savedMessageUniqueId",
+                    // By using LOAD, we force the browser to actually wait the entire page to be loaded
+                    Page.NavigateOptions().setWaitUntil(WaitUntilState.LOAD),
+                )
 
-            // We don't need to wait anything to load, because Playwright makes sure that everything is loaded when using "WaitUntilState.LOAD" :3
+                // We don't need to wait anything to load, because Playwright makes sure that everything is loaded when using "WaitUntilState.LOAD" :3
+            }
+
+            logger.info { "Took ${timedValueLoadPage.duration} to load the message preview page for $messageId! (render request: $savedMessageUniqueId)" }
+
+            val timedValueTakingScreenshot = measureTimedValue {
+                logger.info { "Taking screenshot of message $messageId! (render request: $savedMessageUniqueId)" }
+
+                page.querySelector("#wrapper").screenshot(ElementHandle.ScreenshotOptions())
+            }
+
+            logger.info { "Took ${timedValueTakingScreenshot.duration} to generate a message screenshot for $messageId! (render request: $savedMessageUniqueId)" }
+
+            return timedValueTakingScreenshot.value
         }
-
-        logger.info { "Took ${timedValueLoadPage.duration} to load the message preview page for $messageId! (render request: $savedMessageUniqueId)" }
-
-        val timedValueTakingScreenshot = measureTimedValue {
-            logger.info { "Taking screenshot of message $messageId! (render request: $savedMessageUniqueId)" }
-
-            page.querySelector("#wrapper").screenshot(ElementHandle.ScreenshotOptions())
-        }
-
-        logger.info { "Took ${timedValueTakingScreenshot.duration} to generate a message screenshot for $messageId! (render request: $savedMessageUniqueId)" }
-
-        return timedValueTakingScreenshot.value
     }
 
     override fun close() {
