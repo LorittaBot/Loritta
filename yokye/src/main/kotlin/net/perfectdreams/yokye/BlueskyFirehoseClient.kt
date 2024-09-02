8a2b2e0bab6df2ebce7e5a1934d7efed1b21dc8c
MrPowerGamerBR <git@mrpowergamerbr.com>
2024-09-01 22:49:58 -0300

    Add last event timestamp to the logs too

--- yokye/src/main/kotlin/net/perfectdreams/yokye/BlueskyFirehoseClient.kt
+++ yokye/src/main/kotlin/net/perfectdreams/yokye/BlueskyFirehoseClient.kt
@@ -66,7 +66,7 @@ class BlueskyFirehoseClient {
                     launch {
                         while (true) {
                             delay(5_000) // Initial delay
-                            logger.info { "Checking if Firehose stopped receiving events... Last event received at $lastEventReceivedAt; Last sequence: $lastSequence; Last header: $lastHeaderReceived; Last body: $lastBodyReceived" }
+                            logger.info { "Checking if Firehose stopped receiving events... Last event received at $lastEventReceivedAt; Event timestamp: $lastEventTime; Last sequence: $lastSequence; Last header: $lastHeaderReceived; Last body: $lastBodyReceived" }
                             val diff = Duration.between(lastEventReceivedAt, Instant.now())
                             if (diff.seconds >= 5) {
                                 logger.warn { "Stopped receiving Firehose events! Something may have gone wrong! Restarting..." }
