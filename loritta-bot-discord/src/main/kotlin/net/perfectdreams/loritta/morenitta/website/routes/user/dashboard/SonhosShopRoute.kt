08720b41b24182e16b274fa4236ea752c7305150
MrPowerGamerBR <git@mrpowergamerbr.com>
2024-10-08 11:43:32 -0300

    Fix sonhos shop route always using the dark theme

--- loritta-bot-discord/src/main/kotlin/net/perfectdreams/loritta/morenitta/website/routes/user/dashboard/SonhosShopRoute.kt
+++ loritta-bot-discord/src/main/kotlin/net/perfectdreams/loritta/morenitta/website/routes/user/dashboard/SonhosShopRoute.kt
@@ -6,7 +6,7 @@ import net.perfectdreams.loritta.cinnamon.pudding.tables.SonhosBundles
 import net.perfectdreams.loritta.common.locale.BaseLocale
 import net.perfectdreams.loritta.common.utils.UserPremiumPlans
 import net.perfectdreams.loritta.morenitta.LorittaBot
-import net.perfectdreams.loritta.morenitta.website.routes.RequiresDiscordLoginLocalizedRoute
+import net.perfectdreams.loritta.morenitta.website.routes.RequiresDiscordLoginLocalizedDashboardRoute
 import net.perfectdreams.loritta.morenitta.website.utils.extensions.respondHtml
 import net.perfectdreams.loritta.morenitta.website.views.dashboard.user.SonhosShopView
 import net.perfectdreams.loritta.serializable.ColorTheme
@@ -15,8 +15,15 @@ import net.perfectdreams.loritta.temmiewebsession.LorittaJsonWebSession
 import net.perfectdreams.temmiediscordauth.TemmieDiscordAuth
 import org.jetbrains.exposed.sql.selectAll
 
-class SonhosShopRoute(loritta: LorittaBot) : RequiresDiscordLoginLocalizedRoute(loritta, "/dashboard/sonhos-shop") {
-	override suspend fun onAuthenticatedRequest(call: ApplicationCall, locale: BaseLocale, i18nContext: I18nContext, discordAuth: TemmieDiscordAuth, userIdentification: LorittaJsonWebSession.UserIdentification) {
+class SonhosShopRoute(loritta: LorittaBot) : RequiresDiscordLoginLocalizedDashboardRoute(loritta, "/dashboard/sonhos-shop") {
+	override suspend fun onDashboardAuthenticatedRequest(
+		call: ApplicationCall,
+		locale: BaseLocale,
+		i18nContext: I18nContext,
+		discordAuth: TemmieDiscordAuth,
+		userIdentification: LorittaJsonWebSession.UserIdentification,
+		colorTheme: ColorTheme
+	) {
 		val sonhosBundles = loritta.transaction {
 			SonhosBundles.selectAll()
 				.where { SonhosBundles.active eq true }
