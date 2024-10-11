76e1ea4971db44826a04d00f68bde8eda39f29ef
MrPowerGamerBR <git@mrpowergamerbr.com>
2024-10-10 21:29:53 -0300

    I forgot to commit these whoops

---
+++ pudding/client/src/main/kotlin/net/perfectdreams/loritta/cinnamon/pudding/tables/SonhosTransferRequests.kt
@@ -0,0 +1,15 @@
+package net.perfectdreams.loritta.cinnamon.pudding.tables
+
+import net.perfectdreams.exposedpowerutils.sql.javatime.timestampWithTimeZone
+import org.jetbrains.exposed.dao.id.LongIdTable
+
+object SonhosTransferRequests : LongIdTable() {
+    val giver = long("giver")
+    val giverAcceptedAt = timestampWithTimeZone("giver_accepted_at").nullable()
+    val receiver = long("receiver")
+    val receiverAcceptedAt = timestampWithTimeZone("receiver_accepted_at").nullable()
+    val quantity = long("quantity")
+    val requestedAt = timestampWithTimeZone("requested_at")
+    val expiresAt = timestampWithTimeZone("expires_at")
+    val transferredAt = timestampWithTimeZone("transferred_at").nullable()
+}
