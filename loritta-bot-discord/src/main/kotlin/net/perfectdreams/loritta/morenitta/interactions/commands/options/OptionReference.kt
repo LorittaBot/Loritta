4be13b14c0b37b3952ca4d07d3c8ba9656d5c655
MrPowerGamerBR <git@mrpowergamerbr.com>
2025-04-10 14:31:12 -0300

    Refactor ImageReference and ImageReferenceOrAttachment code to avoid code duplication

--- loritta-bot-discord/src/main/kotlin/net/perfectdreams/loritta/morenitta/interactions/commands/options/OptionReference.kt
+++ loritta-bot-discord/src/main/kotlin/net/perfectdreams/loritta/morenitta/interactions/commands/options/OptionReference.kt
@@ -14,6 +14,7 @@ import net.perfectdreams.loritta.cinnamon.emotes.Emotes
 import net.perfectdreams.loritta.i18n.I18nKeysData
 import net.perfectdreams.loritta.morenitta.interactions.UnleashedContext
 import net.perfectdreams.loritta.morenitta.interactions.commands.autocomplete.AutocompleteExecutor
+import net.perfectdreams.loritta.morenitta.interactions.commands.options.ImageReference.Companion.getImageUrl
 import kotlin.streams.toList
 
 sealed class OptionReference<T>(
@@ -130,74 +131,96 @@ class AttachmentDiscordOptionReference<T>(name: String, description: StringI18nD
 class ImageReferenceDiscordOptionReference<T>(name: String) : OptionReference<T>(name)
 
 class ImageReference(val dataValue: String) {
-    suspend fun get(context: UnleashedContext, searchHistory: Boolean = true): String {
-        if (dataValue != null) {
-            if (dataValue.startsWith("http")) {
-                // It is a URL!
-                // TODO: Use a RegEx to check if it is a valid URL
-                return dataValue
-            }
+    companion object {
+        /**
+         * Gets an image URL from the [context] and [dataValue]
+         *
+         * @param context       the context
+         * @param dataValue     the data value of the command
+         * @param searchHistory if the channel history should be searched to find other images
+         * @return an image URL string, of null if nothing was found
+         */
+        internal suspend fun getImageUrl(
+            context: UnleashedContext,
+            dataValue: String?,
+            searchHistory: Boolean
+        ): String? {
+            if (dataValue != null) {
+                if (dataValue.startsWith("http")) {
+                    // It is a URL!
+                    // TODO: Use a RegEx to check if it is a valid URL
+                    return dataValue
+                }
 
-            // It is a user mention!
-            if (dataValue.startsWith("<@") && dataValue.endsWith(">")) {
-                val userIdAsString = dataValue.removePrefix("<@").removeSuffix(">")
-                val userId = userIdAsString.toLongOrNull()
+                // It is a user mention!
+                if (dataValue.startsWith("<@") && dataValue.endsWith(">")) {
+                    val userIdAsString = dataValue.removePrefix("<@").removeSuffix(">")
+                    val userId = userIdAsString.toLongOrNull()
 
-                // If it is a valid long (so, it must be a snowflake...)
-                if (userId != null) {
-                    // And if the user is present in the mentions list...
-                    val user = context.mentions.users.firstOrNull { it.idLong == userId }
-                    if (user != null)
-                    // Then yay! Get the user avatar!!
-                        return user.effectiveAvatarUrl
+                    // If it is a valid long (so, it must be a snowflake...)
+                    if (userId != null) {
+                        // And if the user is present in the mentions list...
+                        val user = context.mentions.users.firstOrNull { it.idLong == userId }
+                        if (user != null)
+                        // Then yay! Get the user avatar!!
+                            return user.effectiveAvatarUrl
+                    }
+                }
+
+                // It is a emote!
+                // Discord emotes always starts with "<" and ends with ">"
+                if (dataValue.startsWith("<") && dataValue.endsWith(">")) {
+                    val emoteId = dataValue.substringAfterLast(":").substringBefore(">")
+                    return "https://cdn.discordapp.com/emojis/${emoteId}.png?v=1"
+                }
+
+                // It is a unicode emoji!
+                if (context.loritta.unicodeEmojiManager.regex.matches(dataValue)) {
+                    val emoteId = dataValue.codePoints().toList()
+                        .joinToString(separator = "-") { String.format("\\u%04x", it).substring(2) }
+                    return "https://abs.twimg.com/emoji/v2/72x72/$emoteId.png"
                 }
             }
 
-            // It is a emote!
-            // Discord emotes always starts with "<" and ends with ">"
-            if (dataValue.startsWith("<") && dataValue.endsWith(">")) {
-                val emoteId = dataValue.substringAfterLast(":").substringBefore(">")
-                return "https://cdn.discordapp.com/emojis/${emoteId}.png?v=1"
-            }
+            if (searchHistory) {
+                // If no image was found, we will try to find the first recent message in this chat
+                val channel = context.channelOrNull
+                val guild = context.guildOrNull
 
-            // It is a unicode emoji!
-            if (context.loritta.unicodeEmojiManager.regex.matches(dataValue)) {
-                val emoteId = dataValue.codePoints().toList()
-                    .joinToString(separator = "-") { String.format("\\u%04x", it).substring(2) }
-                return "https://abs.twimg.com/emoji/v2/72x72/$emoteId.png"
-            }
-        }
+                // Only non-detached channels have the ability of querying the channel history
+                if (channel != null && !channel.isDetached) {
+                    val canQuery = if (guild != null && channel is GuildChannel) {
+                        guild.selfMember.hasPermission(channel, Permission.MESSAGE_HISTORY, Permission.MESSAGE_SEND)
+                    } else true
 
-        if (searchHistory) {
-            // If no image was found, we will try to find the first recent message in this chat
-            val channel = context.channelOrNull
-            val guild = context.guildOrNull
+                    if (canQuery) {
+                        val messages = channel.history.retrievePast(100)
+                            .await()
 
-            // Only non-detached channels have the ability of querying the channel history
-            if (channel != null && !channel.isDetached) {
-                val canQuery = if (guild != null && channel is GuildChannel) {
-                    guild.selfMember.hasPermission(channel, Permission.MESSAGE_HISTORY, Permission.MESSAGE_SEND)
-                } else true
+                        // Sort from the newest message to the oldest message
+                        val attachmentUrl = messages.sortedByDescending { it.timeCreated }
+                            .flatMap { it.attachments }
+                            .firstOrNull {
+                                // Only get filenames ending with "image" extensions
+                                it.contentType != null && it.contentType in ContentTypeUtils.COMMON_IMAGE_CONTENT_TYPES
+                            }?.url
 
-                if (canQuery) {
-                    val messages = channel.history.retrievePast(100)
-                        .await()
-
-                    // Sort from the newest message to the oldest message
-                    val attachmentUrl = messages.sortedByDescending { it.timeCreated }
-                        .flatMap { it.attachments }
-                        .firstOrNull {
-                            // Only get filenames ending with "image" extensions
-                            it.contentType != null && it.contentType in ContentTypeUtils.COMMON_IMAGE_CONTENT_TYPES
-                        }?.url
-
-                    if (attachmentUrl != null) {
-                        // Found a valid URL, let's go!
-                        return attachmentUrl
+                        if (attachmentUrl != null) {
+                            // Found a valid URL, let's go!
+                            return attachmentUrl
+                        }
                     }
                 }
             }
+
+            return null
         }
+    }
+
+    suspend fun get(context: UnleashedContext, searchHistory: Boolean = true): String {
+        val imageUrl = getImageUrl(context, dataValue, searchHistory)
+        if (imageUrl != null)
+            return imageUrl
 
         context.fail(context.wasInitiallyDeferredEphemerally ?: true) {
             styled(
@@ -230,73 +253,9 @@ class ImageReferenceOrAttachment(
             }
         }
 
-        if (dataValue != null) {
-            if (dataValue.startsWith("http")) {
-                // It is a URL!
-                // TODO: Use a RegEx to check if it is a valid URL
-                return dataValue
-            }
-
-            // It is a user mention!
-            if (dataValue.startsWith("<@") && dataValue.endsWith(">")) {
-                val userIdAsString = dataValue.removePrefix("<@").removeSuffix(">")
-                val userId = userIdAsString.toLongOrNull()
-
-                // If it is a valid long (so, it must be a snowflake...)
-                if (userId != null) {
-                    // And if the user is present in the mentions list...
-                    val user = context.mentions.users.firstOrNull { it.idLong == userId }
-                    if (user != null)
-                        // Then yay! Get the user avatar!!
-                        return user.effectiveAvatarUrl
-                }
-            }
-
-            // It is a emote!
-            // Discord emotes always starts with "<" and ends with ">"
-            if (dataValue.startsWith("<") && dataValue.endsWith(">")) {
-                val emoteId = dataValue.substringAfterLast(":").substringBefore(">")
-                return "https://cdn.discordapp.com/emojis/${emoteId}.png?v=1"
-            }
-
-            // It is a unicode emoji!
-            if (context.loritta.unicodeEmojiManager.regex.matches(dataValue)) {
-                val emoteId = dataValue.codePoints().toList()
-                    .joinToString(separator = "-") { String.format("\\u%04x", it).substring(2) }
-                return "https://abs.twimg.com/emoji/v2/72x72/$emoteId.png"
-            }
-        }
-
-        if (searchHistory) {
-            // If no image was found, we will try to find the first recent message in this chat
-            val channel = context.channelOrNull
-            val guild = context.guildOrNull
-
-            // Only non-detached channels have the ability of querying the channel history
-            if (channel != null && !channel.isDetached) {
-                val canQuery = if (guild != null && channel is GuildChannel) {
-                    guild.selfMember.hasPermission(channel, Permission.MESSAGE_HISTORY, Permission.MESSAGE_SEND)
-                } else true
-
-                if (canQuery) {
-                    val messages = channel.history.retrievePast(100)
-                            .await()
-
-                    // Sort from the newest message to the oldest message
-                    val attachmentUrl = messages.sortedByDescending { it.timeCreated }
-                            .flatMap { it.attachments }
-                            .firstOrNull {
-                                // Only get filenames ending with "image" extensions
-                                it.contentType != null && it.contentType in ContentTypeUtils.COMMON_IMAGE_CONTENT_TYPES
-                            }?.url
-
-                    if (attachmentUrl != null) {
-                        // Found a valid URL, let's go!
-                        return attachmentUrl
-                    }
-                }
-            }
-        }
+        val imageUrl = getImageUrl(context, dataValue, searchHistory)
+        if (imageUrl != null)
+            return imageUrl
 
         context.fail(context.wasInitiallyDeferredEphemerally ?: true) {
             styled(
