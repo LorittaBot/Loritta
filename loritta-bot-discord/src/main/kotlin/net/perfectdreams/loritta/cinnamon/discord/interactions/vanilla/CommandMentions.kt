027eae6d030b33669135cfeb554ca2859117c5ed
MrPowerGamerBR <git@mrpowergamerbr.com>
2025-04-22 21:21:00 -0300

    Fix CommandMentions portuguese label finder
    Before we were using labels in portuguese but they didn't work correctly because Discord wants us to the canonical name of the command, even if we were providing the correct command ID, so we needed to actually find the canonical name (including label, subcommand and subcommand group) of the command to use it as a mention

--- loritta-bot-discord/src/main/kotlin/net/perfectdreams/loritta/cinnamon/discord/interactions/vanilla/CommandMentions.kt
+++ loritta-bot-discord/src/main/kotlin/net/perfectdreams/loritta/cinnamon/discord/interactions/vanilla/CommandMentions.kt
@@ -21,7 +21,7 @@ class CommandMentions(private val registeredCommands: List<DiscordCommand>) {
     val brokerInfo = commandMention("corretora info")
     val brokerBuy = commandMention("corretora comprar")
     val brokerSell = commandMention("corretora vender")
-    val brokerPortfolio = commandMention("corretora portfolio")
+    val brokerPortfolio = commandMention("corretora portfólio")
 
     val coinflipBet = commandMention("coinflipbet")
     val coinflipBetGlobal = commandMention("coinflipbetglobal")
@@ -51,16 +51,41 @@ class CommandMentions(private val registeredCommands: List<DiscordCommand>) {
      * Creates a command mention of [path]. If the command doesn't exist, an error will be thrown.
      */
     private fun commandMention(path: String): String {
-        val rootCommandLabel = path.substringBefore(" ")
-
         // This seems weird, but hear me out:
         // In the past we did use the command label here, which is english
         // However, this is bad because all of the strings are first created in portuguese, then translated to english
         // So a command that was not translated yet to english WILL cause issues after it is translated
         // (ESPECIALLY if it is being used as a command mention!)
-        // So now we use the localized portuguese label :3
-        val registeredCommand = registeredCommands.firstOrNull { it.nameLocalizations[DiscordLocale.PORTUGUESE_BRAZILIAN] == rootCommandLabel } ?: error("Couldn't find a command with label $rootCommandLabel!")
+        //
+        // However, because we are relying on the portuguese version of the commands, we need to get the original canonical english name
+        val commandParts = path.split(" ")
+        when (commandParts.size) {
+            1 -> {
+                val rootLabel = commandParts[0]
+                val registeredCommand = registeredCommands.firstOrNull { it.nameLocalizations[DiscordLocale.PORTUGUESE_BRAZILIAN] == rootLabel } ?: error("Couldn't find a command with label $rootLabel!")
+                return "</$rootLabel:${registeredCommand.id}>"
+            }
+            2 -> {
+                val rootLabel = commandParts[0]
+                val subcommandLabel = commandParts[1]
 
-        return "</${path}:${registeredCommand.id}>"
+                val registeredCommand = registeredCommands.firstOrNull { it.nameLocalizations[DiscordLocale.PORTUGUESE_BRAZILIAN] == rootLabel } ?: error("Couldn't find a command with label $rootLabel!")
+                val subcommand = registeredCommand.subcommands.firstOrNull { it.nameLocalizations[DiscordLocale.PORTUGUESE_BRAZILIAN] == subcommandLabel } ?: error("Couldn't find a subcommand with label $subcommandLabel!")
+
+                return "</${registeredCommand.name} ${subcommand.name}:${registeredCommand.id}>"
+            }
+            3 -> {
+                val rootLabel = commandParts[0]
+                val subcommandGroupLabel = commandParts[1]
+                val subcommandLabel = commandParts[2]
+
+                val registeredCommand = registeredCommands.firstOrNull { it.nameLocalizations[DiscordLocale.PORTUGUESE_BRAZILIAN] == rootLabel } ?: error("Couldn't find a command with label $rootLabel!")
+                val subcommandGroup = registeredCommand.subcommandGroups.firstOrNull { it.nameLocalizations[DiscordLocale.PORTUGUESE_BRAZILIAN] == subcommandGroupLabel } ?: error("Couldn't find a subcommand group with label $subcommandGroupLabel!")
+                val subcommand = subcommandGroup.subcommands.firstOrNull { it.nameLocalizations[DiscordLocale.PORTUGUESE_BRAZILIAN] == subcommandLabel } ?: error("Couldn't find a subcommand with label $subcommandLabel!")
+
+                return "</${registeredCommand.name} ${subcommandGroup.name} ${subcommand.name}:${registeredCommand.id}>"
+            }
+            else -> error("Unsupported command format for $path, there are ${commandParts.size} parts, only 1, 2 or 3 are supported!")
+        }
     }
 }
--- loritta-bot-discord/src/main/kotlin/net/perfectdreams/loritta/morenitta/interactions/commands/DiscordCommand.kt
+++ loritta-bot-discord/src/main/kotlin/net/perfectdreams/loritta/morenitta/interactions/commands/DiscordCommand.kt
@@ -8,13 +8,40 @@ import net.dv8tion.jda.api.interactions.commands.Command
 data class DiscordCommand(
     val id: Long,
     val name: String,
-    val nameLocalizations: Map<DiscordLocale, String>
+    val nameLocalizations: Map<DiscordLocale, String>,
+    val subcommands: List<DiscordSubcommand>,
+    val subcommandGroups: List<DiscordSubcommandGroup>,
 ) {
     companion object {
         fun from(command: Command) = DiscordCommand(
             command.idLong,
             command.name,
-            command.nameLocalizations.toMap()
+            command.nameLocalizations.toMap(),
+            command.subcommands.map {
+                DiscordSubcommand(it.name, it.nameLocalizations.toMap())
+            },
+            command.subcommandGroups.map {
+                DiscordSubcommandGroup(
+                    it.name,
+                    it.nameLocalizations.toMap(),
+                    it.subcommands.map {
+                        DiscordSubcommand(it.name, it.nameLocalizations.toMap())
+                    }
+                )
+            }
         )
     }
+
+    @Serializable
+    data class DiscordSubcommand(
+        val name: String,
+        val nameLocalizations: Map<DiscordLocale, String>
+    )
+
+    @Serializable
+    data class DiscordSubcommandGroup(
+        val name: String,
+        val nameLocalizations: Map<DiscordLocale, String>,
+        val subcommands: List<DiscordSubcommand>
+    )
 }
