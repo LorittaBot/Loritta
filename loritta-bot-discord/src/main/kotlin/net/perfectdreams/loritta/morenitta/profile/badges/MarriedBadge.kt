93697466ed75bd870530c0fc19019140ea740ba3
MrPowerGamerBR <git@mrpowergamerbr.com>
2025-05-15 15:23:03 -0300

    Revert marriage badge code because the new marriage tables are not live yet

--- loritta-bot-discord/src/main/kotlin/net/perfectdreams/loritta/morenitta/profile/badges/MarriedBadge.kt
+++ loritta-bot-discord/src/main/kotlin/net/perfectdreams/loritta/morenitta/profile/badges/MarriedBadge.kt
@@ -1,13 +1,11 @@
 package net.perfectdreams.loritta.morenitta.profile.badges
 
 import net.perfectdreams.loritta.cinnamon.pudding.Pudding
-import net.perfectdreams.loritta.cinnamon.pudding.tables.MarriageParticipants
 import net.perfectdreams.loritta.common.emojis.LorittaEmojis
 import net.perfectdreams.loritta.morenitta.dao.Profile
 import net.perfectdreams.loritta.morenitta.profile.Badge
 import net.perfectdreams.loritta.morenitta.profile.ProfileDesignManager
 import net.perfectdreams.loritta.morenitta.profile.ProfileUserInfoData
-import org.jetbrains.exposed.sql.selectAll
 import java.util.*
 
 class MarriedBadge(val pudding: Pudding) : Badge.LorittaBadge(
@@ -19,14 +17,7 @@ class MarriedBadge(val pudding: Pudding) : Badge.LorittaBadge(
 	190
 ) {
 	override suspend fun checkIfUserDeservesBadge(user: ProfileUserInfoData, profile: Profile, mutualGuilds: Set<Long>): Boolean {
-		val isMarried = pudding.transaction {
-			MarriageParticipants.selectAll()
-				.where {
-					MarriageParticipants.user eq user.id
-				}
-				.count() != 0L
-		}
-
-		return isMarried
+		val marriage = pudding.transaction { profile.marriage }
+		return marriage != null
 	}
 }
