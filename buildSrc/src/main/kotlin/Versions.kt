a08fa038e9e9bb9011008ed22e56ce236e83b834
MrPowerGamerBR <git@mrpowergamerbr.com>
2025-06-03 15:30:06 -0300

    Update Kotlin

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,7 +1,7 @@
 object Versions {
     const val LORITTA = "2023-SNAPSHOT"
     const val PUDDING = "0.0.3-SNAPSHOT"
-    const val KOTLIN = "2.1.20"
+    const val KOTLIN = "2.1.21"
     const val KTOR = "3.1.3"
     const val KOTLIN_SERIALIZATION = "1.6.0"
     const val KOTLIN_COROUTINES = "1.6.0"
