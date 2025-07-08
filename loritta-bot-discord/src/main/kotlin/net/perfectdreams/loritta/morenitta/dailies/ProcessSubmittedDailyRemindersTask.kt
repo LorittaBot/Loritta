efc1db3390fed4ff0c30489866a54da81226640a
MrPowerGamerBR <git@mrpowergamerbr.com>
2025-07-08 14:00:39 -0300

    Add more logs

--- loritta-bot-discord/src/main/kotlin/net/perfectdreams/loritta/morenitta/dailies/ProcessSubmittedDailyRemindersTask.kt
+++ loritta-bot-discord/src/main/kotlin/net/perfectdreams/loritta/morenitta/dailies/ProcessSubmittedDailyRemindersTask.kt
@@ -32,6 +32,8 @@ class ProcessSubmittedDailyRemindersTask(val m: LorittaBot) {
 
     suspend fun processDailyReminders() {
         while (true) {
+            logger.info { "Processing pending daily reminder notifications..." }
+            
             try {
                 val i18nContext = m.languageManager.defaultI18nContext
 
