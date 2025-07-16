6964447b8c11e5e70753a83db1a4e09e1628cdae
hechfx <hechfx@hotmail.com>
2025-07-16 17:43:02 -0300

    Change GuildCommand.kt to **ExpressionsCommand.kt** because it makes more sense.
    Also refactored the code repetition in MessageCommand version of sticker add.

--- loritta-bot-discord/src/main/kotlin/net/perfectdreams/loritta/morenitta/interactions/JDAExtensions.kt
+++ loritta-bot-discord/src/main/kotlin/net/perfectdreams/loritta/morenitta/interactions/JDAExtensions.kt
@@ -1,21 +1,12 @@
 package net.perfectdreams.loritta.morenitta.interactions
 
-import dev.minn.jda.ktx.coroutines.await
 import dev.minn.jda.ktx.interactions.components.ButtonDefaults
-import net.dv8tion.jda.api.entities.Guild
 import net.dv8tion.jda.api.entities.emoji.Emoji
-import net.dv8tion.jda.api.entities.sticker.GuildSticker
 import net.dv8tion.jda.api.components.button.Button
 import net.dv8tion.jda.api.components.button.ButtonStyle
-import net.dv8tion.jda.api.utils.FileUpload
-import net.perfectdreams.loritta.cinnamon.discord.interactions.commands.styled
-import net.perfectdreams.loritta.cinnamon.emotes.Emotes
 import net.perfectdreams.loritta.common.emotes.DiscordEmote
 import net.perfectdreams.loritta.common.emotes.Emote
 import net.perfectdreams.loritta.common.emotes.UnicodeEmote
-import net.perfectdreams.loritta.morenitta.interactions.vanilla.discord.GuildCommand.Companion.I18N_PREFIX
-import net.perfectdreams.loritta.morenitta.utils.LorittaUtils
-import net.perfectdreams.loritta.morenitta.utils.SimpleImageInfo
 
 
 /**
@@ -45,44 +36,3 @@ fun linkButton(
         null -> null
     }
 ).withDisabled(disabled)
-
-suspend fun Guild.newSticker(
-    context: UnleashedContext,
-    name: String,
-    description: String,
-    sticker: String,
-    tags: List<String>
-): GuildSticker? {
-    val image = LorittaUtils.downloadFile(context.loritta, sticker)
-        ?: context.fail(true) {
-            styled(
-                context.i18nContext.get(
-                    I18N_PREFIX.Sticker.Add.InvalidUrl
-                ),
-                Emotes.Error
-            )
-        }
-
-    val allowedImageTypes = setOf("png", "gif", "json", "jpeg", "jpg")
-    var imageInfo = SimpleImageInfo(image)
-    var imageType = imageInfo.mimeType!!.split("/")[1]
-
-    val imageData = if (imageType in allowedImageTypes) {
-        when (imageType) {
-            "jpeg", "jpg" -> LorittaUtils.convertImage(image, "png", true)
-            else -> image
-        }
-    } else {
-        null
-    }!!
-
-    imageInfo = SimpleImageInfo(imageData)
-    imageType = imageInfo.mimeType!!.split("/")[1]
-
-    return context.guild.createSticker(
-        name,
-        description,
-        FileUpload.fromData(imageData, "sticker.$imageType"),
-        tags
-    ).submit(false).await()
-}
