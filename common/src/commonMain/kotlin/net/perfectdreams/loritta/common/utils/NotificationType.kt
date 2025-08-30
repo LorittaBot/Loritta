4aa7198505ccef64a816ff3dae441faf7c672071
MrPowerGamerBR <git@mrpowergamerbr.com>
2025-08-29 23:22:25 -0300

    Whoops, forgot to commit the NotificationType

---
+++ common/src/commonMain/kotlin/net/perfectdreams/loritta/common/utils/NotificationType.kt
@@ -0,0 +1,10 @@
+package net.perfectdreams.loritta.common.utils
+
+enum class NotificationType {
+    DAILY_REMINDER,
+    MARRIAGE_LOVE_LETTER,
+    MARRIAGE_EXPIRATION_REMINDER,
+    MARRIAGE_RENEWED,
+    MARRIAGE_EXPIRED,
+    EXPERIENCE_LEVEL_UP
+}
