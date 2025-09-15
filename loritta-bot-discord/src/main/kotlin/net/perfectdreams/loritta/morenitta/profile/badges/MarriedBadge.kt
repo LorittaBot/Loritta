84f586d93bc69391bac014d285ea8d87e5f4d8d8
MrPowerGamerBR <git@mrpowergamerbr.com>
2025-09-14 23:04:04 -0300

    Fix marriage badge showing up even if the marriage expired

--- loritta-bot-discord/src/main/kotlin/net/perfectdreams/loritta/morenitta/profile/badges/MarriedBadge.kt
+++ loritta-bot-discord/src/main/kotlin/net/perfectdreams/loritta/morenitta/profile/badges/MarriedBadge.kt
@@ -2,11 +2,13 @@ package net.perfectdreams.loritta.morenitta.profile.badges
 
 import net.perfectdreams.loritta.cinnamon.pudding.Pudding
 import net.perfectdreams.loritta.cinnamon.pudding.tables.MarriageParticipants
+import net.perfectdreams.loritta.cinnamon.pudding.tables.UserMarriages
 import net.perfectdreams.loritta.common.emojis.LorittaEmojis
 import net.perfectdreams.loritta.morenitta.dao.Profile
 import net.perfectdreams.loritta.morenitta.profile.Badge
 import net.perfectdreams.loritta.morenitta.profile.ProfileDesignManager
 import net.perfectdreams.loritta.morenitta.profile.ProfileUserInfoData
+import org.jetbrains.exposed.sql.and
 import org.jetbrains.exposed.sql.selectAll
 import java.util.*
 
@@ -21,9 +23,10 @@ class MarriedBadge(val pudding: Pudding) : Badge.LorittaBadge(
 ) {
 	override suspend fun checkIfUserDeservesBadge(user: ProfileUserInfoData, profile: Profile, mutualGuilds: Set<Long>): Boolean {
 		val isMarried = pudding.transaction {
-			MarriageParticipants.selectAll()
+			MarriageParticipants.innerJoin(UserMarriages)
+                .selectAll()
 				.where {
-					MarriageParticipants.user eq user.id
+					MarriageParticipants.user eq user.id and (UserMarriages.expiredAt.isNotNull())
 				}
 				.count() != 0L
 		}
