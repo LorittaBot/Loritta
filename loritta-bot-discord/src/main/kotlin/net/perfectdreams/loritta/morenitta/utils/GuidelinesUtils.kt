525fc89a6afb8d9fe877ebe78b38b657d02e74e5
MrPowerGamerBR <git@mrpowergamerbr.com>
2025-10-22 12:14:33 -0300

    Update guidelines version

--- loritta-bot-discord/src/main/kotlin/net/perfectdreams/loritta/morenitta/utils/GuidelinesUtils.kt
+++ loritta-bot-discord/src/main/kotlin/net/perfectdreams/loritta/morenitta/utils/GuidelinesUtils.kt
@@ -2,5 +2,5 @@ package net.perfectdreams.loritta.morenitta.utils
 
 object GuidelinesUtils {
     // In the future it would be cool to refactor this and move the version somewhere else (database?)
-    const val GUIDELINES_VERSION = 1
+    const val GUIDELINES_VERSION = 2
 }
