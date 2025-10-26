3f270aeb2f646333f528dcbb6aca9899d9598900
MrPowerGamerBR <git@mrpowergamerbr.com>
2025-10-26 16:35:41 -0300

    Replace the character counter with a vanilla JS approach too

--- loritta-dashboard/frontend/src/jsMain/kotlin/net/perfectdreams/loritta/dashboard/frontend/components/CharacterCounterComponent.kt
+++ loritta-dashboard/frontend/src/jsMain/kotlin/net/perfectdreams/loritta/dashboard/frontend/components/CharacterCounterComponent.kt
@@ -1,15 +1,6 @@
 package net.perfectdreams.loritta.dashboard.frontend.components
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import net.perfectdreams.bliss.BlissComponent
-import org.jetbrains.compose.web.attributes.name
-import org.jetbrains.compose.web.dom.Button
-import org.jetbrains.compose.web.dom.HiddenInput
-import org.jetbrains.compose.web.dom.Text
-import org.jetbrains.compose.web.renderComposable
-import web.dom.Element
 import web.dom.document
 import web.events.addEventHandler
 import web.html.HTMLDivElement
@@ -18,19 +9,19 @@ import web.input.INPUT
 import web.input.InputEvent
 
 class CharacterCounterComponent : BlissComponent<HTMLDivElement>() {
-    var characterCount by mutableStateOf(0)
-
     override fun onMount() {
         val elementToBeListenedTo = document.body.querySelector(mountedElement.getAttribute("character-counter-listen")!!) ?: error("Could not find element to listen to!")
         require(elementToBeListenedTo is HTMLInputElement)
 
-        registeredEvents += elementToBeListenedTo.addEventHandler(InputEvent.INPUT) {
-            characterCount = elementToBeListenedTo.value.length
+        fun updatePreview() {
+            this.mountedElement.textContent = "${elementToBeListenedTo.value.length}/${elementToBeListenedTo.maxLength}"
         }
 
-        renderComposable(mountedElement) {
-            Text("$characterCount/${elementToBeListenedTo.maxLength}")
+        registeredEvents += elementToBeListenedTo.addEventHandler(InputEvent.INPUT) {
+            updatePreview()
         }
+
+        updatePreview()
     }
 
     override fun onUnmount() {}
