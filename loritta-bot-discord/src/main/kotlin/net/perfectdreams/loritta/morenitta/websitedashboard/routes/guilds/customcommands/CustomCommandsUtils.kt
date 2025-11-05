4f4abc3a67a9713fc7de93d5632d338c435f5c88
MrPowerGamerBR <git@mrpowergamerbr.com>
2025-11-05 12:30:19 -0300

    Increase max custom guild commands

--- loritta-bot-discord/src/main/kotlin/net/perfectdreams/loritta/morenitta/websitedashboard/routes/guilds/customcommands/CustomCommandsUtils.kt
+++ loritta-bot-discord/src/main/kotlin/net/perfectdreams/loritta/morenitta/websitedashboard/routes/guilds/customcommands/CustomCommandsUtils.kt
@@ -1,5 +1,5 @@
 package net.perfectdreams.loritta.morenitta.websitedashboard.routes.guilds.customcommands
 
 object CustomCommandsUtils {
-    const val MAX_CUSTOM_COMMANDS = 100
+    const val MAX_CUSTOM_COMMANDS = 5_000
 }
