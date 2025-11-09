e1e22a1d359830a3930f0c39a57aa931853cfa2e
MrPowerGamerBR <git@mrpowergamerbr.com>
2025-11-09 02:29:44 -0300

    Don't use nirn-proxy's URL when trying to get the OAuth2 token
    For some reason using nirn-proxy is causing a hang *sometimes* when trying to get the OAuth2 token

--- loritta-bot-discord/src/main/kotlin/net/perfectdreams/loritta/morenitta/websitedashboard/DiscordOAuth2Endpoints.kt
+++ loritta-bot-discord/src/main/kotlin/net/perfectdreams/loritta/morenitta/websitedashboard/DiscordOAuth2Endpoints.kt
@@ -3,14 +3,16 @@ package net.perfectdreams.loritta.morenitta.websitedashboard
 import net.perfectdreams.loritta.morenitta.LorittaBot
 
 class DiscordOAuth2Endpoints(val loritta: LorittaBot) {
-    private fun discordBaseUrl(): String {
-        val baseUrl = if (loritta.config.loritta.discord.baseUrl != null) {
+    private fun discordBaseUrl(forceDiscord: Boolean): String {
+        val baseUrl = if (!forceDiscord && loritta.config.loritta.discord.baseUrl != null) {
             loritta.config.loritta.discord.baseUrl
         } else "https://discord.com/"
         return baseUrl.removeSuffix("/")
     }
 
-    val OAuth2TokenEndpoint = "${discordBaseUrl()}/api/oauth2/token"
-    val UserIdentificationEndpoint = "${discordBaseUrl()}/api/v10/users/@me"
-    val UserGuildsEndpoint = "${discordBaseUrl()}/api/v10/users/@me/guilds"
+    // Don't put nirn behind the OAuth2 token, if you put it behind nirn, sometimes it may hang the request forever for... some reason?
+    // https://discord.com/channels/634032658275500033/812743672759189544/1436950264342118470
+    val OAuth2TokenEndpoint = "${discordBaseUrl(true)}/api/oauth2/token"
+    val UserIdentificationEndpoint = "${discordBaseUrl(false)}/api/v10/users/@me"
+    val UserGuildsEndpoint = "${discordBaseUrl(false)}/api/v10/users/@me/guilds"
 }
