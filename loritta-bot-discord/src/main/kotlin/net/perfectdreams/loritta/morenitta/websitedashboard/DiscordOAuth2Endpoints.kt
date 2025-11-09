f9e54a9395e7c8399dd3f11d40b4781cb95de208
MrPowerGamerBR <git@mrpowergamerbr.com>
2025-11-09 02:31:50 -0300

    Use versioned OAuth2 token endpoint

--- loritta-bot-discord/src/main/kotlin/net/perfectdreams/loritta/morenitta/websitedashboard/DiscordOAuth2Endpoints.kt
+++ loritta-bot-discord/src/main/kotlin/net/perfectdreams/loritta/morenitta/websitedashboard/DiscordOAuth2Endpoints.kt
@@ -12,7 +12,7 @@ class DiscordOAuth2Endpoints(val loritta: LorittaBot) {
 
     // Don't put nirn behind the OAuth2 token, if you put it behind nirn, sometimes it may hang the request forever for... some reason?
     // https://discord.com/channels/634032658275500033/812743672759189544/1436950264342118470
-    val OAuth2TokenEndpoint = "${discordBaseUrl(true)}/api/oauth2/token"
+    val OAuth2TokenEndpoint = "${discordBaseUrl(true)}/api/v10/oauth2/token"
     val UserIdentificationEndpoint = "${discordBaseUrl(false)}/api/v10/users/@me"
     val UserGuildsEndpoint = "${discordBaseUrl(false)}/api/v10/users/@me/guilds"
 }
