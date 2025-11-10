00a3192ba8fb2154ce4e0d99a8d1b33ab777b4d9
MrPowerGamerBR <git@mrpowergamerbr.com>
2025-11-10 15:02:01 -0300

    Allow using the configure notifications command via prefixes

--- loritta-bot-discord/src/main/kotlin/net/perfectdreams/loritta/morenitta/interactions/vanilla/utils/NotificationsCommand.kt
+++ loritta-bot-discord/src/main/kotlin/net/perfectdreams/loritta/morenitta/interactions/vanilla/utils/NotificationsCommand.kt
@@ -1,34 +1,12 @@
 package net.perfectdreams.loritta.morenitta.interactions.vanilla.utils
 
-import dev.minn.jda.ktx.messages.InlineMessage
-import kotlinx.datetime.toJavaInstant
-import net.dv8tion.jda.api.utils.messages.MessageCreateData
-import net.perfectdreams.i18nhelper.core.I18nContext
-import net.perfectdreams.loritta.cinnamon.discord.interactions.commands.styled
-import net.perfectdreams.loritta.cinnamon.discord.utils.NotificationUtils
 import net.perfectdreams.loritta.common.commands.CommandCategory
-import net.perfectdreams.loritta.common.utils.GACampaigns
-import net.perfectdreams.loritta.common.utils.LorittaColors
 import net.perfectdreams.loritta.i18n.I18nKeysData
 import net.perfectdreams.loritta.morenitta.LorittaBot
 import net.perfectdreams.loritta.morenitta.interactions.UnleashedContext
-import net.perfectdreams.loritta.morenitta.interactions.commands.LegacyMessageCommandContext
-import net.perfectdreams.loritta.morenitta.interactions.commands.LorittaLegacyMessageCommandExecutor
-import net.perfectdreams.loritta.morenitta.interactions.commands.LorittaSlashCommandExecutor
-import net.perfectdreams.loritta.morenitta.interactions.commands.SlashCommandArguments
-import net.perfectdreams.loritta.morenitta.interactions.commands.SlashCommandDeclarationBuilder
-import net.perfectdreams.loritta.morenitta.interactions.commands.SlashCommandDeclarationWrapper
-import net.perfectdreams.loritta.morenitta.interactions.commands.options.ApplicationCommandOptions
+import net.perfectdreams.loritta.morenitta.interactions.commands.*
 import net.perfectdreams.loritta.morenitta.interactions.commands.options.OptionReference
-import net.perfectdreams.loritta.morenitta.interactions.commands.slashCommand
-import net.perfectdreams.loritta.morenitta.interactions.vanilla.`fun`.GiveawayBuilderScreen
-import net.perfectdreams.loritta.morenitta.interactions.vanilla.`fun`.GiveawayCommand
-import net.perfectdreams.loritta.serializable.CorreiosPackageUpdateUserNotification
-import net.perfectdreams.loritta.serializable.DailyTaxTaxedUserNotification
-import net.perfectdreams.loritta.serializable.DailyTaxWarnUserNotification
-import net.perfectdreams.loritta.serializable.UnknownUserNotification
-import net.perfectdreams.loritta.serializable.UserId
-import java.util.UUID
+import java.util.*
 
 class NotificationsCommand(val m: LorittaBot) : SlashCommandDeclarationWrapper {
     companion object {
@@ -41,7 +19,8 @@ class NotificationsCommand(val m: LorittaBot) : SlashCommandDeclarationWrapper {
         category = CommandCategory.UTILS,
         uniqueId = UUID.fromString("f5e8087d-63e1-40fa-b92d-4f4a9d5af69c")
     ) {
-        alternativeLegacyAbsoluteCommandPaths.apply {
+        this.enableLegacyMessageSupport = true
+        this.alternativeLegacyAbsoluteCommandPaths.apply {
             add("notificacoes")
         }
 
@@ -50,11 +29,18 @@ class NotificationsCommand(val m: LorittaBot) : SlashCommandDeclarationWrapper {
         }
     }
 
-    class ConfigureNotificationsExecutor(val m: LorittaBot) : LorittaSlashCommandExecutor() {
+    class ConfigureNotificationsExecutor(val m: LorittaBot) : LorittaSlashCommandExecutor(), LorittaLegacyMessageCommandExecutor {
         override suspend fun execute(context: UnleashedContext, args: SlashCommandArguments) {
             val screen = NotificationsSetupScreen.Setup(m)
 
             context.reply(false, screen.render(context))
+        }
+
+        override suspend fun convertToInteractionsArguments(
+            context: LegacyMessageCommandContext,
+            args: List<String>
+        ): Map<OptionReference<*>, Any?> {
+            return emptyMap()
         }
     }
 }
