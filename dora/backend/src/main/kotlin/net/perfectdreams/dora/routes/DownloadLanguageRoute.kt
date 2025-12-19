de058948a95f14d92025d2a6f34501158393a1d9
MrPowerGamerBR <git@mrpowergamerbr.com>
2025-12-19 20:18:58 -0300

    Put the download language bundle behind authD

--- dora/backend/src/main/kotlin/net/perfectdreams/dora/routes/DownloadLanguageRoute.kt
+++ dora/backend/src/main/kotlin/net/perfectdreams/dora/routes/DownloadLanguageRoute.kt
@@ -4,36 +4,30 @@ import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.util.*
-import kotlinx.serialization.json.*
+import kotlinx.serialization.json.Json
 import net.perfectdreams.dora.DoraBackend
+import net.perfectdreams.dora.DoraUserSession
+import net.perfectdreams.dora.Project
+import net.perfectdreams.dora.ProjectPermissionLevel
 import net.perfectdreams.dora.tables.LanguageTargets
-import net.perfectdreams.dora.tables.Projects
 import net.perfectdreams.dora.tables.SourceStrings
 import net.perfectdreams.dora.tables.TranslationsStrings
-import net.perfectdreams.sequins.ktor.BaseRoute
 import org.jetbrains.exposed.sql.and
 import org.jetbrains.exposed.sql.leftJoin
 import org.jetbrains.exposed.sql.selectAll
 
-class DownloadLanguageRoute(val m: DoraBackend) : BaseRoute("/projects/{projectId}/languages/{languageId}/download") {
+class DownloadLanguageRoute(val m: DoraBackend) : RequiresProjectAuthDashboardRoute(m, "/languages/{languageId}/download") {
     val json = Json {
         this.prettyPrint = true
     }
 
-    override suspend fun onRequest(call: ApplicationCall) {
-        val projectId = call.parameters.getOrFail("projectId")
+    override suspend fun onAuthenticatedProjectRequest(call: ApplicationCall, session: DoraUserSession, project: Project, projectPermissionLevel: ProjectPermissionLevel) {
         val languageId = call.parameters.getOrFail("languageId")
 
         val translatableStrings = m.pudding.transaction {
-            val project = Projects.selectAll()
-                .where {
-                    Projects.slug eq projectId
-                }
-                .first()
-
             val language = LanguageTargets.selectAll()
                 .where {
-                    LanguageTargets.languageId eq languageId and (LanguageTargets.project eq project[Projects.id])
+                    LanguageTargets.languageId eq languageId and (LanguageTargets.project eq project.id)
                 }
                 .first()
 
@@ -42,7 +36,7 @@ class DownloadLanguageRoute(val m: DoraBackend) : BaseRoute("/projects/{projectI
                     TranslationsStrings.language eq language[LanguageTargets.id]
                 }
                 .selectAll()
-                .where { SourceStrings.project eq project[Projects.id] }
+                .where { SourceStrings.project eq project.id }
                 .orderBy(SourceStrings.id)
                 .toList()
         }
