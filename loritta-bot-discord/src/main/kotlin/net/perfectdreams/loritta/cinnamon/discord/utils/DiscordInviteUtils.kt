a2bf27cec45ef8fbc9b9ec9a6fd515f51cd725c6
MrPowerGamerBR <git@mrpowergamerbr.com>
2026-01-24 14:01:54 -0300

    Fix Loritta not validating invite links with dashes
    Some vanity URLs have it for some reason

--- loritta-bot-discord/src/main/kotlin/net/perfectdreams/loritta/cinnamon/discord/utils/DiscordInviteUtils.kt
+++ loritta-bot-discord/src/main/kotlin/net/perfectdreams/loritta/cinnamon/discord/utils/DiscordInviteUtils.kt
@@ -1,11 +1,11 @@
 package net.perfectdreams.loritta.cinnamon.discord.utils
 
 object DiscordInviteUtils {
-    private val shortInviteRegex = Regex("(?:https?://)?discord.gg/([A-z0-9]+)")
+    private val shortInviteRegex = Regex("(?:https?://)?discord.gg/([A-z0-9\\-]+)")
     // "discord.media", "discordsays.com" are present in "discord.gg/lori" source code
     // "discord.co" is Discord's old staging URL, now it redirects to discord.com
     // they redirect to discord.com, but well, let's block them too just because they are official Discord URLs
-    private val longInviteRegex = Regex("(?:https?://)?(discord(?:app)?\\.com|discord\\.media|discordsays\\.com|discord\\.co)/invite/([A-z0-9]+)")
+    private val longInviteRegex = Regex("(?:https?://)?(discord(?:app)?\\.com|discord\\.media|discordsays\\.com|discord\\.co)/invite/([A-z0-9\\-]+)")
     val inviteCodeRegex = Regex("[A-z0-9]+")
 
     /**
