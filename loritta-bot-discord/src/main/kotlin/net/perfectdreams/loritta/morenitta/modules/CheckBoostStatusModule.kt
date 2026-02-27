30c744efdacb106209e200eb6d75a38d0492bbc4
MrPowerGamerBR <git@mrpowergamerbr.com>
2026-02-27 14:21:36 -0300

    Update nitro boost key check because now we store it on the UserPremiumKeys

--- loritta-bot-discord/src/main/kotlin/net/perfectdreams/loritta/morenitta/modules/CheckBoostStatusModule.kt
+++ loritta-bot-discord/src/main/kotlin/net/perfectdreams/loritta/morenitta/modules/CheckBoostStatusModule.kt
@@ -5,6 +5,7 @@ import com.github.salomonbrys.kotson.obj
 import com.google.gson.JsonParser
 import net.perfectdreams.i18nhelper.core.I18nContext
 import net.perfectdreams.loritta.cinnamon.pudding.tables.Payments
+import net.perfectdreams.loritta.cinnamon.pudding.tables.UserPremiumKeys
 import net.perfectdreams.loritta.cinnamon.pudding.utils.PaymentGateway
 import net.perfectdreams.loritta.common.locale.BaseLocale
 import net.perfectdreams.loritta.morenitta.LorittaBot
@@ -14,19 +15,22 @@ import net.perfectdreams.loritta.morenitta.dao.ServerConfig
 import net.perfectdreams.loritta.morenitta.events.LorittaMessageEvent
 import net.perfectdreams.loritta.morenitta.utils.LorittaUser
 import net.perfectdreams.loritta.morenitta.utils.NitroBoostUtils
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 import org.jetbrains.exposed.sql.and
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.selectAll
 
 class CheckBoostStatusModule(val loritta: LorittaBot) : MessageReceivedModule {
 	val config = loritta.config.loritta.donatorsOstentation
 
 	override suspend fun matches(
-        event: LorittaMessageEvent,
-        lorittaUser: LorittaUser,
-        lorittaProfile: Profile?,
-        serverConfig: ServerConfig,
-        locale: BaseLocale,
-        i18nContext: I18nContext
-    ): Boolean {
+		event: LorittaMessageEvent,
+		lorittaUser: LorittaUser,
+		lorittaProfile: Profile?,
+		serverConfig: ServerConfig,
+		locale: BaseLocale,
+		i18nContext: I18nContext
+	): Boolean {
 		val guildId = event.guild?.idLong ?: return false
 
 		if (!config.boostEnabledGuilds.any { it.id == guildId })
@@ -50,15 +54,16 @@ class CheckBoostStatusModule(val loritta: LorittaBot) : MessageReceivedModule {
 		i18nContext: I18nContext
 	): Boolean {
 		val donations = loritta.pudding.transaction {
-			Payment.find {
-				(Payments.gateway eq PaymentGateway.NITRO_BOOST) and (Payments.userId eq event.author.idLong)
-			}.toList()
+			UserPremiumKeys.selectAll()
+				.where {
+					UserPremiumKeys.userId eq event.author.idLong
+				}.toList()
 		}
 
 		var hasPaymentHere = false
 
 		for (nitroBoostPayment in donations) {
-			val metadata = nitroBoostPayment.metadata?.let { JsonParser.parseString(it) }
+			val metadata = nitroBoostPayment[UserPremiumKeys.metadata]?.let { JsonParser.parseString(it) }
 			val isFromThisGuild = metadata != null && metadata.obj["guildId"].nullLong == event.guild!!.idLong
 
 			if (isFromThisGuild) {
